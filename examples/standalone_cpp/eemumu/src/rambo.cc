1c9cfbdfae573dbcdf7bc6004526c497fdb2b2a0
Stefan Roiser <stefan.roiser@cern.ch>
2020-02-21 15:16:31 +0100

    clang-format

--- examples/standalone_cpp/eemumu/src/rambo.cc
+++ examples/standalone_cpp/eemumu/src/rambo.cc
@@ -90,7 +90,7 @@ double rn(int idummy) {
 vector<double *> get_momenta(int ninitial, double energy, vector<double> masses,
                              double &wgt) {
   //---- auxiliary function to change convention between MadGraph5_aMC@NLO and
-  //rambo
+  // rambo
   //---- four momenta.
   int nexternal = masses.size();
   int nfinal = nexternal - ninitial;
