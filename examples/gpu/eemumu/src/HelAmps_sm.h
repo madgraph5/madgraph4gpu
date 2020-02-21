b08ab5f9d40cd5dcb02f77f09e547f9a9112436f
Stefan Roiser <stefan.roiser@cern.ch>
2020-02-21 15:24:16 +0100

    cudaize

--- examples/gpu/eemumu/src/HelAmps_sm.h
+++ examples/gpu/eemumu/src/HelAmps_sm.h
@@ -9,57 +9,71 @@
 #define HelAmps_sm_H
 
 #include <cmath>
-#include <complex>
 
-using namespace std;
+#include <thrust/complex.h>
 
-namespace MG5_sm {
-double Sgn(double e, double f);
+namespace gMG5_sm {
 
-void oxxxxx(double p[4], double fmass, int nhel, int nsf,
-            std::complex<double> fo[6]);
+// double Sgn(double e, double f);
 
-void sxxxxx(double p[4], int nss, std::complex<double> sc[3]);
+__global__ void oxxxxx(double p[4], double fmass, int nhel, int nsf,
+                       thrust::complex<double> fo[6]);
 
-void ixxxxx(double p[4], double fmass, int nhel, int nsf,
-            std::complex<double> fi[6]);
+__global__ void sxxxxx(double p[4], int nss, thrust::complex<double> sc[3]);
 
-void txxxxx(double p[4], double tmass, int nhel, int nst,
-            std::complex<double> fi[18]);
+__global__ void ixxxxx(double p[4], double fmass, int nhel, int nsf,
+                       thrust::complex<double> fi[6]);
 
-void vxxxxx(double p[4], double vmass, int nhel, int nsv,
-            std::complex<double> v[6]);
+__global__ void txxxxx(double p[4], double tmass, int nhel, int nst,
+                       thrust::complex<double> fi[18]);
 
-void FFV1_0(std::complex<double> F1[], std::complex<double> F2[],
-            std::complex<double> V3[], std::complex<double> COUP,
-            std::complex<double> &vertex);
+__global__ void vxxxxx(double p[4], double vmass, int nhel, int nsv,
+                       thrust::complex<double> v[6]);
 
-void FFV2_3(std::complex<double> F1[], std::complex<double> F2[],
-            std::complex<double> COUP, double M3, double W3,
-            std::complex<double> V3[]);
-void FFV2_4_3(std::complex<double> F1[], std::complex<double> F2[],
-              std::complex<double> COUP1, std::complex<double> COUP2, double M3,
-              double W3, std::complex<double> V3[]);
+__global__ void FFV1_0(thrust::complex<double> F1[],
+                       thrust::complex<double> F2[],
+                       thrust::complex<double> V3[],
+                       thrust::complex<double> COUP,
+                       thrust::complex<double> &vertex);
 
-void FFV1P0_3(std::complex<double> F1[], std::complex<double> F2[],
-              std::complex<double> COUP, double M3, double W3,
-              std::complex<double> V3[]);
+__global__ void FFV2_3(thrust::complex<double> F1[],
+                       thrust::complex<double> F2[],
+                       thrust::complex<double> COUP, double M3, double W3,
+                       thrust::complex<double> V3[]);
 
-void FFV4_0(std::complex<double> F1[], std::complex<double> F2[],
-            std::complex<double> V3[], std::complex<double> COUP,
-            std::complex<double> &vertex);
+__global__ void FFV2_4_3(thrust::complex<double> F1[],
+                         thrust::complex<double> F2[],
+                         thrust::complex<double> COUP1,
+                         thrust::complex<double> COUP2, double M3, double W3,
+                         thrust::complex<double> V3[]);
 
-void FFV4_3(std::complex<double> F1[], std::complex<double> F2[],
-            std::complex<double> COUP, double M3, double W3,
-            std::complex<double> V3[]);
+__global__ void FFV1P0_3(thrust::complex<double> F1[],
+                         thrust::complex<double> F2[],
+                         thrust::complex<double> COUP, double M3, double W3,
+                         thrust::complex<double> V3[]);
 
-void FFV2_0(std::complex<double> F1[], std::complex<double> F2[],
-            std::complex<double> V3[], std::complex<double> COUP,
-            std::complex<double> &vertex);
-void FFV2_4_0(std::complex<double> F1[], std::complex<double> F2[],
-              std::complex<double> V3[], std::complex<double> COUP1,
-              std::complex<double> COUP2, std::complex<double> &vertex);
+__global__ void FFV4_0(thrust::complex<double> F1[],
+                       thrust::complex<double> F2[],
+                       thrust::complex<double> V3[],
+                       thrust::complex<double> COUP,
+                       thrust::complex<double> &vertex);
 
-} // end namespace MG5_sm
+__global__ void FFV4_3(thrust::complex<double> F1[],
+                       thrust::complex<double> F2[],
+                       thrust::complex<double> COUP, double M3, double W3,
+                       thrust::complex<double> V3[]);
+
+__global__ void FFV2_0(thrust::complex<double> F1[],
+                       thrust::complex<double> F2[],
+                       thrust::complex<double> V3[],
+                       thrust::complex<double> COUP,
+                       thrust::complex<double> &vertex);
+
+__global__ void
+FFV2_4_0(thrust::complex<double> F1[], thrust::complex<double> F2[],
+         thrust::complex<double> V3[], thrust::complex<double> COUP1,
+         thrust::complex<double> COUP2, thrust::complex<double> &vertex);
+
+} // namespace gMG5_sm
 
 #endif // HelAmps_sm_H
