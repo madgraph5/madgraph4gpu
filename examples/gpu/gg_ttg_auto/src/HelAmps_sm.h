70da7f2979a7c01937dc019199c3eb74dfe2f0ec
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2020-07-20 22:08:07 +0200

    add debug statement to gg_ttg

--- examples/gpu/gg_ttg_auto/src/HelAmps_sm.h
+++ examples/gpu/gg_ttg_auto/src/HelAmps_sm.h
@@ -60,12 +60,12 @@ __device__ void VVVV3P0_1(thrust::complex<double> V2[], const
     thrust::complex<double> COUP, const double M1, const double W1,
     thrust::complex<double> V1[]);
 
-__device__ void VVVV4P0_1(thrust::complex<double> V2[], const
+__device__ void VVVV1P0_1(thrust::complex<double> V2[], const
     thrust::complex<double> V3[], const thrust::complex<double> V4[], const
     thrust::complex<double> COUP, const double M1, const double W1,
     thrust::complex<double> V1[]);
 
-__device__ void VVVV1P0_1(thrust::complex<double> V2[], const
+__device__ void VVVV4P0_1(thrust::complex<double> V2[], const
     thrust::complex<double> V3[], const thrust::complex<double> V4[], const
     thrust::complex<double> COUP, const double M1, const double W1,
     thrust::complex<double> V1[]);
