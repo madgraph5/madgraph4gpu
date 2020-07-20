ee90dd2129ad3438bc2d63b7d0bd9dc2ce6a11f5
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2020-07-20 15:40:47 +0200

    add __restrict__ keyword for ee_mumu_auto

--- examples/gpu/eemumu_auto/src/HelAmps_sm.h
+++ examples/gpu/eemumu_auto/src/HelAmps_sm.h
@@ -31,37 +31,47 @@ thrust::complex<double> fi[18]);
 __device__ void vxxxxx(double p[4], double vmass, int nhel, int nsv, 
 thrust::complex<double> v[6]); 
 
-__device__ void FFV2_0(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> V3[], thrust::complex<double> COUP,
+__device__ void FFV1_0(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    V3[], const __restrict__ thrust::complex<double> COUP, __restrict__
     thrust::complex<double> * vertex);
 
-__device__ void FFV2_3(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> COUP, double M3, double W3,
-    thrust::complex<double> V3[]);
+__device__ void FFV1P0_3(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    COUP, const __restrict__ double M3, const __restrict__ double W3,
+    __restrict__ thrust::complex<double> V3[]);
 
-__device__ void FFV4_0(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> V3[], thrust::complex<double> COUP,
+__device__ void FFV2_0(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    V3[], const __restrict__ thrust::complex<double> COUP, __restrict__
     thrust::complex<double> * vertex);
 
-__device__ void FFV4_3(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> COUP, double M3, double W3,
-    thrust::complex<double> V3[]);
+__device__ void FFV2_3(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    COUP, const __restrict__ double M3, const __restrict__ double W3,
+    __restrict__ thrust::complex<double> V3[]);
 
-__device__ void FFV1_0(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> V3[], thrust::complex<double> COUP,
+__device__ void FFV4_0(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    V3[], const __restrict__ thrust::complex<double> COUP, __restrict__
     thrust::complex<double> * vertex);
 
-__device__ void FFV1P0_3(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> COUP, double M3, double W3,
+__device__ void FFV4_3(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    COUP, const __restrict__ double M3, const __restrict__ double W3,
+    __restrict__ thrust::complex<double> V3[]);
+
+__device__ void FFV2_4_0(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    V3[], const __restrict__ thrust::complex<double> COUP1, const __restrict__
+    thrust::complex<double> COUP2, __restrict__ thrust::complex<double> *
+    vertex);
+
+__device__ void FFV2_4_3(thrust::complex<double> F1[], const __restrict__
+    thrust::complex<double> F2[], const __restrict__ thrust::complex<double>
+    COUP1, const __restrict__ thrust::complex<double> COUP2, const __restrict__
+    double M3, const __restrict__ double W3, __restrict__
     thrust::complex<double> V3[]);
-
-__device__ void FFV2_4_0(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> V3[], thrust::complex<double> COUP1,
-    thrust::complex<double> COUP2, thrust::complex<double> * vertex);
-
-__device__ void FFV2_4_3(thrust::complex<double> F1[], thrust::complex<double>
-    F2[], thrust::complex<double> COUP1, thrust::complex<double> COUP2, double
-    M3, double W3, thrust::complex<double> V3[]);
 
 }  // end namespace MG5_sm
 
