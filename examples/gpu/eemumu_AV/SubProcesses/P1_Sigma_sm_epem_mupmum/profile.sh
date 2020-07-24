776fa10c4d4c18d6e9dfcfe1d06c2c887e3fc46d
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-24 18:18:54 +0200

    Add a script to profile

---
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cmd="./check.exe -p 2048 256 12"
+trace=logs/eemumuAV_`date +%m%d_%H%M`
+time ${cmd} | tee ${trace}.txt
+/usr/local/cuda-11.0/bin/ncu -o ${trace} ${cmd}
+/usr/local/cuda-10.1/bin/nsys profile -o ${trace} ${cmd}
