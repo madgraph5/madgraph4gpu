7dfa7377ea5bc50b1e44df0961b10856e20d9f19
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-24 18:23:35 +0200

    Print the UI tools

--- examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
@@ -4,3 +4,8 @@ trace=logs/eemumuAV_`date +%m%d_%H%M`
 time ${cmd} | tee ${trace}.txt
 /usr/local/cuda-11.0/bin/ncu -o ${trace} ${cmd}
 /usr/local/cuda-10.1/bin/nsys profile -o ${trace} ${cmd}
+
+echo ""
+echo "TO ANALYSE TRACE FILES:"
+echo "  /usr/local/cuda-11.0/bin/ncu-ui &"
+echo "  /usr/local/cuda-10.1/bin/nsight-sys &"
