472c648d12d6e9c6dbec5c57fc31ea569e246fe9
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2020-07-26 23:09:03 +0200

    fix typo issue

--- examples/gpu/gg_ttg_auto/src/HelAmps_sm.h
+++ examples/gpu/gg_ttg_auto/src/HelAmps_sm.h
@@ -31,14 +31,6 @@ thrust::complex<double> fi[18]);
 __device__ void vxxxxx(double p[4], double vmass, int nhel, int nsv, 
 thrust::complex<double> v[6]); 
 
-__device__ void VVV1_0(thrust::complex<double> V1[], const
-    thrust::complex<double> V2[], const thrust::complex<double> V3[], const
-    thrust::complex<double> COUP, thrust::complex<double> * vertex);
-
-__device__ void VVV1P0_1(thrust::complex<double> V2[], const
-    thrust::complex<double> V3[], const thrust::complex<double> COUP, const
-    double M1, const double W1, thrust::complex<double> V1[]);
-
 __device__ void FFV1_0(thrust::complex<double> F1[], const
     thrust::complex<double> F2[], const thrust::complex<double> V3[], const
     thrust::complex<double> COUP, thrust::complex<double> * vertex);
@@ -54,6 +46,14 @@ __device__ void FFV1_2(thrust::complex<double> F1[], const
 __device__ void FFV1P0_3(thrust::complex<double> F1[], const
     thrust::complex<double> F2[], const thrust::complex<double> COUP, const
     double M3, const double W3, thrust::complex<double> V3[]);
+
+__device__ void VVV1_0(thrust::complex<double> V1[], const
+    thrust::complex<double> V2[], const thrust::complex<double> V3[], const
+    thrust::complex<double> COUP, thrust::complex<double> * vertex);
+
+__device__ void VVV1P0_1(thrust::complex<double> V2[], const
+    thrust::complex<double> V3[], const thrust::complex<double> COUP, const
+    double M1, const double W1, thrust::complex<double> V1[]);
 
 __device__ void VVVV4P0_1(thrust::complex<double> V2[], const
     thrust::complex<double> V3[], const thrust::complex<double> V4[], const
