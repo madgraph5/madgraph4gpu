63670e65224dfbb8716b94f169e32e90861bdacb
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-26 18:19:12 +0000

    Port script to raplab

--- examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+host=$(hostname)
+if [ "${host%%raplab*}" != "${host}" ]; then
+  logs=logs_raplab
+else
+  logs=logs
+fi
 cmd="./check.exe -p 2048 256 12"
-trace=logs/eemumuAV_`date +%m%d_%H%M`
+trace=$logs/eemumuAV_`date +%m%d_%H%M`
 time ${cmd} | tee ${trace}.txt
-/usr/local/cuda-11.0/bin/ncu -o ${trace} ${cmd}
-/usr/local/cuda-10.1/bin/nsys profile -o ${trace} ${cmd}
+if [ "${host%%raplab*}" != "${host}" ]; then
+  ncu -o ${trace} ${cmd}
+  nsys profile -o ${trace} ${cmd}
+else
+  /usr/local/cuda-11.0/bin/ncu -o ${trace} ${cmd}
+  /usr/local/cuda-10.1/bin/nsys profile -o ${trace} ${cmd}
+  echo ""
+  echo "TO ANALYSE TRACE FILES:"
+  echo "  /usr/local/cuda-11.0/bin/ncu-ui &"
+  echo "  /usr/local/cuda-10.1/bin/nsight-sys &"
+fi
 
-echo ""
-echo "TO ANALYSE TRACE FILES:"
-echo "  /usr/local/cuda-11.0/bin/ncu-ui &"
-echo "  /usr/local/cuda-10.1/bin/nsight-sys &"
