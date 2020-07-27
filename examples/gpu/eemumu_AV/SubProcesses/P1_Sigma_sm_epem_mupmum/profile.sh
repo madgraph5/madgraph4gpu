940a7e1bd6d6c2a6b5ad045477a2aa8180711ce7
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-27 10:39:34 +0200

    Fix profile for lxbatch

--- examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
@@ -2,21 +2,27 @@
 host=$(hostname)
 if [ "${host%%raplab*}" != "${host}" ]; then
   logs=logs_raplab
+elif [ "${host%%cern.ch}" != "${host}" ] && [ "${host##b}" != "${host}" ]; then
+  logs=logs_lxbatch
 else
   logs=logs
 fi
 cmd="./check.exe -p 2048 256 12"
 trace=$logs/eemumuAV_`date +%m%d_%H%M`
 ( time ${cmd} ) 2>&1 | tee ${trace}.txt
-if [ "${host%%raplab*}" != "${host}" ]; then
-  ncu -o ${trace} ${cmd}
-  nsys profile -o ${trace} ${cmd}
-else
+if [ "${host%%cern.ch}" != "${host}" ] && [ "${host##b}" != "${host}" ]; then
   /usr/local/cuda-11.0/bin/ncu -o ${trace} ${cmd}
   /usr/local/cuda-10.1/bin/nsys profile -o ${trace} ${cmd}
   echo ""
   echo "TO ANALYSE TRACE FILES:"
   echo "  /usr/local/cuda-11.0/bin/ncu-ui &"
   echo "  /usr/local/cuda-10.1/bin/nsight-sys &"
+else
+  ncu -o ${trace} ${cmd}
+  nsys profile -o ${trace} ${cmd}
+  echo ""
+  echo "TO ANALYSE TRACE FILES:"
+  echo "  ncu-ui &"
+  echo "  nsight-sys &"
 fi
 
