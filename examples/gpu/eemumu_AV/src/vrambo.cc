60acab58189c1629d591a575792e0d0192699c75
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-29 13:26:46 +0200

    Remove std::vectors also from rambo. Now down from 12s to 4s!
    time ./check.exe -p 2048 256 12
    ***********************************
    NumIterations         = 12
    NumThreadsPerBlock    = 256
    NumBlocksPerGrid      = 2048
    -----------------------------------
    NumberOfEntries       = 12
    TotalTimeInWaveFuncs  = 3.996576e-02 sec
    MeanTimeInWaveFuncs   = 3.330480e-03 sec
    StdDevTimeInWaveFuncs = 1.021512e-03 sec
    MinTimeInWaveFuncs    = 2.996479e-03 sec
    MaxTimeInWaveFuncs    = 6.718128e-03 sec
    -----------------------------------
    ProcessID:            = 32267
    NProcesses            = 1
    NumMatrixElements     = 6291456
    MatrixElementsPerSec  = 1.574211e+08 sec^-1
    ***********************************
    NumMatrixElements     = 6291456
    MeanMatrixElemValue   = 1.371745e-02 GeV^0
    StdErrMatrixElemValue = 3.268633e-06 GeV^0
    StdDevMatrixElemValue = 8.198638e-03 GeV^0
    MinMatrixElemValue    = 6.071582e-03 GeV^0
    MaxMatrixElemValue    = 3.374925e-02 GeV^0
    real    0m4.588s
    user    0m4.065s
    sys     0m0.501s

--- examples/gpu/eemumu_AV/src/vrambo.cc
+++ examples/gpu/eemumu_AV/src/vrambo.cc
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 #include "vrambo.h"
 #include "Random.h"
@@ -9,6 +10,8 @@
 //get_momenta(int ninitial, double energy, const std::vector<double> masses, double &wgt, int nevt) {
 
 // Auxiliary function to change convention between MadGraph5_aMC@NLO and rambo four momenta.
+// Draw random momenta and the corresponding weights for nevt events
+// Both initial-state and final-state particle momenta and masses are considered
 void get_momenta( const int ninitial,    // input: #particles_initial
                   const double energy,   // input: energy
                   const double masses[], // input: masses[npar]
@@ -17,13 +20,11 @@ void get_momenta( const int ninitial,    // input: #particles_initial
                   const int npar,        // input: #particles (==nexternal==nfinal+ninitial)
                   const int nevt )       // input: #events
 {
-  const int nexternal = npar;
-  const int nfinal = nexternal - ninitial;
+  const int nparf = npar - ninitial; // (previously was nfinal = nexternal - ninitial)
   const double e2 = pow(energy, 2);
   const double m1 = masses[0];
 
   const int np4 = 4; // the dimension of 4-momenta (E,px,py,pz)
-
   double (*momenta)[npar][np4] = (double (*)[npar][np4]) momenta1d; // cast to multiD array pointer
 
   // #Initial==1
@@ -35,19 +36,15 @@ void get_momenta( const int ninitial,    // input: #particles_initial
       momenta[ievt][0][2] = 0;
       momenta[ievt][0][3] = 0;
       // Momenta for the outgoing particles
-      std::vector<double> finalmasses;
-      for (int ipar = 0; ipar < npar-ninitial; ++ipar) // loop over npar-ninitial particles from rambo
-        finalmasses.push_back( masses[ipar+ninitial] ); // skip the first ninitial masses
+      const double* massesf = masses+ninitial; // skip the first ninitial masses
+      double pf_rambo[nparf][np4]; // rambo draws random momenta only for final-state particles
       double wgt;
-      std::vector<double *> p_rambo = rambo(m1, finalmasses, wgt);
-      for (int ipar = 0; ipar < npar-ninitial; ++ipar) // loop over npar-ninitial particles from rambo
+      rambo( m1, massesf, pf_rambo, wgt, nparf ); // NB input 'energy' is ignored for ninitial==1
+      for (int iparf = 0; iparf < nparf; ++iparf) // loop over npar-ninitial particles from rambo
         for (int ip4 = 0; ip4 < np4; ++ip4)
-          momenta[ievt][ipar+ninitial][ip4] = p_rambo[ipar][ip4];
+          momenta[ievt][iparf+ninitial][ip4] = pf_rambo[iparf][ip4];
       // Event weight
       wgts[ievt] = wgt;
-      // Match the 'new double[4]' in rambo
-      for (int ipar = 0; ipar < npar-ninitial; ++ipar) // loop over npar-ninitial particles from rambo
-        delete[] p_rambo[ipar];
     }
     return;
   }
@@ -60,13 +57,13 @@ void get_momenta( const int ninitial,    // input: #particles_initial
 
   // #Initial==2
   else {
-    double m2 = masses[1];
-    double mom =
+    const double m2 = masses[1];
+    const double mom =
       sqrt((pow(e2, 2) - 2 * e2 * pow(m1, 2) + pow(m1, 4) -
             2 * e2 * pow(m2, 2) - 2 * pow(m1, 2) * pow(m2, 2) + pow(m2, 4)) /
            (4 * e2));
-    double energy1 = sqrt(pow(mom, 2) + pow(m1, 2));
-    double energy2 = sqrt(pow(mom, 2) + pow(m2, 2));
+    const double energy1 = sqrt(pow(mom, 2) + pow(m1, 2));
+    const double energy2 = sqrt(pow(mom, 2) + pow(m2, 2));
     for (int ievt = 0; ievt < nevt; ++ievt) {
       // Momenta for the incoming particles
       momenta[ievt][0][0] = energy1;
@@ -78,139 +75,135 @@ void get_momenta( const int ninitial,    // input: #particles_initial
       momenta[ievt][1][2] = 0;
       momenta[ievt][1][3] = -mom;
       // #Initial==2, #Final==1
-      if (nfinal == 1) {
+      if (nparf == 1) {
         // Momenta for the outgoing particle
         momenta[ievt][2][0] = m1;
         momenta[ievt][2][1] = 0;
         momenta[ievt][2][2] = 0;
         momenta[ievt][2][3] = 0;
         // Event weight
-        double wgt = 1;
-        wgts[ievt] = wgt;
+        wgts[ievt] = 1;
       }
       // #Initial==2, #Final>1
       else {
         // Momenta for the outgoing particles
-        std::vector<double> finalmasses;
-        for (int ipar = 0; ipar < npar-ninitial; ++ipar) // loop over npar-ninitial particles from rambo
-          finalmasses.push_back( masses[ipar+ninitial] ); // skip the first ninitial masses
+        const double* massesf = masses+ninitial; // skip the first ninitial masses
+        double pf_rambo[nparf][np4]; // rambo draws random momenta only for final-state particles
         double wgt;
-        std::vector<double *> p_rambo = rambo(energy, finalmasses, wgt);
-        for (int ipar = 0; ipar < npar-ninitial; ++ipar) // loop over npar-ninitial particles from rambo
+        rambo( energy, massesf, pf_rambo, wgt, nparf );
+        for (int iparf = 0; iparf < nparf; ++iparf) // loop over npar-ninitial particles from rambo
           for (int ip4 = 0; ip4 < np4; ++ip4)
-            momenta[ievt][ipar+ninitial][ip4] = p_rambo[ipar][ip4];
+            momenta[ievt][iparf+ninitial][ip4] = pf_rambo[iparf][ip4];
         // Event weight
         wgts[ievt] = wgt;
-        // Match the 'new double[4]' in rambo
-        for (int ipar = 0; ipar < npar-ninitial; ++ipar) // loop over npar-ninitial particles from rambo
-          delete[] p_rambo[ipar];
       }
     }
     return;
   }
 }
 
-std::vector<double *>  // output is a struct: momenta[npar-ninitial][4]
-rambo(double et, const std::vector<double> &xm, double &wt) {
-  /**********************************************************************
-   *                       rambo                                         *
-   *    ra(ndom)  m(omenta)  b(eautifully)  o(rganized)                  *
-   *                                                                     *
-   *    a democratic multi-particle phase space generator                *
-   *    authors:  s.d. ellis,  r. kleiss,  w.j. stirling                 *
-   *    this is version 1.0 -  written by r. kleiss                      *
-   *    -- adjusted by hans kuijf, weights are logarithmic (20-08-90)    *
-   *                                                                     *
-   *    n  = number of particles                                         *
-   *    et = total centre-of-mass energy                                 *
-   *    xm = particle masses ( dim=nexternal-nincoming )                 *
-   *    p  = particle momenta ( dim=(4,nexternal-nincoming) )            *
-   *    wt = weight of the event                                         *
-   ***********************************************************************/
-  int n = xm.size();
-  std::vector<double *> q, p;
-  std::vector<double> z(n), r(4), b(3), p2(n), xm2(n), e(n), v(n);
+//std::vector<double *>  // output is a struct: momenta[npar-ninitial][4]
+//rambo(double et, const std::vector<double> &xm, double &wt) {
+
+// Draw random momenta and the corresponding weight for a single event
+// Only final-state particle momenta and masses are considered
+void rambo( const double energy, // input: energy
+            const double xmf[],  // input: masses[nparf]
+            double p[][4],       // output: momenta[nparf][4] as struct
+            double& wt,          // output: weight
+            const int nparf )    // input: #particles_final (==nfinal==nexternal-ninitial)
+{
+  /****************************************************************************
+   *                       rambo                                              *
+   *    ra(ndom)  m(omenta)  b(eautifully)  o(rganized)                       *
+   *                                                                          *
+   *    a democratic multi-particle phase space generator                     *
+   *    authors:  s.d. ellis,  r. kleiss,  w.j. stirling                      *
+   *    this is version 1.0 -  written by r. kleiss                           *
+   *    -- adjusted by hans kuijf, weights are logarithmic (1990-08-20)       *
+   *    -- adjusted by andrea valassi, remove std::vectors (2020-07-29)       *
+   *                                                                          *
+   *    nparf  = number of final-state particles ( =nexternal-nincoming )     *
+   *    energy = total centre-of-mass energy                                  *
+   *    xmf    = final-state particle masses ( dim=nexternal-nincoming )      *
+   *    p      = final-state particle momenta ( dim=(nexternal-nincoming,4) ) *
+   *    wt     = weight of the event                                          *
+   ****************************************************************************/  
+  double q[nparf][4];
+  double z[nparf], r[4], b[3], p2[nparf], xmf2[nparf], e[nparf], v[nparf];
   static std::vector<int> iwarn(5, 0);
   static double acc = 1e-14;
   static int itmax = 6, ibegin = 0;
   static double twopi = 8. * atan(1.);
   static double po2log = log(twopi / 4.);
 
-  for (int i = 0; i < n; i++) {
-    q.push_back(new double[4]);
-    p.push_back(new double[4]);
-  }
   // initialization step: factorials for the phase space weight
   if (ibegin == 0) {
     ibegin = 1;
     z[1] = po2log;
-    for (int k = 2; k < n; k++)
-      z[k] = z[k - 1] + po2log - 2. * log(double(k - 1));
-    for (int k = 2; k < n; k++)
-      z[k] = (z[k] - log(double(k)));
+    for (int kpar = 2; kpar < nparf; kpar++)
+      z[kpar] = z[kpar - 1] + po2log - 2. * log(double(kpar - 1));
+    for (int kpar = 2; kpar < nparf; kpar++)
+      z[kpar] = (z[kpar] - log(double(kpar)));
   }
   // check on the number of particles
-  if (n < 1 || n > 101) {
-    std::cout << "Too few or many particles: " << n << std::endl;
+  if (nparf < 1 || nparf > 101) {
+    std::cout << "Too few or many particles: " << nparf << std::endl;
     exit(-1);
   }
   // check whether total energy is sufficient; count nonzero masses
-  double xmt = 0.;
+  double xmft = 0.;
   int nm = 0;
-  for (int i = 0; i < n; i++) {
-    if (xm[i] != 0.)
+  for (int iparf = 0; iparf < nparf; iparf++) {
+    if (xmf[iparf] != 0.)
       nm = nm + 1;
-    xmt = xmt + abs(xm[i]);
+    xmft = xmft + abs(xmf[iparf]);
   }
-  if (xmt > et) {
-    std::cout << "Too low energy: " << et << " needed " << xmt << std::endl;
+  if (xmft > energy) {
+    std::cout << "Too low energy: " << energy << " needed " << xmft << std::endl;
     exit(-1);
   }
   // the parameter values are now accepted
 
   // generate n massless momenta in infinite phase space
-  for (int i = 0; i < n; i++) {
+  for (int iparf = 0; iparf < nparf; iparf++) {
     double r1 = rn(1);
     double c = 2. * r1 - 1.;
     double s = sqrt(1. - c * c);
     double f = twopi * rn(2);
     r1 = rn(3);
     double r2 = rn(4);
-    q[i][0] = -log(r1 * r2);
-    q[i][3] = q[i][0] * c;
-    q[i][2] = q[i][0] * s * cos(f);
-    q[i][1] = q[i][0] * s * sin(f);
+    q[iparf][0] = -log(r1 * r2);
+    q[iparf][3] = q[iparf][0] * c;
+    q[iparf][2] = q[iparf][0] * s * cos(f);
+    q[iparf][1] = q[iparf][0] * s * sin(f);
   }
   // calculate the parameters of the conformal transformation
-  for (int i = 0; i < 4; i++)
-    r[i] = 0.;
-  for (int i = 0; i < n; i++) {
-    for (int k = 0; k < 4; k++)
-      r[k] = r[k] + q[i][k];
+  for (int i4 = 0; i4 < 4; i4++)
+    r[i4] = 0.;
+  for (int iparf = 0; iparf < nparf; iparf++) {
+    for (int i4 = 0; i4 < 4; i4++)
+      r[i4] = r[i4] + q[iparf][i4];
   }
   double rmas = sqrt(pow(r[0], 2) - pow(r[3], 2) - pow(r[2], 2) - pow(r[1], 2));
-  for (int k = 1; k < 4; k++)
-    b[k - 1] = -r[k] / rmas;
+  for (int i4 = 1; i4 < 4; i4++)
+    b[i4-1] = -r[i4] / rmas;
   double g = r[0] / rmas;
   double a = 1. / (1. + g);
-  double x = et / rmas;
+  double x = energy / rmas;
 
   // transform the q's conformally into the p's
-  for (int i = 0; i < n; i++) {
-    double bq = b[0] * q[i][1] + b[1] * q[i][2] + b[2] * q[i][3];
-    for (int k = 1; k < 4; k++)
-      p[i][k] = x * (q[i][k] + b[k - 1] * (q[i][0] + a * bq));
-    p[i][0] = x * (g * q[i][0] + bq);
+  for (int iparf = 0; iparf < nparf; iparf++) {
+    double bq = b[0] * q[iparf][1] + b[1] * q[iparf][2] + b[2] * q[iparf][3];
+    for (int i4 = 1; i4 < 4; i4++)
+      p[iparf][i4] = x * (q[iparf][i4] + b[i4-1] * (q[iparf][0] + a * bq));
+    p[iparf][0] = x * (g * q[iparf][0] + bq);
   }
 
-  for (int i = 0; i < n; ++i) {
-    delete[] q[i];
-  }
-
-  // calculate weight and possible warnings
+  // calculate weight and possible warnings (NB return log of weight)
   wt = po2log;
-  if (n != 2)
-    wt = (2. * n - 4.) * log(et) + z[n - 1];
+  if (nparf != 2)
+    wt = (2. * nparf - 4.) * log(energy) + z[nparf-1];
   if (wt < -180.) {
     if (iwarn[0] <= 5)
       std::cout << "Too small wt, risk for underflow: " << wt << std::endl;
@@ -224,27 +217,26 @@ rambo(double et, const std::vector<double> &xm, double &wt) {
 
   // return for weighted massless momenta
   if (nm == 0) {
-    // return log of weight
-    return p;
+    return;
   }
 
   // massive particles: rescale the momenta by a factor x
-  double xmax = sqrt(1. - pow(xmt / et, 2));
-  for (int i = 0; i < n; i++) {
-    xm2[i] = pow(xm[i], 2);
-    p2[i] = pow(p[i][0], 2);
+  double xmax = sqrt(1. - pow(xmft / energy, 2));
+  for (int iparf = 0; iparf < nparf; iparf++) {
+    xmf2[iparf] = pow(xmf[iparf], 2);
+    p2[iparf] = pow(p[iparf][0], 2);
   }
   int iter = 0;
   x = xmax;
-  double accu = et * acc;
+  double accu = energy * acc;
   while (true) {
-    double f0 = -et;
+    double f0 = -energy;
     double g0 = 0.;
     double x2 = x * x;
-    for (int i = 0; i < n; i++) {
-      e[i] = sqrt(xm2[i] + x2 * p2[i]);
-      f0 = f0 + e[i];
-      g0 = g0 + p2[i] / e[i];
+    for (int iparf = 0; iparf < nparf; iparf++) {
+      e[iparf] = sqrt(xmf2[iparf] + x2 * p2[iparf]);
+      f0 = f0 + e[iparf];
+      g0 = g0 + p2[iparf] / e[iparf];
     }
     if (abs(f0) <= accu)
       break;
@@ -256,23 +248,23 @@ rambo(double et, const std::vector<double> &xm, double &wt) {
     }
     x = x - f0 / (x * g0);
   }
-  for (int i = 0; i < n; i++) {
-    v[i] = x * p[i][0];
-    for (int k = 1; k < 4; k++)
-      p[i][k] = x * p[i][k];
-    p[i][0] = e[i];
+  for (int iparf = 0; iparf < nparf; iparf++) {
+    v[iparf] = x * p[iparf][0];
+    for (int i4 = 1; i4 < 4; i4++)
+      p[iparf][i4] = x * p[iparf][i4];
+    p[iparf][0] = e[iparf];
   }
 
   // calculate the mass-effect weight factor
   double wt2 = 1.;
   double wt3 = 0.;
-  for (int i = 0; i < n; i++) {
-    wt2 = wt2 * v[i] / e[i];
-    wt3 = wt3 + pow(v[i], 2) / e[i];
+  for (int iparf = 0; iparf < nparf; iparf++) {
+    wt2 = wt2 * v[iparf] / e[iparf];
+    wt3 = wt3 + pow(v[iparf], 2) / e[iparf];
   }
-  double wtm = (2. * n - 3.) * log(x) + log(wt2 / wt3 * et);
+  double wtm = (2. * nparf - 3.) * log(x) + log(wt2 / wt3 * energy);
 
-  // return for  weighted massive momenta
+  // return for weighted massive momenta
   wt = wt + wtm;
   if (wt < -180.) {
     if (iwarn[2] <= 5)
@@ -284,6 +276,5 @@ rambo(double et, const std::vector<double> &xm, double &wt) {
       std::cout << "Too large wt, risk for overflow: " << wt << std::endl;
     iwarn[3] = iwarn[3] + 1;
   }
-  // return log of weight
-  return p;
+  return;
 }
--- examples/gpu/eemumu_AV/src/vrambo.h
+++ examples/gpu/eemumu_AV/src/vrambo.h
@@ -1,20 +1,26 @@
-#include <vector>
+//#include <vector>
 
 //std::vector<std::vector<double *>> // output is an AOS: momenta[nevt][nexternal][4]
 //get_momenta(int ninitial, double energy, const std::vector<double> masses, double &wgt, int nevt);
 
-void get_momenta( int ninitial,          // input: #particles_initial
-                  double energy,         // input: energy
+// Auxiliary function to change convention between MadGraph5_aMC@NLO and rambo four momenta
+// Draw random momenta and the corresponding weights for nevt events
+// Both initial-state and final-state particle momenta and masses are considered
+void get_momenta( const int ninitial,    // input: #particles_initial
+                  const double energy,   // input: energy
                   const double masses[], // input: masses[npar]
                   double momenta[],      // output: momenta[nevt][npar][4] as an AOS
-                  double wgts[],         // output: wgts[nevt]
-                  int npar,              // input: #particles (==nexternal==nfinal+ninitial)
-                  int nevt );            // input: #events
-
-std::vector<double *>  // output is a struct: momenta[npar-ninitial][4]
-rambo(double et, const std::vector<double> &xm, double &wt);
+                  double wgts[],         // output: weights[nevt]
+                  const int npar,        // input: #particles (==nexternal==nfinal+ninitial)
+                  const int nevt );      // input: #events
 
 //std::vector<double *>  // output is a struct: momenta[npar-ninitial][4]
-//void rambo( double et, 
-//            const std::vector<double> &xm, 
-//            double &wt );
+//rambo(double et, const std::vector<double> &xm, double &wt);
+
+// Draw random momenta and the corresponding weight for a single event
+// Only final-state particle momenta and masses are considered
+void rambo( const double energy,    // input: energy
+            const double massesf[], // input: masses[nparf]
+            double p[][4],          // output: momenta[nparf][4] as struct
+            double& wt,             // output: weight
+            const int nparf );      // input: #particles_final (==nfinal==nexternal-ninitial)
