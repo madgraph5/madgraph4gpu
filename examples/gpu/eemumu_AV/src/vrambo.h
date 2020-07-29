ccc2b21fe4e72e6b460e19bdf917aff374cf6eda
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-29 08:52:44 +0200

    Rename dim as nevt aslo in the header

--- examples/gpu/eemumu_AV/src/vrambo.h
+++ examples/gpu/eemumu_AV/src/vrambo.h
@@ -1,7 +1,7 @@
 #include <vector>
 
 std::vector<std::vector<double *>> 
-get_momenta(int ninitial, double energy, const std::vector<double> masses, double &wgt, int dim);
+get_momenta(int ninitial, double energy, const std::vector<double> masses, double &wgt, int nevt);
 
 std::vector<double *> 
 rambo(double et, const std::vector<double> &xm, double &wt);
