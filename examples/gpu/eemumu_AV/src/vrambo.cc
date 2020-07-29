fee2ff900ac42b7cf710ddd24fd312f6789fff4d
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-29 16:35:33 +0200

    Go back to SOA as a default. Clean up by removing old code fragments

--- examples/gpu/eemumu_AV/src/vrambo.cc
+++ examples/gpu/eemumu_AV/src/vrambo.cc
@@ -11,8 +11,11 @@
 void get_momenta( const int ninitial,    // input: #particles_initial
                   const double energy,   // input: energy
                   const double masses[], // input: masses[npar]
-                  //double momenta1d[],    // output: momenta[nevt][npar][4] as an AOS
+#ifdef RAMBO_USES_SOA
                   double momenta1d[],    // output: momenta[npar][4][nevt] as a SOA
+#else
+                  double momenta1d[],    // output: momenta[nevt][npar][4] as an AOS
+#endif
                   double wgts[],         // output: wgts[nevt]
                   const int npar,        // input: #particles (==nexternal==nfinal+ninitial)
                   const int nevt )       // input: #events
@@ -112,20 +115,6 @@ void get_momenta( const int ninitial,    // input: #particles_initial
     return;
   }
 }
-
-/*
-// Draw random momenta and the corresponding weight for a single event
-// Only final-state particle momenta and masses are considered
-void rambo( const double energy, // input: energy
-            const double xmf[],  // input: masses[nparf]
-            double p[][4],       // output: momenta[nparf][4] as struct
-            double& wt,          // output: weight
-            const int nparf )    // input: #particles_final (==nfinal==nexternal-ninitial)
-{
-  // TODO...
-}
-*/
-
 
 // Draw random momenta and the corresponding weight for event ievt out of nevt
 // *** NB: vrambo only uses final-state masses and fills in final-state momenta,
--- examples/gpu/eemumu_AV/src/vrambo.h
+++ examples/gpu/eemumu_AV/src/vrambo.h
@@ -1,5 +1,5 @@
-#undef RAMBO_USES_SOA
-//#define RAMBO_USES_SOA 1 // presently fails check_sa.cu "error: expression must have a constant value"
+//#undef RAMBO_USES_SOA
+#define RAMBO_USES_SOA 1 // presently fails check_sa.cu "error: expression must have a constant value"
 
 // Auxiliary function to change convention between MadGraph5_aMC@NLO and rambo four momenta
 // Draw random momenta and the corresponding weights for nevt events
@@ -15,16 +15,6 @@ void get_momenta( const int ninitial,    // input: #particles_initial
                   double wgts[],         // output: weights[nevt]
                   const int npar,        // input: #particles (==nexternal==nfinal+ninitial)
                   const int nevt );      // input: #events
-
-/*
-// Draw random momenta and the corresponding weight for a single event
-// Only final-state particle momenta and masses are considered
-void rambo( const double energy,    // input: energy
-            const double massesf[], // input: masses[nparf]
-            double p[][4],          // output: momenta[nparf][4] as struct
-            double& wt,             // output: weight
-            const int nparf );      // input: #particles_final (==nfinal==nexternal-ninitial)
-*/
 
 // Draw random momenta and the corresponding weight for event ievt out of nevt
 // *** NB: vrambo only uses final-state masses and fills in final-state momenta,
