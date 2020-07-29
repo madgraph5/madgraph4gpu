f886105097b4a2a0c23a66b310a1591f47005f81
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-29 07:57:19 +0200

    Add Random.h and Random.cc after splitting them off from rambo.

---
+++ examples/gpu/eemumu_AV/src/Random.h
@@ -0,0 +1,12 @@
+class Random {
+public:
+  double ranmar();
+  void rmarin(int ij, int kl);
+
+private:
+  double ranu[98];
+  double ranc, rancd, rancm;
+  int iranmr, jranmr;
+};
+
+double rn(int idummy);
