695895f2278779d3c0e02d8d6a69cbc4a76d3711
Andrea Valassi <andrea.valassi@cern.ch>
2020-07-31 11:17:56 +0200

    Go back to SOA default. No obvious difference within the fluctuations
    time ./check.exe -p 2048 256 12
    ***********************************
    NumIterations         = 12
    NumThreadsPerBlock    = 256
    NumBlocksPerGrid      = 2048
    -----------------------------------
    Memory layout         = SOA
    -----------------------------------
    NumberOfEntries       = 12
    TotalTimeInWaveFuncs  = 2.746679e-02 sec
    MeanTimeInWaveFuncs   = 2.288899e-03 sec
    StdDevTimeInWaveFuncs = 9.655217e-06 sec
    MinTimeInWaveFuncs    = 2.280545e-03 sec
    MaxTimeInWaveFuncs    = 2.318624e-03 sec
    -----------------------------------
    ProcessID:            = 9810
    NProcesses            = 1
    NumMatrixElements     = 6291456
    MatrixElementsPerSec  = 2.290569e+08 sec^-1
    ***********************************
    NumMatrixElements     = 6291456
    MeanMatrixElemValue   = 1.371890e-02 GeV^0
    StdErrMatrixElemValue = 3.269925e-06 GeV^0
    StdDevMatrixElemValue = 8.201879e-03 GeV^0
    MinMatrixElemValue    = 6.071582e-03 GeV^0
    MaxMatrixElemValue    = 3.374924e-02 GeV^0
    ***********************************
    00 CudaFree : 0.192291 sec
    0a InitProc : 0.000714 sec
    0b MemAlloc : 0.048984 sec
    1  RnNumGen : 0.928741 sec
    2  RamboMap : 2.526795 sec
    3a CopyHToD : 0.082483 sec
    3b SigmaKin : 0.000322 sec
    3c CopyDToH : 0.027145 sec
    9a DumpLoop : 0.133081 sec
    9b DumpAll  : 0.055417 sec
    9c MemFree  : 0.074522 sec
          TOTAL : 4.070495 sec
    ***********************************
    
    real    0m4.091s
    user    0m3.640s
    sys     0m0.443s

--- examples/gpu/eemumu_AV/src/mgOnGpuConfig.h
+++ examples/gpu/eemumu_AV/src/mgOnGpuConfig.h
@@ -2,6 +2,6 @@
 #define MGONGPUCONFIG_H 1
 
 #undef MGONGPU_USES_AOS
-#define MGONGPU_USES_AOS 1 // by default use SOA: comment out to use AOS
+//#define MGONGPU_USES_AOS 1 // by default use SOA: comment out to use AOS
 
 #endif // MGONGPUCONFIG_H
