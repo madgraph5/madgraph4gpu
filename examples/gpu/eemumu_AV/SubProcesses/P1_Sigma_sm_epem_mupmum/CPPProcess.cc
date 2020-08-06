5194ab569dbe818a94695286a7456971ecbce789
Andrea Valassi <andrea.valassi@cern.ch>
2020-08-06 09:48:31 +0200

    Proof of concept on cuda, ok. No performance difference.
    time ./gcheck.exe -p 2048 256 12
    ***********************************
    NumIterations         = 12
    NumThreadsPerBlock    = 256
    NumBlocksPerGrid      = 2048
    -----------------------------------
    Momenta memory layout = AOSOA[256]
    Curand generation     = DEVICE (CUDA code)
    -----------------------------------
    NumberOfEntries       = 12
    TotalTimeInWaveFuncs  = 1.134171e-02 sec
    MeanTimeInWaveFuncs   = 9.451423e-04 sec
    StdDevTimeInWaveFuncs = 1.483761e-05 sec
    MinTimeInWaveFuncs    = 9.383460e-04 sec
    MaxTimeInWaveFuncs    = 9.412669e-04 sec
    -----------------------------------
    ProcessID:            = 11244
    NProcesses            = 1
    NumMatrixElements     = 6291456
    MatrixElementsPerSec  = 5.547186e+08 sec^-1
    ***********************************
    NumMatrixElements     = 6291456
    MeanMatrixElemValue   = 1.749672e+02 GeV^0
    StdErrMatrixElemValue = 3.052291e-02 GeV^0
    StdDevMatrixElemValue = 7.655992e+01 GeV^0
    MinMatrixElemValue    = 8.557040e-03 GeV^0
    MaxMatrixElemValue    = 2.683868e+02 GeV^0
    ***********************************
    00 CudaFree : 0.142554 sec
    0a ProcInit : 0.000582 sec
    0b MemAlloc : 0.075028 sec
    0c GenCreat : 0.014452 sec
    1a GenSeed  : 0.000019 sec
    1b GenRnGen : 0.007819 sec
    2a RamboIni : 0.000110 sec
    2b RamboFin : 0.000054 sec
    2c CpDTHwgt : 0.007151 sec
    2d CpDTHmom : 0.072987 sec
    3a SigmaKin : 0.000100 sec
    3b CpDTHmes : 0.022489 sec
    4a DumpLoop : 0.022541 sec
    9a DumpAll  : 0.036424 sec
    9b GenDestr : 0.000223 sec
    9c MemFree  : 0.021763 sec
    9d CudReset : 0.040366 sec
          TOTAL : 0.464663 sec
    ***********************************
    
    real    0m0.469s
    user    0m0.187s
    sys     0m0.277s

--- examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
@@ -59,35 +59,52 @@ namespace MG5_sm
 #ifdef __CUDACC__
   __device__
 #endif
-  void imzxxxM0(const double pvec[4],
-                //const double fmass,
-                const int nhel,
-                const int nsf,
-                dcomplex fi[6])
+  void imzxxxM0( const double* allmomenta, // input[(npar=4)*(np4=4)*nevt]
+                 //const double fmass,
+                 const int nhel,
+                 const int nsf,
+                 dcomplex fi[6],
+                 const int ipar )          // input: particle# out of npar
   {
-    fi[0] = dcomplex (-pvec[0] * nsf, -pvec[3] * nsf);
-    fi[1] = dcomplex (-pvec[1] * nsf, -pvec[2] * nsf);
-    const int nh = nhel * nsf;
-    // ASSUMPTIONS FMASS = 0 and
-    // (PX = PY = 0 and E = -P3 > 0)
+#ifndef __CUDACC__
+    // ** START LOOP ON IEVT **
+    for (int ievt = 0; ievt < nevt; ++ievt)
+#endif
     {
-      const dcomplex chi0( 0, 0 );
-      const dcomplex chi1( -nhel * sqrt(2 * pvec[0]), 0 );
-      if (nh == 1)
+#ifdef __CUDACC__
+      const int idim = blockDim.x * blockIdx.x + threadIdx.x; // event# == threadid (previously was: tid)
+      const int ievt = idim;
+      //printf( "imzxxxM0: ievt %d\n", ievt );
+#endif
+      const double& pvec0 = pIparIp4Ievt( allmomenta, ipar, 0, ievt );
+      const double& pvec1 = pIparIp4Ievt( allmomenta, ipar, 0, ievt );
+      const double& pvec2 = pIparIp4Ievt( allmomenta, ipar, 0, ievt );
+      const double& pvec3 = pIparIp4Ievt( allmomenta, ipar, 0, ievt );
+      fi[0] = dcomplex (-pvec0 * nsf, -pvec3 * nsf);
+      fi[1] = dcomplex (-pvec1 * nsf, -pvec2 * nsf);
+      const int nh = nhel * nsf;
+      // ASSUMPTIONS FMASS = 0 and
+      // (PX = PY = 0 and E = -P3 > 0)
       {
-        fi[2] = dcomplex (0, 0);
-        fi[3] = dcomplex (0, 0);
-        fi[4] = chi0;
-        fi[5] = chi1;
-      }
-      else
-      {
-        fi[2] = chi1;
-        fi[3] = chi0;
-        fi[4] = dcomplex (0, 0);
-        fi[5] = dcomplex (0, 0);
+        const dcomplex chi0( 0, 0 );
+        const dcomplex chi1( -nhel * sqrt(2 * pvec0), 0 );
+        if (nh == 1)
+        {
+          fi[2] = dcomplex (0, 0);
+          fi[3] = dcomplex (0, 0);
+          fi[4] = chi0;
+          fi[5] = chi1;
+        }
+        else
+        {
+          fi[2] = chi1;
+          fi[3] = chi0;
+          fi[4] = dcomplex (0, 0);
+          fi[5] = dcomplex (0, 0);
+        }
       }
     }
+    // ** END LOOP ON IEVT **
     return;
   }
 
@@ -365,7 +382,7 @@ namespace Proc
 #endif
 
     MG5_sm::oxzxxxM0(local_mom[0], cHel[ihel][0], -1, w[0]);
-    MG5_sm::imzxxxM0(local_mom[1], cHel[ihel][1], +1, w[1]);
+    MG5_sm::imzxxxM0(allmomenta, cHel[ihel][1], +1, w[1], 1);
     MG5_sm::ixzxxxM0(local_mom[2], cHel[ihel][2], -1, w[2]);
     MG5_sm::oxzxxxM0(local_mom[3], cHel[ihel][3], +1, w[3]);
 
