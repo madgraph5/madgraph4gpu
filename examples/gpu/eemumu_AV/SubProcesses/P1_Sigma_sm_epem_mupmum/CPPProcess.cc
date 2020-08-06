d4bb0f267b84a697049de00f487cf6b0ee8173b0
Andrea Valassi <andrea.valassi@cern.ch>
2020-08-06 11:15:46 +0200

    This now functionally works, but is slower!
    time ./gcheck.exe -p 2048 256 12
    ***********************************
    NumIterations         = 12
    NumThreadsPerBlock    = 256
    NumBlocksPerGrid      = 2048
    -----------------------------------
    Momenta memory layout = AOSOA[256]
    Curand generation     = DEVICE (CUDA code)
    -----------------------------------
    NumberOfEntries       = 12
    TotalTimeInWaveFuncs  = 2.009846e-01 sec
    MeanTimeInWaveFuncs   = 1.674872e-02 sec
    StdDevTimeInWaveFuncs = 2.619339e-03 sec
    MinTimeInWaveFuncs    = 1.580349e-02 sec
    MaxTimeInWaveFuncs    = 1.589832e-02 sec
    -----------------------------------
    ProcessID:            = 1864
    NProcesses            = 1
    NumMatrixElements     = 6291456
    MatrixElementsPerSec  = 3.130317e+07 sec^-1
    ***********************************
    NumMatrixElements     = 6291456
    MeanMatrixElemValue   = 1.394824e-02 GeV^0
    StdErrMatrixElemValue = 3.037159e-06 GeV^0
    StdDevMatrixElemValue = 7.618038e-03 GeV^0
    MinMatrixElemValue    = 6.071582e-03 GeV^0
    MaxMatrixElemValue    = 3.374925e-02 GeV^0
    ***********************************
    00 CudaFree : 0.149680 sec
    0a ProcInit : 0.000555 sec
    0b MemAlloc : 0.083142 sec
    0c GenCreat : 0.015106 sec
    1a GenSeed  : 0.000018 sec
    1b GenRnGen : 0.008003 sec
    2a RamboIni : 0.000120 sec
    2b RamboFin : 0.000068 sec
    2c CpDTHwgt : 0.008443 sec
    2d CpDTHmom : 0.094246 sec
    3a SigmaKin : 0.007925 sec
    3b CpDTHmes : 0.386123 sec
    4a DumpLoop : 0.023752 sec
    9a DumpAll  : 0.036359 sec
    9b GenDestr : 0.000539 sec
    9c MemFree  : 0.026355 sec
    9d CudReset : 0.046705 sec
          TOTAL : 0.887139 sec
    ***********************************
    
    real    0m0.705s
    user    0m0.307s
    sys     0m0.396s

--- examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
@@ -406,11 +406,8 @@ namespace Proc
     //dcomplex_v (*wv)[6] = (dcomplex_v (*)[6]) wv0; // dcomplex_v wv[5][6] i.e. dcomplex[5][6][256]
 
     //__shared__ double_v wv10[2*6]; // dcomplex_v wv1[6] gives "dynamic initialization is not supported"
-    //double_v wv10[2*6]; // dcomplex_v wv1[6] gives "dynamic initialization is not supported"
-    //dcomplex_v* wv1 = (dcomplex_v*) wv10; // dcomplex_v wv1[6] i.e. dcomplex[6][256]
-
-    //dcomplex_v wv1[6];
-    dcomplex wv1[6][256];
+    double_v wv10[2*6]; // dcomplex_v wv1[6] gives "dynamic initialization is not supported"
+    dcomplex_v* wv1 = (dcomplex_v*) wv10; // dcomplex_v wv1[6] i.e. dcomplex[6][256]
 #endif
 
     MG5_sm::oxzxxxM0(local_mom[0], cHel[ihel][0], -1, w[0]);
