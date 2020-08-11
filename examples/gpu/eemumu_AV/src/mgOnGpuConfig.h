29f4dee455dbb8c58b7b46b349c0da160cf9c456
Andrea Valassi <andrea.valassi@cern.ch>
2020-08-11 15:02:01 +0200

    SHARED is 30% slower...
    time ./gcheck.exe -p 16384 32 12
    ***************************************
    NumIterations             = 12
    NumThreadsPerBlock        = 32
    NumBlocksPerGrid          = 16384
    ---------------------------------------
    FP precision              = DOUBLE (nan=0)
    Complex type              = THRUST::COMPLEX
    Momenta memory layout     = AOSOA[32]
    Wavefunction GPU memory   = SHARED (15360 bytes)
    Curand generation         = DEVICE (CUDA code)
    ---------------------------------------
    NumberOfEntries           = 12
    TotalTimeInWaveFuncs      = 1.812474e-02 sec
    MeanTimeInWaveFuncs       = 1.510395e-03 sec
    StdDevTimeInWaveFuncs     = 1.993042e-05 sec
    MinTimeInWaveFuncs        = 1.497547e-03 sec
    MaxTimeInWaveFuncs        = 1.504945e-03 sec
    ---------------------------------------
    NumMatrixElementsComputed = 6291456
    MatrixElementsPerSec      = 3.471197e+08 sec^-1
    ***************************************
    NumMatrixElements(notNan) = 6291456
    MeanMatrixElemValue       = 1.394735e-02 GeV^0
    StdErrMatrixElemValue     = 3.034488e-06 GeV^0
    StdDevMatrixElemValue     = 7.611337e-03 GeV^0
    MinMatrixElemValue        = 6.071582e-03 GeV^0
    MaxMatrixElemValue        = 3.374925e-02 GeV^0
    ***************************************
    00 CudaFree : 0.174391 sec
    0a ProcInit : 0.000660 sec
    0b MemAlloc : 0.067660 sec
    0c GenCreat : 0.017064 sec
    1a GenSeed  : 0.000014 sec
    1b GenRnGen : 0.007353 sec
    2a RamboIni : 0.000114 sec
    2b RamboFin : 0.000064 sec
    2c CpDTHwgt : 0.008839 sec
    2d CpDTHmom : 0.099689 sec
    3a SigmaKin : 0.000086 sec
    3b CpDTHmes : 0.018039 sec
    4a DumpLoop : 0.022054 sec
    9a DumpAll  : 0.046620 sec
    9b GenDestr : 0.000424 sec
    9c MemFree  : 0.022330 sec
    9d CudReset : 0.061562 sec
          TOTAL : 0.546962 sec
    ***************************************
    
    real    0m0.561s
    user    0m0.209s
    sys     0m0.343s

--- examples/gpu/eemumu_AV/src/mgOnGpuConfig.h
+++ examples/gpu/eemumu_AV/src/mgOnGpuConfig.h
@@ -13,13 +13,13 @@
 //#define MGONGPU_CURAND_ONHOST 1
 
 // Use global memory or shared memory for wavefunctions (CHOOSE ONLY ONE)
-#define MGONGPU_WFMEM_LOCAL 1 // default (~5.00E8)
+//#define MGONGPU_WFMEM_LOCAL 1 // default (~5.00E8)
 //#define MGONGPU_WFMEM_GLOBAL 1
-//#define MGONGPU_WFMEM_SHARED 1
+#define MGONGPU_WFMEM_SHARED 1 // 30% slower (~3.5E8 against 5.0E8)
 
 // Floating point precision (CHOOSE ONLY ONE)
 #define MGONGPU_FPTYPE_DOUBLE 1 // default (~5.00E8)
-//#define MGONGPU_FPTYPE_FLOAT 1 // 2.3x faster (~1.14E9)
+//#define MGONGPU_FPTYPE_FLOAT 1 // 2.3x faster (~1.14E9 against 5.00E8)
 
 // Complex type in cuda: thrust or cucomplex (CHOOSE ONLY ONE)
 #ifdef __CUDACC__
