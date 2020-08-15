d4121efad0bc1cebb2d4f679611bb1a775c012c0
Andrea Valassi <andrea.valassi@cern.ch>
2020-08-15 18:43:05 +0200

    Add the option to use ncu in a sinmpler no-GUI way

--- examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
+++ examples/gpu/eemumu_AV/SubProcesses/P1_Sigma_sm_epem_mupmum/profile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 usage(){
-  echo "Usage: $0 [--cc] [-p #blocks #threads #iterations] [-l label]"
+  echo "Usage (GUI analysis): $0 [-cc] [-l label] [-p #blocks #threads #iterations]"
+  echo "Usage (CL analysis):  $0 -nogui [-p #blocks #threads #iterations]"
   exit 1
 }
 
@@ -15,8 +16,23 @@ label=
 # Command line arguments
 while [ "$1" != "" ]; do
   # Profile C++ instead of cuda
-  if [ "$1" == "--cc" ]; then
-    tag=cc
+  if [ "$1" == "-cc" ]; then
+    if [ "$tag" != "nogui" ]; then
+      tag=cc
+      shift
+    else
+      echo "ERROR! Incompaticle options -gui and -cc"
+      usage
+    fi
+  # Fast no-GUI profiling with ncu
+  elif [ "$1" == "-nogui" ]; then
+    if [ "$tag" != "cc" ]; then
+      tag=nogui
+      shift
+    else
+      echo "ERROR! Incompaticle options -gui and -cc"
+      usage
+    fi
   # Override blocks/threads/iterations
   # (NB do not exceed 12 iterations: profiling overhead per iteration is huge)
   elif [ "$1" == "-p" ]; then
@@ -40,52 +56,13 @@ while [ "$1" != "" ]; do
   fi
 done
 
-if [ "$label" == "" ]; then
-  echo "ERROR! You must specify a label"
-  usage
-fi
-
-if [ "$tag" == "cu" ]; then
-  if [ "$args" == "" ]; then args=$cuargs; fi
-  cmd="./gcheck.exe -p $args"
-else
+if [ "$tag" == "cc" ]; then
   if [ "$args" == "" ]; then args=$ccargs; fi
   cmd="./check.exe -p $args"
-fi
-
-arg1=$(echo $args | cut -d' ' -f1)
-arg2=$(echo $args | cut -d' ' -f2)
-arg3=$(echo $args | cut -d' ' -f3)
-
-host=$(hostname)
-if [ "${host%%raplab*}" != "${host}" ]; then
-  logs=logs_raplab
-elif [ "${host%%cern.ch}" != "${host}" ] && [ "${host##b}" != "${host}" ]; then
-  logs=logs_lxbatch
 else
-  logs=logs
+  if [ "$args" == "" ]; then args=$cuargs; fi
+  cmd="./gcheck.exe -p $args"
 fi
-trace=$logs/eemumuAV_${tag}_`date +%m%d_%H%M`_b${arg1}_t${arg2}_i${arg3}
-if [ "$label" != "" ]; then trace=${trace}_${label}; fi
-
-echo
-echo "PROFILING: ${cmd}"
-echo "OUTPUT: ${trace}.*"
-echo
-
-\rm -f ${trace}.*
-
-hostname > ${trace}.txt
-echo "nproc=$(nproc)" >> ${trace}.txt
-echo >> ${trace}.txt
-( time ${cmd} ) 2>&1 | tee -a ${trace}.txt
-nvidia-smi -q -d CLOCK >> ${trace}.txt
-
-# See https://developer.nvidia.com/blog/using-nsight-compute-to-inspect-your-kernels/
-# These used to be called gld_transactions and global_load_requests
-# See also https://docs.nvidia.com/nsight-compute/2019.5/NsightComputeCli/index.html#nvprof-metric-comparison
-# See also https://stackoverflow.com/questions/60535867
-metrics=l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum,l1tex__t_requests_pipe_lsu_mem_global_op_ld.sum
 
 ncu="ncu"
 nsys="nsys"
@@ -100,17 +77,64 @@ if [ "${host%%cern.ch}" != "${host}" ] && [ "${host##b}" != "${host}" ]; then
   nsysgui="Launch the Nsight System GUI from Windows"
 fi
 
-if [ "$tag" == "cu" ]; then
-  ncu --set full --metrics ${metrics} -o ${trace} ${cmd}
+# See https://developer.nvidia.com/blog/using-nsight-compute-to-inspect-your-kernels/
+# These used to be called gld_transactions and global_load_requests
+# See also https://docs.nvidia.com/nsight-compute/2019.5/NsightComputeCli/index.html#nvprof-metric-comparison
+# See also https://stackoverflow.com/questions/60535867
+metrics=l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum,l1tex__t_requests_pipe_lsu_mem_global_op_ld.sum
+
+# GUI analysis
+if [ "$tag" != "nogui" ]; then
+
+  if [ "$label" == "" ]; then
+    echo "ERROR! You must specify a label"
+    usage
+  fi
+
+  arg1=$(echo $args | cut -d' ' -f1)
+  arg2=$(echo $args | cut -d' ' -f2)
+  arg3=$(echo $args | cut -d' ' -f3)
+  
+  host=$(hostname)
+  if [ "${host%%raplab*}" != "${host}" ]; then
+    logs=logs_raplab
+  elif [ "${host%%cern.ch}" != "${host}" ] && [ "${host##b}" != "${host}" ]; then
+    logs=logs_lxbatch
+  else
+    logs=logs
+  fi
+  trace=$logs/eemumuAV_${tag}_`date +%m%d_%H%M`_b${arg1}_t${arg2}_i${arg3}
+  if [ "$label" != "" ]; then trace=${trace}_${label}; fi
+  
+  echo
+  echo "PROFILING: ${cmd}"
+  echo "OUTPUT: ${trace}.*"
+  echo
+  
+  \rm -f ${trace}.*
+  
+  hostname > ${trace}.txt
+  echo "nproc=$(nproc)" >> ${trace}.txt
+  echo >> ${trace}.txt
+  ( time ${cmd} ) 2>&1 | tee -a ${trace}.txt
+  nvidia-smi -q -d CLOCK >> ${trace}.txt
+  
+  if [ "$tag" == "cu" ]; then
+    ${ncu} --set full --metrics ${metrics} -o ${trace} ${cmd}
+  fi
+  ${nsys} profile -o ${trace} ${cmd}
+  echo ""
+  echo "TO ANALYSE TRACE FILES:"
+  echo "  ${ncugui}"
+  echo "  ${nsysgui}"
+  
+# NO-GUI analysis
+else
+
+  echo
+  echo "PROFILING: ${cmd}"
+  echo "${ncu} --metrics ${metrics} ${cmd}"
+  echo
+  ${ncu} --metrics ${metrics} ${cmd}
+
 fi
-nsys profile -o ${trace} ${cmd}
-echo ""
-echo "TO ANALYSE TRACE FILES:"
-echo "  ${ncugui}"
-echo "  ${nsysgui}"
-
-#echo
-#echo "PROFILING: ${cmd}"
-#echo "OUTPUT: ${trace}.*"
-#echo
-
