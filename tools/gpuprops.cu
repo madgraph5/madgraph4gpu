e3f689eb92b032b074fc64a7770a462d870159fe
Stefan Roiser <stefan.roiser@cern.ch>
2020-11-27 11:23:06 +0100

    some info for calculating the # of cores / multiprocessor

--- tools/gpuprops.cu
+++ tools/gpuprops.cu
@@ -151,6 +151,48 @@ public:
 
 };
 
+/* calculate cores per streaming multiprocessor 
+https://stackoverflow.com/questions/32530604/how-can-i-get-number-of-cores-in-cuda-device
+#include "cuda_runtime_api.h"
+// you must first call the cudaGetDeviceProperties() function, then pass
+// the devProp structure returned to this function:
+int getSPcores(cudaDeviceProp devProp)
+{
+    int cores = 0;
+    int mp = devProp.multiProcessorCount;
+    switch (devProp.major){
+     case 2: // Fermi
+      if (devProp.minor == 1) cores = mp * 48;
+      else cores = mp * 32;
+      break;
+     case 3: // Kepler
+      cores = mp * 192;
+      break;
+     case 5: // Maxwell
+      cores = mp * 128;
+      break;
+     case 6: // Pascal
+      if ((devProp.minor == 1) || (devProp.minor == 2)) cores = mp * 128;
+      else if (devProp.minor == 0) cores = mp * 64;
+      else printf("Unknown device type\n");
+      break;
+     case 7: // Volta and Turing
+      if ((devProp.minor == 0) || (devProp.minor == 5)) cores = mp * 64;
+      else printf("Unknown device type\n");
+      break;
+     case 8: // Ampere
+      if (devProp.minor == 0) cores = mp * 64;
+      else if (devProp.minor == 6) cores = mp * 128;
+      else printf("Unknown device type\n");
+      break;
+     default:
+      printf("Unknown device type\n");
+      break;
+      }
+    return cores;
+}
+*/
+
 
 int main() {
   Properties p;
