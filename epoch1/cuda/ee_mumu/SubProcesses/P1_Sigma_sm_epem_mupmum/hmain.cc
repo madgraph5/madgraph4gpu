3448b80fdaf7db872d478de3f00ff456ec2d11d5
Andrea Valassi <andrea.valassi@cern.ch>
2020-12-04 13:30:09 +0100

    Fix het main (was executing twice CPU!), also put gpu first again

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/hmain.cc
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/hmain.cc
@@ -7,15 +7,15 @@ int gcheck( int argc, char **argv, std::string& out ); // from gcheck.cu (compil
 // This is compiled with g++ and linked with objects compiled with nvcc or g++
 int main( int argc, char **argv )
 {
+  std::string gpuOut;
+  int gpuStatus = gcheck( argc, argv, gpuOut );
+  std::cout << gpuOut;
+  if ( gpuStatus != 0 ) return 1;
+
   std::string cpuOut;
   int cpuStatus = check( argc, argv, cpuOut );
   std::cout << cpuOut;
-  if ( cpuStatus != 0 ) return 1;
-
-  std::string gpuOut;
-  int gpuStatus = check( argc, argv, gpuOut );
-  std::cout << gpuOut;
-  if ( gpuStatus != 0 ) return 2;
+  if ( cpuStatus != 0 ) return 2;
 
   return 0;
 }
