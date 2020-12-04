0f5bc4bcb9f5825cb2af46fa5b70083c45c8f0e0
Andrea Valassi <andrea.valassi@cern.ch>
2020-12-04 14:35:51 +0100

    Launch in parallel on cpu and gpu on two different threads

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/hmain.cc
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/hmain.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <vector>
 
 int gcheck( int argc, char **argv, std::string& out, std::vector<double>& stats ); // from gcheck.cu (compiled with nvcc)
@@ -10,17 +11,23 @@ int main( int argc, char **argv )
 {
   std::string gpuOut;
   std::vector<double> gpuStats;
-  int gpuStatus = gcheck( argc, argv, gpuOut, gpuStats );
-  std::cout << gpuOut;
-  //for ( auto& stat : gpuStats ) std::cout << stat << std::endl;
-  if ( gpuStatus != 0 ) return 1;
+  int gpuStatus;
+  std::thread gpuThread( [&]{ gpuStatus = gcheck( argc, argv, gpuOut, gpuStats ); });
 
   std::string cpuOut;
   std::vector<double> cpuStats;
-  int cpuStatus = check( argc, argv, cpuOut, cpuStats );
+  int cpuStatus;
+  std::thread cpuThread( [&]{ cpuStatus = check( argc, argv, cpuOut, cpuStats ); });
+
+  gpuThread.join();
+  std::cout << gpuOut;
+  //for ( auto& stat : gpuStats ) std::cout << stat << std::endl;
+
+  cpuThread.join();
   std::cout << cpuOut;
   //for ( auto& stat : cpuStats ) std::cout << stat << std::endl;
-  if ( cpuStatus != 0 ) return 2;
 
+  if ( gpuStatus != 0 ) return 1;
+  if ( cpuStatus != 0 ) return 2;
   return 0;
 }
