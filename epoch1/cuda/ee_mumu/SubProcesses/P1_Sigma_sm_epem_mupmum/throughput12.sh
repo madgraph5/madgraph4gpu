ec816e2526fc207e961b0bc8fb73bee43461ac56
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-02 20:18:00 +0200

    [hetep12] Add het executable to timing
    (CPU) Process                     = EPOCH1_EEMUMU_CPP
    (CPU) OMP threads / `nproc --all` = 1 / 4
    (CPU) EvtsPerSec[MatrixElems] (3) = ( 1.154338e+06                 )  sec^-1
    (CPU) MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    (CPU) TOTAL       :     7.970653 sec
    real    0m8.000s
    -------------------------------------------------------------------------
    (GPU) Process                     = EPOCH1_EEMUMU_CUDA
    (GPU) EvtsPerSec[MatrixElems] (3) = ( 5.516747e+08                 )  sec^-1
    (GPU) MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    (GPU) TOTAL       :     0.832888 sec
    real    0m1.146s
    -------------------------------------------------------------------------
    (GPU) Process                     = EPOCH1_EEMUMU_CUDA
    (GPU) EvtsPerSec[MatrixElems] (3) = ( 6.227706e+08                 )  sec^-1
    (GPU) MeanMatrixElemValue         = ( 1.371704e-02 +- 3.852995e-07 )  GeV^0
    (GPU) TOTAL       :    17.492838 sec
    (CPU) Process                     = EPOCH1_EEMUMU_CPP
    (CPU) OMP threads / `nproc --all` = 4 / 4
    (CPU) EvtsPerSec[MatrixElems] (3) = ( 3.044463e+06                 )  sec^-1
    (CPU) MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    (CPU) TOTAL       :     4.690226 sec
    (GPU) EvtsPerSec[MatrixElems] (3) = ( 6.227706e+08                 )  sec^-1
    (CPU) OMP threads / `nproc --all` = 1 / 4
    (CPU) EvtsPerSec[MatrixElems] (3) = ( 3.044463e+06                 )  sec^-1
    (HET) EvtsPerSec[MatrixElems] (3) = ( 6.258150e+08                 )  sec^-1
    real    0m18.384s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.096710e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     8.259931 sec
    real    0m8.289s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.081856e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.827000 sec
    real    0m1.148s
    -------------------------------------------------------------------------

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -3,11 +3,12 @@
 exes=
 exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
 exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/hcheck.exe"
 exes="$exes ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
 exes="$exes ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
 
 for exe in $exes; do
   # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
-  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
+  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
   echo "-------------------------------------------------------------------------"
 done
