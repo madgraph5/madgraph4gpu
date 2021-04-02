0e8332c0d3315708bc4e14a1bfaeff6f24232e5f
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-02 16:31:10 +0200

    [ep2to2ep1] Add script throughput12.sh to check performances as the code changes
    As mentioned bfeore, epoch1 is slightly faster[slower] than epoch2 in C++[Cuda]
    
    Process                     = EPOCH1_EEMUMU
    MatrixElements compiler     = gcc (GCC) 9.2.0
    EvtsPerSec[MatrixElems] (3) = ( 1.152193e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.005549 sec
    real    0m8.038s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU
    MatrixElements compiler     = nvcc 11.0.221
    EvtsPerSec[MatrixElems] (3) = ( 5.600203e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.899564 sec
    real    0m1.208s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU
    MatrixElements compiler     = gcc (GCC) 9.2.0
    EvtsPerSec[MatrixElems] (3) = ( 1.096791e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.236883 sec
    real    0m8.267s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU
    MatrixElements compiler     = nvcc 11.0.221
    EvtsPerSec[MatrixElems] (3) = ( 6.087068e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.834394 sec
    real    0m1.143s
    -------------------------------------------------------------------------

---
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+exes=
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
+exes="$exes ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
+exes="$exes ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
+
+for exe in $exes; do
+  # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|compiler|EvtsPerSec\[MatrixElems\]|MeanMatrixElemValue|TOTAL       :)'
+  echo "-------------------------------------------------------------------------"
+done
