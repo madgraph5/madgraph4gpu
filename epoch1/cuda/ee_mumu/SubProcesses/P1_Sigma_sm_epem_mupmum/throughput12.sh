57311528d9748f35a7d14749b0678e248ea22056
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-02 19:48:14 +0200

    [klas2ep1ep2Bis] First time measurement for klas2 + ep2ep1
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CPP
    Internal loops fptype_sv    = VECTOR[1] == SCALAR (no SIMD)
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.290709e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     7.401336 sec
    real    0m7.432s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 5.419518e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     0.838333 sec
    real    0m1.146s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CPP
    Internal loops fptype_sv    = VECTOR[4] (AVX512F)
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 4.779335e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     3.767201 sec
    real    0m3.798s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 5.409424e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     0.839485 sec
    real    0m1.147s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.097223e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     8.261949 sec
    real    0m8.292s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.126317e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.371706e-02 +- 3.270315e-06 )  GeV^0
    TOTAL       :     0.868091 sec
    real    0m1.190s
    -------------------------------------------------------------------------

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 exes=
-exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
-exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/build.none/check.exe"
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/build.none/gcheck.exe"
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/build.avx512/check.exe"
+exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/build.avx512/gcheck.exe"
 exes="$exes ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
 exes="$exes ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
 
+pushd ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum >& /dev/null
+  pwd
+  make AVX=avx512
+  make AVX=none
+popd >& /dev/null
+
+pushd ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum >& /dev/null
+  pwd
+  make
+popd >& /dev/null
+
 for exe in $exes; do
-  # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
-  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
   echo "-------------------------------------------------------------------------"
+  # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep '(Process|fptype_sv|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
 done
+echo "-------------------------------------------------------------------------"
