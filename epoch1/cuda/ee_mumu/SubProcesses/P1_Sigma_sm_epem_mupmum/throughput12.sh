70f2e3868ebf6f722263234edae745adc1845b6f
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-06 10:30:46 +0200

    [ep2to2ep1] throughput12.sh : use full OMP only if -omp is specified
    Note that throughputs with full OMP are often lower than expected (less than 4x).
    This is on a completely random basis... probably too few events for this test.

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+omp=0
+if [ "$1" == "-omp" ]; then
+  omp=1
+  shift
+fi
+
+if [ "$1" != "" ]; then
+  echo "Usage: $0 [-omp]"
+  exit 1
+fi
+
 exes=
 exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/check.exe"
 exes="$exes ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/gcheck.exe"
@@ -11,7 +22,7 @@ for exe in $exes; do
   # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
   TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
   echo "-------------------------------------------------------------------------"
-  if [ "${exe%%/check*}" != "${exe}" ]; then 
+  if [ "${omp}" == "1" ] && [ "${exe%%/check*}" != "${exe}" ]; then 
     export OMP_NUM_THREADS=$(nproc --all)
     TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
     echo "-------------------------------------------------------------------------"
