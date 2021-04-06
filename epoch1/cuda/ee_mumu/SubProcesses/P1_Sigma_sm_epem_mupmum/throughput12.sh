8957cfef768a537fd2aa60d99443ca0762dd4bbf
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-06 16:20:16 +0200

    [ep2to2ep1] throughput12.sh : add sigmaKin registers from ncu
    Note that epoch2 now uses less than epoch1 - which probably explains why it is faster
    
    Process                     = EPOCH1_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.148347e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.026716 sec
    real    0m8.058s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 5.503729e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.839778 sec
    real    0m1.149s
    ==PROF== Profiling "_ZN5gProc8sigmaKinEPKdPd": launch__registers_per_thread 174
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.103863e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.232970 sec
    real    0m8.264s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.148951e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     1.033001 sec
    real    0m1.342s
    ==PROF== Profiling "_ZN5gProc8sigmaKinEPKdPd": launch__registers_per_thread 164
    -------------------------------------------------------------------------

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -21,10 +21,12 @@ for exe in $exes; do
   unset OMP_NUM_THREADS
   # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
   TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
-  echo "-------------------------------------------------------------------------"
   if [ "${omp}" == "1" ] && [ "${exe%%/check*}" != "${exe}" ]; then 
+    echo "-------------------------------------------------------------------------"
     export OMP_NUM_THREADS=$(nproc --all)
     TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
-    echo "-------------------------------------------------------------------------"
+  elif [ "${exe%%/gcheck*}" != "${exe}" ]; then 
+    sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ncu) --metrics launch__registers_per_thread --target-processes all -k "sigmaKinE" --kernel-regex-base mangled --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
   fi
+  echo "-------------------------------------------------------------------------"
 done
