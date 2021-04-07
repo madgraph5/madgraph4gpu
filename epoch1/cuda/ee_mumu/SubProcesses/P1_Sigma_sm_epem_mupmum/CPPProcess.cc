b7116bfb6cccaf86e181aa5b8d9dad65ad05b6c0
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-07 19:39:47 +0200

    [ep2to2ep1] CPPProcess.cc epoch1 : go back to ixzxxx/imzxxxx instead of ixxxxx
    Back to the usual performance. Will copy to epoch2 and later vectorize
    
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.132769e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.048635 sec
    real    0m8.075s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.547247e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.811278 sec
    real    0m1.124s
    ==PROF== Profiling "_ZN5gProc8sigmaKinEPKdPd": launch__registers_per_thread 164
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.130969e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.065505 sec
    real    0m8.093s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.638448e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.819528 sec
    real    0m1.125s
    ==PROF== Profiling "_ZN5gProc8sigmaKinEPKdPd": launch__registers_per_thread 164
    -------------------------------------------------------------------------

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
@@ -896,19 +896,19 @@ namespace Proc
 #endif
 
 #ifdef __CUDACC__
-    //imzxxx( allmomenta, cHel[ihel][1], +1, w[1], 1 );
-    ixxxxx( allmomenta, 0, cHel[ihel][1], +1, w[1], 1 ); // tested ok (slower)
+    imzxxx( allmomenta, cHel[ihel][1], +1, w[1], 1 );
+    //ixxxxx( allmomenta, 0, cHel[ihel][1], +1, w[1], 1 ); // tested ok (slower)
 #else
-    //imzxxx( allmomenta, cHel[ihel][1], +1, w[1], ievt, 1 );
-    ixxxxx( allmomenta, 0, cHel[ihel][1], +1, w[1], ievt, 1 ); // tested ok (a bit slower)
+    imzxxx( allmomenta, cHel[ihel][1], +1, w[1], ievt, 1 );
+    //ixxxxx( allmomenta, 0, cHel[ihel][1], +1, w[1], ievt, 1 ); // tested ok (a bit slower)
 #endif
 
 #ifdef __CUDACC__
-    //ixzxxx( allmomenta, cHel[ihel][2], -1, w[2], 2 );
-    ixxxxx( allmomenta, 0, cHel[ihel][2], -1, w[2], 2 ); // tested ok (a bit slower)
+    ixzxxx( allmomenta, cHel[ihel][2], -1, w[2], 2 );
+    //ixxxxx( allmomenta, 0, cHel[ihel][2], -1, w[2], 2 ); // tested ok (a bit slower)
 #else
-    //ixzxxx( allmomenta, cHel[ihel][2], -1, w[2], ievt, 2 );
-    ixxxxx( allmomenta, 0, cHel[ihel][2], -1, w[2], ievt, 2 ); // tested ok (a bit slower)
+    ixzxxx( allmomenta, cHel[ihel][2], -1, w[2], ievt, 2 );
+    //ixxxxx( allmomenta, 0, cHel[ihel][2], -1, w[2], ievt, 2 ); // tested ok (a bit slower)
 #endif
 
 #ifdef __CUDACC__
