d07f7da74045e7286b3bd958d0f4bc35284faf3b
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-07 18:37:29 +0200

    [ep2to2ep1] CPPProcess.cc epoch1 : "clean up" ixxxxx a bit
    This may hopefully give some performance, and will help in vectorization
    
    Results are correct, but no performance gain at all
    
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.045520e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.498546 sec
    real    0m8.525s
    -------------------------------------------------------------------------
    Process                     = EPOCH1_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.185252e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.845187 sec
    real    0m1.149s
    ==PROF== Profiling "_ZN5gProc8sigmaKinEPKdPd": launch__registers_per_thread 184
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CPP
    OMP threads / `nproc --all` = 1 / 4
    EvtsPerSec[MatrixElems] (3) = ( 1.132483e+06                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     8.033916 sec
    real    0m8.059s
    -------------------------------------------------------------------------
    Process                     = EPOCH2_EEMUMU_CUDA
    EvtsPerSec[MatrixElems] (3) = ( 6.599302e+08                 )  sec^-1
    MeanMatrixElemValue         = ( 1.372152e-02 +- 3.269516e-06 )  GeV^0
    TOTAL       :     0.800913 sec
    real    0m1.106s
    ==PROF== Profiling "_ZN5gProc8sigmaKinEPKdPd": launch__registers_per_thread 164
    -------------------------------------------------------------------------

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/CPPProcess.cc
@@ -57,27 +57,22 @@ namespace MG5_sm
     using std::max;
     using std::min;
 #endif
-    //const fptype& pvec0 = pIparIp4Ievt( allmomenta, ipar, 0, ievt );
-    const fptype& pvec1 = pIparIp4Ievt(allmomenta, ipar, 1, ievt);
-    const fptype& pvec2 = pIparIp4Ievt(allmomenta, ipar, 2, ievt);
-    const fptype& pvec3 = pIparIp4Ievt(allmomenta, ipar, 3, ievt);
-    cxtype chi[2];
-    fptype sf[2], sfomega[2], omega[2], pp, pp3, sqp0p3, sqm[2];
-    int ip, im, nh;
-    fptype p[4] = {0, pvec1, pvec2, pvec3};
-    p[0] = sqrt(p[1] * p[1] + p[2] * p[2] + p[3] * p[3] + fmass * fmass);
-    fi[0] = cxtype(-p[0] * nsf, -p[3] * nsf);
-    fi[1] = cxtype(-p[1] * nsf, -p[2] * nsf);
-    nh = nhel * nsf;
-    if (fmass != 0.0)
+    const fptype& pvec1 = pIparIp4Ievt( allmomenta, ipar, 1, ievt );
+    const fptype& pvec2 = pIparIp4Ievt( allmomenta, ipar, 2, ievt );
+    const fptype& pvec3 = pIparIp4Ievt( allmomenta, ipar, 3, ievt );
+    const fptype p[4] = { sqrt( pvec1 * pvec1 + pvec2 * pvec2 + pvec3 * pvec3 + fmass * fmass ), pvec1, pvec2, pvec3 };
+    fi[0] = cxmake( -p[0] * nsf, -p[3] * nsf );
+    fi[1] = cxmake( -p[1] * nsf, -p[2] * nsf );
+    const int nh = nhel * nsf;
+    if ( fmass != 0. )
     {
-      pp = min(p[0], sqrt(p[1] * p[1] + p[2] * p[2] + p[3] * p[3]));
-      if (pp == 0.0)
+      const fptype pp = min( p[0], sqrt(p[1] * p[1] + p[2] * p[2] + p[3] * p[3] ) );
+      if ( pp == 0. )
       {
-        sqm[0] = sqrt(std::abs(fmass));
-        sqm[1] = (fmass < 0) ? - abs(sqm[0]) : abs(sqm[0]);
-        ip = (1 + nh)/2;
-        im = (1 - nh)/2;
+        fptype sqm[2] = { sqrt( std::abs( fmass ) ), 0 };
+        sqm[1] = ( fmass < 0 ? - abs( sqm[0] ) : abs( sqm[0] ) );
+        const int ip = ( 1 + nh ) / 2;
+        const int im = ( 1 - nh ) / 2;
         fi[2] = ip * sqm[ip];
         fi[3] = im * nsf * sqm[ip];
         fi[4] = ip * nsf * sqm[im];
@@ -85,25 +80,15 @@ namespace MG5_sm
       }
       else
       {
-        sf[0] = (1 + nsf + (1 - nsf) * nh) * 0.5;
-        sf[1] = (1 + nsf - (1 - nsf) * nh) * 0.5;
-        omega[0] = sqrt(p[0] + pp);
-        omega[1] = fmass/omega[0];
-        ip = (1 + nh)/2;
-        im = (1 - nh)/2;
-        sfomega[0] = sf[0] * omega[ip];
-        sfomega[1] = sf[1] * omega[im];
-        pp3 = max(pp + p[3], 0.0);
-        chi[0] = cxtype(sqrt(pp3 * 0.5/pp), 0);
-        if (pp3 == 0.0)
-        {
-          chi[1] = cxtype(-nh, 0);
-        }
-        else
-        {
-          chi[1] =
-            cxtype(nh * p[1], p[2])/sqrt(2.0 * pp * pp3);
-        }
+        const fptype sf[2] = { (1 + nsf + (1 - nsf) * nh) * 0.5, (1 + nsf - (1 - nsf) * nh) * 0.5 };
+        fptype omega[2] = { sqrt( p[0] + pp ), 0 };
+        omega[1] = fmass / omega[0];
+        const int ip = ( 1 + nh ) / 2;
+        const int im = ( 1 - nh ) / 2;
+        const fptype sfomega[2] = { sf[0] * omega[ip], sf[1] * omega[im] };
+        const fptype pp3 = max( pp + p[3], 0. );
+        const cxtype chi[2] = { cxmake( sqrt ( pp3 * 0.5 / pp ), 0 ), 
+                               ( pp3 == 0. ? cxmake( -nh, 0 ) : cxmake( nh * p[1], p[2] ) / sqrt( 2. * pp * pp3 ) ) };
         fi[2] = sfomega[0] * chi[im];
         fi[3] = sfomega[0] * chi[ip];
         fi[4] = sfomega[1] * chi[im];
@@ -112,27 +97,13 @@ namespace MG5_sm
     }
     else
     {
-      if (p[1] == 0.0 and p[2] == 0.0 and p[3] < 0.0)
+      const fptype sqp0p3 = ( p[1] == 0. and p[2] == 0. and p[3] < 0. ? 0. : sqrt( max( p[0] + p[3], 0. ) ) * nsf );
+      const cxtype chi[2] = { cxmake( sqp0p3, 0. ),
+                              ( sqp0p3 == 0. ? cxmake( -nhel * sqrt( 2. * p[0] ), 0. ) : cxmake( nh * p[1], p[2] ) / sqp0p3 ) };
+      if ( nh == 1 )
       {
-        sqp0p3 = 0.0;
-      }
-      else
-      {
-        sqp0p3 = sqrt(max(p[0] + p[3], 0.0)) * nsf;
-      }
-      chi[0] = cxtype(sqp0p3, 0.0);
-      if (sqp0p3 == 0.0)
-      {
-        chi[1] = cxtype(-nhel * sqrt(2.0 * p[0]), 0.0);
-      }
-      else
-      {
-        chi[1] = cxtype(nh * p[1], p[2])/sqp0p3;
-      }
-      if (nh == 1)
-      {
-        fi[2] = cxtype(0.0, 0.0);
-        fi[3] = cxtype(0.0, 0.0);
+        fi[2] = cxmake( 0, 0 );
+        fi[3] = cxmake( 0, 0 );
         fi[4] = chi[0];
         fi[5] = chi[1];
       }
@@ -140,8 +111,8 @@ namespace MG5_sm
       {
         fi[2] = chi[1];
         fi[3] = chi[0];
-        fi[4] = cxtype(0.0, 0.0);
-        fi[5] = cxtype(0.0, 0.0);
+        fi[4] = cxmake( 0, 0 );
+        fi[5] = cxmake( 0, 0 );
       }
     }
     mgDebug( 1, __FUNCTION__ );
