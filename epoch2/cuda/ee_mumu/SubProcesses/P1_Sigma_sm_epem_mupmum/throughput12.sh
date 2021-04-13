2493022c00017cde8e9c50defd4bdc8904cb9db7
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-13 18:39:50 +0200

    [klas2ep12] epoch1/2 throughput12.sh remove sudo before calling ncu (fixed RQF1777100)

--- epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -27,7 +27,8 @@ for exe in $exes; do
     export OMP_NUM_THREADS=$(nproc --all)
     TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep -i '(process|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|TOTAL       :)'
   elif [ "${exe%%/gcheck*}" != "${exe}" ]; then 
-    sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ncu) --metrics launch__registers_per_thread --target-processes all -k "sigmaKinE" --kernel-regex-base mangled --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
+    ###sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ncu) --metrics launch__registers_per_thread --target-processes all -k "sigmaKinE" --kernel-regex-base mangled --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
+    $(which ncu) --metrics launch__registers_per_thread --target-processes all -k "sigmaKinE" --kernel-regex-base mangled --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
   fi
 done
 echo "-------------------------------------------------------------------------"
