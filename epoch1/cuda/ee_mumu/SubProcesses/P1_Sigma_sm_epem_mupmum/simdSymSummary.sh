0f902586e0be0fe8d8bb3cb804e3187e7181bb45
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-19 16:31:08 +0200

    [klas2ep12] epoch1 optionally select only the helamps namespace in simdSymSummary.sh
    for avx in none sse4 avx2 512y 512z; do f=./build.$avx/CPPProcess.o; ./simdSymSummary.sh $f; done
    === Symbols in ./build.none/CPPProcess.o === (~sse4:  611) (avx2:    0) (512y:    0) (512z:    0)
    === Symbols in ./build.sse4/CPPProcess.o === (~sse4: 3278) (avx2:    0) (512y:    0) (512z:    0)
    === Symbols in ./build.avx2/CPPProcess.o === (~sse4:    0) (avx2: 2766) (512y:    0) (512z:    0)
    === Symbols in ./build.512y/CPPProcess.o === (~sse4:    0) (avx2: 2597) (512y:   94) (512z:    0)
    === Symbols in ./build.512z/CPPProcess.o === (~sse4:    0) (avx2: 1182) (512y:  208) (512z: 2035)
    
    for avx in none sse4 avx2 512y 512z; do f=./build.$avx/CPPProcess.o; ./simdSymSummary.sh -helamps $f; done
    === Symbols in ./build.none/CPPProcess.o === (~sse4:  536) (avx2:    0) (512y:    0) (512z:    0)
    === Symbols in ./build.sse4/CPPProcess.o === (~sse4: 3145) (avx2:    0) (512y:    0) (512z:    0)
    === Symbols in ./build.avx2/CPPProcess.o === (~sse4:    0) (avx2: 2500) (512y:    0) (512z:    0)
    === Symbols in ./build.512y/CPPProcess.o === (~sse4:    0) (avx2: 2369) (512y:   77) (512z:    0)
    === Symbols in ./build.512z/CPPProcess.o === (~sse4:    0) (avx2:  979) (512y:  189) (512z: 2015)

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/simdSymSummary.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/simdSymSummary.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#----------------------------------------------------------------------------------------------
+
 # Auxiliary function of mainSummarizeSyms
 # Env: $dumptmp must point to the stripped objdump file
 function stripSyms() {
@@ -13,39 +15,64 @@ function stripSyms() {
   done; echo $cnt
 }
 
+#----------------------------------------------------------------------------------------------
+
 function mainSummarizeSyms() {
+
+  # Command line arguments: HelAmps functions only?
+  helamps=0
+  if [ "$1" == "-helamps" ]; then
+    helamps=1
+    shift
+  fi
+
   # Command line arguments: select file
   if [ "$1" == "" ] || [ "$2" != "" ]; then
-    echo "Usage:   $0 <filename>"
+    echo "Usage:   $0 [-helamps] <filename>"
     echo "Example: $0 ./check.exe"
     exit 1
   fi
   file=$1
+
   # Disassemble selected file
   # Use cut -f3- to print only the assembly code after two leading fields separated by tabs
   dumptmp=${file}.objdump.tmp
-  objdump -d -C $file | awk '/^ +[[:xdigit:]]+:\t/' | cut -f3- > ${dumptmp}
+  if [ "$helamps" == "0" ]; then
+    objdump -d -C $file | awk '/^ +[[:xdigit:]]+:\t/' | cut -f3- > ${dumptmp}
+  else
+    objdump -d -C $file | awk -v RS= '/^[[:xdigit:]]+ <MG5_sm::.*/' | awk '/^ +[[:xdigit:]]+:\t/' | cut -f3- > ${dumptmp}
+  fi
   ###ls -l $dumptmp
+
   # Count and strip AVX512 zmm symbols
   cnt512z=$(stripSyms '^v.*zmm')
   ###echo $cnt512z; ls -l $dumptmp
+
   # Count and strip AVX512 ymm/xmm symbols
+  # [NB: these are AVX512VL symbols, i.e. implementing AVX512 on xmm/ymm registers]
   cnt512y=$(stripSyms '^v.*dqa(32|64).*(x|y)mm' '^v.*(32|64)x2.*(x|y)mm' '^vpcmpneqq.*(x|y)mm')
   ###echo $cnt512y; ls -l $dumptmp
+
   # Count and strip AVX2 symbols
   cntavx2=$(stripSyms '^v.*(x|y)mm')
   ###echo $cntavx2; ls -l $dumptmp
-  # Count and strip SSE4 symbols
+
+  # Count and strip ~SSE4 symbols
   ###cntsse4=$(stripSyms '^[^v].*xmm') # 'too many'(*): includes many symbols from "none" build
   cntsse4=$(stripSyms '^[^v].*p(d|s).*xmm') # okish, representative enough of what sse4/nehalem adds
   ###cntsse4=$(stripSyms '^(mul|add|sub)p(d|s).*xmm') # too few: should also include moves
   ###echo $cntsse4; ls -l $dumptmp
+
   # Is there anything else?...
   ###cntelse=$(stripSyms '.*(x|y|z)mm') # only makes sense when counting 'too many'(*) as sse4
   ###echo $cntelse; ls -l $dumptmp
   ###if [ "$cntelse" != "0" ]; then echo "ERROR! cntelse='$cntelse'"; exit 1; fi 
+
   # Final report
   printf "=== Symbols in $file === (~sse4:%5d) (avx2:%5d) (512y:%5d) (512z:%5d)\n" $cntsse4 $cntavx2 $cnt512y $cnt512z
+
 }
+
 mainSummarizeSyms $*
 
+#----------------------------------------------------------------------------------------------
