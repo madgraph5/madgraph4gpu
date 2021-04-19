f8328c4c120b7a0a2be64148e9aeab91b29f36de
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-19 16:17:35 +0200

    [klas2ep12] epoch1 Move simd symbol summary to a separate script

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/simdSym.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/simdSym.sh
@@ -69,57 +69,6 @@ function mainCountSyms() {
 
 #----------------------------------------------------------------------------------------------
 
-# Auxiliary function of mainSummarizeSyms
-# Env: $dumptmp must point to the stripped objdump file
-function stripSyms() {
-  if [ "$dumptmp" == "" ] || [ ! -e $dumptmp ]; then echo "ERROR! File '$dumptmp' not found"; exit 1; fi 
-  cnt=0
-  for sym in "$@"; do
-    # Use cut -f3- to print only the assembly code after two leading fields separated by tabs
-    (( cnt += $(cat $dumptmp | egrep "$sym" | wc -l) ))
-    cat $dumptmp | egrep -v "$sym" > ${dumptmp}.new
-    \mv ${dumptmp}.new $dumptmp
-  done; echo $cnt
-}
-
-function mainSummarizeSyms() {
-  # Command line arguments: select file
-  if [ "$1" == "" ] || [ "$2" != "" ]; then
-    echo "Usage:   $0 <filename>"
-    echo "Example: $0 ./check.exe"
-    exit 1
-  fi
-  file=$1
-  # Disassemble selected file
-  # Use cut -f3- to print only the assembly code after two leading fields separated by tabs
-  dumptmp=${file}.objdump.tmp
-  objdump -d -C $file | awk '/^ +[[:xdigit:]]+:\t/' | cut -f3- > ${dumptmp}
-  ###ls -l $dumptmp
-  # Count and strip AVX512 zmm symbols
-  cnt512z=$(stripSyms '^v.*zmm')
-  ###echo $cnt512z; ls -l $dumptmp
-  # Count and strip AVX512 ymm/xmm symbols
-  cnt512y=$(stripSyms '^v.*dqa(32|64).*(x|y)mm' '^v.*(32|64)x2.*(x|y)mm' '^vpcmpneqq.*(x|y)mm')
-  ###echo $cnt512y; ls -l $dumptmp
-  # Count and strip AVX2 symbols
-  cntavx2=$(stripSyms '^v.*(x|y)mm')
-  ###echo $cntavx2; ls -l $dumptmp
-  # Count and strip SSE4 symbols
-  ###cntsse4=$(stripSyms '^[^v].*xmm') # 'too many'(*): includes many symbols from "none" build
-  cntsse4=$(stripSyms '^[^v].*p(d|s).*xmm') # okish, representative enough of what sse4/nehalem adds
-  ###cntsse4=$(stripSyms '^(mul|add|sub)p(d|s).*xmm') # too few: should also include moves
-  ###echo $cntsse4; ls -l $dumptmp
-  # Is there anything else?...
-  ###cntelse=$(stripSyms '.*(x|y|z)mm') # only makes sense when counting 'too many'(*) as sse4
-  ###echo $cntelse; ls -l $dumptmp
-  ###if [ "$cntelse" != "0" ]; then echo "ERROR! cntelse='$cntelse'"; exit 1; fi 
-  # Final report
-  printf "=== Symbols in $file === (~sse4:%5d) (avx2:%5d) (512y:%5d) (512z:%5d)\n" $cntsse4 $cntavx2 $cnt512y $cnt512z
-}
-mainSummarizeSyms $*
-
-#----------------------------------------------------------------------------------------------
-
 # Auxiliary function of mainListSyms
 # Env: $dump must point to the objdump file
 function listSyms() {
