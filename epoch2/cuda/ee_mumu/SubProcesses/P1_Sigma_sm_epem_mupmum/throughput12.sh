6d5f2d08c6441cdb8e5049e85ef3c788189a72e8
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-26 21:51:19 +0200

    [ccache] further clean up throughput12.sh ep1/2, remove "sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"

--- epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -32,7 +32,6 @@ for exe in $exes; do
     export OMP_NUM_THREADS=$(nproc --all)
     TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep "$pattern"
   elif [ "${exe%%/gcheck*}" != "${exe}" ]; then 
-    ###sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ncu) --metrics launch__registers_per_thread --target-processes all -k "sigmaKinE" --kernel-regex-base mangled --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
     $(which ncu) --metrics launch__registers_per_thread --target-processes all --kernel-id "::sigmaKin:" --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
   fi
 done
