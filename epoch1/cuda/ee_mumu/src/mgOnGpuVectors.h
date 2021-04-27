5a0056bd42774eca0c684338cabe55a46eaa598c
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-27 17:35:40 +0200

    [klas3] final test of cxtype_ref on clang12.0: still fails
    ccache /cvmfs/sft.cern.ch/lcg/releases/clang/12.0.0-8916b/x86_64-centos7/bin/clang++  -O3 -std=c++17 -I.  -Wall -Wshadow -Wextra -fopenmp -DMGONGPU_COMMONRAND_ONHOST -ffast-math  -march=haswell   -c rambo.cc -o rambo.o
    In file included from rambo.cc:1:
    In file included from ./rambo.h:6:
    ./mgOnGpuVectors.h:71:54: error: no matching constructor for initialization of 'mgOnGpu::cxtype_ref'
        cxtype_ref operator[]( size_t i ) const { return cxtype_ref( m_real[i], m_imag[i] ); }
                                                         ^           ~~~~~~~~~~~~~~~~~~~~
    ./mgOnGpuVectors.h:46:5: note: candidate constructor not viable: 1st argument ('const mgOnGpu::fptype' (aka 'const double')) would lose const qualifier
        cxtype_ref( fptype& r, fptype& i ) : m_real{r}, m_imag{i} {}
        ^

--- epoch1/cuda/ee_mumu/src/mgOnGpuVectors.h
+++ epoch1/cuda/ee_mumu/src/mgOnGpuVectors.h
@@ -28,7 +28,8 @@ namespace mgOnGpu
   // If set: return a pair of (fptype&, fptype&) by non-const reference in cxtype_v::operator[]
   // This is forbidden in clang ("non-const reference cannot bind to vector element")
   // See also https://stackoverflow.com/questions/26554829
-#undef MGONGPU_HAS_CXTYPE_REF // clang default
+#define MGONGPU_HAS_CXTYPE_REF 1 // clang test
+  //#undef MGONGPU_HAS_CXTYPE_REF // clang default
 #else
 #define MGONGPU_HAS_CXTYPE_REF 1 // gcc default
   //#undef MGONGPU_HAS_CXTYPE_REF // gcc test (very slightly slower? issue #182)
