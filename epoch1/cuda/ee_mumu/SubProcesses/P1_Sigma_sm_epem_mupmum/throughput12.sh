5884d6335625ad0995990c4a3bb95ee2047feaf1
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-27 18:39:01 +0200

    [klas3] streamline throughput12.sh as in the hetklas branch

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -37,7 +37,7 @@ pushd ../../../../../epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum >&
   make AVX=none
   if [ "${avxall}" == "1" ]; then make AVX=sse4; fi
   if [ "${avxall}" == "1" ]; then make AVX=avx2; fi
-  make AVX=512y
+  make AVX=512y # always consider 512y as the reference, even if for clang avx2 is slightly faster...
   if [ "${avxall}" == "1" ]; then make AVX=512z; fi
 popd >& /dev/null
 
@@ -46,28 +46,38 @@ pushd ../../../../../epoch2/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum >&
   make
 popd >& /dev/null
 
-pattern="Process|fptype_sv|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|FP precision|TOTAL       :"
-# Optionally add other patterns here for some specific configurations (e.g. clang)
-pattern="${pattern}|CUCOMPLEX"
-pattern="${pattern}|COMMON RANDOM"
-pattern="(${pattern})"
+function runExe() {
+  exe=$1
+  ###echo "runExe $exe OMP=$OMP_NUM_THREADS"
+  pattern="Process|fptype_sv|OMP threads|EvtsPerSec\[Matrix|MeanMatrix|FP precision|TOTAL       :"
+  # Optionally add other patterns here for some specific configurations (e.g. clang)
+  pattern="${pattern}|CUCOMPLEX"
+  pattern="${pattern}|COMMON RANDOM"
+  # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep "(${pattern})"
+}
+
+function runNcu() {
+  exe=$1
+  ###echo "runExe $exe OMP=$OM_NUM_THREADS (NCU)"
+  $(which ncu) --metrics launch__registers_per_thread --target-processes all --kernel-id "::sigmaKin:" --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
+}
 
 echo -e "\nOn $HOSTNAME:"
 for exe in $exes; do
   if [ ! -f $exe ]; then continue; fi
   echo "-------------------------------------------------------------------------"
   unset OMP_NUM_THREADS
-  # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
-  TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep "$pattern"
+  runExe $exe
   if [ "${exe%%/check*}" != "${exe}" ]; then 
     obj=${exe%%/check*}/CPPProcess.o; ./simdSymSummary.sh -stripdir ${obj}
     if [ "${omp}" == "1" ]; then 
       echo "-------------------------------------------------------------------------"
       export OMP_NUM_THREADS=$(nproc --all)
-      TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep "$pattern"
+      runExe $exe
     fi
   elif [ "${exe%%/gcheck*}" != "${exe}" ]; then 
-    $(which ncu) --metrics launch__registers_per_thread --target-processes all --kernel-id "::sigmaKin:" --print-kernel-base mangled $exe -p 2048 256 1 | egrep '(sigmaKin|registers)' | tr "\n" " " | awk '{print $1, $2, $3, $15, $17}'
+    runNcu $exe
   fi
 done
 echo "-------------------------------------------------------------------------"
