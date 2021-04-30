d3e1a65ab846e99d2e9df66a4c4f9afda2b9437a
Andrea Valassi <andrea.valassi@cern.ch>
2021-04-30 16:54:35 +0200

    [lxplus] throughput12.sh do not use perf if it is not available

--- epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
+++ epoch1/cuda/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/throughput12.sh
@@ -58,13 +58,16 @@ function runExe() {
   # Optionally add other patterns here for some specific configurations (e.g. clang)
   pattern="${pattern}|CUCOMPLEX"
   pattern="${pattern}|COMMON RANDOM"
-  # -- Older version using time
-  # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
-  ###TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep "(${pattern})"
-  # -- Newer version using perf stat
-  pattern="${pattern}|instructions|cycles"
-  pattern="${pattern}|elapsed"
-  perf stat $exe -p 2048 256 12 2>&1 | egrep "(${pattern})" | grep -v "Performance counter stats"
+  if perf --version >& /dev/null; then
+    # -- Newer version using perf stat
+    pattern="${pattern}|instructions|cycles"
+    pattern="${pattern}|elapsed"
+    perf stat $exe -p 2048 256 12 2>&1 | egrep "(${pattern})" | grep -v "Performance counter stats"
+  else
+    # -- Older version using time
+    # For TIMEFORMAT see https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+    TIMEFORMAT=$'real\t%3lR' && time $exe -p 2048 256 12 2>&1 | egrep "(${pattern})"
+  fi
 }
 
 function runNcu() {
