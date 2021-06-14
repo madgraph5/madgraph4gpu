11eb1761ea49ff394615e4302b5bf741f146e0e8
Andrea Valassi <andrea.valassi@cern.ch>
2021-06-14 17:31:20 +0200

    [ggttgg] copy mgOnGpuTypes.h from eemumu
    perf stat -d ../../../../../epoch2/cuda/gg_ttgg/SubProcesses/P1_Sigma_sm_gg_ttxgg/gcheck.exe -p 64 256 1 |& egrep '(Process|fptype_sv|OMP threads|EvtsPerSec\[MECalc|MeanMatrix|FP precision|TOTAL       :|EvtsPerSec\[Matrix|CUCOMPLEX|COMMON RANDOM|ERROR|instructions|cycles|elapsed)' | grep -v 'Performance counter stats'
    FP precision               = DOUBLE (nan=0)
    EvtsPerSec[MatrixElems] (3)= ( 4.431257e+05                 )  sec^-1
    MeanMatrixElemValue        = ( 5.532387e+01 +- 5.501866e+01 )  GeV^-4
    TOTAL       :     5.664586 sec
        14,242,402,487      cycles                    #    2.645 GHz
        24,438,431,992      instructions              #    1.72  insn per cycle
           5.955651007 seconds time elapsed
    
    perf stat -d ../../../../../epoch2/cuda/gg_ttgg/SubProcesses/P1_Sigma_sm_gg_ttxgg/check.exe -p 64 256 1 |& egrep '(Process|fptype_sv|OMP threads|EvtsPerSec\[MECalc|MeanMatrix|FP precision|TOTAL       :|EvtsPerSec\[Matrix|CUCOMPLEX|COMMON RANDOM|ERROR|instructions|cycles|elapsed)' | grep -v 'Performance counter stats'
    FP precision               = DOUBLE (nan=0)
    EvtsPerSec[MatrixElems] (3)= ( 1.827445e+03                 )  sec^-1
    MeanMatrixElemValue        = ( 5.532387e+01 +- 5.501866e+01 )  GeV^-4
    TOTAL       :     8.981804 sec
        24,058,806,303      cycles                    #    2.677 GHz
        73,925,907,974      instructions              #    3.07  insn per cycle
           8.991724619 seconds time elapsed

--- epoch2/cuda/gg_ttgg/src/mgOnGpuConfig.h
+++ epoch2/cuda/gg_ttgg/src/mgOnGpuConfig.h
@@ -38,6 +38,16 @@
 
 namespace mgOnGpu
 {
+
+  // --- Type definitions
+
+  // Floating point type: fptype
+#if defined MGONGPU_FPTYPE_DOUBLE
+  typedef double fptype; // double precision (8 bytes, fp64)
+#elif defined MGONGPU_FPTYPE_FLOAT
+  typedef float fptype; // single precision (4 bytes, fp32)
+#endif
+
   // --- Physics process-specific constants that are best declared at compile time
 
   const int np4 = 4; // the dimension of 4-momenta (E,px,py,pz)
@@ -84,6 +94,9 @@ namespace mgOnGpu
   //const int neppM = 32; // older default
 
 }
+
+// Expose typedefs and operators outside the namespace
+using mgOnGpu::fptype;
 
 // Cuda nsight compute (ncu) debug: add dummy lines to ease SASS program flow navigation
 // Arguments (not used so far): text is __FUNCTION__, code is 0 (start) or 1 (end)
