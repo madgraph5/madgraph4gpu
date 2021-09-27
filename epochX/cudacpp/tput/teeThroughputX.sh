7d6e6b2b907cc8f81eca8dcff784d108e5511ac1
Andrea Valassi <andrea.valassi@cern.ch>
2021-09-27 17:23:38 +0200

    [epochX] improve the tee script to handle separate logs - and rerun the tests

--- epochX/cudacpp/tput/teeThroughputX.sh
+++ epochX/cudacpp/tput/teeThroughputX.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
 cd $(dirname $0)
-if [ "$*" == "" ]; then args="-eemumu -ggttgg -auto"; else args="$*"; fi
-if ! ./throughputX.sh -makeonly $args; then exit 1; fi
-./throughputX.sh $args | tee throughputX_log.txt
+
+if [ "$*" == "" ]; then
+  args="-eemumu -ggttgg"
+  auto="-auto"
+else 
+  args=
+  auto=
+  for arg in $*; do
+    if [ "$arg" == "-auto" ] || [ "$arg" == "-autoonly" ]; then
+      if [ "$auto" != "" ] && [ "$auto" != "$arg" ]; then
+        echo "ERROR! Incompatible options -auto and -autoonly"
+        exit 1
+      fi
+      auto=$arg
+    else
+      args="$args $arg"
+    fi  
+  done
+fi
+echo "args=$args"
+echo "auto=$auto"
+
+if ! ./throughputX.sh -makeonly $args $auto; then exit 1; fi
+
+if [ "$auto" != "-autoonly" ]; then
+  ./throughputX.sh $args | tee throughputX_log.txt
+fi 
+
+if [ "$auto" == "-auto" ] || [ "$auto" == "-autoonly" ]; then
+  ./throughputX.sh $args -autoonly | tee throughputX_log_auto.txt
+fi
