b8a394ef463aa60e61083d73ea1dcd8b9065b66a
Andrea Valassi <andrea.valassi@cern.ch>
2021-09-27 21:04:54 +0200

    [epochX2] reorder functions in HelAmps.h from epoch2 (https://bugs.launchpad.net/mg5amcnlo/+bug/1944444)

--- epochX/cudacpp/gg_ttgg/src/HelAmps_sm.h
+++ epochX/cudacpp/gg_ttgg/src/HelAmps_sm.h
@@ -92,18 +92,6 @@ __device__ void VVV1_0(const cxtype V1[], const cxtype V2[], const cxtype V3[],
 __device__ void VVV1P0_1(const cxtype V2[], const cxtype V3[], const cxtype
     COUP, const fptype M1, const fptype W1, cxtype V1[]);
 
-__device__ void VVVV1_0(const cxtype V1[], const cxtype V2[], const cxtype
-    V3[], const cxtype V4[], const cxtype COUP, cxtype * vertex);
-
-__device__ void VVVV1P0_1(const cxtype V2[], const cxtype V3[], const cxtype
-    V4[], const cxtype COUP, const fptype M1, const fptype W1, cxtype V1[]);
-
-__device__ void VVVV3_0(const cxtype V1[], const cxtype V2[], const cxtype
-    V3[], const cxtype V4[], const cxtype COUP, cxtype * vertex);
-
-__device__ void VVVV3P0_1(const cxtype V2[], const cxtype V3[], const cxtype
-    V4[], const cxtype COUP, const fptype M1, const fptype W1, cxtype V1[]);
-
 __device__ void FFV1_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
     const cxtype COUP, cxtype * vertex);
 
@@ -115,6 +103,18 @@ __device__ void FFV1_2(const cxtype F1[], const cxtype V3[], const cxtype COUP,
 
 __device__ void FFV1P0_3(const cxtype F1[], const cxtype F2[], const cxtype
     COUP, const fptype M3, const fptype W3, cxtype V3[]);
+
+__device__ void VVVV1_0(const cxtype V1[], const cxtype V2[], const cxtype
+    V3[], const cxtype V4[], const cxtype COUP, cxtype * vertex);
+
+__device__ void VVVV1P0_1(const cxtype V2[], const cxtype V3[], const cxtype
+    V4[], const cxtype COUP, const fptype M1, const fptype W1, cxtype V1[]);
+
+__device__ void VVVV3_0(const cxtype V1[], const cxtype V2[], const cxtype
+    V3[], const cxtype V4[], const cxtype COUP, cxtype * vertex);
+
+__device__ void VVVV3P0_1(const cxtype V2[], const cxtype V3[], const cxtype
+    V4[], const cxtype COUP, const fptype M1, const fptype W1, cxtype V1[]);
 
 __device__ void VVVV4_0(const cxtype V1[], const cxtype V2[], const cxtype
     V3[], const cxtype V4[], const cxtype COUP, cxtype * vertex);
