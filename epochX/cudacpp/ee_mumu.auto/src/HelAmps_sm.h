fbf0569bc1332a023570b637acd889ce2c017014
Andrea Valassi <andrea.valassi@cern.ch>
2021-10-08 21:42:08 +0200

    [epochX2/em] regenerate eemumu auto all files

--- epochX/cudacpp/ee_mumu.auto/src/HelAmps_sm.h
+++ epochX/cudacpp/ee_mumu.auto/src/HelAmps_sm.h
@@ -1,5 +1,5 @@
 //==========================================================================
-// This file has been automatically generated for C++ Standalone
+// This file has been automatically generated for CUDA/C++ standalone
 // MadGraph5_aMC@NLO v. 2.9.5, 2021-08-22
 // By the MadGraph5_aMC@NLO Development Team
 // Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
@@ -9,7 +9,7 @@
 #ifndef HelAmps_sm_H
 #define HelAmps_sm_H
 
-#include <cmath> 
+#include <cmath>
 #include "mgOnGpuConfig.h"
 #include "mgOnGpuTypes.h"
 #include "mgOnGpuVectors.h"
@@ -18,23 +18,23 @@ using namespace std;
 
 namespace MG5_sm
 {
-// [NB the helas.h template is currently empty]
+  // [NB the helas.h template is currently empty - the following comes from get_header_txt]
 
-__device__ void FFV1_0(const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP, cxtype * vertex);
+  __device__ void FFV1_0( const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP, cxtype* vertex );
 
-__device__ void FFV1P0_3(const cxtype F1[], const cxtype F2[], const cxtype COUP, const fptype M3, const fptype W3, cxtype V3[]);
+  __device__ void FFV1P0_3( const cxtype F1[], const cxtype F2[], const cxtype COUP, const fptype M3, const fptype W3, cxtype V3[] );
 
-__device__ void FFV2_0(const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP, cxtype * vertex);
+  __device__ void FFV2_0( const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP, cxtype* vertex );
 
-__device__ void FFV2_3(const cxtype F1[], const cxtype F2[], const cxtype COUP, const fptype M3, const fptype W3, cxtype V3[]);
+  __device__ void FFV2_3( const cxtype F1[], const cxtype F2[], const cxtype COUP, const fptype M3, const fptype W3, cxtype V3[] );
 
-__device__ void FFV4_0(const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP, cxtype * vertex);
+  __device__ void FFV4_0( const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP, cxtype* vertex );
 
-__device__ void FFV4_3(const cxtype F1[], const cxtype F2[], const cxtype COUP, const fptype M3, const fptype W3, cxtype V3[]);
+  __device__ void FFV4_3( const cxtype F1[], const cxtype F2[], const cxtype COUP, const fptype M3, const fptype W3, cxtype V3[] );
 
-__device__ void FFV2_4_0(const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP1, const cxtype COUP2, cxtype * vertex);
+  __device__ void FFV2_4_0( const cxtype F1[], const cxtype F2[], const cxtype V3[], const cxtype COUP1, const cxtype COUP2, cxtype* vertex );
 
-__device__ void FFV2_4_3(const cxtype F1[], const cxtype F2[], const cxtype COUP1, const cxtype COUP2, const fptype M3, const fptype W3, cxtype V3[]);
+  __device__ void FFV2_4_3( const cxtype F1[], const cxtype F2[], const cxtype COUP1, const cxtype COUP2, const fptype M3, const fptype W3, cxtype V3[] );
 
 } // end namespace MG5_sm
 
