2a4b32e4969ab31ea255789f5f69104e22e2f825
Andrea Valassi <andrea.valassi@cern.ch>
2021-10-13 15:48:17 +0200

    [epochX3] copy manual HelAmps_sm.h from auto (better comments and signatures, no other diffs)

--- epochX/cudacpp/ee_mumu/src/HelAmps_sm.h
+++ epochX/cudacpp/ee_mumu/src/HelAmps_sm.h
@@ -1,6 +1,6 @@
 //==========================================================================
-// This file has been automatically generated for C++ Standalone
-// MadGraph5_aMC@NLO v. 2.8.2, 2020-10-30
+// This file has been automatically generated for CUDA/C++ standalone
+// MadGraph5_aMC@NLO v. 2.9.5, 2021-08-22
 // By the MadGraph5_aMC@NLO Development Team
 // Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
 // GPU Template
@@ -27,12 +27,13 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fi[6] from the input momenta[npar*4*nevt]
   __device__ INLINE
-  void ixxxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               const fptype fmass,
+  void ixxxxx( const fptype_sv* momenta,
+               const fptype fmass,             // input: fermion mass
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fi,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fi[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -40,12 +41,14 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fi[6] from the input momenta[npar*4*nevt]
+  // ASSUMPTIONS: (FMASS == 0) and (PX == PY == 0 and E == +PZ > 0)
   __device__ INLINE
-  void ipzxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               //const fptype fmass,           // ASSUME fmass==0
+  void ipzxxx( const fptype_sv* momenta,
+               //const fptype fmass,           // ASSUME fermion mass==0
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fi,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fi[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -53,12 +56,14 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fi[6] from the input momenta[npar*4*nevt]
+  // ASSUMPTIONS: (FMASS == 0) and (PX == PY == 0 and E == -PZ > 0)
   __device__ INLINE
-  void imzxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               //const fptype fmass,           // ASSUME fmass==0
+  void imzxxx( const fptype_sv* momenta,
+               //const fptype fmass,           // ASSUME fermion mass==0
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fi,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fi[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -66,12 +71,14 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fi[6] from the input momenta[npar*4*nevt]
+  // ASSUMPTIONS: (FMASS == 0) and (PT > 0)
   __device__ INLINE
-  void ixzxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               //const fptype fmass,           // ASSUME fmass==0
+  void ixzxxx( const fptype_sv* momenta,
+               //const fptype fmass,           // ASSUME fermion mass==0
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fi,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fi[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -79,12 +86,13 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction vc[6] from the input momenta[npar*4*nevt]
   __device__ INLINE
-  void vxxxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               const fptype vmass,
+  void vxxxxx( const fptype_sv* momenta,
+               const fptype vmass,             // input: vector boson mass
                const int nhel,                 // input: -1, 0 (only if vmass!=0) or +1 (helicity of vector boson)
                const int nsv,                  // input: +1 (final) or -1 (initial)
-               cxtype_sv* vc,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv vc[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -92,12 +100,13 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction sc[3] from the input momenta[npar*4*nevt]
   __device__ INLINE
-  void sxxxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               const fptype,                   // WARNING: "smass" unused (missing in Fortran)
-               const int,                      // WARNING: "nhel" unused (missing in Fortran) - scalar has no helicity
+  void sxxxxx( const fptype_sv* momenta,
+               const fptype,                   // WARNING: input "smass" unused (missing in Fortran) - scalar boson mass
+               const int,                      // WARNING: input "nhel" unused (missing in Fortran) - scalar has no helicity!
                const int nss,                  // input: +1 (final) or -1 (initial)
-               cxtype_sv sc[3],                // output: wavefunction[3] - not [6], this is for scalars
+               cxtype_sv sc[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -105,12 +114,13 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fo[6] from the input momenta[npar*4*nevt]
   __device__ INLINE
-  void oxxxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               const fptype fmass,
+  void oxxxxx( const fptype_sv* momenta,
+               const fptype fmass,             // input: fermion mass
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fo,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fo[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -118,11 +128,14 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fo[6] from the input momenta[npar*4*nevt]
+  // ASSUMPTIONS: (FMASS == 0) and (PX == PY == 0 and E == +PZ > 0)
   __device__ INLINE
-  void opzxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
+  void opzxxx( const fptype_sv* momenta,
+               //const fptype fmass,           // ASSUME fermion mass==0
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fo,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fo[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -130,11 +143,14 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fo[6] from the input momenta[npar*4*nevt]
+  // ASSUMPTIONS: (FMASS == 0) and (PX == PY == 0 and E == -PZ > 0)
   __device__ INLINE
-  void omzxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
+  void omzxxx( const fptype_sv* momenta,
+               //const fptype fmass,           // ASSUME fermion mass==0
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fo,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fo[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -142,12 +158,13 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction fo[6] from the input momenta[npar*4*nevt]
   __device__ INLINE
-  void oxzxxx( const fptype_sv* allmomenta,    // input[(npar=4)*(np4=4)*nevt]
-               //const fptype fmass,           // ASSUME fmass==0
+  void oxzxxx( const fptype_sv* momenta,
+               //const fptype fmass,           // ASSUME fermion mass==0
                const int nhel,                 // input: -1 or +1 (helicity of fermion)
                const int nsf,                  // input: +1 (particle) or -1 (antiparticle)
-               cxtype_sv* fo,                  // output: wavefunction[(nw6==6)]
+               cxtype_sv fo[],
 #ifndef __CUDACC__
                const int ipagV,
 #endif
@@ -155,81 +172,89 @@ namespace MG5_sm
 
   //--------------------------------------------------------------------------
 
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
   __device__ INLINE
-  void FFV1_0( const cxtype_sv F1[],                // input: wavefunction1[6]
-               const cxtype_sv F2[],                // input: wavefunction2[6]
-               const cxtype_sv V3[],                // input: wavefunction3[6]
+  void FFV1_0( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype_sv V3[],
                const cxtype COUP,
-               cxtype_sv* vertex ) ALWAYS_INLINE;   // output: amplitude
+               cxtype_sv* vertex ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
   __device__ INLINE
-  void FFV1P0_3( const cxtype_sv F1[],              // input: wavefunction1[6]
-                 const cxtype_sv F2[],              // input: wavefunction2[6]
+  void FFV1P0_3( const cxtype_sv F1[],
+                 const cxtype_sv F2[],
                  const cxtype COUP,
                  const fptype M3,
                  const fptype W3,
-                 cxtype_sv V3[] ) ALWAYS_INLINE;    // output: wavefunction3[6]
+                 cxtype_sv V3[] ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
   __device__ INLINE
-  void FFV2_0( const cxtype_sv F1[],                // input: wavefunction1[6]
-               const cxtype_sv F2[],                // input: wavefunction2[6]
-               const cxtype_sv V3[],                // input: wavefunction3[6]
+  void FFV2_0( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype_sv V3[],
                const cxtype COUP,
-               cxtype_sv* vertex ) ALWAYS_INLINE;   // output: amplitude
+               cxtype_sv* vertex ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
   __device__ INLINE
-  void FFV2_3( const cxtype_sv F1[],                // input: wavefunction1[6]
-               const cxtype_sv F2[],                // input: wavefunction2[6]
-               const cxtype COUP,
-               const fptype M3,
-               const fptype W3,
-               cxtype_sv V3[] ) ALWAYS_INLINE;      // output: wavefunction3[6]
-
-  //--------------------------------------------------------------------------
-
-  __device__ INLINE
-  void FFV4_0( const cxtype_sv F1[],                // input: wavefunction1[6]
-               const cxtype_sv F2[],                // input: wavefunction2[6]
-               const cxtype_sv V3[],                // input: wavefunction3[6]
-               const cxtype COUP,
-               cxtype_sv* vertex ) ALWAYS_INLINE;   // output: amplitude
-
-  //--------------------------------------------------------------------------
-
-  __device__ INLINE
-  void FFV4_3( const cxtype_sv F1[],                // input: wavefunction1[6]
-               const cxtype_sv F2[],                // input: wavefunction2[6]
+  void FFV2_3( const cxtype_sv F1[],
+               const cxtype_sv F2[],
                const cxtype COUP,
                const fptype M3,
                const fptype W3,
-               cxtype_sv V3[] ) ALWAYS_INLINE;      // output: wavefunction3[6]
+               cxtype_sv V3[] ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
   __device__ INLINE
-  void FFV2_4_0( const cxtype_sv F1[],              // input: wavefunction1[6]
-                 const cxtype_sv F2[],              // input: wavefunction2[6]
-                 const cxtype_sv V3[],              // input: wavefunction3[6]
+  void FFV4_0( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype_sv V3[],
+               const cxtype COUP,
+               cxtype_sv* vertex ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
+  __device__ INLINE
+  void FFV4_3( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype COUP,
+               const fptype M3,
+               const fptype W3,
+               cxtype_sv V3[] ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
+  __device__ INLINE
+  void FFV2_4_0( const cxtype_sv F1[],
+                 const cxtype_sv F2[],
+                 const cxtype_sv V3[],
                  const cxtype COUP1,
                  const cxtype COUP2,
-                 cxtype_sv* vertex ) ALWAYS_INLINE; // output: amplitude
+                 cxtype_sv* vertex ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
   __device__ INLINE
-  void FFV2_4_3( const cxtype_sv F1[],              // input: wavefunction1[6]
-                 const cxtype_sv F2[],              // input: wavefunction2[6]
+  void FFV2_4_3( const cxtype_sv F1[],
+                 const cxtype_sv F2[],
                  const cxtype COUP1,
                  const cxtype COUP2,
                  const fptype M3,
                  const fptype W3,
-                 cxtype_sv V3[] ) ALWAYS_INLINE;    // output: wavefunction3[6]
+                 cxtype_sv V3[] ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
