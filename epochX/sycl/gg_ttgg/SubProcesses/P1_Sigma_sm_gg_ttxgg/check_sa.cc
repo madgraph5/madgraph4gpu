fe0bf88f27aec8fecb8482c29a6a7176e4dccc1e
Nathan Nichols <nnichols@anl.gov>
2021-11-08 16:14:02 +0000

    Fixes #294

--- epochX/sycl/gg_ttgg/SubProcesses/P1_Sigma_sm_gg_ttxgg/check_sa.cc
+++ epochX/sycl/gg_ttgg/SubProcesses/P1_Sigma_sm_gg_ttxgg/check_sa.cc
@@ -501,7 +501,11 @@ int main(int argc, char **argv)
     std::vector<double> commonRnd = commonRandomPromises[iiter].get_future().get();
     assert( nRnarray == static_cast<int>( commonRnd.size() ) );
     // NB (PR #45): memcpy is strictly needed only in CUDA (copy to pinned memory), but keep it also in C++ for consistency
+#ifdef SYCL_LANGUAGE_VERSION
+    q_ct1.memcpy(devRnarray, commonRnd.data(), nbytesRnarray).wait();
+#else
     memcpy( hstRnarray.get(), commonRnd.data(), nRnarray * sizeof(hstRnarray[0]) );
+#endif
 #elif defined __CUDACC__
 #ifdef MGONGPU_CURAND_ONDEVICE
     grambo2toNm0::generateRnarray( rnGen, devRnarray.get(), nevt );
