00c7d1854aecee7accba706e58db9a5e4a52fbe5
Andrea Valassi <andrea.valassi@cern.ch>
2021-11-15 14:54:46 +0100

    [gridpackX] use a simpler patch for driver.f in revno 370 (actually derived on revno 365, same patch)

--- epochX/gridpack/CODEGEN/MG5aMC_patches/patch.driver.f
+++ epochX/gridpack/CODEGEN/MG5aMC_patches/patch.driver.f
@@ -1,23 +1,17 @@
 --- ee_mumu.auto/madevent/SubProcesses/P1_ll_ll/driver.f
 +++ ee_mumu.auto/madevent/SubProcesses/P1_ll_ll/driver.f.new
-@@ -73,6 +73,10 @@
- 
-       include 'coupl.inc'
- 
-+c
-+c     initialise C and C++ modules
-+c
-+      call counters_initialise()
+@@ -75,6 +75,7 @@
  C-----
  C  BEGIN CODE
  C----- 
-@@ -201,6 +205,10 @@
++      call counters_initialise()
+       call cpu_time(t_before)
+       CUMULATED_TIMING = t_before
+ c
+@@ -194,6 +195,7 @@
        rewind(lun)
  
        close(lun)
-+c
-+c     finalise C and C++ modules
-+c
 +      call counters_finalise()
        end
  
