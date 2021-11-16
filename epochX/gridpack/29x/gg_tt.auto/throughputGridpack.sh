fe63242e73964cbd49d2d91f86ff3a67cffaace6
Andrea Valassi <andrea.valassi@cern.ch>
2021-11-16 18:16:32 +0100

    [gridpackX] add throughput as "MATRIX1 time" divided by "SMATRIX1 calls", add PROGRAM (3 procs, 29x)

--- epochX/gridpack/29x/gg_tt.auto/throughputGridpack.sh
+++ epochX/gridpack/29x/gg_tt.auto/throughputGridpack.sh
@@ -50,9 +50,25 @@ echo
 
 for Gdir in madevent/SubProcesses/P1_*/G*; do
   log=$Gdir/counters_log.txt
-  if [ -f $log ]; then printf "%-5s " $(basename $Gdir) | tee -a $out; cat $log | grep '^MATRIX1' | tee -a $out; fi
+  if [ -f $log ]; then printf "%-5s " $(basename $Gdir) | tee -a $out; cat $log | grep '^PROGRAM' | tee -a $out; fi
 done
-cat madevent/SubProcesses/P1_*/G*/counters_log.txt | grep ^MATRIX1 | awk 'BEGIN{stot=0;ctot=0};{stot+=substr($3,0,length($3)-1);ctot+=$5};END{printf "TOTAL MATRIX1  : %9.4fs for %8d calls => throughput is %8.2E calls/s\n",stot,ctot,ctot/stot}' | tee -a $out
+cat madevent/SubProcesses/P1_*/G*/counters_log.txt | grep '^PROGRAM' | awk 'BEGIN{stot=0;ctot=0};{stot+=substr($3,0,length($3)-1)};END{printf "TOTAL PROGRAM    : %9.4fs\n",stot}' | tee -a $out
+
+echo | tee -a $out
+
+for Gdir in madevent/SubProcesses/P1_*/G*; do
+  log=$Gdir/counters_log.txt
+  if [ -f $log ]; then printf "%-5s " $(basename $Gdir) | tee -a $out; cat $log | grep '^MATRIX1(a)' | tee -a $out; fi
+done
+cat madevent/SubProcesses/P1_*/G*/counters_log.txt | grep '^MATRIX1(a)' | awk 'BEGIN{stot=0;ctot=0};{stot+=substr($3,0,length($3)-1);ctot+=$5};END{printf "TOTAL MATRIX1(a) : %9.4fs for %8d MATRIX1 calls  => throughput is %8.2E calls/s\n",stot,ctot,ctot/stot}' | tee -a $out
+
+echo | tee -a $out
+
+for Gdir in madevent/SubProcesses/P1_*/G*; do
+  log=$Gdir/counters_log.txt
+  if [ -f $log ]; then printf "%-5s " $(basename $Gdir) | tee -a $out; cat $log | grep '^MATRIX1(b)' | tee -a $out; fi
+done
+cat madevent/SubProcesses/P1_*/G*/counters_log.txt | grep '^MATRIX1(b)' | awk 'BEGIN{stot=0;ctot=0};{stot+=substr($3,0,length($3)-1);ctot+=$5};END{printf "TOTAL MATRIX1(b) : %9.4fs for %8d SMATRIX1 calls => throughput is %8.2E calls/s\n",stot,ctot,ctot/stot}' | tee -a $out
 
 echo | tee -a $out
 
@@ -60,7 +76,7 @@ for Gdir in madevent/SubProcesses/P1_*/G*; do
   log=$Gdir/counters_log.txt
   if [ -f $log ]; then printf "%-5s " $(basename $Gdir) | tee -a $out; cat $log | grep '^SMATRIX1' | tee -a $out; fi
 done
-cat madevent/SubProcesses/P1_*/G*/counters_log.txt | grep ^SMATRIX1 | awk 'BEGIN{stot=0;ctot=0};{stot+=substr($3,0,length($3)-1);ctot+=$5};END{printf "TOTAL SMATRIX1 : %9.4fs for %8d calls => throughput is %8.2E calls/s\n",stot,ctot,ctot/stot}' | tee -a $out
+cat madevent/SubProcesses/P1_*/G*/counters_log.txt | grep ^SMATRIX1 | awk 'BEGIN{stot=0;ctot=0};{stot+=substr($3,0,length($3)-1);ctot+=$5};END{printf "TOTAL SMATRIX1   : %9.4fs for %8d SMATRIX1 calls => throughput is %8.2E calls/s\n",stot,ctot,ctot/stot}' | tee -a $out
 
 echo
 
