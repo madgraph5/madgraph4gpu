cb80c4f5e8a8e8d4b8a30176ad6e45dd88086fbc
Stefan Roiser <stefan.roiser@cern.ch>
2021-11-16 13:35:14 +0100

    add me output, some printouts

--- epoch2/fortran-cuda/pp_ttx/SubProcesses/fbridge.cu
+++ epoch2/fortran-cuda/pp_ttx/SubProcesses/fbridge.cu
@@ -4,6 +4,7 @@
 // m == DOUBLE PRECISION P_MULTI(0:3, NEXTERNAL, NB_PAGE)
 // NEXTERNAL = 4 (nexternal.inc)
 // NB_PAGE = 16 (vector.inc)
+// ncomb = 16 (mgOnGpuConfig.h)
 
 struct CudaInit {
   CudaInit();
@@ -13,13 +14,22 @@ CudaInit::CudaInit() { std::cout << "cuda init" << std::endl; }
 
 static CudaInit cuInit;
 
-void bridge(double *m) {
+void bridge(double *momenta, double **mes) {
 
-  Matrix<double> t = Matrix<double>(16, 4, 4, 2);
-  t.hst_transpose(m);
+  // par 1: number of events (NB_PAGE)
+  // par 2: number of particles / event (NEXTERNAL)
+  // par 3: number of momenta / particle
+  // par 4: stride length
+  // par 5: number of good helicities (ncomb)
+  Matrix<double> t = Matrix<double>(16, 4, 4, 2, 16);
+  t.hst_transpose(momenta, mes);
 }
 
 extern "C" {
-
-void bridge_(double *m) { bridge(m); }
+void bridge_(double *momenta, double *mes) {
+  bridge(momenta, &mes);
+  // for (int i = 0; i < 16; ++i) {
+  //   printf("%f x ", mes[i]);
+  // }
+}
 }
