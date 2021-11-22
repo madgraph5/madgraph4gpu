c12c6f8c1e0992f5349a3337e0a69f19e69db59f
Stefan Roiser <stefan.roiser@cern.ch>
2021-11-22 15:01:19 +0100

    fix pointer to output, change stride length

--- epoch2/fortran-cuda/pp_ttx/SubProcesses/fbridge.cu
+++ epoch2/fortran-cuda/pp_ttx/SubProcesses/fbridge.cu
@@ -14,20 +14,20 @@ CudaInit::CudaInit() { std::cout << "cuda init" << std::endl; }
 
 static CudaInit cuInit;
 
-void bridge(double *momenta, double **mes) {
+void bridge(double *momenta, double *mes) {
 
   // par 1: number of events (NB_PAGE)
   // par 2: number of particles / event (NEXTERNAL)
   // par 3: number of momenta / particle
   // par 4: stride length
   // par 5: number of good helicities (ncomb)
-  Matrix<double> t = Matrix<double>(16, 4, 4, 16, 16);
+  Matrix<double> t = Matrix<double>(16, 4, 4, 4, 16);
   t.hst_transpose(momenta, mes);
 }
 
 extern "C" {
 void bridge_(double *momenta, double *mes) {
-  bridge(momenta, &mes);
+  bridge(momenta, mes);
   // for (int i = 0; i < 16; ++i) {
   //   printf("%f x ", mes[i]);
   // }
