6d62bfd7f608971ac0b286509f43dc042b2596f1
Stefan Roiser <stefan.roiser@cern.ch>
2021-12-01 17:35:50 +0100

    add cpp bridge

--- epoch2/fortran-cuda/pp_ttx/SubProcesses/fbridge.cu
+++ epoch2/fortran-cuda/pp_ttx/SubProcesses/fbridge.cu
@@ -1,18 +1,41 @@
 #include "bridge.h"
 
+#ifdef __CUDACC__
+
 /**
  * The C++ function instantiating the Bridge class with all necessary parameters
  * and calling the host_sequence function to execute the Cuda/C++ code for the
  * matrix element calculation
  */
-void fbridge(double *momenta, double *mes) {
+void cubridge(double *momenta, double *mes) {
   Bridge<double> b = Bridge<double>(16, 4, 4, 4, 16);
-  b.host_sequence(momenta, mes);
+  b.gpu_sequence(momenta, mes);
 }
 
 extern "C" {
 /**
  * The C symbol being called from the fortran code (in audo_dsig1.f)
  */
-void fbridge_(double *momenta, double *mes) { fbridge(momenta, mes); }
+void fcubridge_(double *mom, double *mes) { cubridge(mom, mes); }
 }
+
+#else
+
+/**
+ * The C++ function instantiating the Bridge class with all necessary parameters
+ * and calling the host_sequence function to execute the Cuda/C++ code for the
+ * matrix element calculation
+ */
+void cppbridge(double *momenta, double *mes) {
+  Bridge<double> b = Bridge<double>(16, 4, 4, 4, 16);
+  b.cpu_sequence(momenta, mes);
+}
+
+extern "C" {
+/**
+ * The C symbol being called from the fortran code (in audo_dsig1.f)
+ */
+void fcppbridge_(double *mom, double *mes) { cppbridge(mom, mes); }
+}
+
+#endif // __CUDACC__
