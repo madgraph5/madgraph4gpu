d4cdf272124af04d3ccd3ddd39fefae82bcd2e96
Andrea Valassi <andrea.valassi@cern.ch>
2021-12-01 15:26:35 +0100

    [klas5] test with neppM=1 while neppV>1: around 10% slower on 512y, but functionally correct

--- epoch1/cuda/ee_mumu/src/mgOnGpuConfig.h
+++ epoch1/cuda/ee_mumu/src/mgOnGpuConfig.h
@@ -157,13 +157,13 @@ namespace mgOnGpu
   // --- In practice, neppR, neppM and neppV can now all be different
   // -----------------------------------------------------------------------------------------------
 #ifdef MGONGPU_CPPSIMD
-  const int neppM = MGONGPU_CPPSIMD; // (DEFAULT) neppM=neppV for optimal performance
+  //const int neppM = MGONGPU_CPPSIMD; // (DEFAULT) neppM=neppV for optimal performance
+  //const int neppM = 64/sizeof(fptype); // maximum CPU vector width (512 bits): 8 (DOUBLE) or 16 (FLOAT)
+  //const int neppM = 32/sizeof(fptype); // lower CPU vector width (256 bits): 4 (DOUBLE) or 8 (FLOAT)
+  const int neppM = 1; // *** NB: this is equivalent to AOS ***
 #else
   const int neppM = 1; // (DEFAULT) neppM=neppV for optimal performance (NB: this is equivalent to AOS)
 #endif
-  //const int neppM = 64/sizeof(fptype); // maximum CPU vector width (512 bits): 8 (DOUBLE) or 16 (FLOAT)
-  //const int neppM = 32/sizeof(fptype); // lower CPU vector width (256 bits): 4 (DOUBLE) or 8 (FLOAT)
-  //const int neppM = 1; // *** NB: this is equivalent to AOS ***
 #endif
 
   // Number of Events Per Page in the random number AOSOA memory layout
