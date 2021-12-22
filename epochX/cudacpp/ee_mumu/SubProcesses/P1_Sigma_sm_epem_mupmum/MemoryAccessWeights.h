80a547c27ddfd667a24d1d4f1c897a793bfb01df
Andrea Valassi <andrea.valassi@cern.ch>
2021-12-22 11:45:25 +0100

    [apirambo] split HostAccessWeights and DeviceAccessWeights classes with bool template - all ok, same perf

--- epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/MemoryAccessWeights.h
+++ epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/MemoryAccessWeights.h
@@ -4,6 +4,8 @@
 #include "mgOnGpuConfig.h"
 #include "mgOnGpuTypes.h"
 
+//----------------------------------------------------------------------------
+
 // A class describing the internal layout of memory buffers for weights
 // This implementation uses a plain ARRAY[nevt]
 // [If many implementations are used, a suffix _ARRAYv1 should be appended to the class name]
@@ -36,6 +38,16 @@ public:
     return ieventAccess( const_cast<fptype*>( buffer ), ievt );
   }
 
+};
+
+//----------------------------------------------------------------------------
+
+// A class describing kernel access to memory buffers for weights on a CPU host or on a GPU device
+template<bool onDevice>
+class KernelAccessWeights: public MemoryAccessWeights
+{
+public:
+
   // =========================================================================
   // *** Pattern: ieventAccessInd1..IndN( buffer, ievt [, ind1... indN] )  ***
   // =========================================================================
@@ -48,13 +60,21 @@ public:
   __device__ inline
   fptype& kernelAccess( fptype* buffer )
   {
+    //if constexpr ( !onDevice ) // FIXME! enable this when we move to nvcc supporting c++17
+    if ( !onDevice )
+    {
+      return ieventAccess( buffer, 0 );
+    }
+    else
+    {
 #ifdef __CUDACC__
-    const int ievt = blockDim.x * blockIdx.x + threadIdx.x; // index of event (thread) in grid
-    //printf( "kernelAccess: ievt=%d threadId=%d\n", ievt, threadIdx.x );
-    return ieventAccess( buffer, ievt ); // NB fptype and fptype_sv coincide for CUDA
+      const int ievt = blockDim.x * blockIdx.x + threadIdx.x; // index of event (thread) in grid
+      //printf( "kernelAccessIp4Ipar: ievt=%d threadId=%d\n", ievt, threadIdx.x );
+      return ieventAccess( buffer, ievt ); // NB fptype and fptype_sv coincide for CUDA
 #else
-    return ieventAccess( buffer, 0 );
+      throw std::runtime_error( "KernelAccessWeights on device is only implemented in CUDA" );
 #endif
+    }
   }
 
   // (Const memory access)
@@ -65,8 +85,13 @@ public:
     return kernelAccess( const_cast<fptype*>( buffer ) );
   }
 
-  //--------------------------------------------------------------------------
-
 };
+
+//----------------------------------------------------------------------------
+
+typedef KernelAccessWeights<false> HostAccessWeights;
+typedef KernelAccessWeights<true> DeviceAccessWeights;
+
+//----------------------------------------------------------------------------
 
 #endif // MemoryAccessWeights_H
