47c036fcdf6055ae455f119ea2dc551630ac97e4
Andrea Valassi <andrea.valassi@cern.ch>
2021-12-22 09:49:32 +0100

    [apirambo] remove non-const access for random numbers (only const access from sampling kernels is needed)

--- epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/MemoryAccessRandomNumbers.h
+++ epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/MemoryAccessRandomNumbers.h
@@ -20,23 +20,6 @@ namespace MemoryAccessRandomNumbers//_AOSOAv1
   // Indexed access (WITH an explicit event number) to the memory buffer for momenta
   // Input: a memory buffer for an arbitrary number of events
   // Output: a specific 4-momenta component for a specific particle in one event, given its event number
-  // (Non-const memory access)
-  //static
-  __device__ inline
-  fptype& ieventAccessIp4Iparf( fptype* buffer,
-                                const int ievt,
-                                const int ip4,
-                                const int iparf )
-  {
-    using mgOnGpu::np4;
-    using mgOnGpu::nparf;
-    constexpr int neppR = mgOnGpu::neppR; // AOSOA layout: constant at compile-time
-    const int ipagR = ievt/neppR; // #event "R-page"
-    const int ieppR = ievt%neppR; // #event in the current event R-page
-    //printf( "%2d %2d %8d %8.3f\n", iparf, 0, ievt, buffer[ipagR*nparf*np4*neppR + iparf*np4*neppR + ip4*neppR + ieppR] );
-    return buffer[ipagR*nparf*np4*neppR + iparf*np4*neppR + ip4*neppR + ieppR]; // AOSOA[ipagR][iparf][ip4][ieppR]
-  }
-
   // (Const memory access)
   //static
   __device__ inline
@@ -45,7 +28,13 @@ namespace MemoryAccessRandomNumbers//_AOSOAv1
                                            const int ip4,
                                            const int iparf )
   {
-    return ieventAccessIp4Iparf( const_cast<fptype*>( buffer ), ievt, ip4, iparf );
+    using mgOnGpu::np4;
+    using mgOnGpu::nparf;
+    constexpr int neppR = mgOnGpu::neppR; // AOSOA layout: constant at compile-time
+    const int ipagR = ievt/neppR; // #event "R-page"
+    const int ieppR = ievt%neppR; // #event in the current event R-page
+    //printf( "%2d %2d %8d %8.3f\n", iparf, 0, ievt, buffer[ipagR*nparf*np4*neppR + iparf*np4*neppR + ip4*neppR + ieppR] );
+    return buffer[ipagR*nparf*np4*neppR + iparf*np4*neppR + ip4*neppR + ieppR]; // AOSOA[ipagR][iparf][ip4][ieppR]
   }
 
   // =========================================================================
@@ -58,27 +47,17 @@ namespace MemoryAccessRandomNumbers//_AOSOAv1
   // (Non-const memory access)
   //static
   __device__ inline
-  fptype& kernelAccessIp4Iparf( fptype* buffer,
-                                const int ip4,
-                                const int iparf )
-  {
-#ifdef __CUDACC__
-    const int ievt = blockDim.x * blockIdx.x + threadIdx.x; // index of event (thread) in grid
-    //printf( "kernelAccessIp4Iparf: ievt=%d threadId=%d\n", ievt, threadIdx.x );
-    return ieventAccessIp4Iparf( buffer, ievt, ip4, iparf ); // NB fptype and fptype_sv coincide for CUDA
-#else
-    return ieventAccessIp4Iparf( buffer, 0, ip4, iparf );
-#endif
-  }
-
-  // (Const memory access)
-  //static
-  __device__ inline
   const fptype& kernelConstAccessIp4Iparf( const fptype* buffer,
                                            const int ip4,
                                            const int iparf )
   {
-    return kernelAccessIp4Iparf( const_cast<fptype*>( buffer ), ip4, iparf );
+#ifdef __CUDACC__
+    const int ievt = blockDim.x * blockIdx.x + threadIdx.x; // index of event (thread) in grid
+    //printf( "kernelCoonstAccessIp4Iparf: ievt=%d threadId=%d\n", ievt, threadIdx.x );
+    return ieventConstAccessIp4Iparf( buffer, ievt, ip4, iparf ); // NB fptype and fptype_sv coincide for CUDA
+#else
+    return ieventConstAccessIp4Iparf( buffer, 0, ip4, iparf );
+#endif
   }
 
   //--------------------------------------------------------------------------
