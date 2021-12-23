52ae9fb3652bb3d899c1efa733d854ca09a274e6
Andrea Valassi <andrea.valassi@cern.ch>
2021-12-23 15:44:33 +0100

    [apirambo] This still fails to link...

--- epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/MemoryAccessMomenta.h
+++ epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/MemoryAccessMomenta.h
@@ -78,7 +78,9 @@ public:
 
 //template fptype& DeviceAccessMomentaBase::kernelAccessFIELD<int, int>( fptype*, int, int );
 
+#ifdef __CUDACC__
 template fptype& KernelAccessBase<MemoryAccessMomentaBase, true>::kernelAccessFIELD<int, int>( fptype*, int, int );
+#endif
 template fptype& KernelAccessBase<MemoryAccessMomentaBase, false>::kernelAccessFIELD<int, int>( fptype*, int, int );
 
 //template fptype& MemoryAccessBase<MemoryAccessMomentaBase>::ieventAccessField<int, int>;
