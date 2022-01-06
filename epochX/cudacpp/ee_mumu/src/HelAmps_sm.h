f37bce3dd66858d83e37294b4d29571ba6d8ce70
Andrea Valassi <andrea.valassi@cern.ch>
2022-01-06 09:58:11 +0100

    [apihel] COMPLETE BACKPORT of HelAmps.h/cc - eemumu manual and auto are identical
    (NB moved XXX function implementation earlier on in HelAmps.h manual too)

--- epochX/cudacpp/ee_mumu/src/HelAmps_sm.h
+++ epochX/cudacpp/ee_mumu/src/HelAmps_sm.h
@@ -190,92 +190,6 @@ namespace mg5amcCpu
                const int ipar                  // input: particle# out of npar
                ) ALWAYS_INLINE;
 
-  //--------------------------------------------------------------------------
-
-  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
-  __device__ INLINE
-  void FFV1_0( const cxtype_sv F1[],
-               const cxtype_sv F2[],
-               const cxtype_sv V3[],
-               const cxtype COUP,
-               cxtype_sv* vertex ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
-  __device__ INLINE
-  void FFV1P0_3( const cxtype_sv F1[],
-                 const cxtype_sv F2[],
-                 const cxtype COUP,
-                 const fptype M3,
-                 const fptype W3,
-                 cxtype_sv V3[] ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
-  __device__ INLINE
-  void FFV2_0( const cxtype_sv F1[],
-               const cxtype_sv F2[],
-               const cxtype_sv V3[],
-               const cxtype COUP,
-               cxtype_sv* vertex ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
-  __device__ INLINE
-  void FFV2_3( const cxtype_sv F1[],
-               const cxtype_sv F2[],
-               const cxtype COUP,
-               const fptype M3,
-               const fptype W3,
-               cxtype_sv V3[] ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
-  __device__ INLINE
-  void FFV4_0( const cxtype_sv F1[],
-               const cxtype_sv F2[],
-               const cxtype_sv V3[],
-               const cxtype COUP,
-               cxtype_sv* vertex ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
-  __device__ INLINE
-  void FFV4_3( const cxtype_sv F1[],
-               const cxtype_sv F2[],
-               const cxtype COUP,
-               const fptype M3,
-               const fptype W3,
-               cxtype_sv V3[] ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
-  __device__ INLINE
-  void FFV2_4_0( const cxtype_sv F1[],
-                 const cxtype_sv F2[],
-                 const cxtype_sv V3[],
-                 const cxtype COUP1,
-                 const cxtype COUP2,
-                 cxtype_sv* vertex ) ALWAYS_INLINE;
-
-  //--------------------------------------------------------------------------
-
-  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
-  __device__ INLINE
-  void FFV2_4_3( const cxtype_sv F1[],
-                 const cxtype_sv F2[],
-                 const cxtype COUP1,
-                 const cxtype COUP2,
-                 const fptype M3,
-                 const fptype W3,
-                 cxtype_sv V3[] ) ALWAYS_INLINE;
-
   //==========================================================================
 
   // Compute the output wavefunction fi[6] from the input momenta[npar*4*nevt]
@@ -883,6 +797,92 @@ namespace mg5amcCpu
     mgDebug( 1, __FUNCTION__ );
     return;
   }
+
+  //==========================================================================
+
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
+  __device__ INLINE
+  void FFV1_0( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype_sv V3[],
+               const cxtype COUP,
+               cxtype_sv* vertex ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
+  __device__ INLINE
+  void FFV1P0_3( const cxtype_sv F1[],
+                 const cxtype_sv F2[],
+                 const cxtype COUP,
+                 const fptype M3,
+                 const fptype W3,
+                 cxtype_sv V3[] ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
+  __device__ INLINE
+  void FFV2_0( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype_sv V3[],
+               const cxtype COUP,
+               cxtype_sv* vertex ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
+  __device__ INLINE
+  void FFV2_3( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype COUP,
+               const fptype M3,
+               const fptype W3,
+               cxtype_sv V3[] ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
+  __device__ INLINE
+  void FFV4_0( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype_sv V3[],
+               const cxtype COUP,
+               cxtype_sv* vertex ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
+  __device__ INLINE
+  void FFV4_3( const cxtype_sv F1[],
+               const cxtype_sv F2[],
+               const cxtype COUP,
+               const fptype M3,
+               const fptype W3,
+               cxtype_sv V3[] ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output amplitude 'vertex' from the input wavefunctions F1[6], F2[6], V3[6]
+  __device__ INLINE
+  void FFV2_4_0( const cxtype_sv F1[],
+                 const cxtype_sv F2[],
+                 const cxtype_sv V3[],
+                 const cxtype COUP1,
+                 const cxtype COUP2,
+                 cxtype_sv* vertex ) ALWAYS_INLINE;
+
+  //--------------------------------------------------------------------------
+
+  // Compute the output wavefunction 'V3[6]' from the input wavefunctions F1[6], F2[6]
+  __device__ INLINE
+  void FFV2_4_3( const cxtype_sv F1[],
+                 const cxtype_sv F2[],
+                 const cxtype COUP1,
+                 const cxtype COUP2,
+                 const fptype M3,
+                 const fptype W3,
+                 cxtype_sv V3[] ) ALWAYS_INLINE;
 
   //--------------------------------------------------------------------------
 
