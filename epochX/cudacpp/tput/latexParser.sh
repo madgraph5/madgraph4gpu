456eb3db73f1999418f2a326f9ac8a2e1b5c946f
Andrea Valassi <andrea.valassi@cern.ch>
2022-01-24 18:00:16 +0100

    [tex] handle multiple revisions
    Revision HEAD [nvcc 11.6.55 (gcc 10.2.0)]
                eemumu      ggtt        ggttgg
    CUD/none    1.34e+09    1.41e+08    5.13e+05
    CPP/none    1.67e+06    2.01e+05    1.81e+03
    CPP/sse4    3.12e+06    3.17e+05    3.37e+03
    CPP/avx2    5.57e+06    5.71e+05    6.83e+03
    CPP/512y    5.81e+06    6.12e+05    7.53e+03
    CPP/512z    4.56e+06    3.75e+05    6.54e+03
    
    Revision 6350a75 [nvcc 11.6.55 (icx 20210400 (clang 13.0.0, gcc 10.2.0)]
                eemumu      ggtt        ggttgg
    CUD/none    1.34e+09    1.42e+08    5.13e+05
    CPP/none    7.28e+06    2.14e+05    1.50e+03
    CPP/sse4    7.81e+06    3.81e+05    2.83e+03
    CPP/avx2    1.18e+07    6.84e+05    7.57e+03
    CPP/512y    1.18e+07    7.51e+05    8.48e+03
    CPP/512z    9.16e+06    5.10e+05    5.96e+03

--- epochX/cudacpp/tput/latexParser.sh
+++ epochX/cudacpp/tput/latexParser.sh
@@ -2,19 +2,24 @@
 
 cd $(dirname $0)/..
 
-procs="eemumu ggtt ggttgg"
-files=""
-for proc in $procs; do
-  file=tput/logs_${proc}_manu/log_${proc}_manu_d_inl0_hrd0.txt
-  files="$files $file"
-done
+revs=""
+revs="$revs HEAD" # cuda116/gcc102 (BASELINE)
+revs="$revs 6350a75" # cuda116/icx2021
 
-###cat $files | awk '/^Process/{print $0}; /Workflow/{print $0}; /MECalcOnly/{print $0}'
-cat $files | awk \
-    '/^Process(.)*nvcc/{split($0,a,"["); comp="["a[2]; if ( comp != complast ){print comp; complast=comp}};\
+for rev in $revs; do
+  procs="eemumu ggtt ggttgg"
+  files=""
+  for proc in $procs; do
+    file=tput/logs_${proc}_manu/log_${proc}_manu_d_inl0_hrd0.txt
+    files="$files $file"
+  done
+  git checkout $rev $files >& /dev/null
+  ###cat $files | awk '/^Process/{print $0}; /Workflow/{print $0}; /MECalcOnly/{print $0}'; exit 0
+  cat $files | awk -vrev=$rev\
+    '/^Process(.)*nvcc/{split($0,a,"["); comp="["a[2]; if ( comp != complast ){print "Revision", rev, comp; complast=comp}};\
      /^Process/{proc=""; split($3,a,"_"); proc=a[3]"_"a[4]};\
      /Workflow/{tag=""; split($4,a,":"); tag=a[1]; split($4,a,"+"); split(a[4],b,"/"); tag=tag"/"b[2]};\
-     /MECalcOnly/{tput=sprintf("%.3g", $5); tput_proc_tag[proc,tag]=tput}; \
+     /MECalcOnly/{tput=sprintf("%.2e", $5); tput_proc_tag[proc,tag]=tput}; \
      END{ntag=split("CUD/none CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z",tags);\
          nproc=split("EPEM_MUPMUM GG_TTX GG_TTXGG",procs);\
          procs_txt["EPEM_MUPMUM"]="eemumu"; procs_txt["GG_TTX"]="ggtt"; procs_txt["GG_TTXGG"]="ggttgg";\
@@ -23,3 +28,6 @@ cat $files | awk \
          for(itag=1;itag<=ntag;itag++)\
          {tag=tags[itag]; printf "%-12s", tag;\
           for(iproc=1;iproc<=nproc;iproc++){proc=procs[iproc]; printf "%-12s", tput_proc_tag[proc,tag]}; printf "\n"}}'
+  echo ""
+done
+git checkout HEAD > /dev/null
