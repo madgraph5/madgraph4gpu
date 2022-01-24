517d1fd1b024e50325049ba417dbf1c890b8371a
Andrea Valassi <andrea.valassi@cern.ch>
2022-01-24 08:16:48 +0100

    [pptt] add uu~>tt~ and check that this is what fails in pp to tt (#337)
    DEBUG:  Entering PLUGIN_OneProcessExporter.write_process_cc_file [model_handling.py at line 1005]
    Command "import /data/avalassi/GPU2020/MG5aMC/2.7.0_gpu/CODEGEN_cudacpp_uu_tt.mg" interrupted in sub-command:
    "output standalone_cudacpp CODEGEN_cudacpp_uu_tt" with error:
    AssertionError :

--- epochX/cudacpp/CODEGEN/generateAndCompare.sh
+++ epochX/cudacpp/CODEGEN/generateAndCompare.sh
@@ -20,6 +20,9 @@ function codeGenAndDiff()
     pp_tt)
       cmd="generate p p > t t~"
       ;;
+    uu_tt)
+      cmd="generate u u~ > t t~"
+      ;;
     *)
       echo -e "\nWARNING! Skipping unknown process '$proc'"
       return
