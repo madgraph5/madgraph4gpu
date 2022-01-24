298bd3d39fc062adcd561ad7b4a6b8ba9ef124b5
Andrea Valassi <andrea.valassi@cern.ch>
2022-01-24 13:05:03 +0100

    [tex] add process printout

--- epochX/cudacpp/tput/latexParser.sh
+++ epochX/cudacpp/tput/latexParser.sh
@@ -2,9 +2,16 @@
 
 cd $(dirname $0)/..
 
-file=tput/logs_eemumu_manu/log_eemumu_manu_d_inl0_hrd0.txt
-###cat $file | awk '/Workflow/{print $0}; /MECalcOnly/{print $0}'
-cat $file | awk \
-  '/Workflow/{tag=""; split($4,a,":"); tag=a[1]; split($4,a,"+"); split(a[4],b,"/"); tag=tag"/"b[2]};\
-   /MECalcOnly/{tput=sprintf("%.3g", $5); tputs[tag]=tput};\
-   END{n=split("CUD/none CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z",tags); for(i=1;i<=n;i++){print tags[i], tputs[tags[i]]}}'
+procs="eemumu ggtt ggttgg"
+
+for proc in $procs; do
+
+  file=tput/logs_${proc}_manu/log_${proc}_manu_d_inl0_hrd0.txt
+  ###cat $file | awk '/Workflow/{print $0}; /MECalcOnly/{print $0}'
+  cat $file | awk \
+    '/^Process/{proc=""; split($3,a,"_"); proc=a[3]"_"a[4]};\
+     /Workflow/{tag=""; split($4,a,":"); tag=a[1]; split($4,a,"+"); split(a[4],b,"/"); tag=tag"/"b[2]};\
+     /MECalcOnly/{tput=sprintf("%.3g", $5); tputs[tag]=tput};\
+     END{n=split("CUD/none CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z",tags); for(i=1;i<=n;i++){print proc, tags[i], tputs[tags[i]]}}'
+
+done
