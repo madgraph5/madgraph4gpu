1e7fd9218534928d21c9ecddacaf31d3bd1100a9
Andrea Valassi <andrea.valassi@cern.ch>
2022-01-25 15:36:35 +0100

    [tex] include also ggttg/ggttggg in the table
    Revision c2e67b4 [nvcc 11.6.55 (gcc 10.2.0)] [inlineHel=0]
                eemumu      ggtt        ggttg       ggttgg      ggttggg
    CUD/none    1.35e+09    1.41e+08    1.45e+07    5.20e+05    1.18e+04
    CPP/none    1.67e+06    2.01e+05    2.48e+04    1.81e+03    7.22e+01
    CPP/sse4    3.13e+06    3.17e+05    4.54e+04    3.34e+03    1.32e+02
    CPP/avx2    5.54e+06    5.64e+05    8.86e+04    6.83e+03    2.61e+02
    CPP/512y    5.82e+06    6.15e+05    9.83e+04    7.49e+03    2.88e+02
    CPP/512z    4.65e+06    3.75e+05    7.19e+04    6.52e+03    2.94e+02
    
    Revision 4f3229d [nvcc 11.6.55 (icx 20210400, clang 13.0.0, gcc 10.2.0)] [inlineHel=0]
                eemumu      ggtt        ggttg       ggttgg      ggttggg
    CUD/none    1.33e+09    1.42e+08    1.45e+07    5.14e+05    1.19e+04
    CPP/none    7.60e+06    2.15e+05    2.43e+04    1.50e+03    7.21e+01
    CPP/sse4    7.89e+06    4.45e+05    4.57e+04    2.82e+03    1.05e+02
    CPP/avx2    1.19e+07    6.93e+05    1.04e+05    7.61e+03    2.42e+02
    CPP/512y    1.19e+07    7.50e+05    1.09e+05    8.45e+03    2.74e+02
    CPP/512z    9.37e+06    5.09e+05    7.85e+04    5.85e+03    2.66e+02

--- epochX/cudacpp/tput/latexParser.sh
+++ epochX/cudacpp/tput/latexParser.sh
@@ -4,11 +4,11 @@ cd $(dirname $0)/..
 
 # Select revisions
 revs=""
-revs="$revs 6307b62" # cuda116/gcc102 (BASELINE 24 Jan 2022)
-revs="$revs 37537ce" # cuda116/icx2021 (24 Jan 2022)
+revs="$revs c2e67b4" # cuda116/gcc102 BASELINE (25 Jan 2022) eemumu/ggtt/ggttgg x f/d x inl0/inl1 + ggttg/ggttggg x f/d
+revs="$revs 4f3229d" # cuda116/icx2021 (25 Jan 2022)  eemumu/ggtt/ggttgg x f/d x inl0/inl1 + ggttg/ggttggg x f/d
 
 # Select processes
-procs="eemumu ggtt ggttgg"
+procs="eemumu ggtt ggttg ggttgg ggttggg"
 
 # Select fptype
 fpts="d"
@@ -20,19 +20,22 @@ inls="inl0"
 
 # Iterate through log files
 for rev in $revs; do
+  ###echo "*** REVISION $rev ***"
   files=""
   for proc in $procs; do
     for fpt in $fpts; do
-      for inl in $inlss; do
+      for inl in $inls; do
         file=tput/logs_${proc}_manu/log_${proc}_manu_${fpt}_${inl}_hrd0.txt
-        files="$files $file"
+        if [ -f $file ]; then files="$files $file"; fi
       done
     done
   done
+  ###echo "*** FILES $files ***"
+  if [ "$files" == "" ]; then continue; fi
   git checkout $rev $files >& /dev/null
   ###cat $files | awk '/^Process/{print $0}; /Workflow/{print $0}; /MECalcOnly/{print $0}'; exit 0
   cat $files | awk -vrev=$rev\
-    '/^Process(.)*nvcc/{split($0,a,"["); comp="["a[2]; if ( comp != complast ){print "Revision", rev, comp; complast=comp}};\
+    '/^Process(.)*nvcc/{split($0,a,"["); comp="["a[2]"["a[3]; if ( comp != complast ){print "Revision", rev, comp; complast=comp}};\
      /^Process/{proc=""; split($3,a,"_"); proc=a[3]"_"a[4]};\
      /Workflow/{tag=""; split($4,a,":"); tag=a[1]; split($4,a,"+"); split(a[4],b,"/"); tag=tag"/"b[2]};\
      /MECalcOnly/{tput=sprintf("%.2e", $5); tput_proc_tag[proc,tag]=tput}; \
