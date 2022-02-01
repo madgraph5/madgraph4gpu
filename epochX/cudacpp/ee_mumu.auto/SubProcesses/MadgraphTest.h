99fcd97761b26a609648685425ca16a5cff02afe
Andrea Valassi <andrea.valassi@cern.ch>
2022-02-01 15:14:50 +0100

    [bbtt] regenerate eemumu and ggtt* auto (and resync manu) with fix for rambo nparf==1 (#358)

--- epochX/cudacpp/ee_mumu.auto/SubProcesses/MadgraphTest.h
+++ epochX/cudacpp/ee_mumu.auto/SubProcesses/MadgraphTest.h
@@ -17,60 +17,57 @@
 
 namespace {
 
-struct ReferenceData {
-  std::vector< std::vector<std::array<fptype, mgOnGpu::np4>> > momenta;
-  std::vector<fptype> MEs;
-};
+  struct ReferenceData {
+    std::vector< std::vector<std::array<fptype, mgOnGpu::np4>> > momenta;
+    std::vector<fptype> MEs;
+  };
 
-/// Read batches of reference data from a file and store them in a map.
-std::map<unsigned int, ReferenceData> readReferenceData(const std::string& refFileName)
-{
-  std::ifstream referenceFile(refFileName.c_str());
-  EXPECT_TRUE(referenceFile.is_open()) << refFileName;
-  std::map<unsigned int, ReferenceData> referenceData;
-  unsigned int evtNo;
-  unsigned int batchNo;
-
-  for (std::string line; std::getline(referenceFile, line); )
+  /// Read batches of reference data from a file and store them in a map.
+  std::map<unsigned int, ReferenceData> readReferenceData(const std::string& refFileName)
   {
-    std::stringstream lineStr(line);
-    if (line.empty() || line[0] == '#')
+    std::ifstream referenceFile(refFileName.c_str());
+    EXPECT_TRUE(referenceFile.is_open()) << refFileName;
+    std::map<unsigned int, ReferenceData> referenceData;
+    unsigned int evtNo;
+    unsigned int batchNo;
+    for (std::string line; std::getline(referenceFile, line); )
     {
-      continue;
-    }
-    else if (line.find("Event") != std::string::npos)
-    {
-      std::string dummy;
-      lineStr >> dummy >> evtNo >> dummy >> batchNo;
-    }
-    else if (line.find("ME") != std::string::npos)
-    {
-      if (evtNo <= referenceData[batchNo].MEs.size())
-        referenceData[batchNo].MEs.resize(evtNo + 1);
-
-      std::string dummy;
-      lineStr >> dummy >> referenceData[batchNo].MEs[evtNo];
-    }
-    else
-    {
-      unsigned int particleIndex;
-      lineStr >> particleIndex;
-
-      if (evtNo <= referenceData[batchNo].momenta.size())
-        referenceData[batchNo].momenta.resize(evtNo + 1);
-      if (particleIndex <= referenceData[batchNo].momenta[evtNo].size())
-        referenceData[batchNo].momenta[evtNo].resize(particleIndex + 1);
-
-      auto& fourVec = referenceData[batchNo].momenta[evtNo][particleIndex];
-      for (unsigned int i=0; i < fourVec.size(); ++i) {
-        EXPECT_TRUE(lineStr.good());
-        lineStr >> fourVec[i];
+      std::stringstream lineStr(line);
+      if (line.empty() || line[0] == '#')
+      {
+        continue;
       }
-      EXPECT_TRUE(lineStr.eof());
+      else if (line.find("Event") != std::string::npos)
+      {
+        std::string dummy;
+        lineStr >> dummy >> evtNo >> dummy >> batchNo;
+      }
+      else if (line.find("ME") != std::string::npos)
+      {
+        if (evtNo <= referenceData[batchNo].MEs.size())
+          referenceData[batchNo].MEs.resize(evtNo + 1);
+
+        std::string dummy;
+        lineStr >> dummy >> referenceData[batchNo].MEs[evtNo];
+      }
+      else
+      {
+        unsigned int particleIndex;
+        lineStr >> particleIndex;
+        if (evtNo <= referenceData[batchNo].momenta.size())
+          referenceData[batchNo].momenta.resize(evtNo + 1);
+        if (particleIndex <= referenceData[batchNo].momenta[evtNo].size())
+          referenceData[batchNo].momenta[evtNo].resize(particleIndex + 1);
+        auto& fourVec = referenceData[batchNo].momenta[evtNo][particleIndex];
+        for (unsigned int i=0; i < fourVec.size(); ++i) {
+          EXPECT_TRUE(lineStr.good());
+          lineStr >> fourVec[i];
+        }
+        EXPECT_TRUE(lineStr.eof());
+      }
     }
+    return referenceData;
   }
-  return referenceData;
-}
 
 }
 
@@ -151,10 +148,10 @@ public:
       if (ievt < referenceData.momenta.size()) {
         stream << "ref" << ipar;
         stream << std::setw(width) << referenceData.momenta[ievt][ipar][0]
-            << std::setw(width) << referenceData.momenta[ievt][ipar][1]
-            << std::setw(width) << referenceData.momenta[ievt][ipar][2]
-            << std::setw(width) << referenceData.momenta[ievt][ipar][3]
-            << "\n\n";
+               << std::setw(width) << referenceData.momenta[ievt][ipar][1]
+               << std::setw(width) << referenceData.momenta[ievt][ipar][2]
+               << std::setw(width) << referenceData.momenta[ievt][ipar][3]
+               << "\n\n";
       }
       stream << std::flush << std::defaultfloat; // default format: affects all floats
     }
@@ -196,41 +193,34 @@ TEST_P(MadgraphTest, CompareMomentaAndME)
   constexpr fptype energy = 1500; // historical default, Ecms = 1500 GeV = 1.5 TeV (above the Z peak)
   const fptype toleranceMomenta = std::is_same<double, fptype>::value ? 1.E-10 : 3.E-2;
   const fptype toleranceMEs     = std::is_same<double, fptype>::value ? 1.E-6  : 2.E-3;
-
   std::string dumpFileName = std::string("dump_")
-      + testing::UnitTest::GetInstance()->current_test_info()->test_suite_name()
-      + '.'
-      + testing::UnitTest::GetInstance()->current_test_info()->name()
-      + ".txt";
+    + testing::UnitTest::GetInstance()->current_test_info()->test_suite_name()
+    + '.'
+    + testing::UnitTest::GetInstance()->current_test_info()->name()
+    + ".txt";
   while (dumpFileName.find('/') != std::string::npos) {
     dumpFileName.replace(dumpFileName.find('/'), 1, "_");
   }
   const std::string refFileName = testDriver->getRefFileName();
-
   std::ofstream dumpFile;
   if ( dumpEvents )
   {
     dumpFile.open(dumpFileName, std::ios::trunc);
   }
-
   // Read reference data
   std::map<unsigned int, ReferenceData> referenceData;
   if ( ! dumpEvents ) {
     referenceData = readReferenceData(refFileName);
   }
   ASSERT_FALSE( HasFailure() ); // It doesn't make any sense to continue if we couldn't read the reference file.
-
   // **************************************
   // *** START MAIN LOOP ON #ITERATIONS ***
   // **************************************
   for (unsigned int iiter = 0; iiter < testDriver->niter; ++iiter)
   {
     testDriver->prepareRandomNumbers(iiter);
-
     testDriver->prepareMomenta(energy);
-
     testDriver->runSigmaKin(iiter);
-
     // --- Run checks on all events produced in this iteration
     for (std::size_t ievt = 0; ievt < testDriver->nevt && ! HasFailure(); ++ievt)
     {
@@ -241,10 +231,9 @@ TEST_P(MadgraphTest, CompareMomentaAndME)
         testDriver->dumpParticles(dumpFile, ievt, testDriver->nparticle, 15, ReferenceData());
         // Dump matrix element
         dumpFile << std::setw(4) << "ME" << std::scientific << std::setw(15+8)
-            << testDriver->getMatrixElement(ievt) << "\n" << std::endl << std::defaultfloat;
+                 << testDriver->getMatrixElement(ievt) << "\n" << std::endl << std::defaultfloat;
         continue;
       }
-
       // Check that we have the required reference data
       ASSERT_GT(referenceData.size(), iiter)
         << "Don't have enough reference data for iteration " << iiter << ". Ref file:" << refFileName;
@@ -254,7 +243,6 @@ TEST_P(MadgraphTest, CompareMomentaAndME)
         << "Don't have enough reference momenta for iteration " << iiter << " event " << ievt << ".\nRef file: " << refFileName;
       ASSERT_GE(referenceData[iiter].momenta[ievt].size(), testDriver->nparticle)
         << "Don't have enough reference particles for iteration " << iiter << " event " << ievt << ".\nRef file: " << refFileName;
-
       // This trace will help to understand the event that is being checked.
       // It will only be printed in case of failures:
       std::stringstream eventTrace;
@@ -265,7 +253,6 @@ TEST_P(MadgraphTest, CompareMomentaAndME)
                  << std::setw(4) << "r.ME" << std::scientific << std::setw(15+8)
                  << referenceData[iiter].MEs[ievt] << std::endl << std::defaultfloat;
       SCOPED_TRACE(eventTrace.str());
-
       // Compare Momenta
       for (unsigned int ipar = 0; ipar < testDriver->nparticle; ++ipar) {
         std::stringstream momentumErrors;
@@ -275,21 +262,19 @@ TEST_P(MadgraphTest, CompareMomentaAndME)
           const fptype relDelta = fabs( (pMadg - pOrig)/pOrig );
           if (relDelta > toleranceMomenta) {
             momentumErrors << std::setprecision(15) << std::scientific << "\nparticle " << ipar << "\tcomponent " << icomp
-                << "\n\t madGraph:  " << std::setw(22) << pMadg
-                << "\n\t reference: " << std::setw(22) << pOrig
-                << "\n\t rel delta: " << std::setw(22) << relDelta << " exceeds tolerance of " << toleranceMomenta;
+                           << "\n\t madGraph:  " << std::setw(22) << pMadg
+                           << "\n\t reference: " << std::setw(22) << pOrig
+                           << "\n\t rel delta: " << std::setw(22) << relDelta << " exceeds tolerance of " << toleranceMomenta;
           }
         }
         ASSERT_TRUE(momentumErrors.str().empty()) << momentumErrors.str();
       }
-
       // Compare ME:
       EXPECT_NEAR(testDriver->getMatrixElement(ievt),
-          referenceData[iiter].MEs[ievt],
-          toleranceMEs * referenceData[iiter].MEs[ievt]);
+                  referenceData[iiter].MEs[ievt],
+                  toleranceMEs * referenceData[iiter].MEs[ievt]);
     }
   }
-
   if ( dumpEvents ) {
     std::cout << "Event dump written to " << dumpFileName << std::endl;
   }
