fcac2fd84b0976ef0f7992512e93bc469d1d4f99
Andrea Valassi <andrea.valassi@cern.ch>
2022-02-03 15:28:29 +0100

    [shared] backport to codegen and regenerate eemumu auto

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/Bridge.h
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/Bridge.h
@@ -278,8 +278,8 @@ namespace mg5amcCpu
   }
 #endif
 
-  template <typename T>
-  void hst_transposeMomentaF2C( const T *in, T *out, const int evt )
+  template <typename T, bool F2C>
+  void hst_transposeMomenta( const T *in, T *out, const int evt )
   {
     constexpr bool oldImplementation = false; // default: use new implementation
     if constexpr ( oldImplementation )
@@ -302,7 +302,8 @@ namespace mg5amcCpu
           * (part * mome)          // event size (pos of event)
           + part_i * mome          // particle inside event
           + mome_i;                // momentum inside particle
-        out[pos] = in[inpos]; // F2C (Fortran to C)
+        if constexpr ( F2C ) out[pos] = in[inpos]; // F2C (Fortran to C)
+        else out[inpos] = in[pos]; // C2F (C to Fortran)
       }
     }
     else
@@ -330,93 +331,27 @@ namespace mg5amcCpu
                 int ievt = ipagM*neppM + ieppM;
                 int cpos = ipagM*npar*np4*neppM + ipar*np4*neppM + ip4*neppM + ieppM;
                 int fpos = ievt*npar*np4 + ipar*np4 + ip4;
-                out[cpos] = in[fpos]; // F2C (Fortran to C)
+                if constexpr ( F2C ) out[cpos] = in[fpos]; // F2C (Fortran to C)
+                else out[fpos] = in[cpos]; // C2F (C to Fortran)
               }
       }
     }
   }
 
   template <typename T>
+  void hst_transposeMomentaF2C( const T *in, T *out, const int evt )
+  {
+    constexpr bool F2C = true;
+    hst_transposeMomenta<T, F2C>( in, out, evt );
+  }
+
+  template <typename T>
   void hst_transposeMomentaC2F( const T *in, T *out, const int evt )
   {
-    constexpr int part = mgOnGpu::npar;
-    constexpr int mome = mgOnGpu::np4;
-    constexpr int strd = MemoryAccessMomenta::neppM;
-    int arrlen = evt * part * mome;
-    for (int pos = 0; pos < arrlen; ++pos)
-    {
-      int page_i = pos / (strd * mome * part);
-      int rest_1 = pos % (strd * mome * part);
-      int part_i = rest_1 / (strd * mome);
-      int rest_2 = rest_1 % (strd * mome);
-      int mome_i = rest_2 / strd;
-      int strd_i = rest_2 % strd;
-      int inpos =
-        (page_i * strd + strd_i) // event number
-        * (part * mome)          // event size (pos of event)
-        + part_i * mome          // particle inside event
-        + mome_i;                // momentum inside particle
-      out[inpos] = in[pos]; // C2F (C to Fortran)
-    }
+    constexpr bool F2C = false;
+    hst_transposeMomenta<T, F2C>( in, out, evt );
   }
 
 }
-
-// *****************************************************************************
-
-//
-// BACKUP
-//
-
-/**
-   const int evnt_n = 4;  // the number of events
-   const int part_n = 4;  // number of in/out particles inside an event
-   const int mome_n = 3;  // number of momenta of one particle (usually 4)
-   const int strd_n = 2;  // stride length for aosoa data (# adjacent events)
-   const int array_bytes = evnt_n * part_n * mome_n * sizeof(T);
-*/
-
-// debug
-
-// std::cout << std::string(80, '*') << std::endl << "Momenta:" << std::endl;
-// T *aosoa_p = (T *)hstMomenta.get();
-// for (int i = 0; i < m_evt * m_part * m_mome; ++i) {
-//   if (i && i % m_strd == 0)
-//     std::cout << ", ";
-//   if (i && i % (m_mome * m_strd) == 0)
-//     std::cout << std::endl;
-//   if (i && i % (m_part * m_mome * m_strd) == 0)
-//     std::cout << std::endl;
-//   std::cout << aosoa_p[i] << " ";
-// }
-// std::cout << std::endl << std::string(80, '*') << std::endl;
-
-// template <typename T> void Matrix<T>::fill(T *arr) {
-//
-//   T(*aos)
-//   [m_part][m_mome] = (T(*)[m_part][m_mome])arr; // was ->
-//   m_hstInpArray.get();
-//
-//   for (int i = 0; i < m_evt; ++i) {
-//     for (int j = 0; j < m_part; ++j) {
-//       for (int k = 0; k < m_mome; ++k) {
-//         aos[i][j][k] = (i + 1) * 100 + (j + 1) * 10 + (k + 1);
-//       }
-//     }
-//   }
-//
-// #ifdef DEBUG
-//   std::cout << std::string(80, '*') << std::endl;
-//   T *aos_p = (T *)arr; // was -> m_hstInpArray.get();
-//   for (int i = 0; i < m_evt * m_part * m_mome; ++i) {
-//     if (i && i % m_mome == 0)
-//       std::cout << std::endl;
-//     if (i && i % (m_mome * m_part) == 0)
-//       std::cout << std::endl;
-//     std::cout << aos_p[i] << " ";
-//   }
-//   std::cout << std::endl;
-// #endif // DEBUG
-// }
 
 #endif // BRIDGE_H
