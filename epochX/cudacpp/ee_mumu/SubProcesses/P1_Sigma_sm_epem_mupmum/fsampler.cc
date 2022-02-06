5604df76b66b14bf70f0078e20b3246c7e594dc6
Andrea Valassi <andrea.valassi@cern.ch>
2022-02-06 12:50:04 +0100

    [shared] complete fsampler implementation

--- epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/fsampler.cc
+++ epochX/cudacpp/ee_mumu/SubProcesses/P1_Sigma_sm_epem_mupmum/fsampler.cc
@@ -16,106 +16,91 @@ namespace mg5amcCpu
   class Sampler
   {
   public:
-
     // Constructor
     // @param nevtF (NB_PAGE, vector.inc) number of events in Fortran arrays
     // @param nparF (NEXTERNAL, nexternal.inc) number of external particles in Fortran arrays (KEPT FOR SANITY CHECKS ONLY: remove it?)
     // @param np4F number of momenta components, usually 4, in Fortran arrays (KEPT FOR SANITY CHECKS ONLY: remove it?)
     Sampler( int nevtF, int nparF, int np4F );
-
     // Destructor
     ~Sampler(){}
-
     // Delete copy/move constructors and assignment operators
     Sampler( const Sampler&  ) = delete;
     Sampler( Sampler&&  ) = delete;
     Sampler& operator=( const Sampler& ) = delete;
     Sampler& operator=( Sampler&& ) = delete;
-
     // Draw random numbers and convert them to momenta in C++, then transpose them to Fortran momenta
-    void samplerHostSequence( FORTRANFPTYPE* fortranMomenta )
-    {
-      // HARDCODED DEFAULTS
-      constexpr bool verbose = false;
-      constexpr int gpublocks = 4;
-      constexpr int gputhreads = 32;
-      constexpr int niter = 2;
-      constexpr int ndim = gpublocks * gputhreads; // number of threads in one GPU grid
-      constexpr int nevt = ndim; // number of events in one iteration == number of GPU threads
-
-      // --- 0a. Initialise physics process
-
-      // Create a process object
-      CPPProcess process( niter, gpublocks, gputhreads, verbose );
-
-      // Read param_card and set parameters
-      process.initProc( "../../Cards/param_card.dat" );
-      constexpr fptype energy = 1500; // historical default, Ecms = 1500 GeV = 1.5 TeV (above the Z peak)
-
-      // --- 0b. Allocate memory structures
-
-      // Memory buffers for random numbers
-      HostBufferRandomNumbers hstRnarray( nevt );
-
-      // Memory buffers for momenta
-      HostBufferMomenta hstMomenta( nevt );
-
-      // Memory buffers for sampling weights
-      HostBufferWeights hstWeights( nevt );
-
-      // --- 0c. Create curand or common generator
-
-      // Allocate the appropriate RandomNumberKernel
-      std::unique_ptr<RandomNumberKernelBase> prnk;
-      prnk.reset( new CommonRandomNumberKernel( hstRnarray ) );
-
-      // --- 0c. Create rambo sampling kernel
-      std::unique_ptr<SamplingKernelBase> prsk;
-      prsk.reset( new RamboSamplingKernelHost( energy, hstRnarray, hstMomenta, hstWeights, nevt ) );
-
-      // Memory buffers for matrix elements
-      //HostBufferMatrixElements hstMatrixElements( nevt );
-
-      // --- 0c. Create cross section kernel [keep this in 0c for the moment]
-      //EventStatistics hstStats;
-      //CrossSectionKernelHost xsk( hstWeights, hstMatrixElements, hstStats, nevt );
-
-      // **************************************
-      // *** START MAIN LOOP ON #ITERATIONS ***
-      // **************************************
-      int iiter = 0; // FIXME
-      //std::cout << "Iteration #" << iiter+1 << std::endl;
-
-      // === STEP 1 OF 3
-
-      // --- 1a. Seed curand generator (to get same results on host and device)
-      // [NB This should not be necessary using the host API: "Generation functions
-      // can be called multiple times on the same generator to generate successive
-      // blocks of results. For pseudorandom generators, multiple calls to generation
-      // functions will yield the same result as a single call with a large size."]
-      const unsigned long long seed = 20200805;
-      prnk->seedGenerator( seed+iiter );
-
-      // --- 1b. Generate all relevant numbers to build nevt events (i.e. nevt phase space points) on the host
-      prnk->generateRnarray();
-      //std::cout << "Got random numbers" << std::endl;
-
-      // === STEP 2 OF 3
-
-      // --- 2a. Fill in momenta of initial state particles on the device
-      prsk->getMomentaInitial();
-      //std::cout << "Got initial momenta" << std::endl;
-
-      // --- 2b. Fill in momenta of final state particles using the RAMBO algorithm on the device
-      // (i.e. map random numbers to final-state particle momenta for each of nevt events)
-      prsk->getMomentaFinal();
-      //std::cout << "Got final momenta" << std::endl;
-
-      // --- 2c. TransposeC2F
-      hst_transposeMomentaC2F( hstMomenta.data(), fortranMomenta, nevt );
-    }
-
+    void samplerHostSequence( FORTRANFPTYPE* fortranMomenta );
+  private:
+    const int m_nevt; // The number of events in each iteration
+    HostBufferRandomNumbers m_hstRnarray; // Memory buffers for random numbers
+    HostBufferMomenta m_hstMomenta; // Memory buffers for momenta
+    HostBufferWeights m_hstWeights; // Memory buffers for sampling weights
+    std::unique_ptr<RandomNumberKernelBase> m_prnk; // The appropriate RandomNumberKernel
+    std::unique_ptr<SamplingKernelBase> m_prsk; // The appropriate SamplingKernel
+    int m_iiter; // The iteration counter (for random number seeding)
+    // HARDCODED DEFAULTS
+    static constexpr bool verbose = false;
+    static constexpr int gputhreads = 32;
+    static constexpr fptype energy = 1500; // historical default, Ecms = 1500 GeV = 1.5 TeV (above the Z peak)
   };
+
+  template<typename FORTRANFPTYPE>
+  Sampler<FORTRANFPTYPE>::Sampler( int nevtF, int nparF, int np4F )
+    : m_nevt( nevtF )
+    , m_hstRnarray( nevtF )
+    , m_hstMomenta( nevtF )
+    , m_hstWeights( nevtF )
+    , m_prnk( new CommonRandomNumberKernel( m_hstRnarray ) )
+    , m_prsk( new RamboSamplingKernelHost( energy, m_hstRnarray, m_hstMomenta, m_hstWeights, nevtF ) )
+    , m_iiter( 0 )
+  {
+    if ( m_nevt%gputhreads != 0 )
+    {
+      std::ostringstream sstr;
+      sstr << "Sampler: nevt should be a multiple of gputhreads=" << gputhreads;
+      throw std::runtime_error( sstr.str() );
+    }
+    if ( nparF != mgOnGpu::npar ) throw std::runtime_error( "Sampler constructor: npar mismatch" );
+    if ( np4F != mgOnGpu::np4 ) throw std::runtime_error( "Sampler constructor: np4 mismatch" );
+    const int gpublocks = m_nevt/gputhreads;
+    std::cout << "WARNING! Instantiate Sampler (nevt=" << m_nevt << ", gpublocks=" << gpublocks << ", gputhreads=" << gputhreads
+              << ", gpublocks*gputhreads=" << gpublocks*gputhreads << ")" << std::endl;
+    // Create a process object
+    // FIXME: this can happily go out of scope because it is only needed to read parameters?
+    // FIXME: niter is totally dummy in the CPPProcess ctor!
+    constexpr int niterdummy = 2;
+    CPPProcess process( niterdummy, gpublocks, gputhreads, verbose );
+    // Read param_card and set parameters
+    process.initProc( "../../Cards/param_card.dat" );
+  }
+
+  // Draw random numbers and convert them to momenta in C++, then transpose them to Fortran momenta
+  template<typename FORTRANFPTYPE>
+  void Sampler<FORTRANFPTYPE>::samplerHostSequence( FORTRANFPTYPE* fortranMomenta )
+  {
+    std::cout << "Iteration #" << ++m_iiter << std::endl;
+    // === STEP 1 OF 3
+    // --- 1a. Seed curand generator (to get same results on host and device)
+    // [NB This should not be necessary using the host API: "Generation functions
+    // can be called multiple times on the same generator to generate successive
+    // blocks of results. For pseudorandom generators, multiple calls to generation
+    // functions will yield the same result as a single call with a large size."]
+    constexpr unsigned long long seed = 20200805;
+    m_prnk->seedGenerator( seed+m_iiter );
+    // --- 1b. Generate all relevant numbers to build nevt events (i.e. nevt phase space points) on the host
+    m_prnk->generateRnarray();
+    //std::cout << "Got random numbers" << std::endl;
+    // === STEP 2 OF 3
+    // --- 2a. Fill in momenta of initial state particles on the device
+    m_prsk->getMomentaInitial();
+    //std::cout << "Got initial momenta" << std::endl;
+    // --- 2b. Fill in momenta of final state particles using the RAMBO algorithm on the device
+    // (i.e. map random numbers to final-state particle momenta for each of nevt events)
+    m_prsk->getMomentaFinal();
+    //std::cout << "Got final momenta" << std::endl;
+    // --- 2c. TransposeC2F
+    hst_transposeMomentaC2F( m_hstMomenta.data(), fortranMomenta, m_nevt );
+  }
 
 }
 #endif
