8c646106ff9bc834f3f7100d051ecf4a894f8896
Andrea Valassi <andrea.valassi@cern.ch>
2022-02-08 17:31:25 +0100

    [clf] cosmetics: manual formatting of read_slha.h - may be unstable under clang format...

--- epochX/cudacpp/gg_ttgg/src/read_slha.h
+++ epochX/cudacpp/gg_ttgg/src/read_slha.h
@@ -1,5 +1,5 @@
 #ifndef READ_SLHA_H
-#define READ_SLHA_H
+#define READ_SLHA_H 1
 
 #include <map>
 #include <sstream>
@@ -11,26 +11,11 @@ class SLHABlock
  public:
   SLHABlock( std::string name = "" ) { _name = name; }
   ~SLHABlock() {}
-
-  void
-  set_entry( std::vector<int> indices, double value );
-  double
-  get_entry( std::vector<int> indices, double def_val = 0 );
-  void
-  set_name( std::string name )
-  {
-    _name = name;
-  }
-  std::string
-  get_name()
-  {
-    return _name;
-  }
-  int
-  get_indices()
-  {
-    return _indices;
-  }
+  void set_entry( std::vector<int> indices, double value );
+  double get_entry( std::vector<int> indices, double def_val = 0 );
+  void set_name( std::string name ) { _name = name; }
+  std::string get_name() { return _name; }
+  int get_indices() { return _indices; }
 
  private:
   std::string _name;
@@ -45,21 +30,11 @@ class SLHAReader
   {
     if( file_name != "" ) read_slha_file( file_name, verbose );
   }
-
-  void
-  read_slha_file( std::string file_name, bool verbose );
-  double
-  get_block_entry( std::string block_name, std::vector<int> indices,
-                   double def_val = 0 );
-  double
-  get_block_entry( std::string block_name, int index,
-                   double def_val = 0 );
-  void
-  set_block_entry( std::string block_name, std::vector<int> indices,
-                   double value );
-  void
-  set_block_entry( std::string block_name, int index,
-                   double value );
+  void read_slha_file( std::string file_name, bool verbose );
+  double get_block_entry( std::string block_name, std::vector<int> indices, double def_val = 0 );
+  double get_block_entry( std::string block_name, int index, double def_val = 0 );
+  void set_block_entry( std::string block_name, std::vector<int> indices, double value );
+  void set_block_entry( std::string block_name, int index, double value );
 
  private:
   std::map<std::string, SLHABlock> _blocks;
