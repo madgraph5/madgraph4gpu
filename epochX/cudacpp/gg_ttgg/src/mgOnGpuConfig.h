83ca5a7d4dbf3c3cb650c429a421bb4d77d0a5fe
Andrea Valassi <andrea.valassi@cern.ch>
2022-02-09 16:02:22 +0100

    [clf] move the MGONGPU_CPPSIMD macro definition outside the namespace

--- epochX/cudacpp/gg_ttgg/src/mgOnGpuConfig.h
+++ epochX/cudacpp/gg_ttgg/src/mgOnGpuConfig.h
@@ -114,20 +114,25 @@ namespace mgOnGpu
   constexpr int cppAlign = 64; // alignment requirement for SIMD vectorization (64-byte i.e. 512-bit)
 #endif
 
-  // C++ SIMD vectorization width (this will be used to set neppV)
+}
+
+// Expose typedefs and operators outside the namespace
+using mgOnGpu::fptype;
+
+// C++ SIMD vectorization width (this will be used to set neppV)
 #ifdef __CUDACC__
 #undef MGONGPU_CPPSIMD
 #else
 #if defined __AVX512VL__
 #ifdef MGONGPU_PVW512
-  // "512z" AVX512 with 512 width (512-bit ie 64-byte): 8 (DOUBLE) or 16 (FLOAT)
+// "512z" AVX512 with 512 width (512-bit ie 64-byte): 8 (DOUBLE) or 16 (FLOAT)
 #ifdef MGONGPU_FPTYPE_DOUBLE
 #define MGONGPU_CPPSIMD 8
 #else
 #define MGONGPU_CPPSIMD 16
 #endif
 #else
-  // "512y" AVX512 with 256 width (256-bit ie 32-byte): 4 (DOUBLE) or 8 (FLOAT) [gcc DEFAULT]
+// "512y" AVX512 with 256 width (256-bit ie 32-byte): 4 (DOUBLE) or 8 (FLOAT) [gcc DEFAULT]
 #ifdef MGONGPU_FPTYPE_DOUBLE
 #define MGONGPU_CPPSIMD 4
 #else
@@ -135,28 +140,24 @@ namespace mgOnGpu
 #endif
 #endif
 #elif defined __AVX2__
-  // "avx2" AVX2 (256-bit ie 32-byte): 4 (DOUBLE) or 8 (FLOAT) [clang DEFAULT]
+// "avx2" AVX2 (256-bit ie 32-byte): 4 (DOUBLE) or 8 (FLOAT) [clang DEFAULT]
 #ifdef MGONGPU_FPTYPE_DOUBLE
 #define MGONGPU_CPPSIMD 4
 #else
 #define MGONGPU_CPPSIMD 8
 #endif
 #elif defined __SSE4_2__
-  // "sse4" SSE4.2 (128-bit ie 16-byte): 2 (DOUBLE) or 4 (FLOAT)
+// "sse4" SSE4.2 (128-bit ie 16-byte): 2 (DOUBLE) or 4 (FLOAT)
 #ifdef MGONGPU_FPTYPE_DOUBLE
 #define MGONGPU_CPPSIMD 2
 #else
 #define MGONGPU_CPPSIMD 4
 #endif
 #else
-  // "none" i.e. no SIMD (*** NB: this is equivalent to AOS ***)
+// "none" i.e. no SIMD (*** NB: this is equivalent to AOS ***)
 #undef MGONGPU_CPPSIMD
 #endif
 #endif
-}
-
-// Expose typedefs and operators outside the namespace
-using mgOnGpu::fptype;
 
 // Cuda nsight compute (ncu) debug: add dummy lines to ease SASS program flow navigation
 // Arguments (not used so far): text is __FUNCTION__, code is 0 (start) or 1 (end)
