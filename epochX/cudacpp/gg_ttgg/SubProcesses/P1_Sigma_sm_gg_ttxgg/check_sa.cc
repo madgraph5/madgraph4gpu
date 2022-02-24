a037991ed64a06f9d172b70d6c977daa5b083180
Andrea Valassi <andrea.valassi@cern.ch>
2022-02-24 18:14:17 +0100

    [clf] format check_sa.cc

--- epochX/cudacpp/gg_ttgg/SubProcesses/P1_Sigma_sm_gg_ttxgg/check_sa.cc
+++ epochX/cudacpp/gg_ttgg/SubProcesses/P1_Sigma_sm_gg_ttxgg/check_sa.cc
@@ -953,52 +953,52 @@ int main(int argc, char **argv)
     double stdelem = hstStats.stdME();
 
     jsonFile << "\"NumberOfEntries\": " << niter << "," << std::endl
-      //<< std::scientific // Not sure about this
+             //<< std::scientific // Not sure about this
              << "\"TotalTime[Rnd+Rmb+ME] (123)\": \""
-             << std::to_string(sumgtim+sumrtim+sumwtim) << " sec\","
+             << std::to_string( sumgtim + sumrtim + sumwtim ) << " sec\","
              << std::endl
              << "\"TotalTime[Rambo+ME] (23)\": \""
-             << std::to_string(sumrtim+sumwtim) << " sec\"," << std::endl
+             << std::to_string( sumrtim + sumwtim ) << " sec\"," << std::endl
              << "\"TotalTime[RndNumGen] (1)\": \""
-             << std::to_string(sumgtim) << " sec\"," << std::endl
+             << std::to_string( sumgtim ) << " sec\"," << std::endl
              << "\"TotalTime[Rambo] (2)\": \""
-             << std::to_string(sumrtim) << " sec\"," << std::endl
+             << std::to_string( sumrtim ) << " sec\"," << std::endl
              << "\"TotalTime[MatrixElems] (3)\": \""
-             << std::to_string(sumwtim) << " sec\"," << std::endl
+             << std::to_string( sumwtim ) << " sec\"," << std::endl
              << "\"MeanTimeInMatrixElems\": \""
-             << std::to_string(meanwtim) << " sec\"," << std::endl
+             << std::to_string( meanwtim ) << " sec\"," << std::endl
              << "\"MinTimeInMatrixElems\": \""
-             << std::to_string(minwtim) << " sec\"," << std::endl
+             << std::to_string( minwtim ) << " sec\"," << std::endl
              << "\"MaxTimeInMatrixElems\": \""
-             << std::to_string(maxwtim) << " sec\"," << std::endl
-      //<< "ProcessID:                = " << getpid() << std::endl
-      //<< "NProcesses                = " << process.nprocesses << std::endl
+             << std::to_string( maxwtim ) << " sec\"," << std::endl
+             //<< "ProcessID:                = " << getpid() << std::endl
+             //<< "NProcesses                = " << process.nprocesses << std::endl
              << "\"TotalEventsComputed\": " << nevtALL << "," << std::endl
              << "\"EvtsPerSec[Rnd+Rmb+ME](123)\": \""
-             << std::to_string(nevtALL/(sumgtim+sumrtim+sumwtim)) << " sec^-1\"," << std::endl
+             << std::to_string( nevtALL / ( sumgtim + sumrtim + sumwtim ) ) << " sec^-1\"," << std::endl
              << "\"EvtsPerSec[Rmb+ME] (23)\": \""
-             << std::to_string(nevtALL/(sumrtim+sumwtim)) << " sec^-1\"," << std::endl
+             << std::to_string( nevtALL / ( sumrtim + sumwtim ) ) << " sec^-1\"," << std::endl
              << "\"EvtsPerSec[MatrixElems] (3)\": \""
-             << std::to_string(nevtALL/sumwtim) << " sec^-1\"," << std::endl
+             << std::to_string( nevtALL / sumwtim ) << " sec^-1\"," << std::endl
              << "\"EvtsPerSec[MECalcOnly] (3)\": \""
-             << std::to_string(nevtALL/sumw3atim) << " sec^-1\"," << std::endl
+             << std::to_string( nevtALL / sumw3atim ) << " sec^-1\"," << std::endl
              << "\"NumMatrixElems(notAbnormal)\": " << nevtALL - nabn << "," << std::endl
              << std::scientific
              << "\"MeanMatrixElemValue\": "
-             << "\"" << std::to_string(meanelem) << " GeV^"
-             << std::to_string(meGeVexponent) << "\"," << std::endl
+             << "\"" << std::to_string( meanelem ) << " GeV^"
+             << std::to_string( meGeVexponent ) << "\"," << std::endl
              << "\"StdErrMatrixElemValue\": "
-             << "\"" << std::to_string(stdelem/sqrt(nevtALL)) << " GeV^"
-             << std::to_string(meGeVexponent) << "\"," << std::endl
+             << "\"" << std::to_string( stdelem / sqrt( nevtALL ) ) << " GeV^"
+             << std::to_string( meGeVexponent ) << "\"," << std::endl
              << "\"StdDevMatrixElemValue\": "
-             << "\"" << std::to_string(stdelem)
-             << " GeV^" << std::to_string(meGeVexponent) << "\"," << std::endl
+             << "\"" << std::to_string( stdelem )
+             << " GeV^" << std::to_string( meGeVexponent ) << "\"," << std::endl
              << "\"MinMatrixElemValue\": "
-             << "\"" << std::to_string(minelem) << " GeV^"
-             << std::to_string(meGeVexponent) << "\"," << std::endl
+             << "\"" << std::to_string( minelem ) << " GeV^"
+             << std::to_string( meGeVexponent ) << "\"," << std::endl
              << "\"MaxMatrixElemValue\": "
-             << "\"" << std::to_string(maxelem) << " GeV^"
-             << std::to_string(meGeVexponent) <<  "\"," << std::endl;
+             << "\"" << std::to_string( maxelem ) << " GeV^"
+             << std::to_string( meGeVexponent ) << "\"," << std::endl;
 
     timermap.dump(jsonFile, true); // NB For the active json timer this dumps a partial total
 
