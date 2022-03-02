c042e49bdb61b1df958d7aa7e3f36f2a4d514a6f
Andrea Valassi <andrea.valassi@cern.ch>
2022-03-02 11:58:39 +0100

    [clf] BUG FIX in codegen for CPPProcess.cc (ncolor was hardcoded to 24)

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/model_handling.py
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/model_handling.py
@@ -961,11 +961,10 @@ class PLUGIN_OneProcessExporter(export_cpp.OneProcessExporterGPU):
     mgDebug( 0, __FUNCTION__ );
 #ifndef __CUDACC__
     //printf( "calculate_wavefunctions: nevt %d\\n", nevt );
-#endif
-
-    // The number of colors
-    constexpr int ncolor = 24;
-
+#endif\n""")
+            ret_lines.append("    // The number of colors")
+            ret_lines.append("    constexpr int ncolor = %i;" % len(color_amplitudes[0]))
+            ret_lines.append("""
     // Local TEMPORARY variables for a subset of Feynman diagrams in the given CUDA event (ievt) or C++ event page (ipagV)
     // [NB these variables are reused several times (and re-initialised each time) within the same event or event page]
     //MemoryBufferWavefunctions w_buffer[nwf]{ neppV };
