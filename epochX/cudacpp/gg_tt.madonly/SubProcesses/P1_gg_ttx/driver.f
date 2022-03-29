5ef73d3ef6f9a445d5290ec99d7a867b54fd7d16
Andrea Valassi <andrea.valassi@cern.ch>
2022-03-29 18:36:28 +0200

    [v311] first attempt at adding counters to Fortran in gg_tt.madonly

--- epochX/cudacpp/gg_tt.madonly/SubProcesses/P1_gg_ttx/driver.f
+++ epochX/cudacpp/gg_tt.madonly/SubProcesses/P1_gg_ttx/driver.f
@@ -76,6 +76,7 @@ c      common/to_colstats/ncols,ncolflow,ncolalt,ic
 C-----
 C  BEGIN CODE
 C----- 
+      call counters_initialise()
       call cpu_time(t_before)
       CUMULATED_TIMING = t_before
 c
@@ -200,6 +201,7 @@ c      write(*,*) 'Final xsec: ',xsec
 
       rewind(lun)
 
+      call counters_finalise()
       close(lun)
       end
 
