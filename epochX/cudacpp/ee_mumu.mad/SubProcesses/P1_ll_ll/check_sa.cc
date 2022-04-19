7ec0698f6c824dfc2e296090352639592d2e3112
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-19 16:01:00 +0200

    [v311] regenerate ee_mumu.mad after all merges, several changes

--- epochX/cudacpp/ee_mumu.mad/SubProcesses/P1_ll_ll/check_sa.cc
+++ epochX/cudacpp/ee_mumu.mad/SubProcesses/P1_ll_ll/check_sa.cc
@@ -81,12 +81,12 @@ main( int argc, char** argv )
   bool debug = false;
   bool perf = false;
   bool json = false;
-  int niter = 0;
-  int gpublocks = 1;
-  int gputhreads = 32;
-  int jsondate = 0;
-  int jsonrun = 0;
-  int numvec[5] = { 0, 0, 0, 0, 0 };
+  unsigned int niter = 0;
+  unsigned int gpublocks = 1;
+  unsigned int gputhreads = 32;
+  unsigned int jsondate = 0;
+  unsigned int jsonrun = 0;
+  unsigned int numvec[5] = { 0, 0, 0, 0, 0 };
   int nnum = 0;
   // Random number mode
   enum class RandomNumberMode
@@ -174,7 +174,7 @@ main( int argc, char** argv )
     }
     else if( is_number( argv[argn] ) && nnum < 5 )
     {
-      numvec[nnum++] = atoi( argv[argn] );
+      numvec[nnum++] = strtoul( argv[argn], NULL, 0 );
     }
     else
     {
@@ -262,8 +262,8 @@ main( int argc, char** argv )
   if( !MatrixElementKernelHost::hostSupportsSIMD() ) return 1;
 #endif
 
-  const int ndim = gpublocks * gputhreads; // number of threads in one GPU grid
-  const int nevt = ndim;                   // number of events in one iteration == number of GPU threads
+  const unsigned int ndim = gpublocks * gputhreads; // number of threads in one GPU grid
+  const unsigned int nevt = ndim;                   // number of events in one iteration == number of GPU threads
 
   if( verbose )
     std::cout << "# iterations: " << niter << std::endl;
@@ -414,7 +414,7 @@ main( int argc, char** argv )
   // *** START MAIN LOOP ON #ITERATIONS ***
   // **************************************
 
-  for( int iiter = 0; iiter < niter; ++iiter )
+  for( unsigned long int iiter = 0; iiter < niter; ++iiter )
   {
     //std::cout << "Iteration #" << iiter+1 << " of " << niter << std::endl;
 
@@ -571,7 +571,7 @@ main( int argc, char** argv )
       if( perf ) std::cout << "Wave function time: " << wavetime << std::endl;
     }
 
-    for( int ievt = 0; ievt < nevt; ++ievt ) // Loop over all events in this iteration
+    for( unsigned int ievt = 0; ievt < nevt; ++ievt ) // Loop over all events in this iteration
     {
       if( verbose )
       {
@@ -616,7 +616,7 @@ main( int argc, char** argv )
   //double sqsgtim = 0;
   double mingtim = genrtimes[0];
   double maxgtim = genrtimes[0];
-  for( int iiter = 0; iiter < niter; ++iiter )
+  for( unsigned int iiter = 0; iiter < niter; ++iiter )
   {
     sumgtim += genrtimes[iiter];
     //sqsgtim += genrtimes[iiter]*genrtimes[iiter];
@@ -628,7 +628,7 @@ main( int argc, char** argv )
   //double sqsrtim = 0;
   double minrtim = rambtimes[0];
   double maxrtim = rambtimes[0];
-  for( int iiter = 0; iiter < niter; ++iiter )
+  for( unsigned int iiter = 0; iiter < niter; ++iiter )
   {
     sumrtim += rambtimes[iiter];
     //sqsrtim += rambtimes[iiter]*rambtimes[iiter];
@@ -640,7 +640,7 @@ main( int argc, char** argv )
   //double sqswtim = 0;
   double minwtim = wavetimes[0];
   double maxwtim = wavetimes[0];
-  for( int iiter = 0; iiter < niter; ++iiter )
+  for( unsigned int iiter = 0; iiter < niter; ++iiter )
   {
     sumwtim += wavetimes[iiter];
     //sqswtim += wavetimes[iiter]*wavetimes[iiter];
@@ -654,7 +654,7 @@ main( int argc, char** argv )
   //double sqsw3atim = 0;
   double minw3atim = wv3atimes[0];
   double maxw3atim = wv3atimes[0];
-  for( int iiter = 0; iiter < niter; ++iiter )
+  for( unsigned int iiter = 0; iiter < niter; ++iiter )
   {
     sumw3atim += wv3atimes[iiter];
     //sqsw3atim += wv3atimes[iiter]*wv3atimes[iiter];
@@ -664,7 +664,7 @@ main( int argc, char** argv )
   double meanw3atim = sumw3atim / niter;
   //double stdw3atim = std::sqrt( sqsw3atim / niter - meanw3atim * meanw3atim );
 
-  const int nevtALL = hstStats.nevtALL; // total number of ALL events in all iterations
+  const unsigned int nevtALL = hstStats.nevtALL; // total number of ALL events in all iterations
   if( nevtALL != niter * nevt )
     std::cout << "ERROR! nevtALL mismatch " << nevtALL << " != " << niter * nevt << std::endl; // SANITY CHECK
   int nabn = hstStats.nevtABN;
@@ -763,6 +763,8 @@ main( int argc, char** argv )
 #elif defined __SSE4_2__
 #ifdef __PPC__
   wrkflwtxt += "/ppcv";
+#elif defined __ARM_NEON__
+  wrkflwtxt += "/neon";
 #else
   wrkflwtxt += "/sse4";
 #endif
@@ -792,12 +794,14 @@ main( int argc, char** argv )
   if( perf )
   {
 #ifndef __CUDACC__
+#ifdef _OPENMP
     // Get the output of "nproc --all" (https://stackoverflow.com/a/478960)
     std::string nprocall;
     std::unique_ptr<FILE, decltype( &pclose )> nprocpipe( popen( "nproc --all", "r" ), pclose );
     if( !nprocpipe ) throw std::runtime_error( "`nproc --all` failed?" );
     std::array<char, 128> nprocbuf;
     while( fgets( nprocbuf.data(), nprocbuf.size(), nprocpipe.get() ) != nullptr ) nprocall += nprocbuf.data();
+#endif
 #endif
 #ifdef MGONGPU_CPPSIMD
 #ifdef MGONGPU_HAS_CPPCXTYPEV_BRK
@@ -869,6 +873,8 @@ main( int argc, char** argv )
               << "Internal loops fptype_sv    = VECTOR[" << neppV
 #ifdef __PPC__
               << "] ('sse4': PPC VSX, 128bit)" << cxtref << std::endl
+#elif defined __ARM_NEON__
+              << "] ('sse4': ARM NEON, 128bit)" << cxtref << std::endl
 #else
               << "] ('sse4': SSE4.2, 128bit)" << cxtref << std::endl
 #endif
