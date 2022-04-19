64f74c5d581bc5c3d7baf21b6255fc8a2340dbfe
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-19 11:12:11 +0200

    [v311] backport to codegen the changes in eemumu.mad and ggtt.mad auto_dsig1.f
    (git diff --no-ext-diff e5a2db82 gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f > CODEGEN/MG5aMC_patches/patch.auto_dsig1.f)

--- epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.auto_dsig1.f
+++ epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.auto_dsig1.f
@@ -1,5 +1,5 @@
 diff --git a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f
-index 018ed7a6..0accde15 100644
+index 018ed7a6..e40d30e2 100644
 --- a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f
 +++ b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f
 @@ -281,7 +281,6 @@ C     jamp2 information
@@ -10,7 +10,7 @@ index 018ed7a6..0accde15 100644
  C     
  C     local
  C     
-@@ -443,7 +442,12 @@ C
+@@ -443,10 +442,15 @@ C
        DOUBLE PRECISION JAMP2_MULTI(0:MAXFLOW, NB_PAGE)
  
        INTEGER IVEC
@@ -21,22 +21,31 @@ index 018ed7a6..0accde15 100644
 +      DOUBLE PRECISION OUT2(NB_PAGE)
 +#endif
  
- !$OMP PARALLEL
- !$OMP DO
-@@ -459,6 +463,19 @@ C       &				 selected_hel(IVEC),
+-!$OMP PARALLEL
+-!$OMP DO
++c!$OMP PARALLEL
++c!$OMP DO
+       DO IVEC=1, NB_PAGE
+         CALL SMATRIX1(P_MULTI(0,1,IVEC),
+      &	                         hel_rand(IVEC),
+@@ -457,8 +461,21 @@ C       &				 selected_hel(IVEC),
+      &				 IVEC
+      &				 )
        ENDDO
- !$OMP END DO
- !$OMP END PARALLEL
+-!$OMP END DO
+-!$OMP END PARALLEL
++c!$OMP END DO
++c!$OMP END PARALLEL
 +
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
 +      CALL FBRIDGESEQUENCE(MEEXPORTER_PBRIDGE, P_MULTI, OUT2)
 +      DO IVEC=1, NB_PAGE
-+        DO IEXT=1, NEXTERNAL
-+          WRITE (*,*) P_MULTI(0,IEXT,IVEC), P_MULTI(1,IEXT,IVEC),
-+     &                P_MULTI(2,IEXT,IVEC), P_MULTI(3,IEXT,IVEC)
-+        END DO
++c       DO IEXT=1, NEXTERNAL
++c         WRITE (*,*) P_MULTI(0,IEXT,IVEC), P_MULTI(1,IEXT,IVEC),
++c    &                P_MULTI(2,IEXT,IVEC), P_MULTI(3,IEXT,IVEC)
++c       END DO
 +        WRITE (*,*) IVEC, OUT(IVEC), OUT2(IVEC), OUT2(IVEC)/OUT(IVEC)
-+        WRITE (*,*)
++c       WRITE (*,*)
 +      END DO
 +#endif
 +
