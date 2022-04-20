0f37279ff259d3d6bdd6b936a5f9944b007faaa2
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-20 14:15:00 +0200

    [bug341] remove redefinition of export_cpp.cp (#341), it seems no longer necessary

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/output.py
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/output.py
@@ -19,25 +19,6 @@ import madgraph.various.misc as misc
 
 #------------------------------------------------------------------------------------
 
-# AV - modify madgraph.iolibs.files.cp (preserve symlinks)
-def PLUGIN_cp(path1, path2, log=True, error=False):
-    """ simple cp taking linux or mix entry"""
-    from madgraph.iolibs.files import format_path
-    path1 = format_path(path1)
-    path2 = format_path(path2)
-    try:
-        import shutil
-        ###shutil.copy(path1, path2)
-        shutil.copy(path1, path2, follow_symlinks=False) # AV
-    except:
-        from madgraph.iolibs.files import cp
-        cp(path1, path2, log=log, error=error)
-
-DEFAULT_cp = export_cpp.cp
-export_cpp.cp = PLUGIN_cp
-
-#------------------------------------------------------------------------------------
-
 class PLUGIN_ProcessExporter(export_cpp.ProcessExporterGPU):
     # Class structure information
     #  - object
