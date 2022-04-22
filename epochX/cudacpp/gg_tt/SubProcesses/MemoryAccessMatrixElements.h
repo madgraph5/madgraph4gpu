b2d53f159d493b88abce12835369f9c6a1d92a8f
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-22 19:17:10 +0200

    [alphas] add a non-const vector accessor for matrix elements (#435) - not used yet in CPPProcess

--- epochX/cudacpp/gg_tt/SubProcesses/MemoryAccessMatrixElements.h
+++ epochX/cudacpp/gg_tt/SubProcesses/MemoryAccessMatrixElements.h
@@ -4,6 +4,8 @@
 #include "mgOnGpuConfig.h"
 
 #include "MemoryAccessHelpers.h"
+#include "MemoryAccessVectors.h"
+#include "MemoryBuffers.h" // for HostBufferMatrixElements::isaligned
 
 //----------------------------------------------------------------------------
 
@@ -91,9 +93,25 @@ class KernelAccessMatrixElements
 public:
 
   // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
-  // [Signature (non-const) ===> fptype& kernelAccess( fptype* buffer ) <===]
-  static constexpr auto kernelAccess =
+  // [Signature (non-const, SCALAR) ===> fptype& kernelAccess( fptype* buffer ) <===]
+  static constexpr auto kernelAccess_s =
     KernelAccessHelper<MemoryAccessMatrixElementsBase, onDevice>::template kernelAccessField<>; // requires cuda 11.4
+
+  // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal)
+  // [Signature (non const, SCALAR OR VECTOR) ===> fptype_sv kernelAccess( const fptype* buffer ) <===]
+  static __host__ __device__ inline fptype_sv&
+  kernelAccess( const fptype* buffer )
+  {
+    fptype& out = kernelAccess_s( buffer );
+#ifndef MGONGPU_CPPSIMD
+    return out;
+#else
+    // NB: derived from MemoryAccessMomenta, restricting the implementation to contiguous aligned arrays (#435)
+    static_assert( mg5amcCpu::HostBufferMatrixElements::isaligned() ); // ASSUME ALIGNED ARRAYS (reinterpret_cast will segfault otherwise!)    
+    //assert( (size_t)( buffer ) % mgOnGpu::cppAlign == 0 ); // ASSUME ALIGNED ARRAYS (reinterpret_cast will segfault otherwise!)
+    return mg5amcCpu::fptypevFromAlignedArray( out ); // SIMD bulk load of neppV, use reinterpret_cast
+#endif
+  }
 
   // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
   // [Signature (const) ===> const fptype& kernelAccessConst( const fptype* buffer ) <===]
