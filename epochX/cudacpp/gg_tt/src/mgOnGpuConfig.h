9ef77b5cca1261956e6ccddd66ed30391b979039
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-25 19:37:11 +0200

    [alphas] allow HRDCOD mode in mgOnGpuConfig again (the option had been disabled)
    Note the following compilation warning
    ccache /usr/local/cuda-11.6/bin/nvcc  -O3  -lineinfo -I. -I../../src -I../../../../../tools -I/usr/local/cuda-11.6/include/ -DUSE_NVTX -gencode arch=compute_70,code=compute_70 -gencode arch=compute_70,code=sm_70 -use_fast_math -std=c++17  -ccbin /cvmfs/sft.cern.ch/lcg/releases/gcc/10.2.0-c44b3/x86_64-centos7/bin/g++ -DMGONGPU_FPTYPE_DOUBLE -DMGONGPU_HARDCODE_CIPC -Xcompiler -fPIC -c gCPPProcess.cu -o gCPPProcess.o
    gCPPProcess.cu(53): warning #177-D: variable "mg5amcGpu::cIPC" was declared but never referenced

--- epochX/cudacpp/gg_tt/src/mgOnGpuConfig.h
+++ epochX/cudacpp/gg_tt/src/mgOnGpuConfig.h
@@ -31,7 +31,7 @@
 // [OLD: This optimization can gain 20% in CUDA in eemumu (issue #39)]
 // [OLD: By default, should not hardcode, but allow this macro to be set from outside with e.g. -DMGONGPU_HARDCODE_CIPC]
 // The option to use hardcoded cIPC/cIPD physics parameters is no longer relevant or applicable when alphas is running (#373)
-#undef MGONGPU_HARDCODE_CIPC // default
+//#undef MGONGPU_HARDCODE_CIPC // default
 ////#define MGONGPU_HARDCODE_CIPC 1 // FIXME: this is no longer supported when alphas is running
 
 // Complex type in c++: std::complex or cxsmpl (CHOOSE ONLY ONE)
