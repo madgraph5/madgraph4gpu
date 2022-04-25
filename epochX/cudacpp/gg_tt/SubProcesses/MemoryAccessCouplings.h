b2b251b3c9c1863a8f9b480747135eafb882b4a2
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-25 17:01:46 +0200

    [alphas] revert to previous MemoryAccessCouplings - says AOSOA[npagC][nx2][neppC] but actually ndcoup * AOSOA[npagC][nx2][neppC]
    Revert "[alphas] try to integrate idcoup in MemoryAccessCouplings as AOSOA[npagC][ndcoup][nx2][neppC]"
    This reverts commit 4008822b68f1aa2f19f20885abcd586effc5324d.

--- epochX/cudacpp/gg_tt/SubProcesses/MemoryAccessCouplings.h
+++ epochX/cudacpp/gg_tt/SubProcesses/MemoryAccessCouplings.h
@@ -12,7 +12,7 @@
 //----------------------------------------------------------------------------
 
 // A class describing the internal layout of memory buffers for couplings
-// This implementation uses an AOSOA[npagC][ndcoup][nx2][neppC] where nevt=npagC*neppC
+// This implementation uses an AOSOA[npagC][nx2][neppC] where nevt=npagC*neppC
 // [If many implementations are used, a suffix _AOSOAv1 should be appended to the class name]
 class MemoryAccessCouplingsBase //_AOSOAv1
 {
@@ -30,9 +30,6 @@ private:
   friend class KernelAccessHelper<MemoryAccessCouplingsBase, true>;
   friend class KernelAccessHelper<MemoryAccessCouplingsBase, false>;
 
-  // The number of couplings that dependent on the running alphas QCD in this specific process
-  static constexpr int ndcoup = mgOnGpu::ndcoup;
-
   // The number of floating point components of a complex number
   static constexpr int nx2 = mgOnGpu::nx2;
 
@@ -47,26 +44,24 @@ private:
   ieventAccessRecord( fptype* buffer,
                       const int ievt )
   {
-    const int ipagC = ievt / neppC; // #event "C-page"
-    const int ieppC = ievt % neppC; // #event in the current event C-page
-    constexpr int idcoup = 0;
     constexpr int ix2 = 0;
-    return &( buffer[ipagC * ndcoup * nx2 * neppC + idcoup * nx2 * neppC + ix2 * neppC + ieppC] ); // AOSOA[ipagC][idcoup][ix2][ieppC]
+    const int ipagC = ievt / neppC;                                // #event "C-page"
+    const int ieppC = ievt % neppC;                                // #event in the current event C-page
+    return &( buffer[ipagC * nx2 * neppC + ix2 * neppC + ieppC] ); // AOSOA[ipagC][ix2][ieppC]
   }
 
   //--------------------------------------------------------------------------
 
   // Locate a field (output) of an event record (input) from the given field indexes (input)
   // [Signature (non-const) ===> fptype& decodeRecord( fptype* buffer, Ts... args ) <===]
-  // [NB: expand variadic template "Ts... args" to "const int idcoup, const int ix2" and rename "Field" as "IdcoupIx2"]
+  // [NB: expand variadic template "Ts... args" to "const int ix2" and rename "Field" as "Ix2"]
   static __host__ __device__ inline fptype&
   decodeRecord( fptype* buffer,
-                const int idcoup,
                 const int ix2 )
   {
     constexpr int ipagC = 0;
     constexpr int ieppC = 0;
-    return buffer[ipagC * ndcoup * nx2 * neppC + idcoup * nx2 * neppC + ix2 * neppC + ieppC]; // AOSOA[ipagC][idcoup][ix2][ieppC]
+    return buffer[ipagC * nx2 * neppC + ix2 * neppC + ieppC]; // AOSOA[ipagC][ix2][ieppC]
   }
 };
 
@@ -87,23 +82,23 @@ public:
   static constexpr auto ieventAccessRecordConst = MemoryAccessHelper<MemoryAccessCouplingsBase>::ieventAccessRecordConst;
 
   // Locate a field (output) of an event record (input) from the given field indexes (input)
-  // [Signature (non-const) ===> fptype& decodeRecord( fptype* buffer, const int idcoup, const int ix2 ) <===]
-  static constexpr auto decodeRecordIdcoupIx2 = MemoryAccessHelper<MemoryAccessCouplingsBase>::decodeRecord;
+  // [Signature (non-const) ===> fptype& decodeRecord( fptype* buffer, const int ix2 ) <===]
+  static constexpr auto decodeRecordIx2 = MemoryAccessHelper<MemoryAccessCouplingsBase>::decodeRecord;
 
   // Locate a field (output) of an event record (input) from the given field indexes (input)
-  // [Signature (const) ===> const fptype& decodeRecordConst( const fptype* buffer, const int idcoup, const int ix2 ) <===]
-  static constexpr auto decodeRecordIdcoupIx2Const =
-    MemoryAccessHelper<MemoryAccessCouplingsBase>::template decodeRecordConst<int, int>;
+  // [Signature (const) ===> const fptype& decodeRecordConst( const fptype* buffer, const int ix2 ) <===]
+  static constexpr auto decodeRecordIx2Const =
+    MemoryAccessHelper<MemoryAccessCouplingsBase>::template decodeRecordConst<int>;
 
   // Locate a field (output) in a memory buffer (input) from the given event number (input) and the given field indexes (input)
-  // [Signature (non-const) ===> fptype& ieventAccessIdcoupIx2( fptype* buffer, const ievt, const int idcoup, const int ix2 ) <===]
-  static constexpr auto ieventAccessIdcoupIx2 =
-    MemoryAccessHelper<MemoryAccessCouplingsBase>::template ieventAccessField<int, int>;
+  // [Signature (non-const) ===> fptype& ieventAccessIx2( fptype* buffer, const ievt, const int ix2 ) <===]
+  static constexpr auto ieventAccessIx2 =
+    MemoryAccessHelper<MemoryAccessCouplingsBase>::template ieventAccessField<int>;
 
   // Locate a field (output) in a memory buffer (input) from the given event number (input) and the given field indexes (input)
-  // [Signature (const) ===> const fptype& ieventAccessIdcoupIx2Const( const fptype* buffer, const ievt, const int idcoup, const int ix2 ) <===]
-  static constexpr auto ieventAccessIdcoupIx2Const =
-    MemoryAccessHelper<MemoryAccessCouplingsBase>::template ieventAccessFieldConst<int, int>;
+  // [Signature (const) ===> const fptype& ieventAccessIx2Const( const fptype* buffer, const ievt, const int ix2 ) <===]
+  static constexpr auto ieventAccessIx2Const =
+    MemoryAccessHelper<MemoryAccessCouplingsBase>::template ieventAccessFieldConst<int>;
 };
 
 //----------------------------------------------------------------------------
@@ -114,24 +109,24 @@ template<bool onDevice>
 class KernelAccessCouplings
 {
 public:
-  // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
-  // [Signature (non-const, SCALAR) ===> fptype& kernelAccessIdcoupIx2( fptype* buffer, const int idcoup, const int ix2 ) <===]
-  static constexpr auto kernelAccessIdcoupIx2_s =
-    KernelAccessHelper<MemoryAccessCouplingsBase, onDevice>::template kernelAccessField<int, int>;
 
   // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
-  // [Signature (const, SCALAR) ===> const fptype& kernelAccessIdcoupIx2Const( const fptype* buffer, const int idcoup, const int ix2 ) <===]
-  static constexpr auto kernelAccessIdcoupIx2Const_s =
-    KernelAccessHelper<MemoryAccessCouplingsBase, onDevice>::template kernelAccessFieldConst<int, int>;
+  // [Signature (non-const, SCALAR) ===> fptype& kernelAccessIx2( fptype* buffer, const int ix2 ) <===]
+  static constexpr auto kernelAccessIx2_s =
+    KernelAccessHelper<MemoryAccessCouplingsBase, onDevice>::template kernelAccessField<int>;
 
   // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
-  // [Signature (non const, SCALAR OR VECTOR) ===> fptype_sv& kernelAccessIdcoupIx2( fptype* buffer, const int idcoup, const int ix2 ) <===]
+  // [Signature (const, SCALAR) ===> const fptype& kernelAccessIx2Const( const fptype* buffer, const int ix2 ) <===]
+  static constexpr auto kernelAccessIx2Const_s =
+    KernelAccessHelper<MemoryAccessCouplingsBase, onDevice>::template kernelAccessFieldConst<int>;
+
+  // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
+  // [Signature (non const, SCALAR OR VECTOR) ===> fptype_sv& kernelAccessIx2( fptype* buffer, const int ix2 ) <===]
   static __host__ __device__ inline fptype_sv&
-  kernelAccessIdcoupIx2( fptype* buffer,
-                         const int idcoup,
-                         const int ix2 )
+  kernelAccessIx2( fptype* buffer,
+                   const int ix2 )
   {
-    fptype& out = kernelAccessIdcoupIx2_s( buffer, idcoup, ix2 );
+    fptype& out = kernelAccessIx2_s( buffer, ix2 );
 #ifndef MGONGPU_CPPSIMD
     return out;
 #else
@@ -145,38 +140,66 @@ public:
 #endif
   }
 
-  // [Signature (const, SCALAR OR VECTOR) ===> const fptype_sv& kernelAccessIdcoupIx2( const fptype* buffer, const int idcoup, const int ix2 ) <===]
+  // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
+  // [Signature (const, SCALAR OR VECTOR) ===> const fptype_sv& kernelAccessIx2Const( const fptype* buffer, const int ix2 ) <===]
   static __host__ __device__ inline const fptype_sv&
-  kernelAccessIdcoupIx2Const( const fptype* buffer,
-                              const int idcoup,
-                              const int ix2 )
+  kernelAccessIx2Const( const fptype* buffer,
+                        const int ix2 )
   {
-    return kernelAccessIdcoupIx2( const_cast<fptype*>( buffer ), idcoup, ix2 );
+    return kernelAccessIx2( const_cast<fptype*>( buffer ), ix2 );
   }
 
+  /*
   // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
-  // [Signature (non const, SCALAR OR VECTOR) ===> cxtype_sv_ref kernelAccessIdcoup( fptype* buffer, const int idcoup ) <===]
+  // [Signature (const, SCALAR OR VECTOR) ===> const fptype_sv& kernelAccessIx2Const( const fptype* buffer, const int ix2 ) <===]
+  static __host__ __device__ inline const fptype_sv&
+  kernelAccessIx2Const( const fptype* buffer,
+                        const int ix2 )
+  {
+    const fptype& out = kernelAccessIx2Const_s( buffer, ix2 );
+#ifndef MGONGPU_CPPSIMD
+    return out;
+#else
+    // NB: derived from MemoryAccessMomenta, restricting the implementation to contiguous aligned arrays
+    constexpr int neppC = MemoryAccessCouplingsBase::neppC;
+    static_assert( neppC >= neppV ); // ASSUME CONTIGUOUS ARRAYS
+    static_assert( neppC % neppV == 0 ); // ASSUME CONTIGUOUS ARRAYS
+    static_assert( mg5amcCpu::HostBufferCouplings::isaligned() ); // ASSUME ALIGNED ARRAYS (reinterpret_cast will segfault otherwise!)    
+    //assert( (size_t)( buffer ) % mgOnGpu::cppAlign == 0 ); // ASSUME ALIGNED ARRAYS (reinterpret_cast will segfault otherwise!)
+    return mg5amcCpu::fptypevFromAlignedArray( out ); // SIMD bulk load of neppV, use reinterpret_cast
+#endif
+  }
+  */
+
+  // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
+  // [Signature (non const, SCALAR OR VECTOR) ===> cxtype_sv_ref kernelAccess( fptype* buffer ) <===]
   static __host__ __device__ inline cxtype_sv_ref
-  kernelAccessIdcoup( fptype* buffer,
-                      const int idcoup )
+  kernelAccess( fptype* buffer )
   {
-    fptype_sv& real = kernelAccessIdcoupIx2( buffer, idcoup, 0 );
-    fptype_sv& imag = kernelAccessIdcoupIx2( buffer, idcoup, 1 );
-    //printf( "C_ACCESS::kernelAccessIdcoup: pbuffer=%p idcoup=%d pr=%p pi=%p\n", buffer, idcoup, &real, &imag );
+    /*
+    fptype_sv& real = kernelAccessIx2( buffer, 0 );
+    fptype_sv& imag = kernelAccessIx2( buffer, 1 );
+    printf( "C_ACCESS::kernelAccess: pbuffer=%p pr=%p pi=%p\n", buffer, &real, &imag );
     return cxtype_sv_ref( real, imag );
+    */
+    return cxtype_sv_ref( kernelAccessIx2( buffer, 0 ),
+                          kernelAccessIx2( buffer, 1 ) );
   }
 
   // Locate a field (output) in a memory buffer (input) from a kernel event-indexing mechanism (internal) and the given field indexes (input)
-  // [Signature (const, SCALAR OR VECTOR) ===> cxtype_sv kernelAccessIdcoupConst( const fptype* buffer, const int idcoup ) <===]
+  // [Signature (const, SCALAR OR VECTOR) ===> cxtype_sv kernelAccessConst( const fptype* buffer ) <===]
   static __host__ __device__ inline cxtype_sv
-  kernelAccessIdcoupConst( const fptype* buffer,
-                           const int idcoup )
+  kernelAccessConst( const fptype* buffer )
   {
-    const fptype_sv& real = kernelAccessIdcoupIx2Const( buffer, idcoup, 0 );
-    const fptype_sv& imag = kernelAccessIdcoupIx2Const( buffer, idcoup, 1 );
-    //printf( "C_ACCESS::kernelAccessIdcoupConst: pbuffer=%p idcoup=%d pr=%p pi=%p\n", buffer, idcoup, &real, &imag );
-    return cxtype_sv_ref( real, imag );
-  }  
+    /*
+    const fptype_sv& real = kernelAccessIx2Const( buffer, 0 );
+    const fptype_sv& imag = kernelAccessIx2Const( buffer, 1 );
+    printf( "C_ACCESS::kernelAccessConst: pbuffer=%p pr=%p pi=%p\n", buffer, &real, &imag );
+    return cxtype_sv( real, imag );
+    */
+    return cxtype_sv( kernelAccessIx2Const( buffer, 0 ),
+                      kernelAccessIx2Const( buffer, 1 ) );
+  }
 };
 
 //----------------------------------------------------------------------------
