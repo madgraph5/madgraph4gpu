007e3296cc5d0411585ae8efdaaa3e2e3995e91d
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-26 17:51:31 +0200

    [alphas] remove optional debug code and improve some comments before codegen in HelAmps

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/aloha/template_files/gpu/helas.cu
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/aloha/template_files/gpu/helas.cu
@@ -1,7 +1,7 @@
 
   //--------------------------------------------------------------------------
 
-  // Compute the output couplings gc10 and gc11 from the input gs
+  // Compute the output couplings (e.g. gc10 and gc11) from the input gs
   template<class G_ACCESS, class C_ACCESS>
   __device__ INLINE void
   G2COUP( const fptype gs[],
