56f00f77d8f897358289a25efd9b24f1527e2e1e
Andrea Valassi <andrea.valassi@cern.ch>
2022-04-27 15:09:36 +0200

    [alphas] (for eemumu) in MemoryAccessCouplingsFixed.h add iicoupAccessBufferConst

--- epochX/cudacpp/ee_mumu/SubProcesses/MemoryAccessCouplingsFixed.h
+++ epochX/cudacpp/ee_mumu/SubProcesses/MemoryAccessCouplingsFixed.h
@@ -9,12 +9,43 @@
 
 //----------------------------------------------------------------------------
 
+// A class describing the internal layout of memory buffers for fixed couplings
+// This implementation uses a STRUCT[ndcoup][nx2] "super-buffer" layout: in practice, the cIPC global array
+// From the "super-buffer" for ndcoup different couplings, use idcoupAccessBuffer to access the buffer for one specific coupling
+// [If many implementations are used, a suffix _Sv1 should be appended to the class name]
+class MemoryAccessCouplingsFixedBase //_Sv1
+{
+public:
+
+  // Locate the buffer for a single coupling (output) in a memory super-buffer (input) from the given coupling index (input)
+  // [Signature (const) ===> const fptype* iicoupAccessBufferConst( const fptype* buffer, const int iicoup ) <===]
+  static __host__ __device__ inline const fptype*
+  iicoupAccessBufferConst( const fptype* buffer, // input "super-buffer": in practice, the cIPC global array
+                           const int iicoup )
+  {
+    constexpr int ix2 = 0;
+    // NB! this effectively adds an offset "iicoup * nx2"
+    return &( buffer[iicoup * nx2 + ix2] ); // STRUCT[idcoup][ix2]
+  }
+
+private:
+
+  // The number of floating point components of a complex number
+  static constexpr int nx2 = mgOnGpu::nx2;
+};
+
+//----------------------------------------------------------------------------
+
 // A class providing access to memory buffers for a given event, based on implicit kernel rules
 // Its methods use the KernelAccessHelper template - note the use of the template keyword in template function instantiations
 template<bool onDevice>
 class KernelAccessCouplingsFixed
 {
 public:
+
+  // Expose selected functions from MemoryAccessCouplingsFixedBase
+  static constexpr auto iicoupAccessBufferConst = MemoryAccessCouplingsFixedBase::iicoupAccessBufferConst;
+
   // TRIVIAL ACCESS to fixed-couplings buffers
   static __host__ __device__ inline cxtype*
   kernelAccess( fptype* buffer )
