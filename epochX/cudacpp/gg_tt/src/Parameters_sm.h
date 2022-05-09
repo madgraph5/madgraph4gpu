205c0c7aa9e74b59728e7dc4a8cda04243947b9c
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-09 15:26:22 +0200

    [heft] regenerate 5 processes auto and resync manu (finally? fix mdl_complexi)

--- epochX/cudacpp/gg_tt/src/Parameters_sm.h
+++ epochX/cudacpp/gg_tt/src/Parameters_sm.h
@@ -213,8 +213,8 @@ namespace Parameters_sm_dependentCouplings
 #ifdef MGONGPU_HARDCODE_PARAM
     using namespace Parameters_sm;
 #endif
-    // NB: hardcode cxtype cI(0,1) instead of cxsmpl mdl_complexi (which exists in Parameters_heft) because:
-    // (1) mdl_complexi is always (0,1); (2) mdl_complexi is undefined in device code; (3) need cxsmpl conversion to cxtype in the following code
+    // NB: hardcode cxtype cI(0,1) instead of cxtype (or hardcoded cxsmpl) mdl_complexi (which exists in Parameters_sm) because:
+    // (1) mdl_complexi is always (0,1); (2) mdl_complexi is undefined in device code; (3) need cxsmpl conversion to cxtype in code below
     const cxtype cI( 0., 1. );
     DependentCouplings_sv out;
     // Begin SM implementation - no special handling of vectors of floats as in EFT (#439)
