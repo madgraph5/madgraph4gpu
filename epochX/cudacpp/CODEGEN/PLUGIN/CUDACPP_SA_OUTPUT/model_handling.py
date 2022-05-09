840bf139c0611487d3db5bb4f15164b9f87dc242
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-09 09:13:12 +0200

    [heft] improve handling of empty spaces in dcoupsetdcoup codegen (more uniform)

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/model_handling.py
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/model_handling.py
@@ -786,7 +786,7 @@ class PLUGIN_UFOModelConverter(PLUGIN_export_cpp.UFOModelConverterGPU):
                     if 'constexpr double G =' in line: foundG = True
             replace_dict['dcoupsetdpar'] = '  ' + '\n'.join( dcoupsetdpar )
             dcoupsetdcoup = [ '    ' + line.replace('constexpr cxsmpl<double> ','out.').replace('mdl_complexi', 'cxmake( 0., 1. )') for line in self.write_hardcoded_parameters(list(self.coups_dep.values())).split('\n') if line != '' ]
-            replace_dict['dcoupsetdcoup'] = '    // FIXME? should this use a model-dependent mdl_complexi instead of a hardcoded cxmake(0,1)?\n  ' + '\n'.join( dcoupsetdcoup )
+            replace_dict['dcoupsetdcoup'] = '      // FIXME? should this use a model-dependent mdl_complexi instead of a hardcoded cxmake(0,1)?\n  ' + '\n'.join( dcoupsetdcoup )
             dcoupaccessbuffer = [ '    fptype* %ss = C_ACCESS::idcoupAccessBuffer( couplings, idcoup_%s );'%( name, name ) for name in self.coups_dep ]
             replace_dict['dcoupaccessbuffer'] = '\n'.join( dcoupaccessbuffer ) + '\n'
             dcoupkernelaccess = [ '    cxtype_sv_ref %ss_sv = C_ACCESS::kernelAccess( %ss );'%( name, name ) for name in self.coups_dep ]
@@ -795,23 +795,23 @@ class PLUGIN_UFOModelConverter(PLUGIN_export_cpp.UFOModelConverterGPU):
             replace_dict['dcoupcompute'] = '\n'.join( dcoupcompute )
             # Special handling for fptype=float using SIMD
             dcoupoutdcoup2 = [ '      fptype& %sr = cxreal( out.%s )[i];\n      fptype& %si = cximag( out.%s )[i];'%(name,name,name,name) for name in self.coups_dep ]
-            replace_dict['dcoupoutdcoup2'] = '\n'.join( dcoupoutdcoup2 )
+            replace_dict['dcoupoutdcoup2'] = '\n' + '\n'.join( dcoupoutdcoup2 )
             replace_dict['dcoupsetdpar2'] = replace_dict['dcoupsetdpar'].replace('fptype_sv','fptype')
             dcoupsetdcoup2 = [ '    ' + line.replace('constexpr cxsmpl<double> ','const cxtype ').replace('mdl_complexi', 'cxmake( 0., 1. )') for line in self.write_hardcoded_parameters(list(self.coups_dep.values())).split('\n') if line != '' ]
             dcoupsetdcoup2 += [ '      %sr = cxreal( %s );\n      %si = cximag( %s );'%(name,name,name,name) for name in self.coups_dep ]
-            replace_dict['dcoupsetdcoup2'] = '    // FIXME? should this use a model-dependent mdl_complexi instead of a hardcoded cxmake(0,1)?\n  ' + '\n'.join( dcoupsetdcoup2 )
+            replace_dict['dcoupsetdcoup2'] = '      // FIXME? should this use a model-dependent mdl_complexi instead of a hardcoded cxmake(0,1)?\n  ' + '\n'.join( dcoupsetdcoup2 )
         else:
             replace_dict['idcoup'] = '  // NB: there are no aS-dependent couplings in this physics process'
             replace_dict['dcoupdecl'] = '    // (none)'
             replace_dict['dcoupsetdpar'] = '      // (none)'
-            replace_dict['dcoupsetdcoup'] = '    // (none)'
+            replace_dict['dcoupsetdcoup'] = '      // (none)'
             replace_dict['dcoupaccessbuffer'] = ''
             replace_dict['dcoupkernelaccess'] = ''
             replace_dict['dcoupcompute'] = '    // NB: there are no aS-dependent couplings in this physics process'
             # Special handling for fptype=float using SIMD
-            replace_dict['dcoupoutdcoup2'] = '  ' + replace_dict['dcoupsetdcoup']
+            replace_dict['dcoupoutdcoup2'] = ''
             replace_dict['dcoupsetdpar2'] = '      // (none)'
-            replace_dict['dcoupsetdcoup2'] = '    // (none)'
+            replace_dict['dcoupsetdcoup2'] = '      // (none)'
         file_h = self.read_template_file(self.param_template_h) % \
                  replace_dict
         file_cc = self.read_template_file(self.param_template_cc) % \
