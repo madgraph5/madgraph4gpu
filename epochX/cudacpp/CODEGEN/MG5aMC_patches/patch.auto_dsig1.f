26a7f67ac61bc7b4fe724f9460feb9bdef61d0a2
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-11 14:51:47 +0200

    [alphas] backport to codegen also the fix for running alphas in auto_dsig1.f #441
    git diff --no-ext-diff 57107a86 gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f > CODEGEN/MG5aMC_patches/patch.auto_dsig1.f

--- epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.auto_dsig1.f
+++ epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.auto_dsig1.f
@@ -1,14 +1,15 @@
 diff --git a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f
-index 3aa1040b..751b86f1 100644
+index 3aa1040b..f69ef9a5 100644
 --- a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f
 +++ b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/auto_dsig1.f
-@@ -454,10 +454,15 @@ C
+@@ -454,10 +454,16 @@ C
        DOUBLE PRECISION JAMP2_MULTI(0:MAXFLOW, NB_PAGE)
  
        INTEGER IVEC
 +      INTEGER IEXT
  
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
++      INCLUDE 'coupl.inc'
 +      INCLUDE 'fbridge.inc'
 +      DOUBLE PRECISION OUT2(NB_PAGE)
 +#endif
@@ -20,7 +21,7 @@ index 3aa1040b..751b86f1 100644
        DO IVEC=1, NB_PAGE
          CALL SMATRIX1(P_MULTI(0,1,IVEC),
       &	                         hel_rand(IVEC),
-@@ -468,8 +473,21 @@ C       &				 selected_hel(IVEC),
+@@ -468,8 +474,21 @@ C       &				 selected_hel(IVEC),
       &				 IVEC
       &				 )
        ENDDO
@@ -30,7 +31,7 @@ index 3aa1040b..751b86f1 100644
 +c!$OMP END PARALLEL
 +
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
-+      CALL FBRIDGESEQUENCE(MEEXPORTER_PBRIDGE, P_MULTI, OUT2)
++      CALL FBRIDGESEQUENCE(MEEXPORTER_PBRIDGE, P_MULTI, ALL_G, OUT2)
 +      DO IVEC=1, NB_PAGE
 +c       DO IEXT=1, NEXTERNAL
 +c         WRITE (*,*) P_MULTI(0,IEXT,IVEC), P_MULTI(1,IEXT,IVEC),
