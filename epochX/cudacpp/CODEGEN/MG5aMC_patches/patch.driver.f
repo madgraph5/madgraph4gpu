d09bea6bca038e0e4f69f50ab6cd0320de94f32a
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-12 15:34:47 +0200

    [fvsc] backport driver.f changes to codegen
    git diff --no-ext-diff e5a2db8228227f2aff gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f > CODEGEN/MG5aMC_patches/patch.driver.f

--- epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.driver.f
+++ epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.driver.f
@@ -1,8 +1,8 @@
 diff --git a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
-index 91e1f5b4..9daa40c8 100644
+index 91e1f5b4..4cfdb381 100644
 --- a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
 +++ b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
-@@ -73,11 +73,20 @@ c      common/to_colstats/ncols,ncolflow,ncolalt,ic
+@@ -73,11 +73,23 @@ c      common/to_colstats/ncols,ncolflow,ncolalt,ic
  
        include 'coupl.inc'
  
@@ -19,11 +19,14 @@ index 91e1f5b4..9daa40c8 100644
 +      CALL COUNTERS_INITIALISE()
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
 +      CALL FBRIDGECREATE(MEEXPORTER_PBRIDGE, NB_PAGE, NEXTERNAL, 4) ! this must be at the beginning as it initialises the CUDA device
++      MEEXPORTER_MODE = -1 ! (CppOnly=1, FortranOnly=0, BothQuiet=-1, BothDebug=-2)
++      MEEXPORTER_CBYFMAX = 0
++      MEEXPORTER_CBYFMIN = 1D100
 +#endif
  c
  c     Read process number
  c
-@@ -199,8 +208,12 @@ c      call sample_result(xsec,xerr)
+@@ -199,8 +211,18 @@ c      call sample_result(xsec,xerr)
  c      write(*,*) 'Final xsec: ',xsec
  
        rewind(lun)
@@ -32,6 +35,12 @@ index 91e1f5b4..9daa40c8 100644
 +
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
 +      CALL FBRIDGEDELETE(MEEXPORTER_PBRIDGE) ! this must be at the end as it shuts down the CUDA device
++      IF( MEEXPORTER_MODE .LE. -1 ) THEN ! (BothQuiet=-1 or BothDebug=-2)
++        WRITE(*,*) 'ME ratio CudaCpp/Fortran: MIN = ', MEEXPORTER_CBYFMIN
++        WRITE(*,*) 'ME ratio CudaCpp/Fortran: MAX = ', MEEXPORTER_CBYFMAX
++        WRITE(*,*) 'ME ratio CudaCpp/Fortran: 1-MIN = ', 1-MEEXPORTER_CBYFMIN
++        WRITE(*,*) 'ME ratio CudaCpp/Fortran: MAX-1 = ', MEEXPORTER_CBYFMAX-1
++      ENDIF
 +#endif
 +      CALL COUNTERS_FINALISE()
        end
