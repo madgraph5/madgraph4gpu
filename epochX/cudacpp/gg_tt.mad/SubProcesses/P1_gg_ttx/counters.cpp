f3bb5de1f212b0fae313a937b2a6794128cab047
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-12 15:00:19 +0200

    [fvsc] remove COUNTERS_USETIMER to simplify the code

--- epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/counters.cpp
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/counters.cpp
@@ -4,9 +4,6 @@
 #include <cassert>
 #include <cstdio>
 
-#undef COUNTERS_USETIMER
-#define COUNTERS_USETIMER 1 // comment out to disable timers and keep only counters
-
 // NB1: The C functions counters_xxx_ in this file are called by Fortran code
 // Hence the trailing "_": 'call counters_end()' links to counters_end_
 // See http://www.yolinux.com/TUTORIALS/LinuxTutorialMixingFortranAndC.html
@@ -42,7 +39,6 @@ extern "C"
     }
   }
 
-#ifdef COUNTERS_USETIMER
   static mgOnGpu::Timer<TIMERTYPE> program_timer;
   static float program_totaltime = 0;
   static mgOnGpu::Timer<TIMERTYPE> matrix1_timer;
@@ -51,50 +47,39 @@ extern "C"
   static float smatrix1_totaltime = 0;
   static mgOnGpu::Timer<TIMERTYPE> smatrix1multi_timer[nimplC];
   static float smatrix1multi_totaltime[nimplC] = { 0 };
-#endif
   static int matrix1_counter = 0;
   static int smatrix1_counter = 0;
   static int smatrix1multi_counter[nimplC] = { 0 };
 
   void counters_initialise_()
   {
-#ifdef COUNTERS_USETIMER
     program_timer.Start();
-#endif
     return;
   }
 
   void counters_matrix1_start_()
   {
     matrix1_counter++;
-#ifdef COUNTERS_USETIMER
     matrix1_timer.Start();
-#endif
     return;
   }
 
   void counters_matrix1_stop_()
   {
-#ifdef COUNTERS_USETIMER
     matrix1_totaltime += matrix1_timer.GetDuration();
-#endif
     return;
   }
 
   void counters_smatrix1_start_()
   {
     smatrix1_counter++;
-#ifdef COUNTERS_USETIMER
     smatrix1_timer.Start();
-#endif
     return;
   }
 
   void counters_smatrix1_stop_()
   {
-#ifdef COUNTERS_USETIMER
     smatrix1_totaltime += smatrix1_timer.GetDuration();
-#endif
     return;
   }
 
@@ -102,18 +87,14 @@ extern "C"
   {
     const unsigned int iimplC = iimplF2C( *iimplF );
     smatrix1multi_counter[iimplC] += *pnevt;
-#ifdef COUNTERS_USETIMER
     smatrix1multi_timer[iimplC].Start();
-#endif
     return;
   }
 
   void counters_smatrix1multi_stop_( const int* iimplF )
   {
     const unsigned int iimplC = iimplF2C( *iimplF );
-#ifdef COUNTERS_USETIMER
     smatrix1multi_totaltime[iimplC] += smatrix1multi_timer[iimplC].GetDuration();
-#endif
     return;
   }
 
@@ -122,7 +103,6 @@ extern "C"
     // Write to file
     FILE* f;
     f = fopen( "counters_log.txt", "w" );
-#ifdef COUNTERS_USETIMER
     program_totaltime += program_timer.GetDuration();
     fprintf( f, "PROGRAM         : %9.4fs\n", program_totaltime );
     //fprintf( f, "MATRIX1(a)      : %9.4fs for %8d MATRIX1 calls  => throughput is %8.2E calls/s\n", matrix1_totaltime, matrix1_counter, matrix1_counter / matrix1_totaltime );
@@ -131,14 +111,8 @@ extern "C"
     for ( unsigned int iimplC=0; iimplC<nimplC; iimplC++ )
       if ( smatrix1multi_counter[iimplC] > 0 )
         fprintf( f, "%15s : %9.4fs for %8d %7s events => throughput is %8.2E events/s\n", iimplC2FUN( iimplC ), smatrix1multi_totaltime[iimplC], smatrix1multi_counter[iimplC], iimplC2TXT( iimplC ), smatrix1multi_counter[iimplC] / smatrix1multi_totaltime[iimplC] );
-#else
-    //fprintf( f, "MATRIX1         %7s : %8d calls\n", "", matrix1_counter );
-    //fprintf( f, "SMATRIX1        %7s : %8d calls\n", "", smatrix1_counter );
-    fprintf( f, "%15s %7s : %8d events\n", iimplC2FUN( iimplC ), iimplC2TXT( iimplC ), smatrix1multi_counter[iimplC] );
-#endif
     fclose( f );
     // Write to stdout
-#ifdef COUNTERS_USETIMER
     printf( "PROGRAM         : %9.4fs\n", program_totaltime );
     //printf( "MATRIX1(a)      : %9.4fs for %8d MATRIX1 calls  => throughput is %8.2E calls/s\n", matrix1_totaltime, matrix1_counter, matrix1_counter / matrix1_totaltime );
     //printf( "MATRIX1(b)      : %9.4fs for %8d SMATRIX1 calls => throughput is %8.2E calls/s\n", matrix1_totaltime, smatrix1_counter, smatrix1_counter / matrix1_totaltime );
@@ -146,11 +120,6 @@ extern "C"
     for ( unsigned int iimplC=0; iimplC<nimplC; iimplC++ )
       if ( smatrix1multi_counter[iimplC] > 0 )
         printf( "%15s : %9.4fs for %8d %7s events => throughput is %8.2E events/s\n", iimplC2FUN( iimplC ), smatrix1multi_totaltime[iimplC], smatrix1multi_counter[iimplC], iimplC2TXT( iimplC ), smatrix1multi_counter[iimplC] / smatrix1multi_totaltime[iimplC] );
-#else
-    printf( "MATRIX1         %7s : %8d calls\n", "", matrix1_counter );
-    printf( "SMATRIX1        %7s : %8d calls\n", "", smatrix1_counter );
-    printf( "%15s %7s : %8d events\n", iimplC2FUN( iimplC ), iimplC2TXT( iimplC ), smatrix1multi_counter[iimplC] );
-#endif
     return;
   }
 }
