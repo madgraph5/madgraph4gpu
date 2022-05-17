4b905331e734c4b40a1a2f6a13d86ee20f1e92d5
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-17 11:17:51 +0200

    [fvsc] backport to driver.f codegen
    git diff --no-ext-diff e5a2db8228227f2aff gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f > CODEGEN/MG5aMC_patches/patch.driver.f

--- epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.driver.f
+++ epochX/cudacpp/CODEGEN/MG5aMC_patches/patch.driver.f
@@ -1,14 +1,15 @@
 diff --git a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
-index 91e1f5b4..4cfdb381 100644
+index 91e1f5b4..5648f6b0 100644
 --- a/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
 +++ b/epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
-@@ -73,11 +73,23 @@ c      common/to_colstats/ncols,ncolflow,ncolalt,ic
+@@ -73,11 +73,27 @@ c      common/to_colstats/ncols,ncolflow,ncolalt,ic
  
        include 'coupl.inc'
  
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
 +      INCLUDE '../../Source/vector.inc'
 +      INCLUDE 'fbridge.inc'
++      INCLUDE 'fbridge_common.inc'
 +#endif
  C-----
  C  BEGIN CODE
@@ -18,15 +19,28 @@ index 91e1f5b4..4cfdb381 100644
 +
 +      CALL COUNTERS_INITIALISE()
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
-+      CALL FBRIDGECREATE(MEEXPORTER_PBRIDGE, NB_PAGE, NEXTERNAL, 4) ! this must be at the beginning as it initialises the CUDA device
-+      MEEXPORTER_MODE = -1 ! (CppOnly=1, FortranOnly=0, BothQuiet=-1, BothDebug=-2)
-+      MEEXPORTER_CBYFMAX = 0
-+      MEEXPORTER_CBYFMIN = 1D100
++      CALL FBRIDGECREATE(FBRIDGE_PBRIDGE, NB_PAGE, NEXTERNAL, 4) ! this must be at the beginning as it initialises the CUDA device
++      FBRIDGE_MODE = -1 ! (CppOnly=1, FortranOnly=0, BothQuiet=-1, BothDebug=-2)
++      FBRIDGE_NCBYF1 = 0
++      FBRIDGE_CBYF1SUM = 0
++      FBRIDGE_CBYF1SUM2 = 0
++      FBRIDGE_CBYF1MAX = -1D100
++      FBRIDGE_CBYF1MIN = 1D100
 +#endif
  c
  c     Read process number
  c
-@@ -199,8 +211,18 @@ c      call sample_result(xsec,xerr)
+@@ -132,7 +148,8 @@ c   If CKKW-type matching, read IS Sudakov grid
+           exit
+  30       issgridfile='../'//issgridfile
+           if(i.eq.5)then
+-            print *,'ERROR: No Sudakov grid file found in lib with ickkw=2'
++            print *,
++     &        'ERROR: No Sudakov grid file found in lib with ickkw=2'
+             stop
+           endif
+         enddo
+@@ -199,8 +216,33 @@ c      call sample_result(xsec,xerr)
  c      write(*,*) 'Final xsec: ',xsec
  
        rewind(lun)
@@ -34,12 +48,27 @@ index 91e1f5b4..4cfdb381 100644
        close(lun)
 +
 +#ifdef MG5AMC_MEEXPORTER_CUDACPP
-+      CALL FBRIDGEDELETE(MEEXPORTER_PBRIDGE) ! this must be at the end as it shuts down the CUDA device
-+      IF( MEEXPORTER_MODE .LE. -1 ) THEN ! (BothQuiet=-1 or BothDebug=-2)
-+        WRITE(*,*) 'ME ratio CudaCpp/Fortran: MIN = ', MEEXPORTER_CBYFMIN
-+        WRITE(*,*) 'ME ratio CudaCpp/Fortran: MAX = ', MEEXPORTER_CBYFMAX
-+        WRITE(*,*) 'ME ratio CudaCpp/Fortran: 1-MIN = ', 1-MEEXPORTER_CBYFMIN
-+        WRITE(*,*) 'ME ratio CudaCpp/Fortran: MAX-1 = ', MEEXPORTER_CBYFMAX-1
++      CALL FBRIDGEDELETE(FBRIDGE_PBRIDGE) ! this must be at the end as it shuts down the CUDA device
++      IF( FBRIDGE_MODE .LE. -1 ) THEN ! (BothQuiet=-1 or BothDebug=-2)
++        WRITE(*,'(a,f10.8,a,e8.2)')
++     &    ' [MERATIOS] ME ratio CudaCpp/Fortran: MIN = ',
++     &    FBRIDGE_CBYF1MIN + 1, ' = 1 - ', -FBRIDGE_CBYF1MIN
++        WRITE(*,'(a,f10.8,a,e8.2)')
++     &    ' [MERATIOS] ME ratio CudaCpp/Fortran: MAX = ',
++     &    FBRIDGE_CBYF1MAX + 1, ' = 1 + ', FBRIDGE_CBYF1MAX
++        WRITE(*,'(a,i6)')
++     &    ' [MERATIOS] ME ratio CudaCpp/Fortran: NENTRIES = ',
++     &    FBRIDGE_NCBYF1
++c        WRITE(*,'(a,e8.2)')
++c    &    ' [MERATIOS] ME ratio CudaCpp/Fortran - 1: AVG = ',
++c    &    FBRIDGE_CBYF1SUM / FBRIDGE_NCBYF1
++c       WRITE(*,'(a,e8.2)')
++c    &    ' [MERATIOS] ME ratio CudaCpp/Fortran - 1: STD = ',
++c    &    SQRT( FBRIDGE_CBYF1SUM2 / FBRIDGE_NCBYF1 ) ! ~standard deviation
++        WRITE(*,'(a,e8.2,a,e8.2)')
++     &    ' [MERATIOS] ME ratio CudaCpp/Fortran - 1: AVG = ',
++     &    FBRIDGE_CBYF1SUM / FBRIDGE_NCBYF1, ' +- ',
++     &    SQRT( FBRIDGE_CBYF1SUM2 ) / FBRIDGE_NCBYF1 ! ~standard error
 +      ENDIF
 +#endif
 +      CALL COUNTERS_FINALISE()
