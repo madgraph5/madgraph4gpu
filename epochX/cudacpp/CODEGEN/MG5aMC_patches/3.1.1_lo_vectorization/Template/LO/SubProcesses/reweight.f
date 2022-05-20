106994d17ec0c8e349d5ea246bc533e190c98450
Andrea Valassi <andrea.valassi@cern.ch>
2022-05-20 22:59:23 +0200

    [v311] move back to my patches over 1003 after testing
    Revert "[v311] temporarely revert my mg5amc patches to as-is 1003 (just to check if files changed in 1009...)"
    This reverts commit 53eb5352c3ca9a0b25e8ca64d12ebbafd74765ee.
    
    The test was useful that these files have changed
    
      ***************** Differences to the current bzr revno 1009 [START]
      === modified file 'Template/LO/SubProcesses/reweight.f'
      Files /tmp/avalassi/bzr-diff-CuKfME/old/Template/LO/SubProcesses/reweight.f and /data/avalassi/GPU2020/MG5aMC/3.1.1_lo_vectorization/Template/LO/SubProcesses/reweight.f differ
      === modified file 'models/template_files/fortran/makefile_madevent'
      Files /tmp/avalassi/bzr-diff-CuKfME/old/models/template_files/fortran/makefile_madevent and /data/avalassi/GPU2020/MG5aMC/3.1.1_lo_vectorization/models/template_files/fortran/makefile_madevent differ
      ***************** Differences to the current bzr revno 1009 [END]

--- epochX/cudacpp/CODEGEN/MG5aMC_patches/3.1.1_lo_vectorization/Template/LO/SubProcesses/reweight.f
+++ epochX/cudacpp/CODEGEN/MG5aMC_patches/3.1.1_lo_vectorization/Template/LO/SubProcesses/reweight.f
@@ -24,6 +24,7 @@ c**************************************************
       include 'nexternal.inc'
       include 'message.inc'
       include 'maxamps.inc'
+      include 'vector.inc'
       include 'cluster.inc'
       include 'sudakov.inc'
       include 'maxparticles.inc'
@@ -97,7 +98,8 @@ c**************************************************
       include 'message.inc'
       include 'nexternal.inc'
       include 'maxamps.inc'
-      include 'cluster.inc'      
+      include 'vector.inc'
+      include 'cluster.inc'
       integer ipdg,imode
       double precision q0, Q11
       double precision gamma,DGAUSS
@@ -562,9 +564,10 @@ c**************************************************
       include 'genps.inc'
       include 'nexternal.inc'
       include 'maxamps.inc'
+      include 'vector.inc'
       include 'cluster.inc'
-      include 'run.inc'
       include 'coupl.inc'
+      include 'run.inc'
       include 'run_config.inc'
 C   
 C   ARGUMENTS 
@@ -1315,9 +1318,10 @@ c**************************************************
       include 'genps.inc'
       include 'nexternal.inc'
       include 'maxamps.inc'
+      include 'vector.inc'
       include 'cluster.inc'
-      include 'run.inc'
       include 'coupl.inc'
+      include 'run.inc'
       include 'run_config.inc'
 C   
 C   ARGUMENTS 
@@ -1790,6 +1794,7 @@ C
       include 'genps.inc'
       include 'run.inc'
       include 'nexternal.inc'
+      include 'vector.inc'
       include 'coupl.inc'
 C      include 'maxparticles.inc'
       
@@ -1855,6 +1860,7 @@ c      ENDIF
 
       include 'nexternal.inc'
       include 'maxamps.inc'
+      include 'vector.inc'
       include 'cluster.inc'
 c     Common block for reweighting info
 c     q2bck holds the central q2fact scales
@@ -1920,6 +1926,7 @@ c     to_rw
 
       include 'nexternal.inc'
       include 'maxamps.inc'
+      include 'vector.inc'
       include 'cluster.inc'
 c     Common block for reweighting info
 c     q2bck holds the central q2fact scales
