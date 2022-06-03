17e110951a35daae1e68f0bc373e10c768629157
Andrea Valassi <andrea.valassi@cern.ch>
2022-06-03 11:18:10 +0200

    [mch] add a -short option to tmad/allTees.sh

--- epochX/cudacpp/tmad/allTees.sh
+++ epochX/cudacpp/tmad/allTees.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./tmad/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -ggttggg -makeclean
+if [ "$1" == "-short" ]; then
+  ./tmad/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -makeclean
+elif [ "$1" == "" ]; then
+  ./tmad/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -ggttggg -makeclean
+else
+  echo "Usage: $0 [-short]"
+  exit 1
+fi
