caa841e61517be9cee8622c377ce8d3184ed10b7
Andrea Valassi <andrea.valassi@cern.ch>
2022-06-15 14:21:09 +0200

    [fvsc] in ggttg mad, add debug printouts in addmothers

--- epochX/cudacpp/gg_ttg.mad/SubProcesses/addmothers.f
+++ epochX/cudacpp/gg_ttg.mad/SubProcesses/addmothers.f
@@ -91,6 +91,8 @@ c
       real ran1
       external pt,ran1,get_color,elim_indices,set_colmp,fix_tchannel_color
 
+      write(*,*) 'DEBUG add_mothers'
+
       if (first_time) then
          include 'props.inc'
          first_time=.false.
@@ -831,6 +833,8 @@ c
       integer is_colors(2,nincoming)
       integer i,j,i3,i3bar
 
+c     write(*,*) 'DEBUG elim_indices', n3, n3bar
+
 c     Successively eliminate color indices in pairs until only the wanted
 c     indices remain
       do i=1,ncolmp
@@ -895,6 +899,7 @@ c        We have a previous epsilon which gives the wrong pop-up index
      $           ires,icol,is_colors)
          else
 c           Don't know how to deal with this
+            write(*,*) 'DEBUG1001', i3, n3, i3bar, n3bar
             call write_error(1001,n3,n3bar)
          endif
       endif
