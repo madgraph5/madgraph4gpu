5c8d4f4375a2fce54e3d2ca70c68f3911f2ad8ca
Andrea Valassi <andrea.valassi@cern.ch>
2022-06-20 18:16:18 +0200

    [fvsc] further improve tmad summary table script

--- epochX/cudacpp/tmad/summaryTable.sh
+++ epochX/cudacpp/tmad/summaryTable.sh
@@ -20,14 +20,27 @@ function oneTable()
       ###/create events.lhe/{print $0}
       /create events.lhe/{par=$2; tag=tag1[par]} # current tag
       /create events.lhe/{fac=substr($5,2)} # current fac
-      /\[COUNTERS\]/{if($3=="TOTAL") typ=1; else if($3=="Overhead") typ=2; else if($3=="MEs") typ=3; else{print "ERROR! Unknown type $3"; exit 1};
+      /\[COUNTERS\]/{if($3=="TOTAL") typ=1; else if($3=="Overhead") typ=2; else if($3=="MEs") typ=3;
+                     else{print "ERROR! Unknown type $3"; exit 1};
                      if($4==":") sec=$5; else sec=$8; sec=substr(sec,1,length(sec)-1); # current sec
                      sec3[tag,fac,typ]=sec}
+      /\[COUNTERS\]/{if(tag!="" && $3=="MEs")
+                     {nevt=$10; ###print tag, nevt;
+                      if(tag=="FORTRAN") nevt1[fac]=nevt; 
+                      else if(tag=="CUDA/8192") nevt1b[fac]=nevt;
+                      else if(nevt1[fac]!=nevt){print "ERROR! nevt mismatch", nevt1[fac], nevt; exit 1}}}
       END{print lsep;
           printf "| %-9s | %-78s |\n", proc, "[sec] Total = Overhead (FORTRAN) + MEs (FORTRAN, CPP or CUDA)";
           print lsep;
           for (itag=1; itag<=ntag; itag++)
-          {tag=tags[itag]; printf "| %-9s |", tag;
+          {tag=tags[itag]; 
+           if(tag=="FORTRAN"){printf "| %-9s | %20s mad | %20s mad | %20s mad |\n",
+                              "", "x"facs[1], "x"facs[2], "x"facs[3];
+                              printf "| %-9s | %24s | %24s | %24s |\n",
+                              "", "["nevt1[facs[1]]"]", "["nevt1[facs[2]]"]", "["nevt1[facs[3]]"]"}
+           else if(tag=="CUDA/8192") printf "| %-9s | %24s | %24s | %24s |\n",
+                              "", "["nevt1b[facs[1]]"]", "["nevt1b[facs[2]]"]", "["nevt1b[facs[3]]"]";
+           printf "| %-9s |", tag;
            for(ifac=1; ifac<=nfac; ifac++)
            {fac=facs[ifac]; printf " %6.2f = %6.2f + %6.2f |", sec3[tag,fac,1], sec3[tag,fac,2], sec3[tag,fac,3]};
            printf "\n"};
@@ -40,12 +53,13 @@ function oneTable()
 cd $(dirname $0)/..
 echo PWD=$(pwd)
 
-procs="eemumu ggtt ggttg ggttgg ggttggg"
-#procs="ggttggg"
 suff=mad
 fpt=d
 inl=inl0
 hrd=hrd0
+
+procs="eemumu ggtt ggttg ggttgg ggttggg"
+###procs="ggttggg"
 
 out=tmad/summaryTable_default.txt
 oneTable
