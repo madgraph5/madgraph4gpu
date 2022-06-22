12d9f3510ab77deece3e42b96bd1672625b82454
Andrea Valassi <andrea.valassi@cern.ch>
2022-06-22 08:41:25 +0200

    [fvsc] further improvements in summary table script and result

--- epochX/cudacpp/tmad/summaryTable.sh
+++ epochX/cudacpp/tmad/summaryTable.sh
@@ -16,7 +16,7 @@ function oneTable()
             if(ntag!=npar){print "ERROR! ntag!=npar", ntag, npar; exit(1)};
             for(i=1;i<=ntag;i++){tag1[pars[i]]=tags[i]}}
       BEGIN{nfac=split(faclist,facs)}
-      BEGIN{lsep=sprintf("%0128d",0); gsub("0","-",lsep)}
+      BEGIN{lsep=sprintf("%0128d",0); lsep2=lsep; gsub("0","-",lsep); gsub("0","=",lsep2)}
       ###/create events.lhe/{print $0}
       /create events.lhe/{par=$2; tag=tag1[par]} # current tag
       /create events.lhe/{fac=substr($5,2)} # current fac
@@ -30,27 +30,27 @@ function oneTable()
                       else if(tag=="CUDA/max") nevt1b[fac]=nevt;
                       else if(nevt1[fac]!=nevt){print "ERROR! nevt mismatch", nevt1[fac], nevt; exit 1};
                       tputm1[tag]=tolower($15)}}
-      /CHECK/{if(tag!="" && $8=="--bridge"){sabp1[tag]=$5"*"$6"*"$7; sabg1[tag]=$5*$6*$7; gcheck="bridge"}}
-      /CHECK/{if(tag!="" && $8!="--bridge"){sap1[tag]=$5"*"$6"*"$7; sag1[tag]=$5*$6*$7; gcheck="nobridge"}}
+      /CHECK/{if(tag!="" && $8=="--bridge"){gcheck="bridge"; sabg1[tag]=$5*$6*$7; 
+                                            if($5=="2048") $5="2k"; sabp1[tag]=$5"*"$6"*"$7}}
+      /CHECK/{if(tag!="" && $8!="--bridge"){gcheck="nobridge"; sag1[tag]=$5*$6*$7; 
+                                            if($5=="2048") $5="2k"; sap1[tag]=$5"*"$6"*"$7}}
       /EvtsPerSec/{if(tag!="" && gcheck=="bridge"){tputb1[tag]=$5}}
       /EvtsPerSec/{if(tag!="" && gcheck!="bridge"){tput1[tag]=$5}}
-      END{print lsep;
+      END{print lsep2;
           printf "| %-10s | %-78s | %-30s |\n", proc, "[sec] Total = Overhead (FORTRAN) + MEs (FORTRAN, CPP or CUDA)", "[MEs/sec]"; 
           print lsep;
+          printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n", "", "mad", "mad", "mad", "sa/brdg", "sa/full";
+          print lsep2;
           for (itag=1; itag<=ntag; itag++)
           {tag=tags[itag]; 
            if(tag=="FORTRAN"){printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n",
-                              "", "mad", "mad", "mad", "sa/brdg", "sa/full";
-                              printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n",
                               "nevt/grid", "32", "32", "32", sabg1["CUDA/32"], sag1["CUDA/32"];
                               printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n",
                               "nevt total", "x"facs[1]" ["nevt1[facs[1]]"]", "x"facs[2]" ["nevt1[facs[2]]"]", "x"facs[3]" ["nevt1[facs[3]]"]",
                               sabp1["CUDA/32"], sap1["CUDA/32"];
                               print lsep}
            else if(tag=="CUDA/max"){
-                              print lsep;
-                              printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n",
-                              "", "mad", "mad", "mad", "sa/brdg", "sa/full";
+                              print lsep2;
                               printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n",
                               "nevt/grid", "8192", "8192", "8192", sabg1[tag], sag1[tag];
                               printf "| %-10s | %24s | %24s | %35s | %8s | %8s |\n",
@@ -64,8 +64,8 @@ function oneTable()
            if(tag=="FORTRAN"){ printf " %8s | %8s |", "---", "---"; }
            else{ printf " %8.2e |", tputb1[tag]; printf " %8.2e |", tput1[tag]; }
            printf "\n"};
-          print lsep;
-          print "";
+          print lsep2;
+          print "\n";
          }' >> $out
   done
 }
