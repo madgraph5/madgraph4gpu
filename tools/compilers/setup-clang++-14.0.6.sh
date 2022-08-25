3923a8a54cb1294e78f569b9eb160d6056196a99
Andrea Valassi <andrea.valassi@cern.ch>
2022-08-25 17:09:47 +0200

    [jorgen] attempt to dowgrade mg-lcg-clang++-14.0.6 to gcc 11.2 (from 12.1), fails because it was built with 12.1
    ccache /data/avalassi/GPU2020/madgraph4gpuX/tools/compilers/mg-lcg-clang++-14.0.6  -O3  -std=c++17 -I.  -fPIC -Wall -Wshadow -Wextra -ffast-math   -march=skylake-avx512 -mprefer-vector-width=256  -DMGONGPU_FPTYPE_DOUBLE -c read_slha.cc -o read_slha.o
    /cvmfs/sft.cern.ch/lcg/releases/clang/14.0.6-14bdb/x86_64-centos7/bin/clang++: /cvmfs/sft.cern.ch/lcg/releases/gcc/11.2.0-ad950/x86_64-centos7/lib64/libstdc++.so.6: version `GLIBCXX_3.4.30' not found (required by /cvmfs/sft.cern.ch/lcg/releases/clang/14.0.6-14bdb/x86_64-centos7/bin/clang++)

--- tools/compilers/setup-clang++-14.0.6.sh
+++ tools/compilers/setup-clang++-14.0.6.sh
@@ -1,7 +1,8 @@
 if [ "$BASH_SOURCE" = "$0" ]; then echo "ERROR! This script ($0) was not sourced"; exit 1; fi
 if [ "$BASH_SOURCE" = "" ]; then echo "ERROR! This script was not sourced from bash"; return 1; fi
 scrdir=$(cd $(dirname ${BASH_SOURCE}); pwd)
-. /cvmfs/sft.cern.ch/lcg/releases/gcc/12.1.0-57c96/x86_64-centos7/setup.sh 
+###. /cvmfs/sft.cern.ch/lcg/releases/gcc/12.1.0-57c96/x86_64-centos7/setup.sh 
+. /cvmfs/sft.cern.ch/lcg/releases/gcc/11.2.0-ad950/x86_64-centos7/setup.sh 
 export ALLOW_UNSUPPORTED_COMPILER_IN_CUDA=1
 export CC=$scrdir/mg-lcg-clang++-14.0.6
 export CXX=$scrdir/mg-lcg-clang++-14.0.6
