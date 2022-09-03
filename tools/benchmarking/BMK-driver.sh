6f2aff1b847a01a919ee927574297c8063eb0945
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-03 15:41:38 +0200

    [bmk] add BMK-driver config for itscrd70

--- tools/benchmarking/BMK-driver.sh
+++ tools/benchmarking/BMK-driver.sh
@@ -3,7 +3,7 @@
 ### TO KILL A TEST:
 ###kill $(ps -ef | egrep '(mg5amc-madgraph4gpu-2022-bmk.sh|throughputX.sh|check.exe)' | grep -v grep | awk '{print $2}')
 
-# For pmpe04
+# Node-specific configuration
 image=oras://registry.cern.ch/hep-workloads/mg5amc-madgraph4gpu-2022-bmk:v0.6
 if [ "$(hostname)" == "pmpe04.cern.ch" ]; then
   export SINGULARITY_CACHEDIR=/data/BMK2021/SINGULARITY_CACHEDIR
@@ -12,6 +12,13 @@ if [ "$(hostname)" == "pmpe04.cern.ch" ]; then
   tstDir=BMK-pmpe04
   mkdir -p ${tstDir}
   jts=""; for j in 1 2 4 8 16 32 48; do for t in 1; do if [ $((j*t)) -le 64 ]; then jts="$jts [$j,$t]"; fi; done; done 
+elif [ "$(hostname)" == "itscrd70.cern.ch" ]; then
+  export SINGULARITY_TMPDIR=/scratch/TMP_AVALASSI/
+  export SINGULARITY_CACHEDIR=/scratch/SINGULARITY_CACHEDIR
+  resDir=/scratch/TMP_RESULTS
+  tstDir=BMK-itscrd70
+  mkdir -p ${tstDir}
+  jts=""; for j in 1 2 4 6; do for t in 1; do if [ $((j*t)) -le 8 ]; then jts="$jts [$j,$t]"; fi; done; done 
 else
   echo "ERROR! Unknown host $(hostname)"; exit 1
 fi
