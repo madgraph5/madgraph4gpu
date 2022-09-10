ea0927a8fc73fa31e60b2ef6fe47e1929d8bf06a
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-11 00:14:44 +0200

    [bmk] add cpudriver config for juwels

--- tools/benchmarking/cpudriver.sh
+++ tools/benchmarking/cpudriver.sh
@@ -5,8 +5,13 @@
 
 startdate=$(date)
 
-# Node-specific configuration
+# Defaults for all nodes (may be overridden)
 image=oras://registry.cern.ch/hep-workloads/mg5amc-madgraph4gpu-2022-bmk:v0.6
+extraargs="--extra-args '-eemumu -ggtt -ggttg -ggttgg -dbl -flt -inl0 -inl1 --cpu'"
+###events=1 # NOT ENOUGH FOR GGTTG AND ALSO GGTT (BMK-1056)
+events=10
+
+# Node-specific configuration
 if [ "$(hostname)" == "pmpe04.cern.ch" ]; then
   export SINGULARITY_CACHEDIR=/data/BMK2021/SINGULARITY_CACHEDIR
   unset SINGULARITY_TMPDIR
@@ -19,20 +24,27 @@ elif [ "$(hostname)" == "itscrd70.cern.ch" ]; then
   resDir=/scratch/TMP_RESULTS
   tstDir=BMK-itscrd70
   jts=""; for j in 1 2 4 6; do for t in 1; do if [ $((j*t)) -le 8 ]; then jts="$jts [$j,$t]"; fi; done; done 
+elif [ "$(hostname)" == "jwlogin08.juwels" ]; then
+  export APPTAINER_TMPDIR=/p/scratch/prpb109/avalassi/TMP_AVALASSI/
+  export APPTAINER_CACHEDIR=/p/scratch/prpb109/avalassi/SINGULARITY_CACHEDIR
+  export SINGULARITY_CACHEDIR=${APPTAINER_CACHEDIR}
+  resDir=/p/scratch/prpb109/avalassi/TMP_RESULTS
+  tstDir=BMK-jwlogin08
+  jts=""; for j in 1 2 5 10 20 40 80; do for t in 1; do if [ $((j*t)) -le 80 ]; then jts="$jts [$j,$t]"; fi; done; done 
+  extraargs="--extra-args '-ggttgg -dbl -flt -inl0 --cpu'" # SHORTER TESTS ON JUWELS
+  events=1 # SHORTER TESTS ON JUWELS
 else
   echo "ERROR! Unknown host $(hostname)"; exit 1
 fi
 
 # Loop over tests
-###events=1 # NOT ENOUGH FOR GGTTG AND ALSO GGTT (BMK-1056)
-events=10
 for jt in $jts; do
   jt=${jt:1:-1}; t=${jt#*,}; j=${jt%,*}
   ###echo "jobs=$j, threads=$t"
   wDir="${tstDir}/sa-cpp$(printf '%s%03i%s%03i%s%03i' '-j' ${j} '-t' ${t} '-e' ${events})"
   ###echo ${resDir}/${wDir}
   \rm -rf ${resDir}/${wDir}
-  singularity run -B ${resDir}:/results ${image} --extra-args '-eemumu -ggtt -ggttg -ggttgg -dbl -flt -inl0 -inl1 --cpu' -c${j} -t${t} -e${events} -w /results/${wDir} -W
+  singularity run -B ${resDir}:/results ${image} ${extraargs} -c${j} -t${t} -e${events} -w /results/${wDir} -W
   ###ls -l ${resDir}/${wDir}
 done
 
