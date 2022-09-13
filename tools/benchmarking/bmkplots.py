bafb231a2ead5a45a5046a61868ce64312c84c8a
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-13 08:37:07 +0200

    [bmk] add a table (including overcommit) for the e0800 itscrd70 scan
    ./bmkplots.py > BMK-itscrd70-cuda/table-f-e0800.txt

--- tools/benchmarking/bmkplots.py
+++ tools/benchmarking/bmkplots.py
@@ -472,8 +472,8 @@ if __name__ == '__main__':
     #loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0100', debug=True )
     #dumpCudaScoresOneKey( loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0100' ), 'ggttgg-sa-cuda-d-inl0' )
     #dumpCudaScoresOneKey( loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0100' ), 'ggttgg-sa-cuda-f-inl0' )
-    dumpCudaScoresOneKey( loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0800' ), 'ggttgg-sa-cuda-d-inl0' )
-    #dumpCudaScoresOneKey( loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0800' ), 'ggttgg-sa-cuda-f-inl0' )
+    #dumpCudaScoresOneKey( loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0800' ), 'ggttgg-sa-cuda-d-inl0' )
+    dumpCudaScoresOneKey( loadCudaRunSet( 'BMK-itscrd70-cuda', evtmatch='-e0800' ), 'ggttgg-sa-cuda-f-inl0' )
 
     # PRODUCTION PLOTS (CUDA)
     #plotCudaST( 'BMK-itscrd70-cuda', score_key='ggttgg-sa-cuda-d-inl0', ylog=False, evtmatch='-e0100', debug=False )
