bbfc44b0a80d5aaac4fb42397851d2254b2d754f
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2022-09-21 07:29:25 +0000

    Add cuda architecture to raplab setup script.

--- raplab_setup.sh
+++ raplab_setup.sh
@@ -2,6 +2,8 @@
 
 module load gcc/9.3.0/cmake
 
+export MADGRAPH_CUDA_ARCHITECTURE=80
+
 if [ -f /usr/bin/gfortran-8 ]; then
   export FC=/usr/bin/gfortran-8
   export LIBRARY_PATH=/usr/lib/gcc/x86_64-linux-gnu/8/
@@ -11,12 +13,12 @@ elif [ -f /usr/bin/gfortran-9 ]; then
 fi
 
 CXX=`which g++`
-
 NVCC=`which nvcc`
 
 echo "Using compilers:"
 echo "FC=${FC}"
 echo "CXX=${CXX}"
 echo "NVCC=${NVCC}"
+echo "MADGRAPH_CUDA_ARCHITECTURE=${MADGRAPH_CUDA_ARCHITECTURE}"
 
 export LD_LIBRARY_PATH=../../lib:${LD_LIBRARY_PATH}
