4cdd1da762ec08c9fea0ddf6892c299f74fe8603
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-21 17:19:13 +0000

    [hack] prefer gfortran9 to 8

--- tools/setup_raplab.sh
+++ tools/setup_raplab.sh
@@ -8,13 +8,13 @@ export NVCC=$(which nvcc)
 echo "Using CXX=${CXX}"
 echo "Using NVCC=${NVCC}"
 
-if gfortran-8 --version >& /dev/null; then
+if gfortran-9 --version >& /dev/null; then
+  export FC=$(which gfortran-9) # no need to change LIBRARY_PATH
+elif gfortran-8 --version >& /dev/null; then
   export FC=$(which gfortran-8)
   export LIBRARY_PATH=$(dirname $(${FC} --print-file-name libgfortran.so)):$LIBRARY_PATH
-elif gfortran-9 --version >& /dev/null; then
-  export FC=$(which gfortran-9) # no need to change LIBRARY_PATH
 else
-  echo "WARNING! Neither gfortran-8 nor gfortran-9 were found in PATH"
+  echo "WARNING! Neither gfortran-9 nor gfortran-8 were found in PATH"
 fi
 echo "Using FC=${FC}"
 echo "Using LIBRARY_PATH=${LIBRARY_PATH}"
