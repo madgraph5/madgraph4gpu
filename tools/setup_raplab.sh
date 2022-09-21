775cd01ad3c123e2b7bbc9b1ca9ab9ea5a329275
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-21 17:15:36 +0000

    [hack] move to gfortran-8 or 9 as done by Stefan, drop nvc++ completely #531

--- tools/setup_raplab.sh
+++ tools/setup_raplab.sh
@@ -1,4 +1,20 @@
 module purge
+
 module load gcc/9.3.0/cmake
-module load compilers/nvhpc-22.5-mpi
 module load cuda/11.6.1
+
+export CXX=$(which g++)
+export NVCC=$(which nvcc)
+echo "Using CXX=${CXX}"
+echo "Using NVCC=${NVCC}"
+
+if gfortran-8 --version >& /dev/null; then
+  export FC=$(which gfortran-8)
+  export LIBRARY_PATH=$(dirname $(${FC} --print-file-name libgfortran.so)):$LIBRARY_PATH
+elif gfortran-9 --version >& /dev/null; then
+  export FC=$(which gfortran-9) # no need to change LIBRARY_PATH
+else
+  echo "WARNING! Neither gfortran-8 nor gfortran-9 were found in PATH"
+fi
+echo "Using FC=${FC}"
+echo "Using LIBRARY_PATH=${LIBRARY_PATH}"
