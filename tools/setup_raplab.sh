ac181b52dec4d95957b197eaa2de15af58858f66
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-22 20:42:18 +0000

    [hack] force the use of gfortran-9 (hence dgx03)

--- tools/setup_raplab.sh
+++ tools/setup_raplab.sh
@@ -7,7 +7,7 @@ if [ "$USER" == "p6asv6bp" ]; then # AV
   export PATH=~/ccache/usr/bin:$PATH
   export USECCACHE=1
   export CCACHE_DIR=~/ccache/CCACHE_DIR
-  export PATH=~/gfortran-9/usr/bin:$PATH
+  ###export PATH=~/gfortran-9/usr/bin:$PATH # NOT OK, liblto_plugin.so is missing
 fi
 
 export CXX=$(which g++)
@@ -15,17 +15,15 @@ export NVCC=$(which nvcc)
 echo "Using CXX=${CXX}"
 echo "Using NVCC=${NVCC}"
 
-if gfortran-9 --version >& /dev/null; then
-  export FC=$(which gfortran-9)
-  export LIBRARY_PATH=$(dirname $(${FC} --print-file-name libgfortran.so)):$LIBRARY_PATH
-elif gfortran-8 --version >& /dev/null; then
-  export FC=$(which gfortran-8)
-  export LIBRARY_PATH=$(dirname $(${FC} --print-file-name libgfortran.so)):$LIBRARY_PATH
-else
-  echo "WARNING! Neither gfortran-9 nor gfortran-8 were found in PATH"
-fi
-echo "Using FC=${FC}"
-echo "Using LIBRARY_PATH=${LIBRARY_PATH}"
-
 export MADGRAPH_CUDA_ARCHITECTURE=80
 echo "Using MADGRAPH_CUDA_ARCHITECTURE=${MADGRAPH_CUDA_ARCHITECTURE}"
+
+if gfortran-9 --version >& /dev/null; then
+  export FC=$(which gfortran-9)
+  ###export LIBRARY_PATH=$(dirname $(${FC} --print-file-name libgfortran.so)):$LIBRARY_PATH
+  echo "Using FC=${FC}"
+  echo "Using LIBRARY_PATH=${LIBRARY_PATH}"
+else
+  echo "ERROR! gfortran-9 is not installed on this node: please use another node"
+fi
+
