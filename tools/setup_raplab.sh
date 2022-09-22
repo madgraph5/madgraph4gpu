164af133adb74b3ece4b99860ae2ed378dac40b2
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-22 18:40:40 +0000

    [hack] move AV environment from .bashrc to the setup script

--- tools/setup_raplab.sh
+++ tools/setup_raplab.sh
@@ -3,6 +3,13 @@ module purge
 module load gcc/9.3.0/cmake
 module load cuda/11.6.1
 
+if [ "$USER" == "p6asv6bp" ]; then # AV
+  export PATH=~/ccache/usr/bin:$PATH
+  export USECCACHE=1
+  export CCACHE_DIR=~/ccache/CCACHE_DIR
+  export PATH=~/gfortran-9/usr/bin:$PATH
+fi
+
 export CXX=$(which g++)
 export NVCC=$(which nvcc)
 echo "Using CXX=${CXX}"
