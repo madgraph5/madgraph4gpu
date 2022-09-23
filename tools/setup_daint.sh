b76f8c9187626f3ef4659977bb6ad2c2a1274ee6
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-23 18:52:16 +0200

    [hack] add setup_daint for AV with ccache

--- tools/setup_daint.sh
+++ tools/setup_daint.sh
@@ -1,8 +1,23 @@
 module purge
 
 module use /apps/daint/UES/hackaton/modules/all
-module load CUDAcore/11.7.1
 module load gcc
+module load CUDAcore/11.7.1
 
-export FC=gfortran
+export CXX=$(which g++)
+export NVCC=$(which nvcc)
+echo "Using CXX=${CXX}"
+echo "Using NVCC=${NVCC}"
+
 export MADGRAPH_CUDA_ARCHITECTURE=60
+echo "Using MADGRAPH_CUDA_ARCHITECTURE=${MADGRAPH_CUDA_ARCHITECTURE}"
+
+export FC=$(which gfortran)
+echo "Using FC=${FC}"
+
+if [ "$USER" == "hck03" ]; then # AV
+  export PATH=~/ccache/bin:$PATH
+  export USECCACHE=1
+  export CCACHE_DIR=~/ccache/CCACHE_DIR
+fi
+
