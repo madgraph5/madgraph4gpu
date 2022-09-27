12b7fa4785f6f72fb9b32c198fd70066007210e3
Andrea Valassi <andrea.valassi@cern.ch>
2022-09-27 16:45:52 +0200

    [hack_ihel] progress in building, after moving back sigmakin
    gCPPProcess.cu(689): warning #20091-D: a __constant__ variable "mg5amcGpu::cNGoodHel" cannot be directly read in a host function
    gCPPProcess.cu(691): warning #20091-D: a __constant__ variable "mg5amcGpu::cGoodHel" cannot be directly read in a host function

--- epochX/cudacpp/gg_tt.sa/SubProcesses/P1_Sigma_sm_gg_ttx/CPPProcess.cc
+++ epochX/cudacpp/gg_tt.sa/SubProcesses/P1_Sigma_sm_gg_ttx/CPPProcess.cc
@@ -84,16 +84,6 @@ namespace mg5amcCpu
 
   //--------------------------------------------------------------------------
 
-  //   __global__ normalise_output(fptype* allMEs, const fptype* allNumetators, cont fptype* allDenominators){
-  //
-  //            int ievt;
-  //            int globadenom = %(denominator)s;
-  //            //todo get ievt;
-  //
-  //            allME[ievt] = allME[ievt] * allNumerators[ievt]/(allDenominators[ievt]*globaldenom);
-  //            return
-  //}
-            
   // Evaluate |M|^2 for each subprocess
   // NB: calculate_wavefunctions ADDS |M|^2 for a given ihel to the running sum of |M|^2 over helicities for the given event(s)
   // (similarly, it also ADDS the numerator and denominator for a given ihel to their running sums over helicities)
@@ -634,18 +624,23 @@ namespace mg5amcCpu
   }
 
   //--------------------------------------------------------------------------
+  // Evaluate |M|^2, part independent of incoming flavour
+  // FIXME: assume process.nprocesses == 1 (eventually: allMEs[nevt] -> allMEs[nevt*nprocesses]?)
 
   void /* clang-format off */
   sigmaKin( const fptype* allmomenta,      // input: momenta[nevt*npar*4]
             const fptype* allcouplings,    // input: couplings[nevt*ndcoup*2]
-            fptype* allMEs                 // output: allMEs[nevt], |M|^2 final_avg_over_helicities
+            fptype* allMEs,                // output: allMEs[nevt], |M|^2 final_avg_over_helicities
 #ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-            , fptype* allNumerators        // output: multichannel numerators[nevt], running_sum_over_helicities
-            , fptype* allDenominators      // output: multichannel denominators[nevt], running_sum_over_helicities
-            , const unsigned int channelId // input: multichannel channel id (1 to #diagrams); 0 to disable channel enhancement
+            fptype* allNumerators,         // output: multichannel numerators[nevt], running_sum_over_helicities
+            fptype* allDenominators,       // output: multichannel denominators[nevt], running_sum_over_helicities
+            const unsigned int channelId,  // input: multichannel channel id (1 to #diagrams); 0 to disable channel enhancement
 #endif
-#ifndef __CUDACC__
-            , const int nevt               // input: #events (for cuda: nevt == ndim == gpublocks*gputhreads)
+#ifdef __CUDACC__
+            const int gpublocks,           // input: cuda gpublocks
+            const int gputhreads           // input: cuda gputhreads
+#else
+            const int nevt                 // input: #events (for cuda: nevt == ndim == gpublocks*gputhreads)
 #endif
             ) /* clang-format on */
   {
@@ -656,24 +651,35 @@ namespace mg5amcCpu
     static_assert( nprocesses == 1, "Assume nprocesses == 1" ); // FIXME (#343): assume nprocesses == 1
     constexpr int denominators[1] = { 256 };
 
+#ifndef __CUDACC__
+    //assert( (size_t)(allmomenta) % mgOnGpu::cppAlign == 0 ); // SANITY CHECK: require SIMD-friendly alignment [COMMENT OUT TO TEST MISALIGNED ACCESS]
+    //assert( (size_t)(allMEs) % mgOnGpu::cppAlign == 0 ); // SANITY CHECK: require SIMD-friendly alignment [COMMENT OUT TO TEST MISALIGNED ACCESS]
+#endif
+
     // Start sigmaKin_lines
     // PART 0 - INITIALISATION (before calculate_wavefunctions)
     // Reset the "matrix elements" - running sums of |M|^2 over helicities for the given event
     // FIXME: assume process.nprocesses == 1 for the moment (eventually: need a loop over processes here?)
 #ifdef __CUDACC__
-    cudaMemSet( allMEs, 0, nevt*sizeof(fptype) );
+    const int nevt = gpublocks*gputhreads;
+    cudaMemset( allMEs, 0, nevt*sizeof(fptype) );
 #ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-    cudaMemSet( allNumerators, 0, nevt*sizeof(fptype) );
-    cudaMemSet( allDenominators, 0, nevt*sizeof(fptype) );
+    cudaMemset( allNumerators, 0, nevt*sizeof(fptype) );
+    cudaMemset( allDenominators, 0, nevt*sizeof(fptype) );
 #endif
 #else
-    for( int ievt = 0; ievt < nevt; ++ievt )
-    {      
-      allMEs[ievt] = 0;
+    const int npagV = nevt / neppV;
+    for( int ipagV = 0; ipagV < npagV; ++ipagV )
+    {
+      for( int ieppV = 0; ieppV < neppV; ieppV++ )
+      {
+        const unsigned int ievt = ipagV * neppV + ieppV;
+        allMEs[ievt] = 0;
 #ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-      allNumerators[ievt] = 0;
-      allDenominators[ievt] = 0;
+        allNumerators[ievt] = 0;
+        allDenominators[ievt] = 0;
 #endif
+      }
     }
 #endif
 
@@ -685,9 +691,9 @@ namespace mg5amcCpu
       const int ihel = cGoodHel[ighel];
 #ifdef __CUDACC__
 #ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-      calculate_wavefunctions<<<m_gpublocks, m_gputhreads>>>( ihel, allmomenta, allcouplings, allMEs, allNumerators, allDenominators, channelId );
+      calculate_wavefunctions<<<gpublocks, gputhreads>>>( ihel, allmomenta, allcouplings, allMEs, allNumerators, allDenominators, channelId );
 #else
-      calculate_wavefunctions<<<m_gpublocks, m_gputhreads>>>( ihel, allmomenta, allcouplings, allMEs );
+      calculate_wavefunctions<<<gpublocks, gputhreads>>>( ihel, allmomenta, allcouplings, allMEs );
 #endif
 #else
 #ifdef MGONGPU_SUPPORTS_MULTICHANNEL
@@ -708,10 +714,11 @@ namespace mg5amcCpu
     // https://www.uzh.ch/cmsssl/physik/dam/jcr:2e24b7b1-f4d7-4160-817e-47b13dbf1d7c/Handout_4_2016-UZH.pdf]
     // FIXME: assume process.nprocesses == 1 for the moment (eventually: need a loop over processes here?)
 #ifdef __CUDACC__
-    normalise_output<<<m_gpublocks, m_gputhreads>>>( all_MEs, allNumerators, allDenominators );
-    //    allMEs[ievt] /= denominators[0]; // FIXME (#343): assume nprocesses == 1
-    //#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-    //    if( channelId > 0 ) allMEs[ievt] *= allNumerators[ievt] / allDenominators[ievt]; // FIXME (#343): assume nprocesses == 1
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    normalise_output<<<gpublocks, gputhreads>>>( allMEs, allNumerators, allDenominators, channelId, denominators[0] );
+#else
+    normalise_output<<<gpublocks, gputhreads>>>( allMEs, denominators[0] );
+#endif
 #else
     for( int ipagV = 0; ipagV < npagV; ++ipagV )
     {
@@ -731,17 +738,24 @@ namespace mg5amcCpu
 
   //--------------------------------------------------------------------------
 
-  // Evaluate |M|^2, part independent of incoming flavour
-  // FIXME: assume process.nprocesses == 1 (eventually: allMEs[nevt] -> allMEs[nevt*nprocesses]?)
-   __global__ void
-   normalise_output( fptype* allMEs, const fptype* allNumerators, const fptype* allDenominators )
-   {
-     const int ievt = blockDim.x * blockIdx.x + threadIdx.x; // index of event (thread)
-     const int globaldenom = 256;
-     //todo get ievt;
-     allMEs[ievt] = allMEs[ievt] * allNumerators[ievt]/(allDenominators[ievt]*globaldenom);
-     return;
-   }
+#ifdef __CUDACC__
+  __global__ void
+  normalise_output( fptype* allMEs,                // output: allMEs[nevt], |M|^2 running_sum_over_helicities
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+                    const fptype* allNumerators,   // output: multichannel numerators[nevt], running_sum_over_helicities
+                    const fptype* allDenominators, // output: multichannel denominators[nevt], running_sum_over_helicities
+                    const unsigned int channelId,  // input: multichannel channel id (1 to #diagrams); 0 to disable channel enhancement
+#endif
+                    const fptype globaldenom )
+  {
+    const int ievt = blockDim.x * blockIdx.x + threadIdx.x; // index of event (thread)
+    allMEs[ievt] /= globaldenom; // FIXME (#343): assume nprocesses == 1
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    if( channelId > 0 ) allMEs[ievt] *= allNumerators[ievt] / allDenominators[ievt]; // FIXME (#343): assume nprocesses == 1
+#endif
+    return;
+  }
+#endif
 
   //--------------------------------------------------------------------------
 
