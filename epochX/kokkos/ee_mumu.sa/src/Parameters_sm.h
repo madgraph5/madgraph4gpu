175d0ae972eaea0841aa1a2156aae715a6b9e88b
Nathan Nichols <8723338+nscottnichols@users.noreply.github.com>
2022-09-30 09:39:08 -0400

    regenerate with new codegen script

--- epochX/kokkos/ee_mumu.sa/src/Parameters_sm.h
+++ epochX/kokkos/ee_mumu.sa/src/Parameters_sm.h
@@ -1,6 +1,6 @@
 //==========================================================================
 // This file has been automatically generated for C++
-// MadGraph5_aMC@NLO v. 2.9.5, 2021-08-22
+// MadGraph5_aMC@NLO v. 3.4.0_lo_vect, 2022-05-06
 // By the MadGraph5_aMC@NLO Development Team
 // Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
 //==========================================================================
@@ -26,10 +26,10 @@ double zero, ZERO;
 // Model parameters independent of aS
   double mdl_WH, mdl_WW, mdl_WZ, mdl_WT, mdl_ymtau, mdl_ymt, mdl_ymb, aS, mdl_Gf, aEWM1, mdl_MH, mdl_MZ, mdl_MTA, mdl_MT, mdl_MB, mdl_conjg__CKM3x3, mdl_conjg__CKM1x1, mdl_CKM3x3, mdl_MZ__exp__2, mdl_MZ__exp__4, mdl_sqrt__2, mdl_MH__exp__2, mdl_aEW, mdl_MW, mdl_sqrt__aEW, mdl_ee, mdl_MW__exp__2, mdl_sw2, mdl_cw, mdl_sqrt__sw2, mdl_sw, mdl_g1, mdl_gw, mdl_vev, mdl_vev__exp__2, mdl_lam, mdl_yb, mdl_yt, mdl_ytau, mdl_muH, mdl_ee__exp__2, mdl_sw__exp__2, mdl_cw__exp__2;
   std::complex<double> mdl_complexi, mdl_I1x33, mdl_I2x33, mdl_I3x33, mdl_I4x33;
-// Model parameters dependent on aS
-  double mdl_sqrt__aS, G, mdl_G__exp__2;
 // Model couplings independent of aS
   std::complex<double> GC_3, GC_50, GC_59;
+// Model parameters dependent on aS
+  double mdl_sqrt__aS, G, mdl_G__exp__2;
 // Model couplings dependent on aS
   // (none)
 
--- epochX/kokkos/gg_tt.sa/SubProcesses/P1_Sigma_sm_gg_ttx/CPPProcess.cc
+++ epochX/kokkos/gg_tt.sa/SubProcesses/P1_Sigma_sm_gg_ttx/CPPProcess.cc
@@ -1,6 +1,6 @@
 //==========================================================================
 // This file has been automatically generated for C++ Standalone by
-// MadGraph5_aMC@NLO v. 2.9.5, 2021-08-22
+// MadGraph5_aMC@NLO v. 3.4.0_lo_vect, 2022-05-06
 // By the MadGraph5_aMC@NLO Development Team
 // Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
 //==========================================================================
