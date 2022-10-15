5976ac3f337a5afd1024ca331eff67cf22ddc2dd
Andrea Valassi <andrea.valassi@cern.ch>
2022-10-15 12:18:18 +0200

    [hackjw] recreate summary table after hackathon MLM changes - a nice factor x2 on overall GPU ggttgg #539
    < +++  REVISION 1efee04 (commit date: 2022-10-13 20:58:53 +0200) +++
    ---
    > +++  REVISION f64a68e (commit date: 2022-10-15 11:50:50 +0200) +++
    15,21c15,21
    < | FORTRAN    |   58.28 =   5.19 +   53.09 |  1.55e+03 (= 1.0) |  1.70e+03 (= 1.0) |       --- |       --- |
    < | CPP/none   |   63.49 =   9.93 +   53.56 |  1.42e+03 (x 0.9) |  1.68e+03 (x 1.0) |  1.74e+03 |  1.74e+03 |
    < | CPP/sse4   |   35.34 =   7.65 +   27.69 |  2.55e+03 (x 1.6) |  3.25e+03 (x 1.9) |  3.34e+03 |  3.34e+03 |
    < | CPP/avx2   |   20.34 =   6.43 +   13.91 |  4.43e+03 (x 2.9) |  6.48e+03 (x 3.8) |  6.67e+03 |  6.66e+03 |
    < | CPP/512y   |   18.60 =   6.29 +   12.31 |  4.84e+03 (x 3.1) |  7.32e+03 (x 4.3) |  7.30e+03 |  7.53e+03 |
    < | CPP/512z   |   20.58 =   6.47 +   14.11 |  4.38e+03 (x 2.8) |  6.39e+03 (x 3.8) |  6.47e+03 |  6.45e+03 |
    < | CUDA/8192  |    6.05 =   5.70 +    0.36 |  1.49e+04 (x 9.6) |  2.54e+05 (x149.) |  2.30e+05 |  2.51e+05 |
    ---
    > | FORTRAN    |   55.30 =   2.40 +   52.90 |  1.63e+03 (= 1.0) |  1.70e+03 (= 1.0) |       --- |       --- |
    > | CPP/none   |   60.59 =   7.10 +   53.50 |  1.49e+03 (x 0.9) |  1.68e+03 (x 1.0) |  1.74e+03 |  1.74e+03 |
    > | CPP/sse4   |   32.49 =   4.82 +   27.67 |  2.77e+03 (x 1.7) |  3.26e+03 (x 1.9) |  3.34e+03 |  3.34e+03 |
    > | CPP/avx2   |   17.52 =   3.62 +   13.91 |  5.14e+03 (x 3.2) |  6.48e+03 (x 3.8) |  6.66e+03 |  6.67e+03 |
    > | CPP/512y   |   15.75 =   3.46 +   12.30 |  5.72e+03 (x 3.5) |  7.33e+03 (x 4.3) |  7.54e+03 |  7.54e+03 |
    > | CPP/512z   |   17.77 =   3.65 +   14.12 |  5.07e+03 (x 3.1) |  6.38e+03 (x 3.8) |  6.48e+03 |  6.47e+03 |
    > | CUDA/8192  |    2.92 =   2.58 +    0.35 |  3.08e+04 (x18.9) |  2.60e+05 (x152.) |  2.15e+05 |  2.51e+05 |

--- epochX/cudacpp/tmad/summaryTable.sh
+++ epochX/cudacpp/tmad/summaryTable.sh
@@ -57,7 +57,8 @@ elif [ "$table" == "ichep22" ]; then
   fpts="d f"
 elif [ "$table" == "ggttgg" ]; then
   procs="ggttgg"
-  mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022table GPU
+  ###mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022table GPU
+  mrevs="$mrevs f64a68e"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hackathon MLM
   taglist="FORTRAN CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUDA/8192 CUDA/max $cuda8tpb"
   fpts="d f"
 fi
