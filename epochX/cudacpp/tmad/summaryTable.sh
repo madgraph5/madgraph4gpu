dd998d68a4f9305cf4174ecf4f14375d3ae73f9f
Andrea Valassi <andrea.valassi@cern.ch>
2022-10-16 08:24:23 +0200

    [hack2] add a ggttggg table - with ichep2022 status to start with

--- epochX/cudacpp/tmad/summaryTable.sh
+++ epochX/cudacpp/tmad/summaryTable.sh
@@ -22,6 +22,7 @@ if [ "$1" == "-ALL" ] && [ "$2" == "" ]; then
   $0 -juwels
   $0 -ichep22
   $0 -ggttgg
+  $0 -ggttggg
   exit 0
 elif [ "$1" == "-default" ]; then
   table="default"; shift
@@ -31,8 +32,10 @@ elif [ "$1" == "-ichep22" ]; then
   table="ichep22"; shift
 elif [ "$1" == "-ggttgg" ]; then
   table="ggttgg"; shift
+elif [ "$1" == "-ggttggg" ]; then
+  table="ggttggg"; shift
 else
-  echo "Usage: $0 [--long] <table [-ALL|-default|-juwels|-ichep22|-ggttgg]>"; exit 1
+  echo "Usage: $0 [--long] <table [-ALL|-default|-juwels|-ichep22|-ggttgg|-ggttggg]>"; exit 1
 fi
 
 # Select revisions and characteristics of mad logs
@@ -60,6 +63,13 @@ elif [ "$table" == "ggttgg" ]; then
   ###mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022table GPU
   ###mrevs="$mrevs f64a68e"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack1 MLM
   mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack2 symmetric matrix
+  taglist="FORTRAN CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUDA/8192 CUDA/max $cuda8tpb"
+  fpts="d f"
+elif [ "$table" == "ggttggg" ]; then
+  procs="ggttggg"
+  mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022 (gave ggttgg results only)
+  ###mrevs="$mrevs f64a68e"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack1 MLM
+  ###mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack2 symmetric matrix
   taglist="FORTRAN CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUDA/8192 CUDA/max $cuda8tpb"
   fpts="d f"
 fi
