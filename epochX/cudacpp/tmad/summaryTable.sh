167a31af426ec98000ffe0312487869470df0bba
Andrea Valassi <andrea.valassi@cern.ch>
2022-10-16 08:39:44 +0200

    [hack2] update ggttggg table to latest log, including hack1/MLM AND hack2/symmetricmatrix
    The hack1/MLM effects are nicely visible! Even more than on ggttgg, as expected
    
    Conversely the hack2/symmetricmatrix effects #475 are surprising:
    - a bit faster for C++
    - quite a bit slower for CUDA?!
    
    Having a better look, this is also visible in ggttgg, but not as much

--- epochX/cudacpp/tmad/summaryTable.sh
+++ epochX/cudacpp/tmad/summaryTable.sh
@@ -61,15 +61,14 @@ elif [ "$table" == "ichep22" ]; then
 elif [ "$table" == "ggttgg" ]; then
   procs="ggttgg"
   ###mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022table GPU
-  ###mrevs="$mrevs f64a68e"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack1 MLM
-  mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack2 symmetric matrix
+  ###mrevs="$mrevs f64a68e"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack1/MLM (NB: no ggttggg here!)
+  mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack2/symmetricmatrix
   taglist="FORTRAN CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUDA/8192 CUDA/max $cuda8tpb"
   fpts="d f"
 elif [ "$table" == "ggttggg" ]; then
   procs="ggttggg"
-  mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022 (gave ggttgg results only)
-  ###mrevs="$mrevs f64a68e"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack1 MLM
-  ###mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack2 symmetric matrix
+  ###mrevs="$mrevs 1efee04"  # cuda117/gcc112  (13 Oct 2022 itscrd70)  ICHEP2022 (gave ggttgg results only)
+  mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack1/MLM and hack2/symmetricmatrix
   taglist="FORTRAN CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUDA/8192 CUDA/max $cuda8tpb"
   fpts="d f"
 fi
