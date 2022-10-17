5c45df51d20fdd52d822adb7e5ad6ad3ae7534b4
Andrea Valassi <andrea.valassi@cern.ch>
2022-10-17 11:06:36 +0200

    [hack3] add -ggttggg option to alltees in tmad

--- epochX/cudacpp/tmad/allTees.sh
+++ epochX/cudacpp/tmad/allTees.sh
@@ -13,19 +13,24 @@ add10x="+10x"
 
 while [ "$1" != "" ]; do
   if [ "$1" == "-short" ]; then
-    short=1
+    short=1 # all but ggttggg
+    shift
+  elif [ "$1" == "-ggttggg" ]; then
+    short=-1 # only ggttggg
     shift
   elif [ "$1" == "-makeclean" ]; then
     makeclean=$1
     shift
   else
-    echo "Usage: $0 [-short] [-makeclean]"
+    echo "Usage: $0 [-short|-ggttggg] [-makeclean]"
     exit 1
   fi
 done
 
 if [ "$short" == "1" ]; then
   ${scrdir}/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg $flts $makeclean $rmrdat $add10x
+elif [ "$short" == "-1" ]; then
+  ${scrdir}/teeMadX.sh -ggttggg $flts $makeclean $rmrdat $add10x
 else
   ${scrdir}/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -ggttggg $flts $makeclean $rmrdat $add10x
 fi
