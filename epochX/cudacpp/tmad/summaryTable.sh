be2535937bd567cfdb775ea05862002e3ca335b6
Andrea Valassi <andrea.valassi@cern.ch>
2022-10-17 15:40:56 +0200

    [hack3] regenerate dfm summary for ggttgg from today (diff is 'd' today to 'm' today)

--- epochX/cudacpp/tmad/summaryTable.sh
+++ epochX/cudacpp/tmad/summaryTable.sh
@@ -67,10 +67,10 @@ elif [ "$table" == "ggttgg" ]; then
   ###mrevs="$mrevs f718e74"  # cuda117/gcc112  (15 Oct 2022 itscrd70)  add hack2/symmetricmatrix(cuda/c++)
   ###mrevs="$mrevs d95e49c"  # cuda117/gcc112  (16 Oct 2022 itscrd70)  modify hack2/symmetricmatrix(c++ only)
   ###mrevs="$mrevs 48e2012"  # cuda117/gcc112  (16 Oct 2022 itscrd70)  modify hack2/constexpr(c++ only)
-  fpts="d f d"
+  ###fpts="d f d"
   # -- After implementing 'm'
   mrevs="$mrevs 9598b41"  # cuda117/gcc112  (17 Oct 2022 itscrd70)  add hack3/mixedfps
-  ###fpts="d f m"
+  fpts="d f m"
 elif [ "$table" == "ggttggg" ]; then
   procs="ggttggg"
   taglist="FORTRAN CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUDA/8192 CUDA/max $cuda8tpb"
