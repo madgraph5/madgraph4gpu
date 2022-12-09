626181b2d1979b051e6815dc0d8d69314b5216d8
Andrea Valassi <andrea.valassi@cern.ch>
2022-12-09 16:05:22 +0100

    [lhe] in gg_tt.mad add VECSIZE_USED as function argument wherever needed, remove its definition in a COMMON
    Replace "IF (VECSIZE_USED.LE.1)" by "IF (VECSIZE_MEMMAX.LE.1)" to detect no-vector mode

--- epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
@@ -73,6 +73,7 @@ c      common/to_colstats/ncols,ncolflow,ncolalt,ic
 
       include 'vector.inc'
       include 'coupl.inc'
+      integer VECSIZE_USED
 
 #ifdef MG5AMC_MEEXPORTER_CUDACPP
       INCLUDE 'fbridge.inc'
@@ -221,7 +222,7 @@ c         itmin = itmin + 1
       endif
 
       write(*,*) "about to integrate ", ndim,ncall,itmax,itmin,ninvar,nconfigs
-      call sample_full(ndim,ncall,itmax,itmin,dsig,ninvar,nconfigs)
+      call sample_full(ndim,ncall,itmax,itmin,dsig,ninvar,nconfigs,VECSIZE_USED)
 
 c
 c     Now write out events to permanent file
