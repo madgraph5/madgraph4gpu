5d25acf7da0ab4ff0dd3dfa20e44469e53f4e9ad
Andrea Valassi <andrea.valassi@cern.ch>
2022-12-11 12:53:04 +0100

    [lhe] #561 add OMP thread printout to madX.sh

--- epochX/cudacpp/tmad/madX.sh
+++ epochX/cudacpp/tmad/madX.sh
@@ -314,11 +314,13 @@ function runmadevent()
   fi
   $timecmd $cmd < ${tmpin} > ${tmp}
   if [ "$?" != "0" ]; then echo "ERROR! '$timecmd $cmd < ${tmpin} > ${tmp}' failed"; tail -10 $tmp; exit 1; fi
+  omp=$(cat ${tmp} | grep --binary-files=text 'omp_get_max_threads() =' | awk '{print $NF}')
   fbm=$(cat ${tmp} | grep --binary-files=text 'FBRIDGE_MODE =' | awk '{print $NF}')
   nbp=$(cat ${tmp} | grep --binary-files=text 'VECSIZE_USED =' | awk '{print $NF}')
   mch=$(cat ${tmp} | grep --binary-files=text 'MULTI_CHANNEL =' | awk '{print $NF}')
   conf=$(cat ${tmp} | grep --binary-files=text 'Running Configuration Number:' | awk '{print $NF}')
   chid=$(cat ${tmp} | grep --binary-files=text 'CHANNEL_ID =' | awk '{print $NF}')
+  echo " [OPENMPTH] omp_get_max_threads/nproc = ${omp}/$(nproc)"
   echo " [XSECTION] VECSIZE_USED = ${nbp}"
   echo " [XSECTION] MultiChannel = ${mch}"
   echo " [XSECTION] Configuration = ${conf}"
