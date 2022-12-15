6b9a148195cec08d9233e36173e3c9540a2f3a01
Andrea Valassi <andrea.valassi@cern.ch>
2022-12-15 19:22:26 +0100

    [lhe] in gg_tt.mad add coloramps.h (ported from Fortran coloramps.inc) for leading color selection #402

---
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/coloramps.h
@@ -0,0 +1,18 @@
+#ifndef COLORAMPS_H
+#define COLORAMPS_H 1
+
+namespace mgOnGpu
+{
+
+  constexpr int ncolor = 2;
+
+  constexpr int nchannel = 3;
+
+  constexpr bool icolamp[nchannel][ncolor] = { // FIXME: assume process.nprocesses == 1 for the moment
+    { true, true },
+    { true, false },
+    { false, true }
+  };
+
+}
+#endif // COLORAMPS_H
