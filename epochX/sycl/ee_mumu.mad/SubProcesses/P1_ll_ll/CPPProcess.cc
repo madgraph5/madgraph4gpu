5b42668c9313c54b0d2a9efb003a93be0549925c
Nathan Nichols <8723338+nscottnichols@users.noreply.github.com>
2023-02-15 13:52:45 -0500

    regenerate with latest mg5amcnlo

--- epochX/sycl/ee_mumu.mad/SubProcesses/P1_ll_ll/CPPProcess.cc
+++ epochX/sycl/ee_mumu.mad/SubProcesses/P1_ll_ll/CPPProcess.cc
@@ -1,6 +1,6 @@
 //==========================================================================
 // This file has been automatically generated for SYCL standalone by
-// MadGraph5_aMC@NLO v. 3.4.0_lo_vect, 2022-05-06
+// MadGraph5_aMC@NLO v. 3.5.0_lo_vect, 2023-01-26
 // By the MadGraph5_aMC@NLO Development Team
 // Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
 //==========================================================================
