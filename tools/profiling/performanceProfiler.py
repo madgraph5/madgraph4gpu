5a97f9246a826def335c32fee9f1ba53c9d90931
Jooorgen <jorgen.teig@gmail.com>
2023-03-29 14:58:00 +0200

    Refactored and added more comments to performanceProfiler script

--- tools/profiling/performanceProfiler.py
+++ tools/profiling/performanceProfiler.py
@@ -1,74 +1,79 @@
-import string
 import sys
-import os
 import subprocess
 import datetime
 import argparse
 
 # Parser arguments defaults
-absLayer = "SYCL"
-branch = "master"
+ABS_LAYER = "SYCL"
+BRANCH = "master"
 
-mgProcesses = ["ee_mumu", "gg_tt", "gg_ttg", "gg_ttgg", "gg_ttggg"]
+# Physics processes
+MG_PROCESSES_SA = ["ee_mumu.sa", "gg_tt.sa", "gg_ttg.sa", "gg_ttgg.sa", "gg_ttggg.sa"]
 
-doublePrecisionConstant = 2560
-#doublePrecisionConstant = 1
-iterations = 10
-#iterations = 1
-threadsPerBlock = [256]
-#threadsPerBlock = [32]
-blocksPerGrid = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
-#blocksPerGrid = [32]
+DOUBLE_PRECISION_CONSTANT = 2560
+ITERATIONS = 10
+THREADS_PER_BLOCK = [256]
+#THREADS_PER_BLOCK = [32, 64, 128, 256]
+BLOCKS_PER_GRID = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
 
 # Parser
 parser = argparse.ArgumentParser(description='A program for profiling GPUs using MadGraph.')
 
-parser.add_argument("-l", help="Choose which abstraction layer you want to use (CUDA/SYCL).", default=absLayer)
-parser.add_argument("-b", help="Choose which branch the madgraph4gpu repo is in.", default=branch)
-
-# Add profiler option in python and build scripts so that correct gcc toolchain can be set through makefile and still not disturb the compilation on Github machines
+parser.add_argument("-l", help="Choose which abstraction layer you want to use (CUDA/SYCL).", default=ABS_LAYER)
+parser.add_argument("-b", help="Choose which branch the madgraph4gpu repo is in.", default=BRANCH)
 
 pyArgs = parser.parse_args()
 
 # How many runs in total the program made
 count = 0
 
-for process in mgProcesses:
-    for TPB in threadsPerBlock:
-        for BPG in blocksPerGrid:
-            if TPB * BPG > doublePrecisionConstant:
+for process in MG_PROCESSES_SA:
+    for TPB in THREADS_PER_BLOCK:
+        for BPG in BLOCKS_PER_GRID:
+            if TPB * BPG > DOUBLE_PRECISION_CONSTANT:
 
                 if pyArgs.l.upper() == 'SYCL':
 
-                    if ".sa" not in process:
-                        process = process + ".sa"
+                    # There is no .sa in br_golden_epochX4
+                    # so it makes sure that .sa is included in everything other than that branch
+                    # if pyArgs.b != 'br_golden_epochX4':
+                    #if ".sa" not in process:
+                    #    process = process + ".sa"
 
-                    bashArgs = ["./buildSYCLProcess.sh", "-n",  process, "-i",  str(iterations), "-t",  str(TPB), "-b", str(BPG), "-r", str(pyArgs.b).lower()]
+                    bashArgs = ["./buildSYCLProcess.sh",
+                                "-n", process,
+                                "-i", str(ITERATIONS),
+                                "-t", str(TPB),
+                                "-b", str(BPG),
+                                "-r", str(pyArgs.b).lower()]
 
                 elif pyArgs.l.upper() == 'CUDA':
 
-                    # There is no .sa in br_golden_epochX4 so it makes sure that .sa is included in everything other than that branch
-                    #if pyArgs.b != 'br_golden_epochX4':
-                    if ".sa" not in process:
-                        process = process + ".sa"
-
-                    bashArgs = ["./buildCUDAProcess.sh", "-n",  process, "-i",  str(iterations), "-t",  str(TPB), "-b", str(BPG), "-r", str(pyArgs.b).lower()]
-
-                    #if len(pyArgs.b) > 0:
-                    #    bashArgs.append('--branch')
-                    #    bashArgs.append(str(pyArgs.b).lower())
+                    bashArgs = ["./buildCUDAProcess.sh",
+                                "-n", process,
+                                "-i", str(ITERATIONS),
+                                "-t", str(TPB),
+                                "-b", str(BPG),
+                                "-r", str(pyArgs.b).lower()]
 
                 else: sys.exit("No abstraction layer matching the supplied string!")
 
-                print(str(datetime.datetime.now().strftime("%H:%M:%S")) + " Started " + process + " with TPB("+ str(TPB) +") * BPG("+ str(BPG) +"): " + str(TPB * BPG) + "!")
-
                 time = str(datetime.datetime.now().strftime("%H:%M:%S"))
+
+                print(time + " Started " + process + " with TPB("+ str(TPB) +") * BPG("+ str(BPG) +"): " + str(TPB * BPG) + "!")
 
                 build = subprocess.run(bashArgs, check=True)#, stdout=subprocess.DEVNULL)
                 if build.returncode != 0:
-                    print(time + " " + process + " FAILED!, threadsPerBlock: " + str(TPB) + ", blocksPerGrid: " + str(BPG) + ", Product: " + str(TPB * BPG))
+                    print(time + " " + process +
+                          " FAILED!, threadsPerBlock: " + str(TPB) +
+                                    ", blocksPerGrid: " + str(BPG) +
+                                    ", Product: " + str(TPB * BPG))
                 else:
-                    print(time + " " + process + " COMPLETED!, threadsPerBlock: " + str(TPB) + ", blocksPerGrid: " + str(BPG) + ", Product: " + str(TPB * BPG))
+                    print(time + " " + process +
+                          " COMPLETED!, threadsPerBlock: " + str(TPB) +
+                                    ", blocksPerGrid: " + str(BPG) +
+                                    ", Product: " + str(TPB * BPG))
+
                 count += 1
 
 print("Builded " + str(count) + " processes!")
