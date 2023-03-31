288a4a2059275bb86f0b34434365387e20e2bdb6
Andrea Valassi <andrea.valassi@cern.ch>
2023-03-31 13:56:15 +0200

    [gqttq] add codegen for SM gq_ttq ("define q = u c d s u~ c~ d~ s~; generate g q > t t~ q")

--- epochX/cudacpp/CODEGEN/generateAndCompare.sh
+++ epochX/cudacpp/CODEGEN/generateAndCompare.sh
@@ -25,6 +25,9 @@ function codeGenAndDiff()
     gg_ttggg)
       cmd="generate g g > t t~ g g g"
       ;;
+    gq_ttq)
+      cmd="define q = u c d s u~ c~ d~ s~; generate g q > t t~ q"
+      ;;
     pp_tt)
       cmd="generate p p > t t~"
       ;;
