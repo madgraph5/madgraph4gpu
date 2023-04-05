29ad1fb3db4506aec00ef2f8d438a8903e59c60c
Andrea Valassi <andrea.valassi@cern.ch>
2023-04-05 07:30:44 +0200

    [gqttq] in gq_ttq.mad, manually rename and duplicate the P1 directory (prepare to regenerate with Olivier's #619 patch that splits in in two)
    git mv gq_ttq.mad/SubProcesses/P1_gq_ttxq gq_ttq.mad/SubProcesses/P1_gu_ttxu
    cp -dpr gq_ttq.mad/SubProcesses/P1_gu_ttxu gq_ttq.mad/SubProcesses/P1_gux_ttxux/
    git add gq_ttq.mad/SubProcesses/P1_gux_ttxux/

---
+++ epochX/cudacpp/gq_ttq.mad/SubProcesses/P1_gux_ttxux/coloramps.h
@@ -0,0 +1,21 @@
+#ifndef COLORAMPS_H
+#define COLORAMPS_H 1
+
+namespace mgOnGpu
+{
+
+  __device__ constexpr bool icolamp[5][4] = { // FIXME: assume process.nprocesses == 1 for the moment
+    { true, false, false, false },
+    { true, false, false, false },
+    { false, false, false, true },
+    { false, false, false, true },
+    { true, false, false, true },
+    { true, false, false, false },
+    { false, false, false, true },
+    { true, false, false, false },
+    { false, false, false, true },
+    { true, false, false, true }
+  };
+
+}
+#endif // COLORAMPS_H
