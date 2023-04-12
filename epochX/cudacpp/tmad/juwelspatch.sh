b9bf0a69c91f736205190f017f2de07949f4d4fc
Andrea Valassi <andrea.valassi@cern.ch>
2023-04-12 09:11:59 +0200

    [vecsizeFIX] in tmad/juwelspatch.sh patch also the coupl.inc file (and the gq_ttq process)

--- epochX/cudacpp/tmad/juwelspatch.sh
+++ epochX/cudacpp/tmad/juwelspatch.sh
@@ -2,9 +2,11 @@
 
 topdir=$(cd $(dirname $0)/..; pwd)
 
-procs="ee_mumu gg_tt gg_ttg gg_ttgg gg_ttggg"
+procs="ee_mumu gg_tt gg_ttg gg_ttgg gg_ttggg gq_ttq"
 for proc in $procs; do
   cd $topdir/${proc}.mad/Source
   cat vector.inc | sed 's/16384/32/' > vector.inc.NEW
   \mv vector.inc.NEW vector.inc
+  cat coupl.inc | sed 's/16384/32/' > coupl.inc.NEW
+  \mv coupl.inc.NEW coupl.inc
 done
