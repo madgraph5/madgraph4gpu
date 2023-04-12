46b5f48dbbc62d802c47d3aa993e228a23b7b610
Andrea Valassi <andrea.valassi@cern.ch>
2023-04-12 10:01:46 +0200

    [vecsizeFIX] cleanup codegen: use Events/.keep (as in upstream Template/LO/Events/.keep) rather than .keepme

--- epochX/cudacpp/CODEGEN/patchMad.sh
+++ epochX/cudacpp/CODEGEN/patchMad.sh
@@ -59,7 +59,7 @@ cat ${dir}/Source/make_opts >> ${dir}/Source/make_opts.new
 # Patch the default Fortran code to provide the integration with the cudacpp plugin
 # (1) Process-independent patches
 echo -e "index.html\n.libs\n.cudacpplibs" > ${dir}/.gitignore
-touch ${dir}/Events/.keepme
+touch ${dir}/Events/.keep # this file should already be present (mg5amcnlo copies it from Template/LO/Events/.keep) 
 \cp -dpr ${scrdir}/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/.clang-format ${dir} # new file
 \cp -dpr ${scrdir}/MG5aMC_patches/${dir_patches}/fbridge_common.inc ${dir}/SubProcesses # new file
 cd ${dir}/SubProcesses
