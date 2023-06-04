d858b67996c12c450f1f676e82d2b30904f68b04
Andrea Valassi <andrea.valassi@cern.ch>
2023-06-04 10:35:53 +0200

    [builds] in gg_tt.mad, strip down CommonRandomNumberKernel.cc and CurandRandomNumberKernel.cc (remove curand and common, resepectively)

--- epochX/cudacpp/gg_tt.mad/SubProcesses/CurandRandomNumberKernel.cc
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/CurandRandomNumberKernel.cc
@@ -5,7 +5,6 @@
 
 #include "RandomNumberKernels.h"
 
-#include "CommonRandomNumbers.h"
 #include "CudaRuntime.h"
 #include "MemoryBuffers.h"
 
@@ -31,25 +30,6 @@ namespace mg5amcCpu
 #endif
 {
   //--------------------------------------------------------------------------
-
-  CommonRandomNumberKernel::CommonRandomNumberKernel( BufferRndNumMomenta& rnarray )
-    : RandomNumberKernelBase( rnarray )
-    , m_seed( 20211220 )
-  {
-    if( m_rnarray.isOnDevice() )
-      throw std::runtime_error( "CommonRandomNumberKernel on host with a device random number array" );
-  }
-
-  //--------------------------------------------------------------------------
-
-  void CommonRandomNumberKernel::generateRnarray()
-  {
-    std::vector<double> rnd = CommonRandomNumbers::generate<double>( m_rnarray.size(), m_seed ); // NB: generate as double (HARDCODED)
-    std::copy( rnd.begin(), rnd.end(), m_rnarray.data() );                                       // NB: copy may imply a double-to-float conversion
-  }
-
-  //--------------------------------------------------------------------------
-
 #ifndef MGONGPU_HAS_NO_CURAND
   CurandRandomNumberKernel::CurandRandomNumberKernel( BufferRndNumMomenta& rnarray, const bool onDevice )
     : RandomNumberKernelBase( rnarray )
