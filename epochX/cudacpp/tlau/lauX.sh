584248c87207e47006556f5f05bda17144bd9af4
Andrea Valassi <andrea.valassi@cern.ch>
2023-06-09 20:32:14 +0200

    [runcard] add a first embryo of a launch script tlau/lauX.sh (#683)
    Several limitations, including
    - the script still stops to ask interactively if run/param card should be edited
    - the param card file is overwritten!
    - the make_opts file is overwritten!

---
+++ epochX/cudacpp/tlau/lauX.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright (C) 2020-2023 CERN and UCLouvain.
+# Licensed under the GNU Lesser General Public License (version 3 or later).
+# Created by: A. Valassi (Jun 2023) for the MG5aMC CUDACPP plugin.
+
+cd $(dirname $0)/..
+proc=gg_tt.mad
+echo "Execute $(basename $0) for process ${proc} in directory $(pwd)"
+
+set -x # verbose
+
+rm -rf $proc; git checkout $proc; cd $proc
+MG5AMC_CARD_PATH=$(pwd)/Cards ./bin/generate_events 
+
