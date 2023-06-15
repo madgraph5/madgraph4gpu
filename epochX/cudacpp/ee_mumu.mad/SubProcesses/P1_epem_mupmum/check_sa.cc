d48c4aba45375e11059e51e1989720ba2d4f7f5e
Jorgen T <jorgen.teig@gmail.com>
2023-06-15 16:53:34 +0200

    Testing 10

--- epochX/cudacpp/ee_mumu.mad/SubProcesses/P1_epem_mupmum/check_sa.cc
+++ epochX/cudacpp/ee_mumu.mad/SubProcesses/P1_epem_mupmum/check_sa.cc
@@ -106,8 +106,10 @@ main( int argc, char** argv )
 #ifdef MGONGPUCPP_CUDACPP
   RandomNumberMode rndgen = RandomNumberMode::CurandDevice; // default on GPU
 #elif not defined MGONGPU_HAS_NO_CURAND
+  #warning Using CurandHost
   RandomNumberMode rndgen = RandomNumberMode::CurandHost;  // default on CPU if build has curand
 #else
+  #warning Using CommonRandom
   RandomNumberMode rndgen = RandomNumberMode::CommonRandom; // default on CPU if build has no curand
 #endif
   // Rambo sampling mode (NB RamboHost implies CommonRandom or CurandHost!)
