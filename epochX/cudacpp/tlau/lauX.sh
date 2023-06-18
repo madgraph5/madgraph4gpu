d5508ecdebfdcf4304c19a5f1a89360ad499d991
Andrea Valassi <andrea.valassi@cern.ch>
2023-06-18 11:43:52 +0200

    [launch] in lauX.sh further decrease unweighted events to 10 in ggttggg as this should also affect refine?

--- epochX/cudacpp/tlau/lauX.sh
+++ epochX/cudacpp/tlau/lauX.sh
@@ -45,12 +45,12 @@ function getnevt()
   elif [ "${proc}" == "gg_ttg" ]; then
     nevt=10000
   elif [ "${proc}" == "gg_ttgg" ]; then
-    nevt=1000
-  elif [ "${proc}" == "gg_ttggg" ]; then
     nevt=100
+  elif [ "${proc}" == "gg_ttggg" ]; then
+    nevt=10
   else
     echo "WARNING! Unknown process ${proc}" > /dev/stderr
-    nevt=100
+    nevt=10
   fi
   echo $nevt
 }
