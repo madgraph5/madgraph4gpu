18dd2623fb5768593b2b96d2544033a0c17e7b9f
Andrea Valassi <andrea.valassi@cern.ch>
2023-07-17 12:48:52 +0200

    [fpe] in ggtt.sa HelAmps_sm.h, new BUG FIX FOR #701 in function vxxxxx
    This builds and runs ok. The FPE (always in debug mode) is now moved from vxxxxx to the next oxxxxx
    
    Running main() from /data/avalassi/GPU2023/madgraph4gpuX/test/googletest/googletest/src/gtest_main.cc
    [==========] Running 3 tests from 3 test suites.
    [----------] Global test environment set-up.
    [----------] 1 test from SIGMA_SM_GG_TTX_CPU_XXX
    [ RUN      ] SIGMA_SM_GG_TTX_CPU_XXX.testxxx
    nsp=-1 ievt=0: 500, 0, 0, 500,
    Prepare test ixxxxx ievt=0
    Prepare test ipzxxx ievt=0
    Prepare test vxxxxx ievt=0
    Prepare test sxxxxx ievt=0
    Prepare test oxxxxx ievt=0
    Floating Point Exception (CPU neppV=4): 'oxxxxx' ievt=0

--- epochX/cudacpp/gg_tt.sa/src/HelAmps_sm.h
+++ epochX/cudacpp/gg_tt.sa/src/HelAmps_sm.h
@@ -450,108 +450,134 @@ namespace mg5amcCpu
           const int ipar )        // input: particle# out of npar
   {
     mgDebug( 0, __FUNCTION__ );
-    const fptype_sv& pvec0 = M_ACCESS::kernelAccessIp4IparConst( momenta, 0, ipar );
-    const fptype_sv& pvec1 = M_ACCESS::kernelAccessIp4IparConst( momenta, 1, ipar );
-    const fptype_sv& pvec2 = M_ACCESS::kernelAccessIp4IparConst( momenta, 2, ipar );
-    const fptype_sv& pvec3 = M_ACCESS::kernelAccessIp4IparConst( momenta, 3, ipar );
-    cxtype_sv* vc = W_ACCESS::kernelAccess( wavefunctions );
+    // NEW IMPLEMENTATION FIXING FLOATING POINT EXCEPTIONS IN SIMD CODE (#701)
+    // USE _SV SUFFIXES EVERYWHERE AND ITERATE EXPLICITLY OVER ALL ELEMENTS OF SIMD VECTORS IF NEEDED
+    const fptype_sv& pvec0_sv = M_ACCESS::kernelAccessIp4IparConst( momenta, 0, ipar );
+    const fptype_sv& pvec1_sv = M_ACCESS::kernelAccessIp4IparConst( momenta, 1, ipar );
+    const fptype_sv& pvec2_sv = M_ACCESS::kernelAccessIp4IparConst( momenta, 2, ipar );
+    const fptype_sv& pvec3_sv = M_ACCESS::kernelAccessIp4IparConst( momenta, 3, ipar );
+    cxtype_sv* vc_sv = W_ACCESS::kernelAccess( wavefunctions );
     const fptype sqh = fpsqrt( 0.5 ); // AV this is > 0!
     const fptype hel = nhel;
-    vc[0] = cxmake( pvec0 * (fptype)nsv, pvec3 * (fptype)nsv );
-    vc[1] = cxmake( pvec1 * (fptype)nsv, pvec2 * (fptype)nsv );
+    vc_sv[0] = cxmake( pvec0_sv * (fptype)nsv, pvec3_sv * (fptype)nsv );
+    vc_sv[1] = cxmake( pvec1_sv * (fptype)nsv, pvec2_sv * (fptype)nsv );
     if( vmass != 0. )
     {
       const int nsvahl = nsv * std::abs( hel );
-      const fptype_sv pt2 = ( pvec1 * pvec1 ) + ( pvec2 * pvec2 );
-      const fptype_sv pp = fpmin( pvec0, fpsqrt( pt2 + ( pvec3 * pvec3 ) ) );
-      const fptype_sv pt = fpmin( pp, fpsqrt( pt2 ) );
+      const fptype_sv pt2_sv = ( pvec1_sv * pvec1_sv ) + ( pvec2_sv * pvec2_sv );
+      const fptype_sv pp_sv = fpmin( pvec0_sv, fpsqrt( pt2_sv + ( pvec3_sv * pvec3_sv ) ) );
+      const fptype_sv pt_sv = fpmin( pp_sv, fpsqrt( pt2_sv ) );
       const fptype hel0 = 1. - std::abs( hel );
-#ifndef MGONGPU_CPPSIMD
-      if( pp == 0. )
+      if( maskand( pp_sv == 0. ) )
       {
-        vc[2] = cxmake( 0., 0. );
-        vc[3] = cxmake( -hel * sqh, 0. );
-        vc[4] = cxmake( 0., nsvahl * sqh );
-        vc[5] = cxmake( hel0, 0. );
+        vc_sv[2] = cxzero_sv();
+        vc_sv[3] = cxtype_sv( -hel * sqh );                              // IIII=0000
+        vc_sv[4] = cxtype_sv( fptype_v{}, fptype_v{} + (nsvahl * sqh) ); // RRRR=0000
+        vc_sv[5] = cxtype_sv( hel0 );                                    // IIII=0000
+      }
+      else if( maskand( pp_sv != 0. ) && maskand( pt_sv == 0. ) )
+      {
+        const fptype_sv emp_sv = pvec0_sv / ( vmass * pp_sv );
+        vc_sv[2] = cxtype_sv( hel0 * pp_sv / vmass );                                       // IIII=0000
+        vc_sv[5] = cxtype_sv( hel0 * pvec3_sv * emp_sv + hel * pt_sv / pp_sv * sqh );       // IIII=0000
+        vc_sv[3] = cxtype_sv( -hel * sqh );                                                 // IIII=0000
+        vc_sv[4] = cxtype_sv( fptype_v{}, nsvahl * fpternary( pvec3_sv < 0., -sqh, sqh ) ); // AV: removed an abs here
+      }
+      else if( maskand( pp_sv != 0. ) && maskand( pt_sv != 0. ) )
+      {
+        const fptype_sv emp_sv = pvec0_sv / ( vmass * pp_sv );
+        vc_sv[2] = cxtype_sv( hel0 * pp_sv / vmass );                                 // IIII=0000
+        vc_sv[5] = cxtype_sv( hel0 * pvec3_sv * emp_sv + hel * pt_sv / pp_sv * sqh ); // IIII=0000
+        const fptype_sv pzpt_sv = pvec3_sv / ( pp_sv * pt_sv ) * sqh * hel;
+        vc_sv[3] = cxmake( hel0 * pvec1_sv * emp_sv - pvec1_sv * pzpt_sv, -nsvahl * pvec2_sv / pt_sv * sqh );
+        vc_sv[4] = cxmake( hel0 * pvec2_sv * emp_sv - pvec2_sv * pzpt_sv, nsvahl * pvec1_sv / pt_sv * sqh );
       }
       else
       {
-        const fptype emp = pvec0 / ( vmass * pp );
-        vc[2] = cxmake( hel0 * pp / vmass, 0. );
-        vc[5] = cxmake( hel0 * pvec3 * emp + hel * pt / pp * sqh, 0. );
-        if( pt != 0. )
+#ifdef MGONGPU_CPPSIMD
+        const fptype_sv emp_sv = pvec0_sv / ( vmass * pp_sv );
+        for( int ieppV = 0; ieppV < neppV; ieppV++ )
         {
-          const fptype pzpt = pvec3 / ( pp * pt ) * sqh * hel;
-          vc[3] = cxmake( hel0 * pvec1 * emp - pvec1 * pzpt, -nsvahl * pvec2 / pt * sqh );
-          vc[4] = cxmake( hel0 * pvec2 * emp - pvec2 * pzpt, nsvahl * pvec1 / pt * sqh );
-        }
-        else
-        {
-          vc[3] = cxmake( -hel * sqh, 0. );
-          // NB: Do not use "abs" for floats! It returns an integer with no build warning! Use std::abs!
-          //vc[4] = cxmake( 0., nsvahl * ( pvec3 < 0. ? -std::abs( sqh ) : std::abs( sqh ) ) ); // AV: why abs here?
-          vc[4] = cxmake( 0., nsvahl * ( pvec3 < 0. ? -sqh : sqh ) ); // AV: removed an abs here
-        }
-      }
+          const fptype& pp = pp_sv[ieppV];
+          const fptype& emp = emp_sv[ieppV];
+          const fptype& pvec1 = pvec1_sv[ieppV];
+          const fptype& pvec2 = pvec2_sv[ieppV];
+          const fptype& pvec3 = pvec3_sv[ieppV];
+          if( pp == 0. )
+          {
+            vc_sv[2][ieppV] = cxtype( 0., 0. );
+            vc_sv[3][ieppV] = cxtype( -hel * sqh, 0. );
+            vc_sv[4][ieppV] = cxtype( 0., nsvahl * sqh );
+            vc_sv[5][ieppV] = cxtype( hel0, 0. );
+          }
+          else
+          {
+            const fptype& pt = pt_sv[ieppV];
+            vc_sv[2][ieppV] = cxtype( hel0 * pp / vmass, 0. );
+            vc_sv[5][ieppV] = cxtype( hel0 * pvec3 * emp + hel * pt / pp * sqh );
+            if ( pt == 0. )
+            {
+              vc_sv[3][ieppV] = cxtype( -hel * sqh, 0. );
+              vc_sv[4][ieppV] = cxtype( 0., nsvahl * ( pvec3 < 0. ? -sqh : sqh ) ); // AV: removed an abs here
+            }
+            else
+            {
+              const fptype pzpt = pvec3 / ( pp * pt ) * sqh * hel;
+              vc_sv[3][ieppV] = cxtype( hel0 * pvec1 * emp - pvec1 * pzpt, -nsvahl * pvec2 / pt * sqh );
+              vc_sv[4][ieppV] = cxtype( hel0 * pvec2 * emp - pvec2 * pzpt, nsvahl * pvec1 / pt * sqh );
+            }
+          }
+        }        
 #else
-      // Branch A: pp == 0.
-      const cxtype vcA_2 = cxmake( 0, 0 );
-      const cxtype vcA_3 = cxmake( -hel * sqh, 0 );
-      const cxtype vcA_4 = cxmake( 0, nsvahl * sqh );
-      const cxtype vcA_5 = cxmake( hel0, 0 );
-      // Branch B: pp != 0.
-      const fptype_v emp = pvec0 / ( vmass * pp );
-      const cxtype_v vcB_2 = cxmake( hel0 * pp / vmass, 0 );
-      const cxtype_v vcB_5 = cxmake( hel0 * pvec3 * emp + hel * pt / pp * sqh, 0 );
-      // Branch B1: pp != 0. and pt != 0.
-      const fptype_v pzpt = pvec3 / ( pp * pt ) * sqh * hel;
-      const cxtype_v vcB1_3 = cxmake( hel0 * pvec1 * emp - pvec1 * pzpt, -(fptype)nsvahl * pvec2 / pt * sqh );
-      const cxtype_v vcB1_4 = cxmake( hel0 * pvec2 * emp - pvec2 * pzpt, (fptype)nsvahl * pvec1 / pt * sqh );
-      // Branch B2: pp != 0. and pt == 0.
-      const cxtype vcB2_3 = cxmake( -hel * sqh, 0. );
-      const cxtype_v vcB2_4 = cxmake( 0., (fptype)nsvahl * fpternary( ( pvec3 < 0 ), -sqh, sqh ) ); // AV: removed an abs here
-      // Choose between the results from branch A and branch B (and from branch B1 and branch B2)
-      const bool_v mask = ( pp == 0. );
-      const bool_v maskB = ( pt != 0. );
-      vc[2] = cxternary( mask, vcA_2, vcB_2 );
-      vc[3] = cxternary( mask, vcA_3, cxternary( maskB, vcB1_3, vcB2_3 ) );
-      vc[4] = cxternary( mask, vcA_4, cxternary( maskB, vcB1_4, vcB2_4 ) );
-      vc[5] = cxternary( mask, vcA_5, vcB_5 );
+        printf( "INTERNAL ERROR in ixxxxx: no path to this statement on GPUs or scalar C++!\n" );
+        assert( false );
 #endif
+      }
     }
     else
     {
-      const fptype_sv& pp = pvec0; // NB: rewrite the following as in Fortran, using pp instead of pvec0
-      const fptype_sv pt = fpsqrt( ( pvec1 * pvec1 ) + ( pvec2 * pvec2 ) );
-      vc[2] = cxzero_sv();
-      vc[5] = cxmake( hel * pt / pp * sqh, 0. );
-#ifndef MGONGPU_CPPSIMD
-      if( pt != 0. )
+      const fptype_sv& pp_sv = pvec0_sv; // NB: rewrite the following as in Fortran, using pp instead of pvec0
+      const fptype_sv pt_sv = fpsqrt( ( pvec1_sv * pvec1_sv ) + ( pvec2_sv * pvec2_sv ) );
+      vc_sv[2] = cxzero_sv();
+      vc_sv[5] = cxmake( hel * pt_sv / pp_sv * sqh, 0. );
+      if( maskand( pt_sv == 0. ) )
       {
-        const fptype pzpt = pvec3 / ( pp * pt ) * sqh * hel;
-        vc[3] = cxmake( -pvec1 * pzpt, -nsv * pvec2 / pt * sqh );
-        vc[4] = cxmake( -pvec2 * pzpt, nsv * pvec1 / pt * sqh );
+        vc_sv[3] = cxtype_sv( -hel * sqh );                                              // IIII=0000
+        vc_sv[4] = cxtype_sv( fptype_v{}, nsv * fpternary( pvec3_sv < 0., -sqh, sqh ) ); // AV: removed an abs here
       }
+      else if( maskand( pt_sv != 0. ) )
+      {
+        const fptype_sv pzpt_sv = pvec3_sv / ( pp_sv * pt_sv ) * sqh * hel;
+        vc_sv[3] = cxmake( -pvec1_sv * pzpt_sv, -nsv * pvec2_sv / pt_sv * sqh );
+        vc_sv[4] = cxmake( -pvec2_sv * pzpt_sv, nsv * pvec1_sv / pt_sv * sqh );
+      }        
       else
       {
-        vc[3] = cxmake( -hel * sqh, 0. );
-        // NB: Do not use "abs" for floats! It returns an integer with no build warning! Use std::abs!
-        //vc[4] = cxmake( 0, nsv * ( pvec3 < 0. ? -std::abs( sqh ) : std::abs( sqh ) ) ); // AV why abs here?
-        vc[4] = cxmake( 0., nsv * ( pvec3 < 0. ? -sqh : sqh ) ); // AV: removed an abs here
-      }
+#ifdef MGONGPU_CPPSIMD
+        for( int ieppV = 0; ieppV < neppV; ieppV++ )
+        {
+          const fptype& pp = pp_sv[ieppV];
+          const fptype& pt = pt_sv[ieppV];
+          const fptype& pvec1 = pvec1_sv[ieppV];
+          const fptype& pvec2 = pvec2_sv[ieppV];
+          const fptype& pvec3 = pvec3_sv[ieppV];
+          if( pt == 0. )
+          {
+            vc_sv[3][ieppV] = cxtype( -hel * sqh );
+            vc_sv[4][ieppV] = cxtype( 0., nsv * ( pvec3 < 0. ? -sqh : sqh ) ); // AV: removed an abs here
+          }
+          else
+          {
+            const fptype pzpt = pvec3 / ( pp * pt ) * sqh * hel;
+            vc_sv[3][ieppV] = cxtype( -pvec1 * pzpt, -nsv * pvec2 / pt * sqh );
+            vc_sv[4][ieppV] = cxtype( -pvec2 * pzpt, nsv * pvec1 / pt * sqh );
+          }
+        }
 #else
-      // Branch A: pt != 0.
-      const fptype_v pzpt = pvec3 / ( pp * pt ) * sqh * hel;
-      const cxtype_v vcA_3 = cxmake( -pvec1 * pzpt, -(fptype)nsv * pvec2 / pt * sqh );
-      const cxtype_v vcA_4 = cxmake( -pvec2 * pzpt, (fptype)nsv * pvec1 / pt * sqh );
-      // Branch B: pt == 0.
-      const cxtype vcB_3 = cxmake( -(fptype)hel * sqh, 0 );
-      const cxtype_v vcB_4 = cxmake( 0, (fptype)nsv * fpternary( ( pvec3 < 0 ), -sqh, sqh ) ); // AV: removed an abs here
-      // Choose between the results from branch A and branch B
-      const bool_v mask = ( pt != 0. );
-      vc[3] = cxternary( mask, vcA_3, vcB_3 );
-      vc[4] = cxternary( mask, vcA_4, vcB_4 );
+        printf( "INTERNAL ERROR in vxxxxx: no path to this statement on GPUs or scalar C++!\n" );
+        assert( false );
 #endif
+      }
     }
     mgDebug( 1, __FUNCTION__ );
     return;
