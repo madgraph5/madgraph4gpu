e70f2ae180dadc85cba2c9874948a43cffad13a6
Andrea Valassi <andrea.valassi@cern.ch>
2023-07-19 15:53:06 +0200

    [fpe] in ggtt.sa HelAmps_sm.h, add a printout that clearly shows the loop where the FPE occurs in oxxxxx
    Running main() from /data/avalassi/GPU2023/madgraph4gpuX/test/googletest/googletest/src/gtest_main.cc
    [==========] Running 6 tests from 6 test suites.
    [----------] Global test environment set-up.
    [----------] 1 test from SIGMA_SM_GG_TTX_CPU_XXX
    [ RUN      ] SIGMA_SM_GG_TTX_CPU_XXX.testxxx
    
    nsp=-1 ievt=0: 500, 0, 0, 500,
    Prepare test ixxxxx ievt=0
    Prepare test ipzxxx ievt=0
    Prepare test vxxxxx ievt=0
    Prepare test sxxxxx ievt=0
    Prepare test oxxxxx ievt=0
    Entering loop
    Floating Point Exception (CPU neppV=4): 'oxxxxx' ievt=0

--- epochX/cudacpp/gg_tt.sa/src/HelAmps_sm.h
+++ epochX/cudacpp/gg_tt.sa/src/HelAmps_sm.h
@@ -717,11 +717,13 @@ namespace mg5amcCpu
                                              fpsqrt( fpmax( pvec0_sv + pvec3_sv, 0. ) ) * (fptype)nsf );
 #ifdef MGONGPU_CPPSIMD
       cxtype_sv chi_sv[2] = { cxmake( sqp0p3_sv, 0. ), cxmake( -(fptype)nhel * fpsqrt( 2. * pvec0_sv ), 0. ) };
+      std::cout << "Entering loop" << std::endl;
       for( int ieppV = 0; ieppV < neppV; ieppV++ )
       {
         if( sqp0p3_sv[ieppV] != 0. )
           chi_sv[1][ieppV] = cxmake( (fptype)nh * pvec1_sv[ieppV], -pvec2_sv[ieppV] ) / sqp0p3_sv[ieppV];
       }
+      std::cout << "Completed loop" << std::endl;
 #else
       const cxtype_sv chi_sv[2] = { cxmake( sqp0p3_sv, 0. ),
                                     ( sqp0p3_sv == 0. ? cxmake( -(fptype)nhel * fpsqrt( 2. * pvec0_sv ), 0. ) : cxmake( (fptype)nh * pvec1_sv, -pvec2_sv ) / sqp0p3_sv ) };
