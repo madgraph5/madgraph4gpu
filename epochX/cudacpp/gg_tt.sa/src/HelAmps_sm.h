4be246bd90a85e4dbdcf38cca155894b68d01a4c
Andrea Valassi <andrea.valassi@cern.ch>
2023-07-20 12:07:59 +0200

    [fpe] in ggtt.sa HelAmps_sm.h, finally(?) fix the FPE issue using the volatile keyword! The FPE has gone.
    (I had to explicitly cast a volatile fptype_v to a const fptype_v so that the cxtype_v function could swallow that)
    
    Running main() from /data/avalassi/GPU2023/madgraph4gpuX/test/googletest/googletest/src/gtest_main.cc
    [==========] Running 6 tests from 6 test suites.
    [----------] Global test environment set-up.
    [----------] 1 test from SIGMA_SM_GG_TTX_CPU_XXX
    [ RUN      ] SIGMA_SM_GG_TTX_CPU_XXX.testxxx
    
    nsp=-1 ievt=0: 500, 0, 0, 500,
    Prepare test ixxxxx ievt=0
    Entering loop
    Completed loop
    Entering loop
    Completed loop
    Prepare test ipzxxx ievt=0
    Prepare test vxxxxx ievt=0
    Prepare test sxxxxx ievt=0
    Prepare test oxxxxx ievt=0
    Floating Point Exception (CPU neppV=4): 'oxxxxx' ievt=0

--- epochX/cudacpp/gg_tt.sa/src/HelAmps_sm.h
+++ epochX/cudacpp/gg_tt.sa/src/HelAmps_sm.h
@@ -280,7 +280,7 @@ namespace mg5amcCpu
       std::cout << "Entering loop" << std::endl;
       volatile fptype_v sqp0p3DENOM = fpternary( sqp0p3 != 0, sqp0p3, 1. ); // hack: dummy sqp0p3DENOM[ieppV]=1 if sqp0p3[ieppV]==0
       cxtype_sv chi[2] = { cxmake( sqp0p3, 0. ),
-                           cxternary( sqp0p3 == 0, cxmake( -(fptype)nhel * fpsqrt( 2. * pvec0 ), 0. ), cxmake( (fptype)nh * pvec1, pvec2 ) / sqp0p3DENOM ) }; // hack: dummy[ieppV] is not used if sqp0p3[ieppV]==0
+                           cxternary( sqp0p3 == 0, cxmake( -(fptype)nhel * fpsqrt( 2. * pvec0 ), 0. ), cxmake( (fptype)nh * pvec1, pvec2 ) / (const fptype_v)sqp0p3DENOM ) }; // hack: dummy[ieppV] is not used if sqp0p3[ieppV]==0
       std::cout << "Completed loop" << std::endl;
 #else
       const cxtype_sv chi[2] = { cxmake( sqp0p3, 0. ),
