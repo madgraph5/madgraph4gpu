223adc23b15376bf224881e4ed867caa9b616d23
Andrea Valassi <andrea.valassi@cern.ch>
2023-07-20 17:25:45 +0200

    [namespace/fpe] in ggtt.mad, fix cpu/gpu namespaces in MemoryAccessNumerators/Denominators.h
    This code is only used in .mad (for historical reasons the .sa does not use the single diagram enhancement)

--- epochX/cudacpp/gg_tt.mad/SubProcesses/MemoryAccessNumerators.h
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/MemoryAccessNumerators.h
@@ -9,15 +9,25 @@
 
 #include "MemoryAccessGs.h"
 
-//----------------------------------------------------------------------------
+// NB: namespaces mg5amcGpu and mg5amcCpu includes types which are defined in different ways for CPU and GPU builds (see #318 and #725)
+#ifdef __CUDACC__
+namespace mg5amcGpu
+#else
+namespace mg5amcCpu
+#endif
+{
 
-// A class describing the internal layout of memory buffers for numerators
-// This implementation reuses the plain ARRAY[nevt] implementation of MemoryAccessGs
+  //----------------------------------------------------------------------------
 
-typedef KernelAccessGs<false> HostAccessNumerators;
-typedef KernelAccessGs<true> DeviceAccessNumerators;
+  // A class describing the internal layout of memory buffers for numerators
+  // This implementation reuses the plain ARRAY[nevt] implementation of MemoryAccessGs
 
-//----------------------------------------------------------------------------
+  typedef KernelAccessGs<false> HostAccessNumerators;
+  typedef KernelAccessGs<true> DeviceAccessNumerators;
+
+  //----------------------------------------------------------------------------
+
+} // end namespace mg5amcGpu/mg5amcCpu
 
 #endif
 #endif // MemoryAccessNumerators_H
