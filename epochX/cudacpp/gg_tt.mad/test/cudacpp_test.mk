db61265cc008c9f38e0801d9e8d9fc73ca30df54
Andrea Valassi <andrea.valassi@cern.ch>
2023-07-26 00:06:01 +0200

    [icx] regenerate ggtt.mad, all ok - note that gg_tt.mad/test/cudacpp_test.mk has also changed now

--- epochX/cudacpp/gg_tt.mad/test/cudacpp_test.mk
+++ epochX/cudacpp/gg_tt.mad/test/cudacpp_test.mk
@@ -5,27 +5,45 @@
 
 THISDIR = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
+# Compiler-specific googletest build directory (#125 and #738)
+# Note: AR, CXX and FC are implicitly defined if not set externally
+# See https://www.gnu.org/software/make/manual/html_node/Implicit-Variables.html
+ifneq ($(shell $(CXX) --version | grep '^Intel(R) oneAPI DPC++/C++ Compiler'),)
+CXXNAME = icpx$(shell $(CXX) --version | head -1 | cut -d' ' -f5)
+else ifneq ($(shell $(CXX) --version | egrep '^clang'),)
+CXXNAME = clang$(shell $(CXX) --version | head -1 | cut -d' ' -f3)
+else ifneq ($(shell $(CXX) --version | grep '^g++ (GCC)'),)
+CXXNAME = gcc$(shell $(CXX) --version | head -1 | cut -d' ' -f3)
+else
+CXXNAME = unknown
+endif
+$(info CXXNAME=$(CXXNAME))
+BUILDDIR = build_$(CXXNAME)
+$(info BUILDDIR=$(BUILDDIR))
+INSTALLDIR = install_$(CXXNAME)
+$(info INSTALLDIR=$(INSTALLDIR))
+
 CXXFLAGS += -Igoogletest/googletest/include/ -std=c++11
 
-all: googletest/install/lib64/libgtest.a
+all: googletest/$(INSTALLDIR)/lib64/libgtest.a
 
 googletest/CMakeLists.txt:
 	git clone https://github.com/google/googletest.git -b release-1.11.0 googletest
 
-googletest/build/Makefile: googletest/CMakeLists.txt
-	mkdir -p googletest/build
-	cd googletest/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$(THISDIR)/googletest/install -DBUILD_GMOCK=OFF ../
+googletest/$(BUILDDIR)/Makefile: googletest/CMakeLists.txt
+	mkdir -p googletest/$(BUILDDIR)
+	cd googletest/$(BUILDDIR) && cmake -DCMAKE_INSTALL_PREFIX:PATH=$(THISDIR)/googletest/install -DBUILD_GMOCK=OFF ../
 
-googletest/build/lib/libgtest.a: googletest/build/Makefile
-	$(MAKE) -C googletest/build
+googletest/$(BUILDDIR)/lib/libgtest.a: googletest/$(BUILDDIR)/Makefile
+	$(MAKE) -C googletest/$(BUILDDIR)
 
 # NB 'make install' is no longer supported in googletest (issue 328)
 # NB keep 'lib64' instead of 'lib' as in LCG cvmfs installations
-googletest/install/lib64/libgtest.a: googletest/build/lib/libgtest.a
-	mkdir -p googletest/install/lib64
-	cp googletest/build/lib/lib*.a googletest/install/lib64/
-	mkdir -p googletest/install/include
-	cp -r googletest/googletest/include/gtest googletest/install/include/
+googletest/$(INSTALLDIR)/lib64/libgtest.a: googletest/$(BUILDDIR)/lib/libgtest.a
+	mkdir -p googletest/$(INSTALLDIR)/lib64
+	cp googletest/$(BUILDDIR)/lib/lib*.a googletest/$(INSTALLDIR)/lib64/
+	mkdir -p googletest/$(INSTALLDIR)/include
+	cp -r googletest/googletest/include/gtest googletest/$(INSTALLDIR)/include/
 
 clean:
 	rm -rf googletest
