0ed592886388eeb50afc00d77cd475ac769aabb1
Andrea Valassi <andrea.valassi@cern.ch>
2023-08-10 09:04:50 +0200

    [color/pptt] ** COMPLETE COLOR/PPTT (1st part of AUG2023 patch) ** regenerate pp_tt012j.mad including coloramps fixes for #655 and #713

--- epochX/cudacpp/pp_tt012j.mad/SubProcesses/P2_gg_ttxuux/coloramps.h
+++ epochX/cudacpp/pp_tt012j.mad/SubProcesses/P2_gg_ttxuux/coloramps.h
@@ -43,8 +43,8 @@ namespace mgOnGpu
     { false, false, false, false, false, true, false, false, false, false, true, false },
     { false, true, false, false, false, false, false, false, false, false, false, false },
     { false, true, false, false, false, false, true, false, false, false, false, false },
-    { false, true, false, false, false, true, true, false, false, false, true, false },
-    { false, false, true, false, false, true, true, false, false, true, false, false }
+    { false, true, true, false, false, true, true, false, false, true, true, false },
+    { false, true, false, false, false, true, true, false, false, false, true, false }
   };
 
 }
