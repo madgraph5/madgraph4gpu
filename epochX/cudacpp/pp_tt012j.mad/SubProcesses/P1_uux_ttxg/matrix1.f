fa23310a912ff3cb374d391c7f92562787be57d6
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2023-09-08 17:39:53 +0200

    Regenerate code for all .mad directories.
    The following changes are included:
    - Updated mg5. mg5 sets zero-width t-channel to True.
    - Removed all timers from smatrix function. This signficantly slowed
      down the Fortran MEs.
    - Link counters.cc and ompnumthreads into subprocesses instead of
    copying them.

--- epochX/cudacpp/pp_tt012j.mad/SubProcesses/P1_uux_ttxg/matrix1.f
+++ epochX/cudacpp/pp_tt012j.mad/SubProcesses/P1_uux_ttxg/matrix1.f
@@ -428,7 +428,6 @@ C     1 T(3,4) T(5,2,1)
 C     ----------
 C     BEGIN CODE
 C     ----------
-      call counters_matrix1_start()
       IF (FIRST) THEN
         FIRST=.FALSE.
         IF(ZERO.NE.0D0) FK_ZERO = SIGN(MAX(ABS(ZERO), ABS(ZERO
@@ -518,7 +517,6 @@ C     JAMPs contributing to orders ALL_ORDERS=1
         ENDDO
       ENDDO
 
-      call counters_matrix1_stop()
       END
 
       SUBROUTINE PRINT_ZERO_AMP_1()
