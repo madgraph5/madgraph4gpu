28b14ae37f237d64ec4338cb8f5304b173700c25
Stefan Roiser <stefan.roiser@cern.ch>
2023-09-24 13:37:49 +0200

    add variable LDEXTRAFLAGS to enable passing in extra flags for the linking, this is currently needed as the latest upgrade to MacOS 13.6 and XCode 15 broke the builds. Until this is fixed the flags need to contain -ld_classic -lstdc++

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/cudacpp_src.mk
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/cudacpp_src.mk
@@ -259,11 +259,11 @@ endif
 ifneq ($(NVCC),)
 $(LIBDIR)/lib$(MG5AMC_COMMONLIB).so : $(cxx_objects) $(cu_objects)
 	@if [ ! -d $(LIBDIR) ]; then echo "mkdir -p $(LIBDIR)"; mkdir -p $(LIBDIR); fi
-	$(NVCC) -shared -o $@ $(cxx_objects) $(cu_objects)
+	$(NVCC) -shared -o $@ $(cxx_objects) $(cu_objects) $(LDEXTRAFLAGS)
 else
 $(LIBDIR)/lib$(MG5AMC_COMMONLIB).so : $(cxx_objects)
 	@if [ ! -d $(LIBDIR) ]; then echo "mkdir -p $(LIBDIR)"; mkdir -p $(LIBDIR); fi
-	$(CXX) -shared -o $@ $(cxx_objects)
+	$(CXX) -shared -o $@ $(cxx_objects) $(LDEXTRAFLAGS)
 endif
 
 #-------------------------------------------------------------------------------
