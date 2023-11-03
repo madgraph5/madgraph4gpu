113d332e6eed6cd8cd43db40284fea876b9f0468
Andrea Valassi <andrea.valassi@cern.ch>
2023-11-03 07:51:52 +0100

    [oct23av] in ggtt.mad cudacpp.mk, use ':=' to set CUDACPP_MAKEFILE only once at the beginning and avoid corruption when make_opts is included #787
    make -f cudacpp.mk cleanall
      CUDACPP_MAKEFILE='cudacpp.mk'
      CUDACPP_MAKEFILE='cudacpp.mk'
      cudacpp.mk:43: *** exit.  Stop.
    
    However, this is still sensitive to variables coming from outside
    
    make -f cudacpp.mk cleanall CUDACPP_MAKEFILE=pippo
      CUDACPP_MAKEFILE='pippo'
      CUDACPP_MAKEFILE='pippo'
      cudacpp.mk:43: *** exit.  Stop.

--- epochX/cudacpp/gg_tt.mad/SubProcesses/cudacpp.mk
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/cudacpp.mk
@@ -5,8 +5,9 @@
 
 #=== Determine the name of this makefile (https://ftp.gnu.org/old-gnu/Manuals/make-3.80/html_node/make_17.html)
 #=== NB: different names (e.g. cudacpp.mk and cudacpp_src.mk) are used in the Subprocess and src directories
+#=== NB: use ':=' to ensure that the value is not modified further down after including make_opts
 
-override CUDACPP_MAKEFILE = $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
+CUDACPP_MAKEFILE := $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
 $(info CUDACPP_MAKEFILE='$(CUDACPP_MAKEFILE)')
 
 CUDACPP_SRC_MAKEFILE = cudacpp_src.mk
