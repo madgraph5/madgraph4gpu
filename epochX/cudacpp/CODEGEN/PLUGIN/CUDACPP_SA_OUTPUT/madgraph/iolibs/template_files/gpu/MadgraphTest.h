55a0af2c369a517883cdf0df5e6547f22fccfbf2
Andrea Valassi <andrea.valassi@cern.ch>
2023-11-04 19:42:41 +0100

    [actions] in CODEGEN MadgraphTest.h, add an env variable CUDACPP_RUNTEST_DUMPEVENTS to decide whether events should be dumped

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/MadgraphTest.h
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/MadgraphTest.h
@@ -215,7 +215,8 @@ TEST_P( MadgraphTest, CompareMomentaAndME )
 #endif
   constexpr fptype energy = 1500; // historical default, Ecms = 1500 GeV = 1.5 TeV (above the Z peak)
   // Dump events to a new reference file?
-  constexpr bool dumpEvents = false;
+  const char* dumpEventsC = getenv( "CUDACPP_RUNTEST_DUMPEVENTS" );
+  const bool dumpEvents = ( dumpEventsC != 0 ) && ( std::string( dumpEventsC ) != "" );
   std::string dumpFileName = std::string( "dump_" ) + testing::UnitTest::GetInstance()->current_test_info()->test_suite_name() + '.' + testing::UnitTest::GetInstance()->current_test_info()->name() + ".txt";
   while( dumpFileName.find( '/' ) != std::string::npos )
   {
