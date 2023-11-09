641754e2d3a5f2a4386d7740d44420dad6c2d6f8
Andrea Valassi <andrea.valassi@cern.ch>
2023-11-09 17:14:51 +0100

    [gpucpp] in CODEGEN __init__.py, mark version 3.5.2 as validated minimum version

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/__init__.py
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/__init__.py
@@ -55,6 +55,6 @@ else:
     __author__ = 'Andrea Valassi'
     __email__ = 'andrea.valassi@cern.ch'
     __version__ = (1,0,0)
-    minimal_mg5amcnlo_version = (3,5,1)
+    minimal_mg5amcnlo_version = (3,5,2)
     maximal_mg5amcnlo_version = (1000,1000,1000)
-    latest_validated_version = (3,5,1)
+    latest_validated_version = (3,5,2)
