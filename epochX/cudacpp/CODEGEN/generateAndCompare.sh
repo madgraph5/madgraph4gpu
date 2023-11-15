b291e3bea0d09176ddfedbdaa0afe4c9b141e7c2
Andrea Valassi <andrea.valassi@cern.ch>
2023-11-15 17:33:36 +0100

    [floating_type_interface] in CODEGEN generateAndCompare.sh, use --vector_size=32 to ensure that vector.inc is recreated when the runcard is modified to use 16384
    Indeed, the warning now appears again "WARNING! CPPRunCard instance has no attribute path" when regenerating ggtt.mad

--- epochX/cudacpp/CODEGEN/generateAndCompare.sh
+++ epochX/cudacpp/CODEGEN/generateAndCompare.sh
@@ -152,7 +152,7 @@ function codeGenAndDiff()
   echo -e "\n+++ Generate code for '$proc'\n"
   ###exit 0 # FOR DEBUGGING
   # Vector size for mad/madonly meexporter (VECSIZE_MEMMAX)
-  vecsize=16384 # NB THIS IS NO LONGER IGNORED (but will eventually be tunable via runcards)
+  vecsize=32 # NB THIS IS NO LONGER IGNORED (but will eventually be tunable via runcards)
   # Generate code for the specific process
   pushd $MG5AMC_HOME >& /dev/null
   mkdir -p ../TMPOUT
