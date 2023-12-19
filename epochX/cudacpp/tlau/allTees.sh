519506a1a1d94cd0439ade7ff64231acba4fac70
Andrea Valassi <andrea.valassi@cern.ch>
2023-12-20 00:11:20 +0100

    [jtmk] fix tlau/allTees.sh with new backend variables

--- epochX/cudacpp/tlau/allTees.sh
+++ epochX/cudacpp/tlau/allTees.sh
@@ -2,10 +2,11 @@
 # Copyright (C) 2020-2023 CERN and UCLouvain.
 # Licensed under the GNU Lesser General Public License (version 3 or later).
 # Created by: A. Valassi (June 2023) for the MG5aMC CUDACPP plugin.
+# Further modified by: A. Valassi (2023) for the MG5aMC CUDACPP plugin.
 
 for proc in gg_tt gg_ttg gg_ttgg gg_ttggg; do
-  ./tlau/lauX.sh -CUDA ${proc}
-  ./tlau/lauX.sh -FORTRAN ${proc}
-  ./tlau/lauX.sh -CPP ${proc}
+  ./tlau/lauX.sh -cuda ${proc}.mad
+  ./tlau/lauX.sh -fortran ${proc}.mad
+  ./tlau/lauX.sh -cpp512y ${proc}.mad
 done
 
