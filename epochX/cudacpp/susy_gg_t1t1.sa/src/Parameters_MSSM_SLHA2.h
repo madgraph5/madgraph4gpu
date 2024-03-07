726a8d4fd0bf9168a60778f1b49712ec477130a5
Andrea Valassi <andrea.valassi@cern.ch>
2024-03-07 16:07:55 +0100

    [susy] in susy_gg_t1t1.sa for HRDCOD=0: add mdl_bsmIndepParam assignments in Param.cc and remove them from Param.h

--- epochX/cudacpp/susy_gg_t1t1.sa/src/Parameters_MSSM_SLHA2.h
+++ epochX/cudacpp/susy_gg_t1t1.sa/src/Parameters_MSSM_SLHA2.h
@@ -88,9 +88,10 @@ namespace mg5amcCpu
 
     // Print couplings that are changed event by event
     //void printDependentCouplings(); // now computed event-by-event (running alphas #373)
+
     // BSM parameters that do not depend on alphaS but are needed in the computation of alphaS-dependent couplings;
     static constexpr int nBsmIndepParam = 12;
-    double mdl_bsmIndepParam[nBsmIndepParam] = { mdl_I51x33.real(), mdl_I51x33.imag(), mdl_I52x33.real(), mdl_I52x33.imag(), mdl_I51x36.real(), mdl_I51x36.imag(), mdl_I52x36.real(), mdl_I52x36.imag(), mdl_I74x33.real(), mdl_I74x33.imag(), mdl_I75x33.real(), mdl_I75x33.imag() };
+    double mdl_bsmIndepParam[nBsmIndepParam];
 
   private:
 
