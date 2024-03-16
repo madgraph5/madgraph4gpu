7fcb96bc2516a103e1f667233d5d43176c58c604
Andrea Valassi <andrea.valassi@cern.ch>
2024-03-16 11:01:31 +0100

    [susy] in heft_gg_h.sa, improve the comments for the special handling of vectors of floats #439 (which now applies also to SM processes)

--- epochX/cudacpp/heft_gg_h.sa/src/Parameters_heft.h
+++ epochX/cudacpp/heft_gg_h.sa/src/Parameters_heft.h
@@ -242,15 +242,14 @@ namespace mg5amcCpu
 #ifdef MGONGPU_HARDCODE_PARAM
       using namespace Parameters_heft;
 #else
-      // No additional parameters needed in constant memory for this BSM model
-
+      // No special handling of non-hardcoded parameters (no additional BSM parameters needed in constant memory)
 #endif
       // NB: hardcode cxtype cI(0,1) instead of cxtype (or hardcoded cxsmpl) mdl_complexi (which exists in Parameters_heft) because:
       // (1) mdl_complexi is always (0,1); (2) mdl_complexi is undefined in device code; (3) need cxsmpl conversion to cxtype in code below
       const cxtype cI( 0., 1. );
       DependentCouplings_sv out;
-      // Begin non-SM (e.g. EFT) implementation - special handling of vectors of floats (#439)
 #if not( defined MGONGPU_CPPSIMD && defined MGONGPU_FPTYPE_FLOAT )
+      // Couplings are (scalar, or vector of) doubles, or scalar floats - default implementation
       {
         const fptype_sv& G = G_sv;
         // Model parameters dependent on aS
@@ -263,9 +262,10 @@ namespace mg5amcCpu
         out.GC_13 = -( cI * mdl_GH );
       }
 #else
-      // ** NB #439: special handling is necessary ONLY FOR VECTORS OF FLOATS (variable Gs are vector floats, fixed parameters are scalar doubles)
+      // Couplings are VECTORS OF FLOATS: #439 special handling is needed (variable Gs are vector floats, fixed parameters are scalar doubles)
       // Use an explicit loop to avoid <<error: conversion of scalar ‘double’ to vector ‘fptype_sv’ {aka ‘__vector(8) float’} involves truncation>>
       // Problems may come e.g. in EFTs from multiplying a vector float (related to aS-dependent G) by a scalar double (aS-independent parameters)
+      // (NB in pure SM processes this special handling is not needed, but we keep it here for simplicity, see PR #824)
       fptype_v GC_13r_v;
       fptype_v GC_13i_v;
       for( int i = 0; i < neppV; i++ )
@@ -284,7 +284,6 @@ namespace mg5amcCpu
       }
       out.GC_13 = cxtype_v( GC_13r_v, GC_13i_v );
 #endif
-      // End non-SM (e.g. EFT) implementation - special handling of vectors of floats (#439)
       return out;
     }
 #ifdef MGONGPUCPP_GPUIMPL
