f4714473f5fef4ca9bfcadf93fb62e6785a97f15
Andrea Valassi <andrea.valassi@cern.ch>
2024-03-16 12:33:31 +0100

    [susy] in susy_gg_t1t1.sa, fix a small bug in code generation (extra "'" in the code)
    Note: the code now builds and tests succeed even if the order of parameters has changed

--- epochX/cudacpp/susy_gg_t1t1.sa/src/Parameters_MSSM_SLHA2.h
+++ epochX/cudacpp/susy_gg_t1t1.sa/src/Parameters_MSSM_SLHA2.h
@@ -772,7 +772,7 @@ namespace mg5amcCpu
 
     // BSM parameters that do not depend on alphaS but are needed in the computation of alphaS-dependent couplings;
     constexpr int nBsmIndepParam = 12;
-    __device__ constexpr double mdl_bsmIndepParam[nBsmIndepParam] = { mdl_I51x36.real(), mdl_I51x36.imag(), mdl_I52x36.real(), mdl_I52x36.imag(), mdl_I52x33.real(), mdl_I52x33.imag(), mdl_I75x33.real(), mdl_I75x33.imag(), mdl_I51x33.real(), mdl_I51x33.imag(), mdl_I74x33.real(), mdl_I74x33.imag() }';
+    __device__ constexpr double mdl_bsmIndepParam[nBsmIndepParam] = { mdl_I51x36.real(), mdl_I51x36.imag(), mdl_I52x36.real(), mdl_I52x36.imag(), mdl_I52x33.real(), mdl_I52x33.imag(), mdl_I75x33.real(), mdl_I75x33.imag(), mdl_I51x33.real(), mdl_I51x33.imag(), mdl_I74x33.real(), mdl_I74x33.imag() };
   }
 
 } // end namespace mg5amcGpu/mg5amcCpu
