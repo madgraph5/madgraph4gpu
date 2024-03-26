2db79afbc6f6131ee377218a6f412e191a2c7848
Andrea Valassi <andrea.valassi@cern.ch>
2024-03-26 19:24:39 +0100

    [jtmk] in CODEGEN cudacpp.mk, export CXXFLAGS so that it can be used in cudacpp_src.mk, and improve comments about other exports

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/cudacpp.mk
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/cudacpp.mk
@@ -21,13 +21,13 @@ override CUDACPP_SRC_MAKEFILE = cudacpp_src.mk
 # Determine CUDACPP_BUILDDIR from a DIRTAG based on BACKEND, FPTYPE, HELINL, HRDCOD and from the user-defined choice of USEBUILDDIR
 include ../../src/cudacpp_builddir.mk
 
-# Export BACKEND, FPTYPE, HELINL, HRDCOD so that there is no need to check/define them again in cudacpp_src.mk
+# Export BACKEND, FPTYPE, HELINL, HRDCOD (so that there is no need to check/define them again in cudacpp_src.mk)
 export BACKEND
 export FPTYPE
 export HELINL
 export HRDCOD
 
-# Export CUDACPP_BUILDDIR so that there is no need to check/define it again in cudacpp_src.mk
+# Export CUDACPP_BUILDDIR (so that there is no need to check/define it again in cudacpp_src.mk)
 export CUDACPP_BUILDDIR
 
 #-------------------------------------------------------------------------------
@@ -83,7 +83,7 @@ endif
 ###$(info CXXNAME=$(CXXNAME))
 override CXXNAMESUFFIX = _$(CXXNAME)
 
-# Export CXXNAMESUFFIX so that there is no need to redefine it in cudacpp_test.mk
+# Export CXXNAMESUFFIX (so that there is no need to check/define it again in cudacpp_test.mk)
 export CXXNAMESUFFIX
 
 # Dependency on test directory
@@ -166,6 +166,9 @@ endif
 ifneq ($(shell $(CXX) --version | egrep '^Apple clang'),)
 CXXFLAGS += -mmacosx-version-min=11.3
 endif
+
+# Export CXXFLAGS (so that there is no need to check/define it again in cudacpp_src.mk)
+export CXXFLAGS
 
 #-------------------------------------------------------------------------------
 
@@ -318,7 +321,7 @@ else
 
 endif
 
-# Export GPUCC and GPUFLAGS (so that there is no need to redefine them in cudacpp_src.mk)
+# Export GPUCC and GPUFLAGS (so that there is no need to check/define them again in cudacpp_src.mk)
 export GPUCC
 export GPUFLAGS
 
@@ -384,7 +387,7 @@ override OMPFLAGS = -fopenmp # enable OpenMP MT by default on all other platform
 ###override OMPFLAGS = # disable OpenMP MT on all other platforms (default before #575)
 endif
 
-# Export OMPFLAGS so that there is no need to check/define it again in cudacpp_src.mk
+# Export OMPFLAGS (so that there is no need to check/define it again in cudacpp_src.mk)
 export OMPFLAGS
 
 #-------------------------------------------------------------------------------
@@ -494,7 +497,7 @@ ifeq ($(GPUCC),)
 CXXFLAGS+= $(AVXFLAGS)
 endif
 
-# Export AVXFLAGS so that there is no need to redefine them in cudacpp_src.mk
+# Export AVXFLAGS (so that there is no need to check/define them again in cudacpp_src.mk)
 export AVXFLAGS
 
 # Set the build flags appropriate to each FPTYPE choice (example: "make FPTYPE=f")
@@ -566,7 +569,7 @@ endif
 # (Rationale: avoid mixing of builds with different random number generators)
 override TAG = $(patsubst cpp%%,%%,$(BACKEND))_$(FPTYPE)_inl$(HELINL)_hrd$(HRDCOD)_$(HASCURAND)_$(HASHIPRAND)
 
-# Export TAG so that there is no need to check/define it again in cudacpp_src.mk
+# Export TAG (so that there is no need to check/define it again in cudacpp_src.mk)
 export TAG
 
 # Build directory: current directory by default, or build.$(DIRTAG) if USEBUILDDIR==1
