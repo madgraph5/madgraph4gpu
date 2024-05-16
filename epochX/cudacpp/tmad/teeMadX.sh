c414a12fb88f8c17dd7ce03f71ecd925210a880b
Andrea Valassi <andrea.valassi@cern.ch>
2024-05-16 17:03:21 +0200

    [ewdim6] in tmad scripts, add -ewdim6udwz

--- epochX/cudacpp/tmad/teeMadX.sh
+++ epochX/cudacpp/tmad/teeMadX.sh
@@ -9,7 +9,7 @@ cd $scrdir
 
 function usage()
 {
-  echo "Usage: $0 <processes [-eemumu][-ggtt][-ggttg][-ggttgg][-ggttggg][-gguu][-gqttq][-heftggbb][-susyggtt][-susyggt1t1][-smeftggtttt]> [-flt|-fltonly|-mix|-mixonly] [-makeonly] [-makeclean] [-rmrdat] [+10x] [-checkonly]" > /dev/stderr
+  echo "Usage: $0 <processes [-eemumu][-ggtt][-ggttg][-ggttgg][-ggttggg][-gguu][-gqttq][-heftggbb][-susyggtt][-susyggt1t1][-smeftggtttt][-ewdim6udwz]> [-flt|-fltonly|-mix|-mixonly] [-makeonly] [-makeclean] [-rmrdat] [+10x] [-checkonly]" > /dev/stderr
   exit 1
 }
 
@@ -25,6 +25,7 @@ heftggbb=
 susyggtt=
 susyggt1t1=
 smeftggtttt=
+ewdim6udwz=
 
 suffs="mad"
 fptypes="d"
@@ -79,6 +80,9 @@ for arg in $*; do
   elif [ "$arg" == "-smeftggtttt" ]; then
     if [ "$smeftggtttt" == "" ]; then procs+=${procs:+ }${arg}; fi
     smeftggtttt=$arg
+  elif [ "$arg" == "-ewdim6udwz" ]; then
+    if [ "$ewdim6udwz" == "" ]; then procs+=${procs:+ }${arg}; fi
+    ewdim6udwz=$arg
   elif [ "$arg" == "-flt" ]; then
     if [ "${fptypes}" != "d" ] && [ "${fptypes}" != "d f" ]; then echo "ERROR! Options -flt, -fltonly, -mix and -mixonly are incompatible"; usage; fi
     fptypes="d f"
