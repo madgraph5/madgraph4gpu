19a2e0cb85e2306affdb143945026623e421b588
Andrea Valassi <andrea.valassi@cern.ch>
2024-06-03 16:51:05 +0200

    [tmad2] in gq_ttq.mad make_opts and cudacpp.mk, temporarely add patches for debug builds (for gdb on SIGFPE crash #845)

--- epochX/cudacpp/gq_ttq.mad/SubProcesses/cudacpp.mk
+++ epochX/cudacpp/gq_ttq.mad/SubProcesses/cudacpp.mk
@@ -387,6 +387,10 @@ else
   ###override OMPFLAGS = # disable OpenMP MT on all other platforms (default before #575)
 endif
 
+# Debug SIGFPE crash #845
+override OMPFLAGS=
+override OPTFLAGS=-g -O3
+
 #-------------------------------------------------------------------------------
 
 #=== Configure defaults and check if user-defined choices exist for RNDGEN (legacy!), HASCURAND, HASHIPRAND
