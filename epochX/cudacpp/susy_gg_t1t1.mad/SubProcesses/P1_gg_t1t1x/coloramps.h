968dd68e45cd484fca92b141398dadf80224f638
Andrea Valassi <andrea.valassi@cern.ch>
2024-06-28 12:05:58 +0200

    Revert "[color] regenerate susy_gg_t1t1 using Olivier's codegen: the results are similar to my proposal, but with some differences" This reverts commit 22328317716dee0b12e99547d4405d7d55a33e21.

--- epochX/cudacpp/susy_gg_t1t1.mad/SubProcesses/P1_gg_t1t1x/coloramps.h
+++ epochX/cudacpp/susy_gg_t1t1.mad/SubProcesses/P1_gg_t1t1x/coloramps.h
@@ -1,50 +1,43 @@
 // Copyright (C) 2020-2024 CERN and UCLouvain.
 // Licensed under the GNU Lesser General Public License (version 3 or later).
 // Created by: A. Valassi (Dec 2022) for the MG5aMC CUDACPP plugin.
-// Further modified by: A. Valassi (2022-2024) for the MG5aMC CUDACPP plugin.
+// Further modified by: O. Mattelaer, A. Valassi (2022-2024) for the MG5aMC CUDACPP plugin.
 
 #ifndef COLORAMPS_H
 #define COLORAMPS_H 1
 
-#include <map>
-
-namespace mgOnGpu
+namespace mgOnGpu /* clang-format off */
 {
   // Summary of numbering and indexing conventions for the relevant concepts (see issue #826 and PR #852)
   // - Diagram number (no variable) in [1, N_diagrams]: all values are allowed (N_diagrams distinct values)
   //   => this number is displayed for information before each block of code in CPPProcess.cc
-  // - Channel number (CHANNEL_ID) in [0, N_diagrams]: not all values are allowed (N_config <= N_diagrams distinct values)
-  //   => this number (with indexing like ps/pdf output) is passed around as an API argument between cudacpp functions
-  //   0 is allowed to fallback to no multi-channel mode.
-  // - Channel number in C indexing: "IconfiC", this is the equivalent of the Fortran iconfig
-  //   iconfigC = iconfig -1
-  //   provides a continuous index [0, N_config-1] for array
-  //  iconfigC = ChannelId_to_iconfigC[channelId]
-  //NOTE: All those ordering are event by event specific (with the intent to have those fix within a vector size/wrap   
+  // - Channel number ("channelId" in C, CHANNEL_ID in F) in [1, N_diagrams]: not all values are allowed (N_config <= N_diagrams distinct values)
+  //   => this number (with F indexing) is passed around as an API argument between cudacpp functions
+  // - Channel number in C indexing: "channelIdC" = channelID - 1
+  //   => this number (with C indexing) is used as the index of the channelIdC_to_iconfig array below
+  // - Config number ("iconfig" in C, ICONFIG in F) in [1, N_config]: all values are allowed (N_config <= N_diagrams distinct values)
+  // - Config number in C indexing: "iconfigC" = iconfig - 1
   
-  // Map channelId to iconfigC
-  // This array has N_diagrams+1 elements, but only N_config <= N_diagrams valid values
-  // unvalid values are set to -1
-  // The 0 entry is a fall back to still write events even if no multi-channel is setup (wrong color selected in that mode) 
-    __device__ constexpr int channelId_to_iconfigC[7] = {
-     0, // channelId=0: This value means not multi-channel, color will be wrong anyway -> pick the first
-     -1, // channelId=1 (diagram=1): Not consider as a channel of integration (presence of 4 point interaction?)
-     0, // channelId=2 (diagram=2) --> iconfig=1 (f77 conv) and iconfigC=0 (c conv)
-     1, // channelId=3 (diagram=3) --> iconfig=2 (f77 conv) and iconfigC=1 (c conv)
-     2, // channelId=4 (diagram=4) --> iconfig=3 (f77 conv) and iconfigC=2 (c conv)
-     3, // channelId=5 (diagram=5) --> iconfig=4 (f77 conv) and iconfigC=3 (c conv)
-     4, // channelId=6 (diagram=6) --> iconfig=5 (f77 conv) and iconfigC=4 (c conv)
+  // Map channelIdC (in C indexing, i.e. channelId-1) to iconfig (in F indexing)
+  // This array has N_diagrams elements, but only N_config <= N_diagrams valid (non-zero) values
+  __device__ constexpr int channelIdC_to_iconfig[6] = {
+    0, // channelId=1 (diagram=1) i.e. channelIdC=0 --> iconfig=None
+    1, // channelId=2 (diagram=2) i.e. channelIdC=1 --> iconfig=1
+    2, // channelId=3 (diagram=3) i.e. channelIdC=2 --> iconfig=2
+    2, // channelId=4 (diagram=4) i.e. channelIdC=3 --> iconfig=3
+    3, // channelId=5 (diagram=5) i.e. channelIdC=4 --> iconfig=4
+    4  // channelId=6 (diagram=6) i.e. channelIdC=5 --> iconfig=5
   };
 
   // Map iconfigC (in C indexing, i.e. iconfig-1) to the set of allowed colors
-  // This array has N_config <= N_diagrams elements    
-    __device__ constexpr bool icolamp[5][2] = {
-    {true, true}, // iconfigC=0, diag=2
-    {true, true}, // iconfigC=1, diag=3
-    {true, false}, // iconfigC=2, diag=4
-    {true, false}, // iconfigC=3, diag=5
-    {false, true}, // iconfigC=4, diag=6
+  // This array has N_config <= N_diagrams elements
+  __device__ constexpr bool icolamp[5][2] = {
+    {  true,  true }, // iconfig=1 i.e. iconfigC=0
+    {  true,  true }, // iconfig=2 i.e. iconfigC=1
+    {  true, false }, // iconfig=3 i.e. iconfigC=2
+    {  true, false }, // iconfig=4 i.e. iconfigC=3
+    { false,  true }  // iconfig=5 i.e. iconfigC=4
   };
 
-}
+} /* clang-format off */
 #endif // COLORAMPS_H
