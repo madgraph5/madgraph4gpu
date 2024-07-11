3941f510f7188c199c684d01441ea99a9324bae3
Andrea Valassi <andrea.valassi@cern.ch>
2024-07-11 15:11:22 +0200

    [clang/june24] in CODEGEN (backport gg_tt.mad) cudacpp.mk, fix clang17 builds #904 (disable OMP also for clang17)

--- epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/cudacpp.mk
+++ epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT/madgraph/iolibs/template_files/gpu/cudacpp.mk
@@ -380,6 +380,8 @@ ifeq ($(USEOPENMP),1)
     ###override OMPFLAGS = # disable OpenMP MT on Intel (was ok without GPUCC but not ok with GPUCC before #578)
   else ifneq ($(shell $(CXX) --version | egrep '^clang version 16'),)
     override OMPFLAGS = # disable OpenMP on clang16 #904
+  else ifneq ($(shell $(CXX) --version | egrep '^clang version 17'),)
+    override OMPFLAGS = # disable OpenMP on clang17 #904
   else ifneq ($(shell $(CXX) --version | egrep '^(clang)'),)
     override OMPFLAGS = -fopenmp
     ###override OMPFLAGS = # disable OpenMP MT on clang (was not ok without or with nvcc before #578)
