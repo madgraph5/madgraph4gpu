503a69d8ac0f7f072fe31689284529602ec7a8d9
Andrea Valassi <andrea.valassi@cern.ch>
2024-07-12 17:34:53 +0200

    [gtest2/june24] in gg_tt.mad runTest.cc, comment out gpuDeviceReset(), this makes the previous problems disappear (but there is a leak...)
    INFO: The following Floating Point Exceptions will cause SIGFPE program aborts: FE_DIVBYZERO, FE_INVALID, FE_OVERFLOW
    [==========] Running 4 tests from 4 test suites.
    [----------] Global test environment set-up.
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_XXX
    [ RUN      ] SIGMA_SM_GG_TTX_GPU_XXX.testxxx
    [       OK ] SIGMA_SM_GG_TTX_GPU_XXX.testxxx (1 ms)
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_XXX (1 ms total)
    
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_MISC
    [ RUN      ] SIGMA_SM_GG_TTX_GPU_MISC.testmisc
    [       OK ] SIGMA_SM_GG_TTX_GPU_MISC.testmisc (14 ms)
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_MISC (14 ms total)
    
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_MADGRAPH1
    [ RUN      ] SIGMA_SM_GG_TTX_GPU_MADGRAPH1.compareMomAndME
    INFO: Opening reference file ../../test/ref/dump_CPUTest.Sigma_sm_gg_ttx.txt
    [       OK ] SIGMA_SM_GG_TTX_GPU_MADGRAPH1.compareMomAndME (193 ms)
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_MADGRAPH1 (193 ms total)
    
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_MADGRAPH2
    [ RUN      ] SIGMA_SM_GG_TTX_GPU_MADGRAPH2.compareMomAndME
    INFO: Opening reference file ../../test/ref/dump_CPUTest.Sigma_sm_gg_ttx.txt
    [       OK ] SIGMA_SM_GG_TTX_GPU_MADGRAPH2.compareMomAndME (171 ms)
    [----------] 1 test from SIGMA_SM_GG_TTX_GPU_MADGRAPH2 (171 ms total)
    
    [----------] Global test environment tear-down
    [==========] 4 tests from 4 test suites ran. (381 ms total)
    [  PASSED  ] 4 tests.
    INFO: No Floating Point Exceptions have been reported
    INFO: No Floating Point Exceptions have been reported

--- epochX/cudacpp/gg_tt.mad/SubProcesses/runTest.cc
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/runTest.cc
@@ -129,7 +129,7 @@ struct CUDATest : public CUDA_CPU_TestBase
   {
     ~DeviceReset()
     {
-      checkGpu( gpuDeviceReset() ); // this is needed by cuda-memcheck --leak-check full
+      //checkGpu( gpuDeviceReset() ); // this is needed by cuda-memcheck --leak-check full
     }
   } deviceResetter;
 
