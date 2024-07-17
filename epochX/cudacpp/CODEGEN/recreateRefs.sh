22de81d0173870f6f38206d39a23383c4d115515
Andrea Valassi <andrea.valassi@cern.ch>
2024-07-17 09:33:11 +0200

    [june24] modify CODEGEN/recreateRefs.sh to recreate txt/txt2 for gg_tt and gg_ttg, but this was not needed...

--- epochX/cudacpp/CODEGEN/recreateRefs.sh
+++ epochX/cudacpp/CODEGEN/recreateRefs.sh
@@ -4,10 +4,12 @@ cudacppdir=$(cd $(dirname $0)/..; pwd)
 echo ${cudacppdir}
 
 cd ${cudacppdir}
-for d in $(git ls-tree --name-only HEAD *.mad/SubProcesses/P*); do
+#for d in $(git ls-tree --name-only HEAD *.mad/SubProcesses/P* | grep 'P1_gg_tt'); do
+for d in gg_tt.mad/SubProcesses/P1_gg_ttx gg_ttg.mad/SubProcesses/P1_gg_ttxg; do
   echo; echo "================================================================================="
   dd=${cudacppdir}/$d
   cd $dd
+  pwd
   ###make cleanall # overkill (executed from every P* subdirectory...)
   make -f cudacpp.mk -j BACKEND=cuda
   CUDACPP_RUNTEST_DUMPEVENTS=1 ./runTest_cuda.exe
