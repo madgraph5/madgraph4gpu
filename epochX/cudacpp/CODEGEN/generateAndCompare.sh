cc8d848cf32188281110167876c7c150ee85d621
Andrea Valassi <andrea.valassi@cern.ch>
2024-07-23 13:25:43 +0200

    [pptt] in CODEGEN/generateAndCompare.sh add gq_ttgq to debug #872 in pp_tt012j (with q = u c d s u~ c~ d~ s~)

--- epochX/cudacpp/CODEGEN/generateAndCompare.sh
+++ epochX/cudacpp/CODEGEN/generateAndCompare.sh
@@ -53,6 +53,9 @@ function codeGenAndDiff()
     gu_ttgu) # debug #872 in pp_tt012j
       cmd="generate g u > t t~ g u"
       ;;
+    gq_ttgq) # debug #872 in pp_tt012j
+      cmd="define q = u c d s u~ c~ d~ s~; generate g q > t t~ g q"
+      ;;
     pp_tt)
       cmd="generate p p > t t~"
       ;;
