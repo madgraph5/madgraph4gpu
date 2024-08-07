56b33d55da0e5c18fb9acc4b96f2775eae32deeb
Andrea Valassi <andrea.valassi@cern.ch>
2024-08-07 20:19:46 +0200

    [grid] further improvements in tlau parseGridpackLogs.sh, iterate over all backends in a directory

--- epochX/cudacpp/tlau/fromgridpacks/parseGridpackLogs.sh
+++ epochX/cudacpp/tlau/fromgridpacks/parseGridpackLogs.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 if [ "$1" == "" ] || [ "$2" != "" ]; then
-  echo "Usage:   $0 <output.txt>"
-  echo "Example: $0 pp_dy012j.mad/fortran/output.txt"
+  echo "Usage:   $0 <proc.mad>"
+  echo "Example: $0 pp_dy012j.mad"
   exit 1
 fi
 
-outfile=$1
-if [ ! -f $outfile ]; then
-  echo "ERROR! File not found $outfile"
+procdir=$1
+if [ ! -d $procdir ]; then
+  echo "ERROR! Directory not found $procdir"
   exit 1
 fi
 
-cat $outfile | grep "__CUDACPP_DEBUG: GridPackCmd.launch finished" | sed 's/__CUDACPP_DEBUG: /----> /'
-for msg in "PROGRAM TOTAL   " "Fortran Overhead" "Fortran MEs     "; do
-  cat $outfile | grep "\[COUNTERS\]" | grep "${msg}" | sed 's/s for.*//' | sed 's/s$//' \
-    | awk -vmsg="${msg}" -vttot=0 '{jtot=$NF; ttot += jtot}; END{print "[madevent COUNTERS]", msg, ttot}'
+for outfile in $procdir/*/output.txt; do
+  echo $outfile
+  cat $outfile | grep "__CUDACPP_DEBUG: GridPackCmd.launch finished" | sed 's/__CUDACPP_DEBUG: /----> /'
+  for msg in "PROGRAM TOTAL   " "Fortran Overhead" "Fortran MEs     "; do
+    cat $outfile | grep "\[COUNTERS\]" | grep "${msg}" | sed 's/s for.*//' | sed 's/s$//' \
+      | awk -vmsg="${msg}" -vttot=0 '{jtot=$NF; ttot += jtot}; END{print "[madevent COUNTERS]", msg, ttot}'
+  done
+  echo "--------------------------------------------------------------------------------"
 done
