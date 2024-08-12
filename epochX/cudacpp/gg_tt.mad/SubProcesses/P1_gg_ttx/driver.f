255c343911ee5d3c847e212048f5242ce9747b40
Andrea Valassi <andrea.valassi@cern.ch>
2024-08-12 18:19:33 +0200

    [prof] in gg_tt.mad, start cleaning up timers: remove the two PROGRAM timers and the three TEST timers
    ./build.cuda_d_inl0_hrd0/madevent_cuda < /tmp/avalassi/input_ggtt_x1_cudacpp
    ./build.cuda_d_inl0_hrd0/madevent_cuda < /tmp/avalassi/input_ggtt_x1_cudacpp
     [COUNTERS] PROGRAM TOTAL               :    1.0922s
     [COUNTERS] Fortran Other        (  0 ) :    0.0113s
     [COUNTERS] Fortran X2F          (  1 ) :    0.0168s for    16399 events => throughput is 1.02E-06 events/s
     [COUNTERS] Fortran PDF          (  2 ) :    0.0947s for    32768 events => throughput is 2.89E-06 events/s
     [COUNTERS] Fortran final_I/O    (  3 ) :    0.2625s for    16399 events => throughput is 1.60E-05 events/s
     [COUNTERS] CudaCpp initialise   (  5 ) :    0.4035s
     [COUNTERS] CudaCpp MEs          (  6 ) :    0.0010s for    16384 events => throughput is 6.07E-08 events/s
     [COUNTERS] Fortran initial_I/O  (  7 ) :    0.0703s
     [COUNTERS] CudaCpp finalise     (  8 ) :    0.0253s
     [COUNTERS] Fortran scale        ( 13 ) :    0.0096s for    16384 events => throughput is 5.87E-07 events/s
     [COUNTERS] Fortran rewgt        ( 14 ) :    0.0488s for    16384 events => throughput is 2.98E-06 events/s
     [COUNTERS] Fortran unwgt        ( 16 ) :    0.1485s for    16384 events => throughput is 9.06E-06 events/s
    
    ./build.none_d_inl0_hrd0/madevent_cpp < /tmp/avalassi/input_ggtt_x1_cudacpp
    ./build.none_d_inl0_hrd0/madevent_cpp < /tmp/avalassi/input_ggtt_x1_cudacpp
     [COUNTERS] PROGRAM TOTAL               :    0.7471s
     [COUNTERS] Fortran Other        (  0 ) :    0.0098s
     [COUNTERS] Fortran X2F          (  1 ) :    0.0168s for    16399 events => throughput is 1.02E-06 events/s
     [COUNTERS] Fortran PDF          (  2 ) :    0.0966s for    32768 events => throughput is 2.95E-06 events/s
     [COUNTERS] Fortran final_I/O    (  3 ) :    0.2632s for    16399 events => throughput is 1.60E-05 events/s
     [COUNTERS] CudaCpp initialise   (  5 ) :    0.0023s
     [COUNTERS] CudaCpp MEs          (  6 ) :    0.0854s for    16384 events => throughput is 5.21E-06 events/s
     [COUNTERS] Fortran initial_I/O  (  7 ) :    0.0656s
     [COUNTERS] CudaCpp finalise     (  8 ) :    0.0002s
     [COUNTERS] Fortran scale        ( 13 ) :    0.0097s for    16384 events => throughput is 5.93E-07 events/s
     [COUNTERS] Fortran rewgt        ( 14 ) :    0.0497s for    16384 events => throughput is 3.03E-06 events/s
     [COUNTERS] Fortran unwgt        ( 16 ) :    0.1479s for    16384 events => throughput is 9.03E-06 events/s

--- epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/driver.f
@@ -103,14 +103,9 @@ c Use null-terminated C-string in COUNTERS_REGISTER_COUNTER calls (maybe it is n
       CALL COUNTERS_REGISTER_COUNTER( 6, 'CudaCpp MEs'//char(0) )
       CALL COUNTERS_REGISTER_COUNTER( 7, 'Fortran initial_I/O'//char(0) )
       CALL COUNTERS_REGISTER_COUNTER( 8, 'CudaCpp finalise'//char(0) )
-      CALL COUNTERS_REGISTER_COUNTER( 12, 'Fortran TEST12'//char(0) )
       CALL COUNTERS_REGISTER_COUNTER( 13, 'Fortran scale'//char(0) )
       CALL COUNTERS_REGISTER_COUNTER( 14, 'Fortran rewgt'//char(0) )
-      CALL COUNTERS_REGISTER_COUNTER( 15, 'Fortran TEST15'//char(0) )
       CALL COUNTERS_REGISTER_COUNTER( 16, 'Fortran unwgt'//char(0) )
-      CALL COUNTERS_REGISTER_COUNTER( 17, 'Fortran TEST17'//char(0) )
-      CALL COUNTERS_REGISTER_COUNTER( 19, 'PROGRAM initial_I/O'//char(0) )
-      CALL COUNTERS_REGISTER_COUNTER( 20, 'PROGRAM sample_full'//char(0) )
 #ifdef MG5AMC_MEEXPORTER_CUDACPP
       fbridge_mode = 1 ! CppOnly=1, default for CUDACPP
 #else
@@ -167,7 +162,6 @@ c
 c     Read process number
 c
       CALL COUNTERS_START_COUNTER( 7, 1 ) ! initial_I/O=7
-      CALL COUNTERS_START_COUNTER( 19, 1 ) ! initial_I/O=7
       call open_file(lun+1, 'dname.mg', fopened)
       if (.not.fopened)then
          goto 11
@@ -239,7 +233,6 @@ c   If CKKW-type matching, read IS Sudakov grid
         endif
       endif
       CALL COUNTERS_STOP_COUNTER( 7 ) ! initial_I/O=7
-      CALL COUNTERS_STOP_COUNTER( 19 ) ! initial_I/O=19
 
 c     
 c     Get user input
@@ -276,9 +269,7 @@ c         itmin = itmin + 1
       endif
 
       write(*,*) "about to integrate ", ndim,ncall,itmax,itmin,ninvar,nconfigs
-      CALL COUNTERS_START_COUNTER( 20, 1 ) ! sample_full=20
       call sample_full(ndim,ncall,itmax,itmin,dsig,ninvar,nconfigs,VECSIZE_USED)
-      CALL COUNTERS_STOP_COUNTER( 20 ) ! sample_full=20
 
 c
 c     Now write out events to permanent file
