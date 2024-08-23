af682f3693439550498fafd5b66f0f77fb1f99e3
Andrea Valassi <andrea.valassi@cern.ch>
2024-08-23 13:16:08 +0200

    [prof] add gux_taptamggux.mad to the repo, for timer tests
    ./build.cuda_d_inl0_hrd0/madevent_cuda < /tmp/avalassi/input_ggtt_x1_cudacpp
     [COUNTERS] *** USING RDTSC-BASED TIMERS ***
     [COUNTERS] PROGRAM TOTAL                         :    3.8456s
     [COUNTERS] Fortran Other                  (  0 ) :    0.1201s
     [COUNTERS] Fortran Initialise(I/O)        (  1 ) :    0.0671s
     [COUNTERS] Fortran PhaseSpaceSampling     (  3 ) :    2.6146s for  1087437 events => throughput is 4.16E+05 events/s
     [COUNTERS] Fortran PDFs                   (  4 ) :    0.0961s for    32768 events => throughput is 3.41E+05 events/s
     [COUNTERS] Fortran UpdateScaleCouplings   (  5 ) :    0.1713s for    16384 events => throughput is 9.56E+04 events/s
     [COUNTERS] Fortran Reweight               (  6 ) :    0.0488s for    16384 events => throughput is 3.36E+05 events/s
     [COUNTERS] Fortran Unweight(LHE-I/O)      (  7 ) :    0.0685s for    16384 events => throughput is 2.39E+05 events/s
     [COUNTERS] Fortran SamplePutPoint         (  8 ) :    0.1250s for  1087437 events => throughput is 8.70E+06 events/s
     [COUNTERS] CudaCpp Initialise             ( 11 ) :    0.4711s
     [COUNTERS] CudaCpp Finalise               ( 12 ) :    0.0271s
     [COUNTERS] CudaCpp MEs                    ( 19 ) :    0.0358s for    16384 events => throughput is 4.58E+05 events/s
     [COUNTERS] OVERALL NON-MEs                ( 31 ) :    3.8099s
     [COUNTERS] OVERALL MEs                    ( 32 ) :    0.0358s for    16384 events => throughput is 4.58E+05 events/s
    
    CUDACPP_RUNTIME_USECHRONOTIMERS=1 ./build.cuda_d_inl0_hrd0/madevent_cuda < /tmp/avalassi/input_ggtt_x1_cudacpp
     [COUNTERS] *** USING STD::CHRONO TIMERS ***
     [COUNTERS] PROGRAM TOTAL                         :    3.9229s
     [COUNTERS] Fortran Other                  (  0 ) :    0.1521s
     [COUNTERS] Fortran Initialise(I/O)        (  1 ) :    0.0677s
     [COUNTERS] Fortran PhaseSpaceSampling     (  3 ) :    2.6424s for  1087437 events => throughput is 4.12E+05 events/s
     [COUNTERS] Fortran PDFs                   (  4 ) :    0.0971s for    32768 events => throughput is 3.37E+05 events/s
     [COUNTERS] Fortran UpdateScaleCouplings   (  5 ) :    0.1721s for    16384 events => throughput is 9.52E+04 events/s
     [COUNTERS] Fortran Reweight               (  6 ) :    0.0488s for    16384 events => throughput is 3.35E+05 events/s
     [COUNTERS] Fortran Unweight(LHE-I/O)      (  7 ) :    0.0687s for    16384 events => throughput is 2.38E+05 events/s
     [COUNTERS] Fortran SamplePutPoint         (  8 ) :    0.1388s for  1087437 events => throughput is 7.83E+06 events/s
     [COUNTERS] CudaCpp Initialise             ( 11 ) :    0.4717s
     [COUNTERS] CudaCpp Finalise               ( 12 ) :    0.0278s
     [COUNTERS] CudaCpp MEs                    ( 19 ) :    0.0355s for    16384 events => throughput is 4.61E+05 events/s
     [COUNTERS] OVERALL NON-MEs                ( 31 ) :    3.8873s

---
+++ epochX/cudacpp/gux_taptamggux.mad/SubProcesses/counters.cc
@@ -0,0 +1,223 @@
+// Copyright (C) 2020-2024 CERN and UCLouvain.
+// Licensed under the GNU Lesser General Public License (version 3 or later).
+// Created by: A. Valassi (Dec 2022) for the MG5aMC CUDACPP plugin.
+// Further modified by: S. Hageboeck, A. Valassi (2022-2024) for the MG5aMC CUDACPP plugin.
+
+#include "timer.h"
+#define TIMERTYPE std::chrono::high_resolution_clock
+
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring> // for strlen
+#include <iostream>
+#include <sstream>
+#include <string_view>
+
+// NB1: The C functions counters_xxx_ in this file are called by Fortran code
+// Hence the trailing "_": 'call counters_end()' links to counters_end_
+// See http://www.yolinux.com/TUTORIALS/LinuxTutorialMixingFortranAndC.html
+
+// NB2: This file also contains C++ code and is built using g++
+// Hence use 'extern "C"' to avoid name mangling by the C++ compiler
+// See https://www.geeksforgeeks.org/extern-c-in-c
+
+extern "C"
+{
+  namespace counters
+  {
+    constexpr int NCOUNTERSMAX = 30;
+    static bool disablecalltimers = false;
+    static bool disabletesttimers = false;
+#ifdef MGONGPU_HASRDTSC
+    static bool usechronotimers = false;
+#else
+    constexpr bool usechronotimers = true;
+#endif
+    // Overall program timer
+    static mgOnGpu::ChronoTimer<TIMERTYPE> program_chronotimer;
+    static mgOnGpu::RdtscTimer program_rdtsctimer;
+    // Individual timers
+    static std::string array_tags[NCOUNTERSMAX + 3];
+    static bool array_istesttimer[NCOUNTERSMAX + 3];
+    static mgOnGpu::ChronoTimer<TIMERTYPE> array_chronotimers[NCOUNTERSMAX + 3];
+    static mgOnGpu::RdtscTimer array_rdtsctimers[NCOUNTERSMAX + 3];
+    static int array_counters[NCOUNTERSMAX + 3] = { 0 };
+  }
+
+  inline bool starts_with( std::string_view str, std::string_view prefix ) // https://stackoverflow.com/a/42844629
+  {
+    return str.size() >= prefix.size() && str.compare( 0, prefix.size(), prefix ) == 0;
+  }
+
+  inline bool ends_with( std::string_view str, std::string_view suffix ) // https://stackoverflow.com/a/42844629
+  {
+    return str.size() >= suffix.size() && str.compare( str.size() - suffix.size(), suffix.size(), suffix ) == 0;
+  }
+
+  void counters_initialise_()
+  {
+    using namespace counters;
+    if( getenv( "CUDACPP_RUNTIME_DISABLECALLTIMERS" ) ) disablecalltimers = true;
+    if( getenv( "CUDACPP_RUNTIME_DISABLETESTTIMERS" ) ) disabletesttimers = true;
+#ifdef MGONGPU_HASRDTSC
+    if( getenv( "CUDACPP_RUNTIME_USECHRONOTIMERS" ) ) usechronotimers = true;
+#endif
+    for( int icounter = 0; icounter < NCOUNTERSMAX + 3; icounter++ )
+    {
+      array_tags[icounter] = "";           // ensure that this is initialized to ""
+      array_istesttimer[icounter] = false; // ensure that this is initialized to false
+    }
+    if( usechronotimers )
+      program_chronotimer.start();
+    else
+      program_rdtsctimer.start();
+    return;
+  }
+
+  void counters_register_counter_( const int* picounter, const char* ctag )
+  {
+    using namespace counters;
+    int icounter = *picounter;
+    std::cout << "INFO: register counter #" << icounter << " with tag '" << ctag << "' (tag strlen=" << strlen( ctag ) << ")" << std::endl;
+    const std::string tag( ctag );
+    if( icounter < 1 || icounter >= NCOUNTERSMAX + 1 )
+    {
+      std::ostringstream sstr;
+      sstr << "ERROR! Invalid counter# '" << icounter << "' (valid values are 1 to " << NCOUNTERSMAX << ")";
+      throw std::runtime_error( sstr.str() );
+    }
+    if( tag == "" )
+    {
+      std::ostringstream sstr;
+      sstr << "ERROR! Invalid empty tag ''";
+      throw std::runtime_error( sstr.str() );
+    }
+    if( array_tags[icounter] == "" )
+    {
+      array_tags[icounter] = tag;
+      if( starts_with( array_tags[icounter], "TEST" ) ) array_istesttimer[icounter] = true;
+    }
+    else
+    {
+      std::ostringstream sstr;
+      sstr << "ERROR! counter #" << icounter << " already exists with tag '" << array_tags[icounter] << "'";
+      throw std::runtime_error( sstr.str() );
+    }
+    return;
+  }
+
+  void counters_start_counter_( const int* picounter, const int* pnevt )
+  {
+    using namespace counters;
+    if( disablecalltimers ) return;
+    int icounter = *picounter;
+    if( disabletesttimers && array_istesttimer[icounter] ) return;
+    if( array_tags[icounter] == "" )
+    {
+      std::ostringstream sstr;
+      sstr << "ERROR! counter #" << icounter << " does not exist";
+      throw std::runtime_error( sstr.str() );
+    }
+    array_counters[icounter] += *pnevt;
+    if( usechronotimers )
+      array_chronotimers[icounter].start();
+    else
+      array_rdtsctimers[icounter].start();
+    return;
+  }
+
+  void counters_stop_counter_( const int* picounter )
+  {
+    using namespace counters;
+    if( disablecalltimers ) return;
+    int icounter = *picounter;
+    if( disabletesttimers && array_istesttimer[icounter] ) return;
+    if( array_tags[icounter] == "" )
+    {
+      std::ostringstream sstr;
+      sstr << "ERROR! counter #" << icounter << " does not exist";
+      throw std::runtime_error( sstr.str() );
+    }
+    if( usechronotimers )
+      array_chronotimers[icounter].stop();
+    else
+      array_rdtsctimers[icounter].stop();
+    return;
+  }
+
+  void counters_finalise_()
+  {
+    using namespace counters;
+    // Dump program counters
+    if( usechronotimers )
+      program_chronotimer.stop();
+    else
+      program_rdtsctimer.stop();
+    float program_totaltime = ( usechronotimers ? program_chronotimer.getTotalDurationSeconds() : program_rdtsctimer.getTotalDurationSeconds() );
+    if( usechronotimers )
+      printf( " [COUNTERS] *** USING STD::CHRONO TIMERS ***\n" );
+    else
+      printf( " [COUNTERS] *** USING RDTSC-BASED TIMERS ***\n" );
+    printf( " [COUNTERS] PROGRAM TOTAL                         : %9.4fs\n", program_totaltime );
+    if( disablecalltimers ) return;
+    // Extract time duration from all timers
+    float array_totaltimes[NCOUNTERSMAX + 3] = { 0 };
+    for( int icounter = 1; icounter < NCOUNTERSMAX + 1; icounter++ )
+    {
+      if( usechronotimers )
+        array_totaltimes[icounter] = array_chronotimers[icounter].getTotalDurationSeconds();
+      else
+        array_totaltimes[icounter] = array_rdtsctimers[icounter].getTotalDurationSeconds();
+    }
+    // Create counter[0] "Fortran Other"
+    array_tags[0] = "Fortran Other";
+    array_counters[0] = 1;
+    array_totaltimes[0] = program_totaltime;
+    for( int icounter = 1; icounter < NCOUNTERSMAX + 1; icounter++ )
+    {
+      if( !starts_with( array_tags[icounter], "PROGRAM" ) &&
+          !starts_with( array_tags[icounter], "TEST" ) ) // skip counters whose tags start with "PROGRAM" or "TEST"
+        array_totaltimes[0] -= array_totaltimes[icounter];
+    }
+    // Create counters[NCOUNTERSMAX+2] "OVERALL MEs" and counters[NCOUNTERSMAX+1] "OVERALL NON-MEs"
+    array_tags[NCOUNTERSMAX + 2] = "OVERALL MEs";
+    array_counters[NCOUNTERSMAX + 2] = 0;
+    array_totaltimes[NCOUNTERSMAX + 2] = 0;
+    for( int icounter = 1; icounter < NCOUNTERSMAX + 1; icounter++ )
+    {
+      if( ends_with( array_tags[icounter], "MEs" ) ) // include counters whose tags end with "MEs"
+      {
+        array_counters[NCOUNTERSMAX + 2] += array_counters[icounter];
+        array_totaltimes[NCOUNTERSMAX + 2] += array_totaltimes[icounter];
+      }
+    }
+    array_tags[NCOUNTERSMAX + 1] = "OVERALL NON-MEs";
+    array_counters[NCOUNTERSMAX + 1] = 1;
+    array_totaltimes[NCOUNTERSMAX + 1] = program_totaltime - array_totaltimes[NCOUNTERSMAX + 2];
+    // Dump individual counters
+    for( int icounter = 0; icounter < NCOUNTERSMAX + 3; icounter++ )
+    {
+      if( array_tags[icounter] != "" )
+      {
+        if( array_counters[icounter] > 1 ) // event counters
+        {
+          printf( " [COUNTERS] %-30s ( %2d ) : %9.4fs for %8d events => throughput is %8.2E events/s\n",
+                  array_tags[icounter].c_str(),
+                  icounter,
+                  array_totaltimes[icounter],
+                  array_counters[icounter],
+                  array_counters[icounter] / array_totaltimes[icounter] );
+        }
+        else if( array_counters[icounter] == 1 ) // one-off counters for initialisation tasks (e.g. helicity filtering)
+        {
+          printf( " [COUNTERS] %-30s ( %2d ) : %9.4fs\n",
+                  array_tags[icounter].c_str(),
+                  icounter,
+                  array_totaltimes[icounter] );
+        }
+      }
+    }
+    return;
+  }
+}
