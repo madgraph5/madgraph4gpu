4879827fd26aaa0cf1789bd523831c7a28359b84
Andrea Valassi <andrea.valassi@cern.ch>
2024-09-26 11:38:17 +0200

    [install] in .github/workflows add script archiver.sh to create the cudacpp.tgz archive

---
+++ .github/workflows/archiver.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (C) 2020-2024 CERN and UCLouvain.
+# Licensed under the GNU Lesser General Public License (version 3 or later).
+# Created by: A. Valassi (Sep 2024) for the MG5aMC CUDACPP plugin.
+# Further modified by: A. Valassi (2024) for the MG5aMC CUDACPP plugin.
+
+# Path to the top directory of madgraphgpu
+# In the CI this would be simply $(pwd), but allow the script to be run also outside the CI
+echo "Executing $0 $*"
+topdir=$(cd $(dirname $0)/../..; pwd)
+
+# Copy all relevant plugin files to a temporary directory
+tmpdir=$(mktemp -d)
+outdir=${tmpdir}/CUDACPP_OUTPUT
+mkdir ${outdir}
+cd ${topdir}/epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT
+for file in $(git ls-tree --name-only HEAD -r); do
+  if [ "${file/acceptance_tests}" != "${file}" ]; then continue; fi # acceptance_tests are not needed for code generation
+  mkdir -p ${outdir}/$(dirname ${file})
+  cp -dp ${file} ${outdir}/${file} # preserve symlinks for AUTHORS, COPYING, COPYING.LESSER and COPYRIGHT
+done
+
+# Create the tgz archive
+outtgz=cudacpp.tgz
+cd ${tmpdir}
+tar -czf ${outtgz} CUDACPP_OUTPUT
+mv ${outtgz} ${topdir}
