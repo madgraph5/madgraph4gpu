1ed39262f576f9f9a9b5d791fef2257c3487a972
Andrea Valassi <andrea.valassi@cern.ch>
2024-09-27 14:37:16 +0200

    [install] in .github/workflows/archiver.sh, add a file VERSION to the cudacpp tarball

--- .github/workflows/archiver.sh
+++ .github/workflows/archiver.sh
@@ -9,10 +9,26 @@
 echo "Executing $0 $*"
 topdir=$(cd $(dirname $0)/../..; pwd)
 
-# Copy all relevant plugin files to a temporary directory
+# Create a temporary directory and a VERSION file
 tmpdir=$(mktemp -d)
 outdir=${tmpdir}/CUDACPP_OUTPUT
 mkdir ${outdir}
+cd ${topdir}/epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT
+outfile=${outdir}/VERSION
+touch ${outfile}
+dateformat='%Y-%m-%d_%H:%M:%S UTC'
+echo "cudacpp_version              = $(cat __init__.py | awk '/__version__/{print $3}' | sed 's/(//' | sed 's/)//' | sed 's/,/./g')" >> ${outfile}
+echo "mg5_version_current          = $(cat ../../../../../MG5aMC/mg5amcnlo/VERSION | awk '/version =/{print $3}' | sed -r 's/[^0-9.]//g')" >> ${outfile}
+echo "" >> ${outfile}
+echo "mg5_version_minimal          = $(cat __init__.py | awk '/minimal_mg5amcnlo_version/{print $3}'  | sed 's/(//' | sed 's/)//' | sed 's/,/./g')" >> ${outfile}
+echo "mg5_version_latest_validated = $(cat __init__.py | awk '/latest_validated_version/{print $3}'  | sed 's/(//' | sed 's/)//' | sed 's/,/./g')" >> ${outfile}
+echo "" >> ${outfile}
+echo "TARBALL DATE:  $(date -u +"${dateformat}")" >> ${outfile}
+echo "" >> ${outfile}
+TZ=UTC git log -n1 --date=format-local:"${dateformat}" --pretty=format:'commit         %h%ntree           %t%nparent         %p%nAuthor:        %an%nAuthorDate:    %ad%nCommitter:     %cn%nCommitterDate: %cd%nMessage:       "%s"%n' >> ${outfile}
+python -c 'print("="*132)'; cat ${outfile}; python -c 'print("="*132)'
+
+# Copy all relevant plugin files to the temporary directory
 cd ${topdir}/epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT
 for file in $(git ls-tree --name-only HEAD -r); do
   if [ "${file/acceptance_tests}" != "${file}" ]; then continue; fi # acceptance_tests are not needed for code generation
