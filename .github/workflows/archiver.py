5219bfb52740801710ec9b2ddce2fa9293662475
Andrea Valassi <andrea.valassi@cern.ch>
2024-09-30 18:56:13 +0200

    [install] in archiver.py, add the creation of an info.htm file: this is not really needed, will revert

--- .github/workflows/archiver.py
+++ .github/workflows/archiver.py
@@ -27,12 +27,31 @@ def create_infodat_file(path, versions):
         for v in versions:
             fsock.write(line%{'repo':GITHUB_REPO, 'prefix':PREFIX, 'version':v, 'suffix':SUFFIX})
 
+def create_infohtm_file(path, versions):
+    from datetime import datetime
+    line = "<li><a href=https://github.com/%(repo)s/releases/tag/%(prefix)s%(version)s%(suffix)s>%(version)s</a>\n"
+    with open(path, 'w') as fsock:
+        fsock.write("""<!DOCTYPE html>
+<html>
+<body>
+<h3>List of cudacpp releases</h3>
+<ul>
+""")
+        for v in versions:
+            fsock.write(line%{'repo':GITHUB_REPO, 'prefix':PREFIX, 'version':v, 'suffix':SUFFIX})
+        fsock.write("""</ul>
+<hr>
+<i>Last modified on %s</i>.
+</body>
+</html>"""%datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC"))
+
 if "__main__" == __name__:
-    if len(sys.argv) != 2:
-        print('Usage: python3 %s <infodat>'%sys.argv[0])
+    if len(sys.argv) != 3:
+        print('Usage: python3 %s <infodat> <infohtml>'%sys.argv[0])
         sys.exit(1)
     tags = get_all_tags()
     ###print('Tags:', tags)
     versions = get_supported_versions(tags)
     print('Supported versions:', versions)
     create_infodat_file(sys.argv[1], versions)
+    create_infohtm_file(sys.argv[2], versions)
