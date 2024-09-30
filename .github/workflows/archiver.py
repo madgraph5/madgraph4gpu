f612d2e8d04d4aa83b26ce4a3c6ccca98eaa4a7f
Andrea Valassi <andrea.valassi@cern.ch>
2024-09-30 18:55:26 +0200

    [install] improve archiver.py so that some default variables can be modified

--- .github/workflows/archiver.py
+++ .github/workflows/archiver.py
@@ -7,29 +7,32 @@
 import subprocess
 import sys
 
+GITHUB_REPO = 'valassi/madgraph4gpu/'
+
+PREFIX = 'cudacpp_for'
+SUFFIX = '_latest'
+
 def get_all_tags():
     out = subprocess.check_output(['git', 'tag']).decode()
     return out.split('\n')
 
 def get_supported_versions(tags):
-    PREFIX = 'cudacpp_for'
-    SUFFIX = '_latest'
     versions = [ t[len(PREFIX):-len(SUFFIX)] for t in tags if t.startswith(PREFIX) and t.endswith(SUFFIX)]
     versions = set(versions)
     return versions
 
-def create_info_file(path, versions):
-    line = "%(version)s https://github.com/valassi/madgraph4gpu/releases/download/TEST_cudacpp_for%(version)s_latest/cudacpp.tar.gz\n"
-    fsock = open(path, 'w')
-    for v in versions:
-        fsock.write(line%{'version':v})
+def create_infodat_file(path, versions):
+    line = "%(version)s https://github.com/%(repo)s/releases/download/%(prefix)s%(version)s%(suffix)s/cudacpp.tar.gz\n"
+    with open(path, 'w') as fsock:
+        for v in versions:
+            fsock.write(line%{'repo':GITHUB_REPO, 'prefix':PREFIX, 'version':v, 'suffix':SUFFIX})
 
 if "__main__" == __name__:
     if len(sys.argv) != 2:
-        print('Usage: python3 %s <filename>'%sys.argv[0])
+        print('Usage: python3 %s <infodat>'%sys.argv[0])
         sys.exit(1)
     tags = get_all_tags()
-    print('Tags:', tags)
+    ###print('Tags:', tags)
     versions = get_supported_versions(tags)
-    print('Versions:', versions)
-    create_info_file(sys.argv[1], versions)
+    print('Supported versions:', versions)
+    create_infodat_file(sys.argv[1], versions)
