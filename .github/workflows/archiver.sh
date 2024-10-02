89227c4a80ed4ca14efb824adf22dfdba22cfb2c
Andrea Valassi <andrea.valassi@cern.ch>
2024-10-02 20:11:48 +0200

    [install] in archiver.sh add mg5_hash_current to VERSION.txt; in archiver.yml, add it to the release notes

--- .github/workflows/archiver.sh
+++ .github/workflows/archiver.sh
@@ -9,19 +9,31 @@
 echo "Executing $0 $*"
 topdir=$(cd $(dirname $0)/../..; pwd)
 
+# Check that all git submodules have been updated
+cd ${topdir}
+if ! git submodule status | grep '^ ' > /dev/null; then
+  echo "ERROR! There are git submodules that need to be updated"
+  git submodule status
+  exit 1
+fi
+mg5_commit_current=$(git submodule status | awk '/ MG5aMC\/mg5amcnlo /{print substr($1,0,9)}')
+
 # Create a temporary directory and a VERSION.txt file
+cd ${topdir}/epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT
 tmpdir=$(mktemp -d)
 outdir=${tmpdir}/CUDACPP_OUTPUT
 mkdir ${outdir}
-cd ${topdir}/epochX/cudacpp/CODEGEN/PLUGIN/CUDACPP_SA_OUTPUT
 outfile=${outdir}/VERSION.txt
 touch ${outfile}
 dateformat='%Y-%m-%d_%H:%M:%S UTC'
+echo "(From CUDACPP_OUTPUT/__init__.py)" >> ${outfile}
 echo "cudacpp_version              = $(cat __init__.py | awk '/__version__/{print $3}' | sed 's/(//' | sed 's/)//' | sed 's/,/./g')" >> ${outfile}
-echo "mg5_version_current          = $(cat ../../../../../MG5aMC/mg5amcnlo/VERSION | awk '/version =/{print $3}' | sed -r 's/[^0-9.]//g')" >> ${outfile}
-echo "" >> ${outfile}
 echo "mg5_version_minimal          = $(cat __init__.py | awk '/minimal_mg5amcnlo_version/{print $3}'  | sed 's/(//' | sed 's/)//' | sed 's/,/./g')" >> ${outfile}
 echo "mg5_version_latest_validated = $(cat __init__.py | awk '/latest_validated_version/{print $3}'  | sed 's/(//' | sed 's/)//' | sed 's/,/./g')" >> ${outfile}
+echo "" >> ${outfile}
+echo "(From MG5AMC/mg5amcnlo)" >> ${outfile}
+echo "mg5_version_current          = $(cat ../../../../../MG5aMC/mg5amcnlo/VERSION | awk '/version =/{print $3}' | sed -r 's/[^0-9.]//g')" >> ${outfile}
+echo "mg5_commit_current           = ${mg5_commit_current}" >> ${outfile}
 echo "" >> ${outfile}
 echo "TARBALL DATE:  $(date -u +"${dateformat}")" >> ${outfile}
 echo "" >> ${outfile}
