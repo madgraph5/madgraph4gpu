ee35379f516d11259f9b9a3e85aef841695a0da1
Andrea Valassi <andrea.valassi@cern.ch>
2024-10-11 20:07:10 +0200

    [logs] in tput/summaryTable.sh remove all legacy tables and keep only v10000

--- epochX/cudacpp/tput/summaryTable.sh
+++ epochX/cudacpp/tput/summaryTable.sh
@@ -20,44 +20,14 @@ fi
 table=
 if [ "$1" == "-ALL" ] && [ "$2" == "" ]; then
   set -e
-  $0 ${quiet} -latest
-  $0 ${quiet} -bridge
-  $0 ${quiet} -hrdcod
-  $0 ${quiet} -juwels
-  $0 ${quiet} -alpaka
-  $0 ${quiet} -macm1
-  $0 ${quiet} -alphas
-  $0 ${quiet} -3xcomp
-  $0 ${quiet} -ol23silv
-  ###$0 ${quiet} -ol23gold
   $0 ${quiet} -v10000
   exit 0
-elif [ "$1" == "-latest" ]; then
-  table="latest"; shift
-elif [ "$1" == "-bridge" ]; then
-  table="bridge"; shift
-elif [ "$1" == "-hrdcod" ]; then
-  table="hrdcod"; shift
-elif [ "$1" == "-juwels" ]; then
-  table="juwels"; shift
-elif [ "$1" == "-alpaka" ]; then
-  table="alpaka"; shift
-elif [ "$1" == "-macm1" ]; then
-  table="macm1"; shift
-elif [ "$1" == "-alphas" ]; then
-  table="alphas"; shift
-elif [ "$1" == "-3xcomp" ]; then
-  table="3xcomp"; shift
-elif [ "$1" == "-ol23silv" ]; then
-  table="ol23silv"; shift
-elif [ "$1" == "-ol23gold" ]; then
-  table="ol23gold"; shift
 elif [ "$1" == "-v10000" ]; then
   table="v10000"; shift
 fi
 
 if [ "$1" != "" ] || [ "$table" == '' ]; then
-  echo "Usage: $0 [-quiet] <table [-ALL|-latest|-bridge|-hrdcod|-juwels|-alpaka|-macm1|-alphas|-3xcomp|-ol23silv|-ol23gold|-v10000]>"; exit 1
+  echo "Usage: $0 [-quiet] <table [-ALL|-v10000]>"; exit 1
 fi
 
 unames=$(uname -s)
@@ -78,59 +48,14 @@ touch $out
 #-------------------------------------------------------------------------------------------------------
 # A few reference points 
 #-------------------------------------------------------------------------------------------------------
-# 1. Code (21 Apr 2022, c0c27684): just before the alphas PR #434
-#    Logs (28 Apr 2022, 88fe36d1): cuda116/gcc102 (56 logs from allTees.sh)
-# 2. Code (28 Apr 2022, bae5c248): about to be merged in the alphas PR #434
-#    Logs (28 Apr 2022, bae5c248): cuda116/gcc102 (56 logs from allTees.sh)
-#-------------------------------------------------------------------------------------------------------
-# 1. Logs (20 Jun 2022, 2d3e789): cuda116/gcc102 (60 logs from allTees.sh) <= previous cuda116/gcc102
-# 2. Logs (26 Aug 2022, 21c4cb8): cuda116/gcc102 (60 logs from allTees.sh) <= LATEST cuda116/gcc102
-# 3. Logs (26 Aug 2022, d250d2d): cuda117/gcc112 (60 logs from allTees.sh) <= LATEST cuda117/gcc112
-#----------------------------------------------------------------------------
 # 1. Logs (03 Oct 2024, cd8e872): cuda120/gcc113 (102 logs from allTees.sh) <= v1.00.00     itscrd90
 # 2. Logs (03 Oct 2024, a3d64bd): -------/gcc114 ( 96 logs from allTees.sh) <= v1.00.00     itgold91
 # 3. Logs (03 Oct 2024, 07c2a53): roc60/clang170 ( 96 logs from allTees.sh) <= v1.00.00+amd lumi
 #----------------------------------------------------------------------------
 
-# Select revisions of cudacpp and alpaka logs
-crevs="" # cudacpp .sa
-arevs="" # alpaka
+# Select revisions of cudacpp logs
 mrevs="" # cudacpp .mad
-if [ "$table" == "latest" ]; then
-  mrevs="$mrevs d250d2d" # cuda117/gcc112  (26 Aug 2022) GCC112   60 logs allTees.sh
-elif [ "$table" == "bridge" ]; then
-  mrevs="$mrevs d250d2d" # cuda117/gcc112  (26 Aug 2022) GCC112   60 logs allTees.sh
-elif [ "$table" == "hrdcod" ]; then
-  mrevs="$mrevs d250d2d" # cuda117/gcc112  (26 Aug 2022) GCC112   60 logs allTees.sh
-  mrevs="$mrevs f3ee68c" # cuda117/icx2022 (27 Aug 2022) ICX2022  60 logs allTees.sh
-elif [ "$table" == "juwels" ]; then
-  crevs="$crevs 09e482e" # cuda116/gcc102  (03 Mar 2022) BASELINE eemumu/ggtt/ggttgg x f/d x hrd0/hrd1 x inl0/inl1 + ggttg/ggttggg x f/d x hrd0/hrd1 x inl0
-  crevs="$crevs 65730b2" # cuda115/gcc112  (18 Feb 2022) JUWELSCL eemumu/ggtt/ggttgg x f/d x inl0/inl1 + ggttg/ggttggg x f/d
-  crevs="$crevs df441ad" # cuda115/gcc112  (18 Feb 2022) JUWELSBO eemumu/ggtt/ggttgg x f/d x inl0/inl1 + ggttg/ggttggg x f/d
-  ###mrevs="$mrevs 2d3e789" # cuda116/gcc102  (20 Jun 2022) BASELINE eemumu/ggtt* x f/d x hrd0 x inl0 x default/bridge
-  ###mrevs="$mrevs fd4ffe1" # cuda115/gcc112  (24 Jun 2022) JUWELSCL eemumu/ggtt* x f/d x hrd0 x inl0 x default/bridge
-elif [ "$table" == "alpaka" ]; then
-  crevs="$crevs 09e482e" # cuda116/gcc102  (03 Mar 2022) BASELINE eemumu/ggtt/ggttgg x f/d x hrd0/hrd1 x inl0/inl1 + ggttg/ggttggg x f/d x hrd0/hrd1 x inl0
-  arevs="$arevs f5a44ba" # cuda116/gcc102  (06 Mar 2022) GOLDEPX4 eemumu/ggtt/ggttg/ggttgg/ggttggg x d x inl0 (golden tag epochx4)
-elif [ "$table" == "macm1" ]; then
-  crevs="$crevs ea28661" # NOGPU/clang120  (10 Apr 2022) MACM1ARM eemumu/ggtt/ggttg/ggttgg/ggttggg x f/d x hrd0 x inl0
-elif [ "$table" == "alphas" ]; then
-  crevs="$crevs 88fe36d" # cuda116/gcc102  (28 Apr 2022) PRE-AS   56 logs allTees.sh
-  crevs="$crevs bae5c24" # cuda116/gcc102  (28 Apr 2022) POST-AS  56 logs allTees.sh
-elif [ "$table" == "3xcomp" ]; then
-  mrevs="$mrevs d250d2d" # cuda117/gcc112  (26 Aug 2022) GCC112   60 logs allTees.sh
-  mrevs="$mrevs 2c77b32" # cuda117/clang13 (27 Aug 2022) CLANG13  60 logs allTees.sh
-  mrevs="$mrevs f3ee68c" # cuda117/icx2022 (27 Aug 2022) ICX2022  60 logs allTees.sh
-elif [ "$table" == "ol23silv" ]; then
-  ###mrevs="$mrevs 49e4381" # cuda120/gcc112  (23 Feb 2023 itscrd90)
-  mrevs="$mrevs 2a6ddd0" # cuda120/gcc121  (24 Feb 2023 itscrd90)
-  mrevs="$mrevs aed7c84" # cuda120/clang14 (23 Feb 2023 itscrd90)
-  mrevs="$mrevs 32e098f" # cuda120/icx2023 (23 Feb 2023 itscrd90)
-elif [ "$table" == "ol23gold" ]; then
-  mrevs="$mrevs e67558d" # nocuda/gcc121   (10 Mar 2023 bmkGold6130)
-  mrevs="$mrevs aa4a5c4" # nocuda/clang14  (09 Mar 2023 bmkGold6130)
-  mrevs="$mrevs 99c1bd6" # nocuda/icx2023  (10 Mar 2023 bmkGold6130)
-elif [ "$table" == "v10000" ]; then
+if [ "$table" == "v10000" ]; then
   mrevs="$mrevs cd8e872" # cuda120/gcc113  (03 Oct 2024 itscrd90) v1.00.00
   mrevs="$mrevs a3d64bd" # -------/gcc114  (03 Oct 2024 gold91)   v1.00.00
   mrevs="$mrevs 07c2a53" # roc60/clang170  (03 Oct 2024 lumi)     v1.00.00+amd
@@ -141,57 +66,9 @@ fi
 # Select processes
 procs="eemumu ggtt ggttg ggttgg ggttggg"
 
-# Select fptype, helinl, hrdcod
-if [ "$table" == "latest" ]; then
-  fpts="d f"
-  inls="inl0 inl1"
-  hrds="hrd0"
-  brds="nobr"
-elif [ "$table" == "bridge" ]; then
-  fpts="d f"
-  inls="inl0"
-  hrds="hrd0"
-  brds="nobr brdg"
-elif [ "$table" == "hrdcod" ]; then
-  fpts="d"
-  inls="inl0"
-  hrds="hrd0 hrd1"
-  brds="nobr"
-elif [ "$table" == "juwels" ]; then
-  fpts="d f"
-  inls="inl0 inl1"
-  hrds="hrd0"
-  ###brds="nobr"
-  brds="nobr brdg"
-elif [ "$table" == "alpaka" ]; then
-  fpts="d"
-  inls="inl0"
-  hrds="hrd0"
-  brds="nobr"
-elif [ "$table" == "macm1" ]; then
-  fpts="d f"
-  inls="inl0"
-  hrds="hrd0"
-  brds="nobr"
-elif [ "$table" == "alphas" ]; then
-  fpts="d f"
-  inls="inl0" # no need to add inl1 (even if data exists)
-  hrds="hrd0 hrd1"
-  brds="nobr"
-elif [ "$table" == "3xcomp" ]; then
-  fpts="d f"
-  inls="inl0 inl1"
-  hrds="hrd0 hrd1"
-  brds="nobr"
-elif [ "$table" == "ol23silv" ] || [ "$table" == "ol23gold" ]; then
-  ###fpts="d f m"
-  fpts="d f"
-  inls="inl0 inl1"
-  ###hrds="hrd0 hrd1"
-  hrds="hrd0"
-  brds="nobr"
-elif [ "$table" == "v10000" ]; then
-  fpts="d f m"
+# Select fptype, helinl, hrdcod, bridge
+if [ "$table" == "v10000" ]; then
+  fpts="d m f"
   ###inls="inl0 inl1"
   inls="inl0"
   ###hrds="hrd0 hrd1"
@@ -202,14 +79,10 @@ else
 fi
 
 # Select tag list
-if [ "$table" == "alpaka" ]; then
-  taglist="CUD/none ALP/none CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z"
-elif [ "$table" == "macm1" ]; then
+if [ "$table" == "macm1" ]; then
   taglist="CPP/none CPP/sse4"
-elif [ "$table" == "bridge" ]; then
-  taglist="CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z CUD/none" # revert order, CUDA last
 else
-  taglist="CUD/none CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z"
+  taglist="CUD/none HIP/none CPP/none CPP/sse4 CPP/avx2 CPP/512y CPP/512z"
 fi
 
 # Kernel function
@@ -274,20 +147,15 @@ function oneTable()
 # Iterate through log files
 for fpt in $fpts; do
   echo -e "*** FPTYPE=$fpt ******************************************************************\n" >> $out
-  for bckend in cudacpp.mad cudacpp.sa alpaka; do
-    if [ "$bckend" == "alpaka" ]; then
-      revs="$arevs"
-      suff=auto
-    elif [ "$bckend" == "cudacpp.sa" ]; then
-      revs="$crevs"
-      suff=manu
-    else
+  for bckend in cudacpp.mad; do
+    if [ "$bckend" == "cudacpp.mad" ]; then
       revs="$mrevs"
       suff=mad
+    else
+      echo "ERROR! Unknown backend '$bckend' (while setting revs and suff)"; exit 1
     fi
     if [ "$revs" == "" ]; then continue; fi
-    ### DIFFERENT SORTINGS
-    if [ "$table" == "3xcomp" ] || [ "$table" == "ol23silv" ] || [ "$table" == "ol23gold" ]; then
+    if [ "$table" == "v10000" ]; then
       ### New sorting (3xcomp)
       for inl in $inls; do
         for hrd in $hrds; do
@@ -296,33 +164,6 @@ for fpt in $fpts; do
             for rev in $revs; do
               echo -e "+++ $bckend REVISION $rev (commit date: $(git log $rev --pretty=format:'%ci' --abbrev-commit -n1)) +++" >> $out
               nodelast=
-              oneTable
-            done
-          done
-        done
-      done
-    ### New sorting (alphas)
-    elif [ "$table" == "alphas" ]; then
-      for inl in $inls; do
-        for hrd in $hrds; do
-          for brd in $brds; do
-            echo -e "-------------------------------------------------------------------------------\n" >> $out
-            for rev in $revs; do
-              echo -e "+++ $bckend REVISION $rev (commit date: $(git log $rev --pretty=format:'%ci' --abbrev-commit -n1)) +++" >> $out
-              nodelast=
-              oneTable
-            done
-          done
-        done
-      done
-    else
-      ### Old sorting (all but alphas, 3xcomp, ol23silv, ol23gold)
-      for rev in $revs; do
-        echo -e "+++ $bckend REVISION $rev (commit date: $(git log $rev --pretty=format:'%ci' --abbrev-commit -n1)) +++" >> $out
-        nodelast=
-        for inl in $inls; do
-          for hrd in $hrds; do
-            for brd in $brds; do
               oneTable
             done
           done
