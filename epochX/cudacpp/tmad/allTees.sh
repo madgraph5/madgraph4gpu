cbc873550e96b89dd450cae70ff6ca811abf9962
Andrea Valassi <andrea.valassi@cern.ch>
2025-09-13 07:35:18 +0200

    [hack_ihel4] in tmad/allTees.sh, add a -checkonly option as in tput/allTees.ch

--- epochX/cudacpp/tmad/allTees.sh
+++ epochX/cudacpp/tmad/allTees.sh
@@ -9,6 +9,19 @@ scrdir=$(cd $(dirname $0); pwd)
 host=$(hostname)
 if [ "${host/juwels}" != "${host}" ]; then ${scrdir}/juwelspatch.sh; fi # workaround for #498
 
+# Usage
+function usage()
+{
+  echo "Usage (1): $0 [-short|-ggttggg] [-bsmonly|-nobsm] [-makeclean] [-no10x] [-hip]"
+  echo "Run tests and check all logs"
+  echo ""
+  echo "Usage (2): $0 -checkonly"
+  echo "Check existing logs without running any tests"
+  exit 1
+}
+
+# Parse command line arguments
+checkonly=0
 short=0
 bsm=
 flts=-dmf # "d m f" (alternative: -d_f i.e. "d f")
@@ -16,8 +29,13 @@ makeclean=
 rmrdat=
 add10x="+10x"
 hip=
-
-while [ "$1" != "" ]; do
+if [ "$1" == "-checkonly" ]; then
+  # Check existing logs without running any tests?
+  checkonly=1
+  shift
+  if [ "$1" != "" ]; then usage; fi
+fi
+while [ "${checkonly}" == "0" ] && [ "$1" != "" ]; do
   if [ "$1" == "-short" ]; then
     short=1 # all (possibly including bsm) but ggttggg
     shift
@@ -40,41 +58,44 @@ while [ "$1" != "" ]; do
     hip=$1
     shift
   else
-    echo "Usage: $0 [-short|-ggttggg] [-bsmonly|-nobsm] [-makeclean] [-no10x] [-hip]"
-    exit 1
+    usage
   fi
 done
 
-started="STARTED  AT $(date)"
-
-if [ "${bsm}" != "-bsmonly" ]; then
-  if [ "$short" == "1" ]; then
-    ${scrdir}/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -gqttq $flts $makeclean $rmrdat $add10x $hip
-  elif [ "$short" == "-1" ]; then
-    ${scrdir}/teeMadX.sh -ggttggg $flts $makeclean $rmrdat $add10x $hip
-  else
-    ${scrdir}/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -gqttq -ggttggg $flts $makeclean $rmrdat $add10x $hip
+# Run all tests
+if [ "${checkonly}" == "0" ]; then
+  started="STARTED  AT $(date)"
+  # SM tests
+  if [ "${bsm}" != "-bsmonly" ]; then
+    if [ "$short" == "1" ]; then
+      ${scrdir}/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -gqttq $flts $makeclean $rmrdat $add10x $hip
+    elif [ "$short" == "-1" ]; then
+      ${scrdir}/teeMadX.sh -ggttggg $flts $makeclean $rmrdat $add10x $hip
+    else
+      ${scrdir}/teeMadX.sh -eemumu -ggtt -ggttg -ggttgg -gqttq -ggttggg $flts $makeclean $rmrdat $add10x $hip
+    fi
   fi
-fi
-status=$?
-ended1="(SM tests)\nENDED(1) AT $(date) [Status=$status]"
-
-if [ "${bsm}" != "-nobsm" ]; then
-  if [ "$short" != "-1" ]; then
-    ${scrdir}/teeMadX.sh -heftggbb -susyggtt -susyggt1t1 -smeftggtttt $flts $makeclean $rmrdat $add10x $hip
+  status=$?
+  ended1="(SM tests)\nENDED(1) AT $(date) [Status=$status]"
+  # BSM tests
+  if [ "${bsm}" != "-nobsm" ]; then
+    if [ "$short" != "-1" ]; then
+      ${scrdir}/teeMadX.sh -heftggbb -susyggtt -susyggt1t1 -smeftggtttt $flts $makeclean $rmrdat $add10x $hip
+    fi
   fi
+  status=$?
+  ended2="(BSM tests)\nENDED(1) AT $(date) [Status=$status]"
+  # Timing information
+  echo
+  printf "\n%80s\n" |tr " " "#"
+  echo
+  echo -e "$started"
+  echo -e "$ended1"
+  echo -e "$ended2"
+  echo
 fi
-status=$?
-ended2="(BSM tests)\nENDED(1) AT $(date) [Status=$status]"
 
 # Print out the number of "OK!"s in each log (expect 24)
-echo
-printf "\n%80s\n" |tr " " "#"
-echo
-echo -e "$started"
-echo -e "$ended1"
-echo -e "$ended2"
-echo
 for f in ${scrdir}/logs_*_mad/log_*; do echo $(cat $f | grep OK  | wc -l) $f; done # expect 24
 
 # Print out the MEK channelid debugging output
