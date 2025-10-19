f92a02b0b776c557b3aeb74c625dbeeed41ecfd7
Andrea Valassi <andrea.valassi@cern.ch>
2025-10-19 09:03:57 +0200

    [hack_ihel4p2] regenerate gg_ttg.mad with 5 diagrams per kernel

--- epochX/cudacpp/gg_ttg.mad/SubProcesses/P1_gg_ttxg/CPPProcess.cc
+++ epochX/cudacpp/gg_ttg.mad/SubProcesses/P1_gg_ttxg/CPPProcess.cc
@@ -499,6 +499,12 @@ namespace mg5amcCpu
       // Case 1 with graphs (gpustream!=0, sigmaKin): create graph nodes if not yet done, else update them with new parameters
       gpuGraphNode_t& node1 = graphNodes[ihel * ndiagramgroups + 0];
       gpuDiagrams( useGraphs, &graph, &graphExec, &node1, nullptr, diagramgroup1, gpublocks, gputhreads, gpustream, wfs, jamps, channelIds, couplings, numerators, denominators, momenta, ihel );
+      gpuGraphNode_t& node2 = graphNodes[ihel * ndiagramgroups + 1];
+      gpuDiagrams( useGraphs, &graph, &graphExec, &node2, &node1, diagramgroup2, gpublocks, gputhreads, gpustream, wfs, jamps, channelIds, couplings, numerators, denominators );
+      gpuGraphNode_t& node3 = graphNodes[ihel * ndiagramgroups + 2];
+      gpuDiagrams( useGraphs, &graph, &graphExec, &node3, &node2, diagramgroup3, gpublocks, gputhreads, gpustream, wfs, jamps, channelIds, couplings, numerators, denominators );
+      gpuGraphNode_t& node4 = graphNodes[ihel * ndiagramgroups + 3];
+      gpuDiagrams( useGraphs, &graph, &graphExec, &node4, &node3, diagramgroup4, gpublocks, gputhreads, gpustream, wfs, jamps, channelIds, couplings, numerators, denominators );
       // Case 1 with graphs (gpustream!=0, sigmaKin): create the graph executor if not yet done, then launch the graph executor
       if( useGraphs && gpustream != 0 )
       {
@@ -512,6 +518,9 @@ namespace mg5amcCpu
       }
 #else
       diagramgroup1( wfs, jamps, channelIds, COUPs, numerators, denominators, momenta, ihel );
+      diagramgroup2( wfs, jamps, channelIds, COUPs, numerators, denominators );
+      diagramgroup3( wfs, jamps, channelIds, COUPs, numerators, denominators );
+      diagramgroup4( wfs, jamps, channelIds, COUPs, numerators, denominators );
 #endif
     }
     // *****************************
