e7eba4f53b6f1d2751784bf6dc208685dc11569d
Andrea Valassi <andrea.valassi@cern.ch>
2025-10-26 06:56:46 +0100

    [hack_ihel5and3p1] ggtt: undo TMP change after cherry-pick 415b0d0c2 (add back 1b launch update_jamp2s)
    This is a manual change to revert commit e8918df12ec0b6886b2c1443f50d293c8429c652

--- epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/CPPProcess.cc
+++ epochX/cudacpp/gg_tt.mad/SubProcesses/P1_gg_ttx/CPPProcess.cc
@@ -1196,6 +1196,14 @@ namespace mg5amcCpu
       calculate_jamps( ihel, allmomenta, allcouplings, hAllJamps, hAllWfs, ghelStreams[ighel], gpublocks, gputhreads );
 #endif
     }
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    // (1b) Then, in multichannel mode, also compute the running sums over helicities of squared jamp2s within each helicity stream
+    for( int ighel = 0; ighel < cNGoodHel; ighel++ )
+    {
+      fptype* hAllJamps = ghelAllJamps + ighel * nevt * ncolor * mgOnGpu::nx2;
+      gpuLaunchKernelStream( update_jamp2s, gpublocks, gputhreads, ghelStreams[ighel], hAllJamps, colAllJamp2s );
+    }
+#endif
     // (2) Then compute the ME for that helicity from the color sum of QCD partial amplitudes jamps
     color_sum_gpu( ghelAllMEs, ghelAllJamps, ghelAllBlasTmp, ghelBlasHandles, ghelStreams, cNGoodHel, gpublocks, gputhreads );
     checkGpu( gpuDeviceSynchronize() ); // do not start helicity/color selection until the loop over helicities has completed
