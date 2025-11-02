235bc657bd7033bbafb581534c3dfac4343b2051
Andrea Valassi <andrea.valassi@cern.ch>
2025-11-02 08:41:33 +0100

    [hack_ihel6] add gg_ttg.dpg5dpf5.sa to the repository

---
+++ epochX/cudacpp/gg_ttg.dpg5dpf5.sa/SubProcesses/P1_Sigma_sm_gg_ttxg/diagrams3.cc
@@ -0,0 +1,150 @@
+// Copyright (C) 2020-2025 CERN and UCLouvain.
+// Licensed under the GNU Lesser General Public License (version 3 or later).
+// Created by: A. Valassi (Sep 2025) for the MG5aMC CUDACPP plugin.
+// Further modified by: A. Valassi (2025) for the MG5aMC CUDACPP plugin.
+
+#include "GpuRuntime.h"
+#include "HelAmps_sm.h"
+#include "MemoryAccessAmplitudes.h"
+#include "MemoryAccessChannelIds.h"
+#include "MemoryAccessCouplings.h"
+#include "MemoryAccessCouplingsFixed.h"
+#include "MemoryAccessWavefunctions.h"
+#include "color_sum.h"
+#include "diagrams.h"
+#include "diagrams_header.h"
+
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+#include "MemoryAccessDenominators.h"
+#include "MemoryAccessNumerators.h"
+#endif
+
+#ifdef MGONGPUCPP_GPUIMPL
+namespace mg5amcGpu
+#else
+namespace mg5amcCpu
+#endif
+{
+  //--------------------------------------------------------------------------
+
+#ifndef MGONGPU_RDC_DIAGRAMS
+  __global__ void
+#else
+  __device__ void
+#endif
+  diagramgroup3( fptype* wfs,                    // input/output wavefunctions[nwf*2*nw6*nevtORneppV]
+#ifdef MGONGPUCPP_GPUIMPL
+                 fptype* jamps,                  // output jamps[ncolor*2*nevt]
+                 const int nGoodHel,             // input: number of good helicities
+                 const fptype* couplings,        // input: dependent couplings[nevt*ndcoup*2] for all events
+#else
+                 cxtype_sv* jamps,               // output jamps[ncolor*2*neppV]
+                 const fptype** COUPs,           // input: dependent and independent COUPs[nxcoup] for this event page
+#endif
+                 const unsigned int* channelIds, // input: channelIds[nevt] for GPU or SCALAR channelId[0] for C++ (1 to #diagrams, 0 to disable SDE)
+                 fptype* numerators,             // input/output: multichannel numerators[nevtORneppV], add helicity ihel
+                 fptype* denominators,           // input/output: multichannel denominators[nevtORneppV], add helicity ihel
+                 const fptype* cIPC,             // input: GPU __device__ or GPU host address of cIPC
+                 const fptype* cIPD )            // input: GPU __device__ or GPU host address of cIPD
+  {
+    // A uniform interface for diagramgroupXXX including channelIDs, numerators and denominators is used also #ifndef MGONGPU_SUPPORTS_MULTICHANNEL
+    // In that case, however, the boilerplate code asserts that all three pointers all nullptr as a sanity check
+#include "diagrams_boilerplate.h"
+
+#ifdef MGONGPUCPP_GPUIMPL
+#ifndef MGONGPU_RDC_DIAGRAMS
+    // *** RETRIEVE WAVEFUNCTIONS FROM PREVIOUS DIAGRAM GROUPS ***
+    //for( int iwf = 0; iwf < nwf; iwf++ ) retrieveWf( wfs, w_cx, nevt, iwf );
+    retrieveWf( wfs, w_cx, nevt, 0 );
+    retrieveWf( wfs, w_cx, nevt, 1 );
+    retrieveWf( wfs, w_cx, nevt, 2 );
+    retrieveWf( wfs, w_cx, nevt, 5 );
+    retrieveWf( wfs, w_cx, nevt, 6 );
+    retrieveWf( wfs, w_cx, nevt, 7 );
+    retrieveWf( wfs, w_cx, nevt, 8 );
+    retrieveWf( wfs, w_cx, nevt, 9 );
+    retrieveWf( wfs, w_cx, nevt, 10 );
+    retrieveWf( wfs, w_cx, nevt, 11 );
+#endif
+#endif
+
+    // *** DIAGRAM 11 OF 16 ***
+    // Wavefunction(s) for diagram number 11
+    // (none)
+    // Amplitude(s) for diagram number 11
+    FFV1_0<W_ACCESS, A_ACCESS, CD_ACCESS>( w_fp[9], w_fp[2], w_fp[5], COUPs[1], 1.0, &amp_fp[0] );
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    // Here the code base generated with multichannel support updates numerators_sv and denominators_sv (#473)
+#endif
+    jamp_sv[1] += cxtype( 0, 1 ) * amp_sv[0];
+    jamp_sv[4] -= cxtype( 0, 1 ) * amp_sv[0];
+
+    // *** DIAGRAM 12 OF 16 ***
+    // Wavefunction(s) for diagram number 12
+    // (none)
+    // Amplitude(s) for diagram number 12
+    VVV1_0<W_ACCESS, A_ACCESS, CD_ACCESS>( w_fp[5], w_fp[1], w_fp[6], COUPs[0], 1.0, &amp_fp[0] );
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    // Here the code base generated with multichannel support updates numerators_sv and denominators_sv (#473)
+#endif
+    jamp_sv[1] += amp_sv[0];
+    jamp_sv[2] -= amp_sv[0];
+    jamp_sv[3] += amp_sv[0];
+    jamp_sv[4] -= amp_sv[0];
+
+    // *** DIAGRAM 13 OF 16 ***
+    // Wavefunction(s) for diagram number 13
+    // (none)
+    // Amplitude(s) for diagram number 13
+    FFV1_0<W_ACCESS, A_ACCESS, CD_ACCESS>( w_fp[8], w_fp[11], w_fp[0], COUPs[1], 1.0, &amp_fp[0] );
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    // Here the code base generated with multichannel support updates numerators_sv and denominators_sv (#473)
+#endif
+    jamp_sv[2] -= amp_sv[0];
+
+    // *** DIAGRAM 14 OF 16 ***
+    // Wavefunction(s) for diagram number 14
+    // (none)
+    // Amplitude(s) for diagram number 14
+    FFV1_0<W_ACCESS, A_ACCESS, CD_ACCESS>( w_fp[9], w_fp[7], w_fp[0], COUPs[1], 1.0, &amp_fp[0] );
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    // Here the code base generated with multichannel support updates numerators_sv and denominators_sv (#473)
+#endif
+    jamp_sv[4] -= amp_sv[0];
+
+    // *** DIAGRAM 15 OF 16 ***
+    // Wavefunction(s) for diagram number 15
+    // (none)
+    // Amplitude(s) for diagram number 15
+    VVV1_0<W_ACCESS, A_ACCESS, CD_ACCESS>( w_fp[0], w_fp[10], w_fp[6], COUPs[0], 1.0, &amp_fp[0] );
+#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
+    // Here the code base generated with multichannel support updates numerators_sv and denominators_sv (#473)
+#endif
+    jamp_sv[0] += amp_sv[0];
+    jamp_sv[1] -= amp_sv[0];
+    jamp_sv[3] -= amp_sv[0];
+    jamp_sv[5] += amp_sv[0];
+
+#ifdef MGONGPUCPP_GPUIMPL
+    // *** STORE JAMPS ***
+    // In CUDA, copy the local jamp to the output global-memory jamp
+    constexpr int ihel0 = 0; // the allJamps buffer already points to a specific helicity _within a super-buffer for nGoodHel helicities_
+    for( int icol = 0; icol < ncolor; icol++ )
+      J_ACCESS::kernelAccessIcolIhelNhel( jamps, icol, ihel0, nGoodHel ) += jamp_sv[icol]; // update jamps
+#else
+    // In C++, copy the local jamp to the output array passed as function argument
+    for( int icol = 0; icol < ncolor; icol++ )
+      jamps[icol] += jamp_sv[icol]; // update jamps
+#endif
+
+#ifdef MGONGPUCPP_GPUIMPL
+#ifndef MGONGPU_RDC_DIAGRAMS
+    // *** STORE WAVEFUNCTIONS FOR NEXT DIAGRAM GROUPS ***
+    //for( int iwf = 0; iwf < nwf; iwf++ ) storeWf( wfs, w_cx, nevt, iwf );
+    // (none)
+#endif
+#endif
+  }
+
+  //--------------------------------------------------------------------------
+}
