4151f7887f75d6c0554703cca3538871cbddd060
Andrea Valassi <andrea.valassi@cern.ch>
2025-11-02 16:39:17 +0100

    [hack_ihel6_ggtt4g] generate gg_ttgggg SA with 10000 diagrams per group and per file
    [avalassi@itscrd-a100 gcc11/usr] /data/avalassi/GPU2023/test-madgraph4gpu3/epochX/cudacpp>
    ./CODEGEN/generateAndCompare.sh gg_ttgggg --maxdiagpergroup 10000 --mindiagperfile 10000
    Code generation and additional checks completed in 446 seconds

---
+++ epochX/cudacpp/gg_ttgggg.dpg10000dpf10000.sa/SubProcesses/MemoryAccessWavefunctions.h
@@ -0,0 +1,66 @@
+// Copyright (C) 2020-2025 CERN and UCLouvain.
+// Licensed under the GNU Lesser General Public License (version 3 or later).
+// Created by: A. Valassi (Jan 2022) for the MG5aMC CUDACPP plugin.
+// Further modified by: A. Valassi (2022-2025) for the MG5aMC CUDACPP plugin.
+
+#ifndef MemoryAccessWavefunctions_H
+#define MemoryAccessWavefunctions_H 1
+
+#include "mgOnGpuConfig.h"
+
+#include "mgOnGpuCxtypes.h"
+
+#include "CPPProcess.h"
+
+// NB: namespaces mg5amcGpu and mg5amcCpu includes types which are defined in different ways for CPU and GPU builds (see #318 and #725)
+#ifdef MGONGPUCPP_GPUIMPL
+namespace mg5amcGpu
+#else
+namespace mg5amcCpu
+#endif
+{
+  //----------------------------------------------------------------------------
+
+#ifdef MGONGPUCPP_GPUIMPL
+
+  // Trivial access in local variables
+  class DeviceAccessWavefunctionsTrivial
+  {
+  public:
+    static __host__ __device__ inline cxtype*
+    kernelAccess( fptype* buffer )
+    {
+      return reinterpret_cast<cxtype*>( buffer );
+    }
+    static __host__ __device__ inline const cxtype*
+    kernelAccessConst( const fptype* buffer )
+    {
+      return reinterpret_cast<const cxtype*>( buffer );
+    }
+  };
+
+#endif
+
+  //----------------------------------------------------------------------------
+
+  // Trivial access in local variables
+  class HostAccessWavefunctions
+  {
+  public:
+    static __host__ __device__ inline cxtype_sv*
+    kernelAccess( fptype* buffer )
+    {
+      return reinterpret_cast<cxtype_sv*>( buffer );
+    }
+    static __host__ __device__ inline const cxtype_sv*
+    kernelAccessConst( const fptype* buffer )
+    {
+      return reinterpret_cast<const cxtype_sv*>( buffer );
+    }
+  };
+
+  //----------------------------------------------------------------------------
+
+} // end namespace mg5amcGpu/mg5amcCpu
+
+#endif // MemoryAccessWavefunctions_H
