5cb61b0688882526c4c5c20185bbcad77888240c
Stefan Roiser <stefan.roiser@cern.ch>
2025-11-21 16:50:30 +0100

    regenerate all processes

---
+++ epochX/cudacpp/pp_tt012j.mad/SubProcesses/P2_uu_ttxuu/processConfig.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 CERN and UCLouvain.
+// Licensed under the GNU Lesser General Public License (version 3 or later).
+// Created by: S. Roiser (May 2025) for the MG5aMC CUDACPP plugin.
+// Further modified by: ... for the MG5aMC CUDACPP plugin.
+
+
+#ifndef MG5_CONFIG_SIGMA_SM_UU_TTXUU_H
+#define MG5_CONFIG_SIGMA_SM_UU_TTXUU_H 1
+
+namespace processConfig {
+
+  constexpr int ndiagrams = 14;
+
+}
+
+#endif // MG5_CONFIG_SIGMA_SM_UU_TTXUU_H
